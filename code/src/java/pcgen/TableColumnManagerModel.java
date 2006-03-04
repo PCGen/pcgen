157da88cf2ef9f15168be96bec67403a0396cfe4
soulcatcher <soulcatcher@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2006-03-04 04:31:06 +0000

    part of the spells checkin
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@94 ef53c05b-3124-4d59-ae7d-fe3273f42be7

---
+++ code/src/java/pcgen/TableColumnManagerModel.java
@@ -0,0 +1,10 @@
+package pcgen;
+
+import java.util.List;
+
+public interface TableColumnManagerModel {
+	public List getMColumnList();
+	public boolean isMColumnDisplayed(int col);
+	public void setMColumnDisplayed(int col, boolean disp);
+	public int getMColumnOffset();
+}
