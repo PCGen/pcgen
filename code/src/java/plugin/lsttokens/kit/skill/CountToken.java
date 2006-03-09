481ee3047f302a7d19b3d5c3a1a55f54617644ad
boomer70 <boomer70@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2006-03-09 02:10:05 +0000

    Pluginized the rest of the Kit tags
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@147 ef53c05b-3124-4d59-ae7d-fe3273f42be7

---
+++ code/src/java/plugin/lsttokens/kit/skill/CountToken.java
@@ -0,0 +1,57 @@
+/*
+ * CountToken.java
+ * Copyright 2006 (C) Aaron Divinsky <boomer70@yahoo.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * Created on March 6, 2006
+ *
+ * Current Ver: $Revision: $
+ * Last Editor: $Author: $
+ * Last Edited: $Date: $
+ */
+
+package plugin.lsttokens.kit.skill;
+
+import pcgen.core.kit.KitSkill;
+import pcgen.persistence.lst.KitSkillLstToken;
+
+public class CountToken implements KitSkillLstToken
+{
+	/**
+	 * Gets the name of the tag this class will parse.
+	 * 
+	 * @return Name of the tag this class handles
+	 */
+	public String getTokenName()
+	{
+		return "COUNT";
+	}
+
+	/**
+	 * parse
+	 * 
+	 * @param kitSkill
+	 *            KitSkill
+	 * @param value
+	 *            String
+	 * @return boolean
+	 */
+	public boolean parse(KitSkill kitSkill, String value)
+	{
+		kitSkill.setChoiceCount(value);
+		return true;
+	}
+}
