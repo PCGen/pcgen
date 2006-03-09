d6618dd130e50e23a6445ef81994d7500cd26144
boomer70 <boomer70@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2006-03-09 02:07:17 +0000

    Pluginized the rest of the Kit tags
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@142 ef53c05b-3124-4d59-ae7d-fe3273f42be7

---
+++ code/src/java/plugin/lsttokens/kit/deity/DeityToken.java
@@ -0,0 +1,52 @@
+/* 
+ * DeityToken.java
+ * Copyright 2006 (C) Aaron Divinsky <boomer70@yahoo.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * Created on March 6, 2006
+ *
+ * Current Ver: $Revision: $
+ * Last Editor: $Author: $
+ * Last Edited: $Date: $
+ */
+
+package plugin.lsttokens.kit.deity;
+
+import pcgen.core.kit.KitDeity;
+import pcgen.persistence.lst.KitDeityLstToken;
+import pcgen.util.Logging;
+
+public class DeityToken implements KitDeityLstToken
+{
+
+	public boolean parse(KitDeity kitDeity, String value)
+	{
+		Logging.errorPrint("Ignoring second DEITY tag \"" + value
+				+ "\" in Kit.");
+		return false;
+	}
+
+	/**
+	 * Gets the name of the tag this class will parse.
+	 * 
+	 * @return Name of the tag this class handles
+	 */
+	public String getTokenName()
+	{
+		return "DEITY";
+	}
+
+}
