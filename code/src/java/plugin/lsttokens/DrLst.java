940f0eac74414d46ed72e5b2bdf5d4110e0bc209
boomer70 <boomer70@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2006-03-21 02:14:51 +0000

    [ 1455081 ] Allow DR to take PRExxx tags
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@343 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- code/src/java/plugin/lsttokens/DrLst.java
+++ code/src/java/plugin/lsttokens/DrLst.java
@@ -6,6 +6,14 @@ package plugin.lsttokens;
 
 import pcgen.core.PObject;
 import pcgen.persistence.lst.GlobalLstToken;
+import pcgen.core.prereq.Prerequisite;
+import pcgen.persistence.lst.prereq.PreParserFactory;
+import pcgen.core.DamageReduction;
+import pcgen.persistence.PersistenceLayerException;
+
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+import java.util.Iterator;
 
 /**
  * @author djones4
@@ -19,11 +27,56 @@ public class DrLst implements GlobalLstToken {
 	}
 
 	public boolean parse(PObject obj, String value, int anInt) {
+		ArrayList preReqs = new ArrayList();
 		if (anInt > -9) {
-			obj.setDR(anInt + "|" + value);
-		} else {
-			obj.setDR(value);
+			try
+			{
+				PreParserFactory factory = PreParserFactory.getInstance();
+				Prerequisite r = factory.parse("PRELEVEL:" + anInt);
+				preReqs.add(r);
+			}
+			catch (PersistenceLayerException notUsed)
+			{
+				// Should never happen
+			}
 		}
+
+		if (".CLEAR".equals(value))
+		{
+			obj.clearDR();
+			return true;
+		 }
+
+		StringTokenizer tok = new StringTokenizer(value, "|");
+		String[] values = tok.nextToken().split("/");
+		if (values.length != 2)
+		{
+			return false;
+		}
+		DamageReduction dr = new DamageReduction(values[0], values[1]);
+
+		if (tok.hasMoreTokens())
+		{
+			try
+			{
+				PreParserFactory factory = PreParserFactory.getInstance();
+				Prerequisite r = factory.parse(tok.nextToken());
+				preReqs.add(r);
+			}
+			catch (PersistenceLayerException notUsed)
+			{
+				return false;
+			}
+		}
+		if (preReqs.size() > 0)
+		{
+			for (Iterator i = preReqs.iterator(); i.hasNext(); )
+			{
+				dr.addPreReq((Prerequisite)i.next());
+			}
+		}
+
+		obj.addDR(dr);
 		return true;
 	}
 }
