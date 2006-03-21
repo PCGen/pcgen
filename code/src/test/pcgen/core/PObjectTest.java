c99901086b5b8391c907c42074eb446f41d4972f
boomer70 <boomer70@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2006-03-21 02:15:28 +0000

    [ 1455081 ] Allow DR to take PRExxx tags
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@344 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- code/src/test/pcgen/core/PObjectTest.java
+++ code/src/test/pcgen/core/PObjectTest.java
@@ -79,14 +79,17 @@ public class PObjectTest extends AbstractCharacterTestCase
 		PCTemplate template = new PCTemplate();
 		race.setName("Template");
 
-		race.setDR("5/Good");
-		assertEquals("Basic DR set.", "5/Good", race.getDR());
+//		race.setDR("5/Good");
+		race.addDR(new DamageReduction("5", "Good"));
+		assertEquals("Basic DR set.", "5/Good", race.getDRList().toString());
 
-		race.setDR(".CLEAR");
-		race.setDR("0/-");
-		assertEquals("Basic DR set.", "0/-", race.getDR());
+		race.clearDR();
+//		race.setDR("0/-");
+		race.addDR(new DamageReduction("0", "-"));
+		assertEquals("Basic DR set.", "0/-", race.getDRList().toString());
 
-		template.setDR("0/-");
+//		template.setDR("0/-");
+		template.addDR(new DamageReduction("0", "-"));
 		template.addBonusList("DR|-|1");
 		PlayerCharacter pc = getCharacter();
 		pc.setRace(race);
