d32504af68424539e16b876524bea6463948d582
nuance <nuance@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2006-03-30 20:23:47 +0000

    add some tests
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@532 ef53c05b-3124-4d59-ae7d-fe3273f42be7

---
+++ code/src/test/pcgen/core/chooser/EquipmentTypeChoiceManagerTest.java
@@ -0,0 +1,108 @@
+/*
+ * EquipmentTypeChoiceManagerTest.java
+ * Copyright 2005 (C) Andrew Wilson <nuance@sourceforge.net>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.     See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * Created on Oct 7, 2005
+ *
+ * $Author: nuance $ 
+ * $Date: 2006-03-26 08:00:03 +0100 (Sun, 26 Mar 2006) $
+ * $Revision: 471 $
+ *
+ */
+package pcgen.core.chooser;
+
+import pcgen.AbstractCharacterTestCase;
+import pcgen.core.PObject;
+import pcgen.core.PlayerCharacter;
+import pcgen.core.EquipmentList;
+import pcgen.util.TestHelper;
+
+import java.lang.Class;
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * {@code EquipmentTypeChoiceManagerTest} test that the EquipmentTypeChoiceManager class is functioning correctly.
+ *
+ * @author Andrew Wilson <nuance@sourceforge.net>
+ */
+
+public class EquipmentTypeChoiceManagerTest extends AbstractCharacterTestCase {
+
+	/**
+	 * Constructs a new {@code EquipmentTypeChoiceManagerTest}.
+	 */
+	public EquipmentTypeChoiceManagerTest()
+	{
+		// Do Nothing
+	}
+
+
+	protected void setUp() throws Exception
+	{
+		super.setUp();
+	}
+	
+	protected void tearDown() throws Exception
+	{
+		super.tearDown();
+		EquipmentList.clearEquipmentMap();
+	}
+
+	/**
+	 * Test the constructor 
+	 */
+	public void test001()
+	{
+		PObject pObj = new PObject();
+		pObj.setName("My PObject");
+		pObj.setChoiceString("COUNT=1|NUMCHOICES=2|EQUIPTYPE|one|two|three|four|five");
+		is(pObj.getChoiceString(), strEq("COUNT=1|NUMCHOICES=2|EQUIPTYPE|one|two|three|four|five"));
+
+		PlayerCharacter aPC  = getCharacter();
+		
+		ChoiceManagerList choiceManager = ChooserUtilities.getChoiceManager(pObj, null, aPC);
+		is(choiceManager, not(eq(null)), "Found the chooser");
+
+		is(choiceManager.typeHandled(), strEq("EQUIPTYPE"), "got expected chooser");
+		
+		try
+		{
+			Class cMClass = choiceManager.getClass();
+
+			Field aField  = (Field) TestHelper.findField(cMClass, "numberOfChoices");
+			is (aField.get(choiceManager), eq(2));
+
+			aField  = (Field) TestHelper.findField(cMClass, "requestedSelections");
+			is (aField.get(choiceManager), eq(1));
+			
+			aField  = (Field) TestHelper.findField(cMClass, "choices");
+			List choices = (List) aField.get(choiceManager);
+			is (new Integer (choices.size()), eq(5));
+			is (choices.get(0), strEq("one"));
+			is (choices.get(1), strEq("two"));
+			is (choices.get(2), strEq("three"));
+			is (choices.get(3), strEq("four"));
+			is (choices.get(4), strEq("five"));
+		}
+		catch (IllegalAccessException e) {
+			System.out.println(e);
+		}
+	}
+}
