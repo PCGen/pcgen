70c41ef3623434cdf4297d14703ab12ba5f81f75
jdempsey <jdempsey@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2006-06-18 00:42:31 +0000

    Fix for bug #928673 - Finesseable only accessable from BONUS:COMBAT|TOHIT   - Copy revision 1080 from 5.10.x branch to trunk
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@1081 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- code/src/java/pcgen/io/exporttoken/WeaponToken.java
+++ code/src/java/pcgen/io/exporttoken/WeaponToken.java
@@ -2310,6 +2310,10 @@ public class WeaponToken extends Token
 			// TODO: remove this old syntax
 			bonus += (int) pc.getTotalBonusTo("DAMAGE", "TYPE." + type);
 		}
+		if (eq.isFinessable(pc) && !eq.isType("Finesseable"))
+		{
+			bonus += (int) pc.getTotalBonusTo("COMBAT", "DAMAGE.Finesseable");
+		}
 
 		if (eq.isMelee() && (meleeDamageStatBonus > 0))
 		{
