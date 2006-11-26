844a16c1985c1c5fe195e7ecdfc91cafd9d5e099
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2006-11-26 17:39:05 +0000

    [ 1600646 ] Deprecate ROOT: Tag in Skill
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@1705 ef53c05b-3124-4d59-ae7d-fe3273f42be7

---
+++ code/src/java/plugin/lsttokens/deprecated/RootToken.java
@@ -0,0 +1,25 @@
+package plugin.lsttokens.deprecated;
+
+import pcgen.core.PObject;
+import pcgen.core.Skill;
+import pcgen.persistence.lst.DeprecatedToken;
+import pcgen.persistence.lst.SkillLstToken;
+
+/**
+ * Class deals with ROOT Token
+ */
+public class RootToken implements SkillLstToken, DeprecatedToken {
+
+	public String getTokenName() {
+		return "ROOT";
+	}
+
+	public boolean parse(Skill skill, String value) {
+		skill.setRootName(value);
+		return true;
+	}
+
+	public String getMessage(PObject obj, String value) {
+		return "ROOT is a non-functioning Token - predates COST, TYPE breakdowns for Skills";
+	}
+}
