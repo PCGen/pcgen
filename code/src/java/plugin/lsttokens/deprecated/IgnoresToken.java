91086ca1ff485a6e4f466ec957ad21fcd302f158
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2006-12-07 04:07:20 +0000

    Tracker [ 1605662 ] Deprecate IGNORES
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@1731 ef53c05b-3124-4d59-ae7d-fe3273f42be7

---
+++ code/src/java/plugin/lsttokens/deprecated/IgnoresToken.java
@@ -0,0 +1,24 @@
+package plugin.lsttokens.deprecated;
+
+import pcgen.core.EquipmentModifier;
+import pcgen.core.PObject;
+import pcgen.persistence.lst.DeprecatedToken;
+import pcgen.persistence.lst.EquipmentModifierLstToken;
+
+/**
+ * Deals with IGNORES token 
+ */
+public class IgnoresToken implements EquipmentModifierLstToken, DeprecatedToken {
+
+	public String getTokenName() {
+		return "IGNORES";
+	}
+
+	public boolean parse(EquipmentModifier mod, String value) {
+		return true;
+	}
+
+	public String getMessage(PObject obj, String value) {
+		return "This was a non-working token, use REPLACES and PRETYPE instead";
+	}
+}
