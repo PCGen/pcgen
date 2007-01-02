fb7c4e9bbf3ccfb3e0a30b1c1be150d8fe8e211c
jdempsey <jdempsey@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2007-01-02 22:26:35 +0000

    Update SVN properties
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@1863 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- code/src/java/plugin/lsttokens/AbilityLst.java
+++ code/src/java/plugin/lsttokens/AbilityLst.java
@@ -17,7 +17,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
  * Current Ver: $Revision$
- * Last Editor: $Author: $
+ * Last Editor: $Author$
  * Last Edited: $Date$
  */
 package plugin.lsttokens;
