66fd0ff840ae37204e05f30c59a194926c88a997
zaister <zaister@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2007-01-04 12:59:01 +0000

    [ 1627789 ] Implement PREREACH
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@1872 ef53c05b-3124-4d59-ae7d-fe3273f42be7

---
+++ code/src/java/plugin/pretokens/test/PreReachTester.java
@@ -0,0 +1,75 @@
+/*
+ * PreReach.java
+ * Copyright 2001 (C) Bryan McRoberts <merton_monk@yahoo.com>
+ * Copyright 2003 (C) Chris Ward <frugal@purplewombat.co.uk>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	   See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * Created on November 28, 2003
+ *
+ * Current Ver: $Revision: 1777 $
+ * Last Editor: $Author: jdempsey $
+ * Last Edited: $Date: 2006-12-17 05:36:01 +0100 (So, 17 Dez 2006) $
+ *
+ */
+package plugin.pretokens.test;
+
+import pcgen.core.PlayerCharacter;
+import pcgen.core.prereq.AbstractPrerequisiteTest;
+import pcgen.core.prereq.Prerequisite;
+import pcgen.core.prereq.PrerequisiteException;
+import pcgen.core.prereq.PrerequisiteTest;
+import pcgen.util.PropertyFactory;
+
+/**
+ * @author wardc
+ *
+ */
+public class PreReachTester extends AbstractPrerequisiteTest implements
+		PrerequisiteTest
+{
+
+	/* (non-Javadoc)
+	 * @see pcgen.core.prereq.PrerequisiteTest#passes(pcgen.core.PlayerCharacter)
+	 */
+	@Override
+	public int passes(final Prerequisite prereq, final PlayerCharacter character)
+		throws PrerequisiteException
+	{
+		int runningTotal;
+		try
+		{
+			final int targetReach = Integer.parseInt(prereq.getOperand());
+
+			runningTotal =
+					prereq.getOperator().compare(character.getReach(),
+						targetReach);
+		}
+		catch (NumberFormatException nfe)
+		{
+			throw new PrerequisiteException(PropertyFactory.getFormattedString(
+				"PreReach.error.badly_formed", prereq.getOperand())); //$NON-NLS-1$
+		}
+		return countedTotal(prereq, runningTotal);
+	}
+
+	/* (non-Javadoc)
+	 * @see pcgen.core.prereq.PrerequisiteTest#kindsHandled()
+	 */
+	public String kindHandled()
+	{
+		return "REACH"; //$NON-NLS-1$
+	}
+}
---
+++ code/src/java/plugin/pretokens/writer/PreReachWriter.java
@@ -0,0 +1,79 @@
+/*
+ * PreReachWriter.java
+ *
+ * Copyright 2004 (C) Frugal <frugal@purplewombat.co.uk>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.       See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * Created on 18-Dec-2003
+ *
+ * Current Ver: $Revision: 1821 $
+ *
+ * Last Editor: $Author: jdempsey $
+ *
+ * Last Edited: $Date: 2006-12-28 07:12:38 +0100 (Do, 28 Dez 2006) $
+ *
+ */
+package plugin.pretokens.writer;
+
+import pcgen.core.prereq.Prerequisite;
+import pcgen.core.prereq.PrerequisiteOperator;
+import pcgen.persistence.PersistenceLayerException;
+import pcgen.persistence.lst.output.prereq.AbstractPrerequisiteWriter;
+import pcgen.persistence.lst.output.prereq.PrerequisiteWriterInterface;
+
+import java.io.IOException;
+import java.io.Writer;
+
+public class PreReachWriter extends AbstractPrerequisiteWriter implements
+		PrerequisiteWriterInterface
+{
+
+	/* (non-Javadoc)
+	 * @see pcgen.persistence.lst.output.prereq.PrerequisiteWriterInterface#kindHandled()
+	 */
+	public String kindHandled()
+	{
+		return "reach";
+	}
+
+	/* (non-Javadoc)
+	 * @see pcgen.persistence.lst.output.prereq.PrerequisiteWriterInterface#operatorsHandled()
+	 */
+	public PrerequisiteOperator[] operatorsHandled()
+	{
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see pcgen.persistence.lst.output.prereq.PrerequisiteWriterInterface#write(java.io.Writer, pcgen.core.prereq.Prerequisite)
+	 */
+	public void write(Writer writer, Prerequisite prereq)
+		throws PersistenceLayerException
+	{
+		try
+		{
+			writer.write("PREREACH" + (prereq.isOverrideQualify() ? "Q:":""));
+			writer.write(prereq.getOperator().toString().toUpperCase());
+			writer.write(':');
+			writer.write(prereq.getOperand());
+		}
+		catch (IOException e)
+		{
+			throw new PersistenceLayerException(e.getMessage());
+		}
+	}
+
+}
