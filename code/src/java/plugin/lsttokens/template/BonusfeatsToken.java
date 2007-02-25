d03c5dfc384faf019c5464d4656eafe2696d8616
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2007-02-25 19:14:33 +0000

    Tighten restrictions on Template's BONUSFEATS
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@2216 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- code/src/java/plugin/lsttokens/template/BonusfeatsToken.java
+++ code/src/java/plugin/lsttokens/template/BonusfeatsToken.java
@@ -2,6 +2,7 @@ package plugin.lsttokens.template;
 
 import pcgen.core.PCTemplate;
 import pcgen.persistence.lst.PCTemplateLstToken;
+import pcgen.util.Logging;
 
 /**
  * Class deals with BONUSFEATS Token
@@ -19,10 +20,19 @@ public class BonusfeatsToken implements PCTemplateLstToken
 	{
 		try
 		{
-			template.setBonusInitialFeats(Integer.parseInt(value));
+			int featCount = Integer.parseInt(value);
+			if (featCount <= 0)
+			{
+				Logging.errorPrint("Invalid integer in " + getTokenName()
+					+ ": must be greater than zero");
+				return false;
+			}
+			template.setBonusInitialFeats(featCount);
 		}
 		catch (NumberFormatException nfe)
 		{
+			Logging.errorPrint("Invalid " + getTokenName()
+				+ ": must be an integer (greater than zero)");
 			return false;
 		}
 		return true;
