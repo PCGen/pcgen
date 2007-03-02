d78ea8fa57a3215339bae88a395764532f1ab52c
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2007-03-02 21:03:33 +0000

    Add check to make CT positive
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@2324 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- code/src/java/plugin/lsttokens/spell/CtToken.java
+++ code/src/java/plugin/lsttokens/spell/CtToken.java
@@ -2,6 +2,7 @@ package plugin.lsttokens.spell;
 
 import pcgen.core.spell.Spell;
 import pcgen.persistence.lst.SpellLstToken;
+import pcgen.util.Logging;
 
 /**
  * Class deals with CT Token
@@ -18,11 +19,20 @@ public class CtToken implements SpellLstToken
 	{
 		try
 		{
-			spell.setCastingThreshold(Integer.parseInt(value));
+			int ct = Integer.parseInt(value);
+			if (ct < 0)
+			{
+				Logging.errorPrint(getTokenName()
+					+ " can not have a negative value");
+				return false;
+			}
+			spell.setCastingThreshold(ct);
 			return true;
 		}
 		catch (NumberFormatException nfe)
 		{
+			Logging.errorPrint(getTokenName()
+				+ " must be an integer (greater than or equal to zero)");
 			return false;
 		}
 	}
