f0ec31afc4608fb2564c0ca991554a559429ab24
nuance <nuance@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2007-03-04 23:52:08 +0000

    [ 1411229 ] count("x") JEP function
    Convert count function to use enums for the valid parameters to the count function.
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@2383 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- code/src/java/plugin/jepcommands/CountCommand.java
+++ code/src/java/plugin/jepcommands/CountCommand.java
@@ -34,9 +34,9 @@ import pcgen.core.AbilityCategory;
 import pcgen.core.PlayerCharacter;
 import pcgen.core.SettingsHandler;
 import pcgen.core.VariableProcessor;
-import pcgen.util.Logging;
 import pcgen.util.PCGenCommand;
 import pcgen.util.enumeration.View;
+
 
 /**
  * <code>CountCommand</code> deals with the count() JEP command.
@@ -51,6 +51,290 @@ import pcgen.util.enumeration.View;
  */
 public class CountCommand extends PCGenCommand
 {
+
+	public enum JepCountEnum
+	{
+		ABILITIES 
+		{
+			/**
+			 * Count a character's abiltiies.
+			 * 
+			 * @param pc The character being counted.
+			 * @param params The parameters determining which abilities get counted.
+			 * @return A Double with the number of matching abilities.
+			 * @throws ParseException If any invalid parameters are encountered.
+			 */
+			public Object count(PlayerCharacter pc, Object[] params) throws ParseException
+			{
+				if (params.length < 2)
+				{
+					throw new ParseException(
+						"Count of abilities had too few parameters.");
+				}
+
+				String visibility = "VISIBLE";
+				String category = null;
+				String nature = "NORMAL";
+
+				// Parse the parameters passed in
+				for (int i = 1; i < params.length; i++)
+				{
+					if (!(params[i] instanceof String))
+					{
+						throw new ParseException(
+							"Invalid parameter type for parameter #" + (i + 1) + " - "
+								+ String.valueOf(params[i]));
+					}
+
+					String[] keyValue = ((String) params[i]).split("=");
+					if ("CATEGORY".equalsIgnoreCase(keyValue[0]))
+					{
+						category = keyValue[1];
+					}
+					else if ("VISIBILITY".equalsIgnoreCase(keyValue[0]))
+					{
+						visibility = keyValue[1];
+					}
+					else if ("NATURE".equalsIgnoreCase(keyValue[0]))
+					{
+						nature = keyValue[1];
+					}
+					else
+					{
+						throw new ParseException(
+							"Invalid parameter key for parameter #" + (i + 1) + " - "
+								+ String.valueOf(params[i]));
+					}
+				}
+
+				// Fetch the requested list of abilities
+				final AbilityCategory aCategory =
+						SettingsHandler.getGame().getAbilityCategory(category);
+				if (aCategory == null)
+				{
+					throw new ParseException("Invalid category specified "
+						+ String.valueOf(category));
+				}
+				final List<Ability> abilities = new ArrayList<Ability>();
+				if ("ALL".equals(nature))
+				{
+					abilities.addAll(pc.getRealAbilityList(aCategory));
+					abilities.addAll(pc.getAutomaticAbilityList(aCategory));
+					abilities.addAll(pc.getVirtualAbilityList(aCategory));
+				}
+				else if ("VIRTUAL".equals(nature))
+				{
+					abilities.addAll(pc.getVirtualAbilityList(aCategory));
+				}
+				else if ("AUTO".equals(nature))
+				{
+					abilities.addAll(pc.getAutomaticAbilityList(aCategory));
+				}
+				else
+				{
+					abilities.addAll(pc.getRealAbilityList(aCategory));
+				}
+
+				// Count those abilities that match the visibility level
+				View view = View.getViewFromName(visibility);
+				int count = 0;
+				for (Ability ability : abilities)
+				{
+					if (ability.getVisibility().isVisibileTo(view, true))
+					{
+						count++;
+					}
+				}
+
+				return new Double(count);
+			}
+		},
+		CLASSES
+		{
+			/**
+			 * Count a character's Classes.
+			 * 
+			 * @param pc The character being counted.
+			 * @param params The parameters determining which classes get counted.
+			 * @return A Double with the number of matching classes.
+			 * @throws ParseException If any invalid parameters are encountered.
+			 */
+
+			public Object count(PlayerCharacter pc, Object[] params) throws ParseException
+			{
+				return Double.valueOf("0.0");
+			}
+		},
+		DOMAINS
+		{
+			/**
+			 * Count a character's Domains.
+			 * 
+			 * @param pc The character being counted.
+			 * @param params The parameters determining which domains get counted.
+			 * @return A Double with the number of matching domains.
+			 * @throws ParseException If any invalid parameters are encountered.
+			 */
+			public Object count(PlayerCharacter pc, Object[] params) throws ParseException
+			{
+				return Double.valueOf("0.0");
+			}
+		},
+		EQUIPMENT
+		{
+			/**
+			 * Count a character's Equipment.
+			 * 
+			 * @param pc The character being counted.
+			 * @param params The parameters determining which equipment gets counted.
+			 * @return A Double with the number of matching pieces of equipment.
+			 * @throws ParseException If any invalid parameters are encountered.
+			 */
+			public Object count(PlayerCharacter pc, Object[] params) throws ParseException
+			{
+				return Double.valueOf("0.0");
+			}
+		},
+		FOLLOWERS
+		{
+			/**
+			 * Count a character's Followers.
+			 * 
+			 * @param pc The character being counted.
+			 * @param params The parameters determining which followers get counted.
+			 * @return A Double with the number of matching followers.
+			 * @throws ParseException If any invalid parameters are encountered.
+			 */
+			public Object count(PlayerCharacter pc, Object[] params) throws ParseException
+			{
+				return Double.valueOf("0.0");
+			}
+		},
+		LANGUAGES
+		{
+			/**
+			 * Count a character's Languages.
+			 * 
+			 * @param pc The character being counted.
+			 * @param params The parameters determining which languages get counted.
+			 * @return A Double with the number of matching languages.
+			 * @throws ParseException If any invalid parameters are encountered.
+			 */
+
+			public Object count(PlayerCharacter pc, Object[] params) throws ParseException
+			{
+				return Double.valueOf("0.0");
+			}
+		},
+		RACESUBTYPES
+		{
+			/**
+			 * Count a character's Race Subtypes.
+			 * 
+			 * @param pc The character being counted.
+			 * @param params The parameters determining which Race subtypes get counted.
+			 * @return A Double with the number of matching Race subtypes.
+			 * @throws ParseException If any invalid parameters are encountered.
+			 */
+			public Object count(PlayerCharacter pc, Object[] params) throws ParseException
+			{
+				return Double.valueOf("0.0");
+			}
+		},
+		SKILLS
+		{
+			/**
+			 * Count a character's Skills.
+			 * 
+			 * @param pc The character being counted.
+			 * @param params The parameters determining which skills get counted.
+			 * @return A Double with the number of matching skills.
+			 * @throws ParseException If any invalid parameters are encountered.
+			 */
+			public Object count(PlayerCharacter pc, Object[] params) throws ParseException
+			{
+				return Double.valueOf("0.0");
+			}
+		},
+		SPELLBOOKS
+		{
+			/**
+			 * Count a character's Spell Books.
+			 * 
+			 * @param pc The character being counted.
+			 * @param params The parameters determining which spell books get counted.
+			 * @return A Double with the number of matching spell books.
+			 * @throws ParseException If any invalid parameters are encountered.
+			 */
+			public Object count(PlayerCharacter pc, Object[] params) throws ParseException
+			{
+				return Double.valueOf("0.0");
+			}
+		},
+		SPELLS
+		{
+			/**
+			 * Count a character's Spells.
+			 * 
+			 * @param pc The character being counted.
+			 * @param params The parameters determining which spells get counted.
+			 * @return A Double with the number of matching spells.
+			 * @throws ParseException If any invalid parameters are encountered.
+			 */
+			public Object count(PlayerCharacter pc, Object[] params) throws ParseException
+			{
+				return Double.valueOf("0.0");
+			}
+		},
+		SPELLSINBOOK
+		{
+			/**
+			 * Count the spells character's Book.
+			 * 
+			 * @param pc The character being counted.
+			 * @param params The parameters determining which book get counted.
+			 * @return A Double with the number of matching spells.
+			 * @throws ParseException If any invalid parameters are encountered.
+			 */
+			public Object count(PlayerCharacter pc, Object[] params) throws ParseException
+			{
+				return Double.valueOf("0.0");
+			}
+		},
+		SPELLSKNOWN
+		{
+			/**
+			 * Count a character's known spells.
+			 * 
+			 * @param pc The character being counted.
+			 * @param params The parameters determining which spells get counted.
+			 * @return A Double with the number of matching spells.
+			 * @throws ParseException If any invalid parameters are encountered.
+			 */
+			public Object count(PlayerCharacter pc, Object[] params) throws ParseException
+			{
+				return Double.valueOf("0.0");
+			}
+		},
+		TEMPLATES
+		{
+			/**
+			 * Count a character's Templates.
+			 * 
+			 * @param pc The character being counted.
+			 * @param params The parameters determining which templates get counted.
+			 * @return A Double with the number of matching templates.
+			 * @throws ParseException If any invalid parameters are encountered.
+			 */
+			public Object count(PlayerCharacter pc, Object[] params) throws ParseException
+			{
+				return Double.valueOf("0.0");
+			}
+		};
+		
+		abstract public Object count(PlayerCharacter pc, Object[] params) throws ParseException;
+
+	}
 
 	/**
 	 * Constructor
@@ -112,112 +396,16 @@ public class CountCommand extends PCGenCommand
 			}
 
 			// Count the requested object type.
-			Object result = null;
-			final String countType = (String) params[0];
-			if ("ABILITIES".equals(countType))
-			{
-				result = countAbilities(pc, params);
-			}
-
+			JepCountEnum CountEnum = JepCountEnum.valueOf((String) params[0]);
+			
+			Double result = (Double) CountEnum.count(pc, params); 
+			
 			inStack.push(result);
+
 		}
 		else
 		{
 			throw new ParseException("Invalid parameter type");
 		}
-	}
-
-	/**
-	 * Count a character's abiltiies.
-	 * 
-	 * @param pc The character being counted.
-	 * @param params The parameters determining which abilities get counted.
-	 * @return A Double with the number of matching abilities.
-	 * @throws ParseException If any invalid parameters are encountered.
-	 */
-	private Object countAbilities(PlayerCharacter pc, Object[] params)
-		throws ParseException
-	{
-		if (params.length < 2)
-		{
-			throw new ParseException(
-				"Count of abilities had too few parameters.");
-		}
-
-		String visibility = "VISIBLE";
-		String category = null;
-		String nature = "NORMAL";
-
-		// Parse the parameters passed in
-		for (int i = 1; i < params.length; i++)
-		{
-			if (!(params[i] instanceof String))
-			{
-				throw new ParseException(
-					"Invalid parameter type for parameter #" + (i + 1) + " - "
-						+ String.valueOf(params[i]));
-			}
-
-			String[] keyValue = ((String) params[i]).split("=");
-			if ("CATEGORY".equalsIgnoreCase(keyValue[0]))
-			{
-				category = keyValue[1];
-			}
-			else if ("VISIBILITY".equalsIgnoreCase(keyValue[0]))
-			{
-				visibility = keyValue[1];
-			}
-			else if ("NATURE".equalsIgnoreCase(keyValue[0]))
-			{
-				nature = keyValue[1];
-			}
-			else
-			{
-				throw new ParseException(
-					"Invalid parameter key for parameter #" + (i + 1) + " - "
-						+ String.valueOf(params[i]));
-			}
-		}
-
-		// Fetch the requested list of abilities
-		final AbilityCategory aCategory =
-				SettingsHandler.getGame().getAbilityCategory(category);
-		if (aCategory == null)
-		{
-			throw new ParseException("Invalid category specified "
-				+ String.valueOf(category));
-		}
-		final List<Ability> abilities = new ArrayList<Ability>();
-		if ("ALL".equals(nature))
-		{
-			abilities.addAll(pc.getRealAbilityList(aCategory));
-			abilities.addAll(pc.getAutomaticAbilityList(aCategory));
-			abilities.addAll(pc.getVirtualAbilityList(aCategory));
-		}
-		else if ("VIRTUAL".equals(nature))
-		{
-			abilities.addAll(pc.getVirtualAbilityList(aCategory));
-		}
-		else if ("AUTO".equals(nature))
-		{
-			abilities.addAll(pc.getAutomaticAbilityList(aCategory));
-		}
-		else
-		{
-			abilities.addAll(pc.getRealAbilityList(aCategory));
-		}
-
-		// Count those abilities that match the visibility level
-		View view = View.getViewFromName(visibility);
-		int count = 0;
-		for (Ability ability : abilities)
-		{
-			if (ability.getVisibility().isVisibileTo(view, true))
-			{
-				count++;
-			}
-		}
-
-		return new Double(count);
-	}
+	}	
 }
