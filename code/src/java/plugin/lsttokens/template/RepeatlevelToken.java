2c01d33f076e7f057280bfb8077baaf9319b9cca
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2007-03-04 21:46:38 +0000

    Add changes to REPEATLEVEL to catch error conditions
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@2379 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- code/src/java/plugin/lsttokens/template/RepeatlevelToken.java
+++ code/src/java/plugin/lsttokens/template/RepeatlevelToken.java
@@ -4,6 +4,7 @@ import java.util.StringTokenizer;
 
 import pcgen.core.PCTemplate;
 import pcgen.persistence.lst.PCTemplateLstToken;
+import pcgen.util.Logging;
 
 /**
  * Class deals with REPEATLEVEL Token
@@ -22,66 +23,91 @@ public class RepeatlevelToken implements PCTemplateLstToken
 		// x|y|z:level:<level assigned item>
 		//
 		final int endRepeat = value.indexOf(':');
-		if (endRepeat > 0)
+		if (endRepeat <= 0)
 		{
-			final int endLevel = value.indexOf(':', endRepeat + 1);
-			if (endLevel > 0)
+			Logging.errorPrint("Invalid " + getTokenName() + " (no colon) : "
+				+ value);
+			return false;
+		}
+		final int endLevel = value.indexOf(':', endRepeat + 1);
+		if (endLevel <= 0)
+		{
+			Logging.errorPrint("Invalid " + getTokenName()
+				+ " (only one colon) : " + value);
+			return false;
+		}
+		String repeatSec = value.substring(0, endRepeat);
+		final StringTokenizer repeatToken = new StringTokenizer(repeatSec, "|");
+		if (repeatToken.countTokens() == 3)
+		{
+			Logging.errorPrint("Invalid " + getTokenName()
+				+ " (repeat section " + repeatSec
+				+ " does not have two pipes) : " + value);
+			return false;
+		}
+		try
+		{
+			final int lvlIncrement = Integer.parseInt(repeatToken.nextToken());
+			if (lvlIncrement <= 0)
 			{
-				final StringTokenizer repeatToken =
-						new StringTokenizer(value.substring(0, endRepeat), "|");
-				if (repeatToken.countTokens() == 3)
+				Logging.errorPrint("Invalid level increment in "
+					+ getTokenName() + ": must be > 0 : " + value);
+				return false;
+			}
+			final int consecutive = Integer.parseInt(repeatToken.nextToken());
+			if (consecutive < 0)
+			{
+				Logging.errorPrint("Invalid consecutive setting in "
+					+ getTokenName() + ": must be >= 0 : " + value);
+				return false;
+			}
+			final int maxLevel = Integer.parseInt(repeatToken.nextToken());
+			if (maxLevel <= 0)
+			{
+				Logging.errorPrint("Invalid max level in " + getTokenName()
+					+ ": must be > 0 : " + value);
+				return false;
+			}
+			int iLevel =
+					Integer.parseInt(value.substring(endRepeat + 1, endLevel));
+			if (iLevel <= 0)
+			{
+				Logging.errorPrint("Invalid start level in " + getTokenName()
+					+ ": must be > 0 : " + value);
+				return false;
+			}
+
+			int count = consecutive;
+			for (; iLevel <= maxLevel; iLevel += lvlIncrement)
+			{
+				if ((consecutive == 0) || (count != 0))
 				{
-					try
+					final StringTokenizer tok =
+							new StringTokenizer(value.substring(endLevel + 1));
+					final String type = tok.nextToken();
+
+					template.addLevelAbility(iLevel, type, tok.nextToken());
+				}
+				if (consecutive != 0)
+				{
+					if (count == 0)
 					{
-						final int lvlIncrement =
-								Integer.parseInt(repeatToken.nextToken());
-						final int consecutive =
-								Integer.parseInt(repeatToken.nextToken());
-						final int maxLevel =
-								Integer.parseInt(repeatToken.nextToken());
-						int iLevel =
-								Integer.parseInt(value.substring(endRepeat + 1,
-									endLevel));
-
-						if ((iLevel > 0) && (lvlIncrement > 0)
-							&& (maxLevel > 0) && (consecutive >= 0))
-						{
-							int count = consecutive;
-							for (; iLevel <= maxLevel; iLevel += lvlIncrement)
-							{
-								if ((consecutive == 0) || (count != 0))
-								{
-									final StringTokenizer tok =
-											new StringTokenizer(value
-												.substring(endLevel + 1));
-									final String type = tok.nextToken();
-
-									template.addLevelAbility(iLevel, type, tok
-										.nextToken());
-								}
-								if (consecutive != 0)
-								{
-									if (count == 0)
-									{
-										count = consecutive;
-									}
-									else
-									{
-										--count;
-									}
-								}
-							}
-						}
-
-						return true;
+						count = consecutive;
 					}
-					catch (NumberFormatException nfe)
+					else
 					{
-						return false;
+						--count;
 					}
 				}
 			}
+
+			return true;
 		}
-		return false;
+		catch (NumberFormatException nfe)
+		{
+			Logging.errorPrint("Invalid " + getTokenName()
+				+ ": number error encountered in :" + value);
+			return false;
+		}
 	}
 }
