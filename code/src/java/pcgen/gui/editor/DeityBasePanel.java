26418f7e666f1907be0395cf446097a3511beeed
jdempsey <jdempsey@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2007-03-25 12:01:20 +0000

    Add error handling to deity tag parsing
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@2580 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- code/src/java/pcgen/gui/editor/DeityBasePanel.java
+++ code/src/java/pcgen/gui/editor/DeityBasePanel.java
@@ -46,9 +46,11 @@ import pcgen.core.PObject;
 import pcgen.core.SettingsHandler;
 import pcgen.core.WeaponProf;
 import pcgen.gui.utils.JComboBoxEx;
+import pcgen.persistence.PersistenceLayerException;
 import pcgen.persistence.lst.DeityLstToken;
 import pcgen.persistence.lst.LstToken;
 import pcgen.persistence.lst.TokenStore;
+import pcgen.util.Logging;
 import pcgen.util.PropertyFactory;
 
 /**
@@ -222,7 +224,14 @@ final class DeityBasePanel extends BasePanel
 			final StringTokenizer tok = new StringTokenizer(desc, "\t");
 			while (tok.hasMoreTokens())
 			{
-				tokenParser.parse((Deity) thisPObject, tok.nextToken());
+				try
+				{
+					tokenParser.parse((Deity) thisPObject, tok.nextToken());
+				}
+				catch (PersistenceLayerException e)
+				{
+					Logging.errorPrint(e.getMessage() + " while parsing " + desc, e);
+				}
 			}
 		}
 	
