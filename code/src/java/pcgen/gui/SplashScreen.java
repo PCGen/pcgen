c7e10f67faf1fb086e8cb94e2a1438d8109c299a
jdempsey <jdempsey@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2007-04-04 22:11:03 +0000

    Switch to the beta splashscreen - just one version too late!
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@2652 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- code/src/java/pcgen/gui/SplashScreen.java
+++ code/src/java/pcgen/gui/SplashScreen.java
@@ -55,7 +55,7 @@ final class SplashScreen extends JWindow
 		splash.setBorder(new CompoundBorder(new MatteBorder(1, 1, 1, 1, Color.black), new EmptyBorder(12, 12, 12, 12)));
 		splash.setBackground(Color.black);
 
-		URL url = getClass().getResource(IconUtilitities.RESOURCE_URL + "SplashPcgen-Alpha.png");
+		URL url = getClass().getResource(IconUtilitities.RESOURCE_URL + "SplashPcgen_Beta.png");
 
 		if (url != null)
 		{
