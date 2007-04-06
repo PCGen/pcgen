3632f3bd22cdb9fad6d7e804c1a14760b43e1b3b
jdempsey <jdempsey@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2007-04-06 00:07:16 +0000

    Fix bug: Psion Test Character and Psicrystal Issue#: 1694765
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@2667 ef53c05b-3124-4d59-ae7d-fe3273f42be7

---
+++ code/src/test/pcgen/gui/pcGenGUIQPsiCrystalTest.java
@@ -0,0 +1,49 @@
+package pcgen.gui;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * Tests a 35e Psion's PsiCrystal.
+ * See the PCG file for details
+ */
+@SuppressWarnings("nls")
+public class pcGenGUIQPsiCrystalTest extends pcGenGUITestCase
+{
+
+	/**
+	 * 
+	 */
+	public pcGenGUIQPsiCrystalTest()
+	{
+		// Empty Constructor
+	}
+
+	/**
+	 * standard JUnit style constructor
+	 * 
+	 * @param name
+	 */
+	public pcGenGUIQPsiCrystalTest(String name)
+	{
+		super(name);
+	}
+
+	/**
+	 * @return A <tt>TestSuite</tt>
+	 */
+	public static Test suite()
+	{
+		return new TestSuite(pcGenGUIQPsiCrystalTest.class);
+	}
+
+	/**
+	 * Loads and outputs the character.
+	 * 
+	 * @throws Exception If an error occurs.
+	 */
+	public void testQPsiCrystal() throws Exception
+	{
+		runTest("Q-PsiCrystal", "35e");
+	}
+}
---
+++ code/src/test/pcgen/gui/pcGenGUIQuasvinTest.java
@@ -0,0 +1,49 @@
+package pcgen.gui;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * Tests a 35e Psion.
+ * See the PCG file for details
+ */
+@SuppressWarnings("nls")
+public class pcGenGUIQuasvinTest extends pcGenGUITestCase
+{
+
+	/**
+	 * 
+	 */
+	public pcGenGUIQuasvinTest()
+	{
+		// Empty Constructor
+	}
+
+	/**
+	 * standard JUnit style constructor
+	 * 
+	 * @param name
+	 */
+	public pcGenGUIQuasvinTest(String name)
+	{
+		super(name);
+	}
+
+	/**
+	 * @return A <tt>TestSuite</tt>
+	 */
+	public static Test suite()
+	{
+		return new TestSuite(pcGenGUIQuasvinTest.class);
+	}
+
+	/**
+	 * Loads and outputs the character.
+	 * 
+	 * @throws Exception If an error occurs.
+	 */
+	public void testQuasvin() throws Exception
+	{
+		runTest("Quasvin", "35e");
+	}
+}
