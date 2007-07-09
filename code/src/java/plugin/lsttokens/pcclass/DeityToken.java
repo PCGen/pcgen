3171d2c87419ca06808e0872d3387c4cc392da41
jdempsey <jdempsey@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2007-07-09 03:23:26 +0000

    Fix bug: DEITY PCClass token not Parsed Issue#: 1750127
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@3368 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- code/src/java/plugin/lsttokens/pcclass/DeityToken.java
+++ code/src/java/plugin/lsttokens/pcclass/DeityToken.java
@@ -12,19 +12,29 @@ import pcgen.persistence.lst.PCClassLstToken;
 public class DeityToken implements PCClassLstToken
 {
 
+	/* (non-Javadoc)
+	 * @see pcgen.persistence.lst.LstToken#getTokenName()
+	 */
 	public String getTokenName()
 	{
 		return "DEITY";
 	}
 
+	/* (non-Javadoc)
+	 * @see pcgen.persistence.lst.PCClassLstToken#parse(pcgen.core.PCClass, java.lang.String, int)
+	 */
 	public boolean parse(PCClass pcclass, String value, int level)
 	{
 		pcclass.clearDeityList();
 
-		StringTokenizer st = new StringTokenizer(Constants.PIPE);
+		StringTokenizer st = new StringTokenizer(value, Constants.PIPE);
 		while (st.hasMoreTokens())
 		{
-			pcclass.addDeity(st.nextToken());
+			String name = st.nextToken();
+			if (name.trim().length() > 0)
+			{
+				pcclass.addDeity(name);
+			}
 		}
 		return true;
 	}
