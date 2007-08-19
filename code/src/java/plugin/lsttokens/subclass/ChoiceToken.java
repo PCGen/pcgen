ea19e334ca175fd479e08bd22299d2d9c528c9fe
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2007-08-19 22:39:20 +0000

    [ 1777050 ] Please Implement - Class CHOICE: Token Issue#: 1777050
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@3812 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- code/src/java/plugin/lsttokens/subclass/ChoiceToken.java
+++ code/src/java/plugin/lsttokens/subclass/ChoiceToken.java
@@ -1,7 +1,9 @@
 package plugin.lsttokens.subclass;
 
+import pcgen.core.Constants;
 import pcgen.core.SubClass;
 import pcgen.persistence.lst.SubClassLstToken;
+import pcgen.util.Logging;
 
 /**
  * Class deals with CHOICE Token
@@ -16,7 +18,33 @@ public class ChoiceToken implements SubClassLstToken
 
 	public boolean parse(SubClass subclass, String value)
 	{
-		subclass.setChoice(value);
-		return true;
+		int pipeLoc = value.indexOf(Constants.PIPE);
+		if (pipeLoc == -1)
+		{
+			Logging.errorPrint(getTokenName() + " uses a deprecated format: "
+				+ value + "\n  "
+				+ "New format must be type|value, e.g. SCHOOL|Abjuration");
+			subclass.setChoice(value);
+			return true;
+		}
+		if (pipeLoc != value.lastIndexOf(Constants.PIPE))
+		{
+			Logging.errorPrint(getTokenName() + " has invalid arguments: "
+				+ value + "\n  cannot have two | characters");
+			return false;
+		}
+		String type = value.substring(0, pipeLoc);
+		if ("SCHOOL".equals(type) || "SUBSCHOOL".equals(type)
+			|| "DESCRIPTOR".equals(type))
+		{
+			// Unfortunately, in 5.14, we have no way of validating that the
+			// input
+			// is correct
+			subclass.setChoice(value.substring(pipeLoc + 1));
+			return true;
+		}
+		Logging
+			.errorPrint(getTokenName() + " did not understand type: " + type);
+		return false;
 	}
 }
