7080348f0742d114a111d583e9a00184521cf527
jdempsey <jdempsey@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2007-10-18 13:10:33 +0000

    Fix bug: PREABILITY doesn't match category   - Same problem was affecting PREFEAT. Refactor so there is only one set of code. Issue#: 1796835
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@4331 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- code/src/java/pcgen/core/prereq/PrerequisiteUtilities.java
+++ code/src/java/pcgen/core/prereq/PrerequisiteUtilities.java
@@ -26,13 +26,23 @@ package pcgen.core.prereq;
 
 import java.io.StringWriter;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.StringTokenizer;
+
+import pcgen.core.Ability;
+import pcgen.core.AbilityCategory;
+import pcgen.core.Domain;
 import pcgen.core.Equipment;
+import pcgen.core.EquipmentList;
+import pcgen.core.Globals;
 import pcgen.core.PObject;
 import pcgen.core.PlayerCharacter;
 import pcgen.core.SettingsHandler;
+import pcgen.core.Skill;
+import pcgen.core.WeaponProf;
+import pcgen.core.spell.Spell;
 import pcgen.persistence.PersistenceLayerException;
 import pcgen.persistence.lst.output.prereq.PrerequisiteWriter;
 import pcgen.util.Logging;
@@ -174,6 +184,364 @@ public final class PrerequisiteUtilities
 			sBuff.append(writer.toString());
 		}
 		return sBuff.toString();
+	}
+	
+
+	/**
+	 * Check if the character passes the ability prerequisite. Refactored here 
+	 * for use by both PREFEAT and PREABILITY.
+	 *  
+	 * @param prereq The prerequisite to be run.
+	 * @param character The character to be checked.
+	 * @param countMults Should multiple occurrences be counted.
+	 * @param numMatches The number of matches required.
+	 * @param key The key that needs to be matched 
+	 * @param subKey The sub key that needs to be matched.
+	 * @param categoryName The name of the required category, null if any category will be matched.
+	 * @param category The category to be matched
+	 * @return The number of matches made, 0 if not enough matches were made.
+	 */
+	public static int passesAbilityTest(final Prerequisite prereq,
+		final PlayerCharacter character, final boolean countMults,
+		final int numMatches, String key, String subKey, String categoryName,
+		AbilityCategory category)
+	{
+		final boolean keyIsAny =
+			key.equalsIgnoreCase("ANY"); //$NON-NLS-1$
+		final boolean keyIsType =
+				key.startsWith("TYPE=") || key.startsWith("TYPE."); //$NON-NLS-1$ //$NON-NLS-2$
+		final boolean subKeyIsType =
+				subKey != null
+					&& (subKey.startsWith("TYPE=") || subKey.startsWith("TYPE.")); //$NON-NLS-1$ //$NON-NLS-2$
+		if (keyIsType)
+		{
+			key = key.substring(5);
+		}
+		if (subKeyIsType)
+		{
+			subKey = subKey.substring(5);
+		}
+
+		int runningTotal = 0;
+		final List<Ability> abilityList =
+				buildAbilityList(character, categoryName, category);
+		if (!abilityList.isEmpty())
+		{
+			for (Ability ability : abilityList)
+			{
+				final String abilityKey = ability.getKeyName();
+				if (keyIsAny || (!keyIsType && abilityKey.equalsIgnoreCase(key))
+					|| (keyIsType && ability.isType(key)))
+				{
+					// either this feat has matched on the name, or the type
+
+					if (subKey != null)
+					{
+						runningTotal +=
+								checkForSubKeyMatch(character, countMults, key,
+									subKey, subKeyIsType, ability);
+					}
+					else
+					{
+						// Subkey == null
+
+						runningTotal++;
+						if (ability.isMultiples() && countMults)
+						{
+							runningTotal += (ability.getAssociatedCount() - 1);
+						}
+					}
+				}
+				else
+				{
+					if (subKey != null)
+					{
+						final String s1 = key + " (" + subKey + ")";
+						final String s2 = key + "(" + subKey + ")";
+						if (abilityKey.equalsIgnoreCase(s1)
+							|| ability.getKeyName().equalsIgnoreCase(s2))
+						{
+							runningTotal++;
+							if (!countMults)
+							{
+								break;
+							}
+						}
+					}
+				}
+			}
+		}
+
+		runningTotal = prereq.getOperator().compare(runningTotal, numMatches);
+		return runningTotal;
+	}
+
+	/**
+	 * Having matched the ability on the other criteria, check for a match 
+	 * against the sub-key.
+	 * 
+	 * @param character The character being tested.
+	 * @param countMults Should multiple occurrences be counted?
+	 * @param key The key that needs to be matched 
+	 * @param subKey The sub key that needs to be matched.
+	 * @param subKeyIsType Does the subkey refer to a type?
+	 * @param aFeat The ability being checked for a match.
+	 * @return The number of matches made
+	 */
+	private static int checkForSubKeyMatch(final PlayerCharacter character, final boolean countMults, String key, String subKey, final boolean subKeyIsType, Ability aFeat)
+	{
+		final String cType = subKey;
+		final List availableList = new ArrayList();
+		final List selectedList = new ArrayList();
+		final String aChoiceString = aFeat.getChoiceString();
+		int runningTotal = 0;
+
+		aFeat.modChoices(availableList, selectedList, false,
+			character, true, null);
+		availableList.clear();
+
+		if (subKeyIsType) // TYPE syntax
+		{
+			if (aChoiceString.startsWith("SKILL")) //$NON-NLS-1$
+			{
+				runningTotal =
+						subKeySkill(countMults, runningTotal,
+							cType, selectedList);
+			}
+			else if (aChoiceString.startsWith("WEAPONPROFS")) //$NON-NLS-1$
+			{
+				runningTotal =
+						subKeyWeaponProf(countMults,
+							runningTotal, cType, selectedList);
+			}
+			else if (aChoiceString.startsWith("DOMAIN")) //$NON-NLS-1$
+			{
+				runningTotal =
+						subKeyDomain(countMults, runningTotal,
+							cType, selectedList);
+			}
+			else if (aChoiceString.startsWith("SPELL")) //$NON-NLS-1$
+			{
+				runningTotal =
+						subKeySpell(countMults, runningTotal,
+							cType, selectedList);
+			}
+			// End. subKeyIsType
+		}
+		else
+		{
+			if (aFeat.getKeyName().equalsIgnoreCase(key)
+				&& aFeat.containsAssociated(subKey))
+			{
+				runningTotal++;
+				if (aFeat.isMultiples() && countMults)
+				{
+					runningTotal +=
+							(aFeat.getAssociatedCount() - 1);
+				}
+			}
+			else
+			{
+				final int wildCardPos = subKey.indexOf('%');
+
+				if (wildCardPos > -1)
+				{
+					for (int k = 0; k < aFeat
+						.getAssociatedCount(); ++k)
+					{
+
+						final String fString =
+								aFeat.getAssociated(k)
+									.toUpperCase();
+						if (wildCardPos == 0
+							|| fString.startsWith(subKey
+								.substring(0, wildCardPos - 1)
+								.toUpperCase()))
+						{
+							runningTotal++;
+							if (!countMults)
+							{
+								break;
+							}
+						}
+					}
+				}
+			}
+		}
+		return runningTotal;
+	}
+
+	/**
+	 * Build up a list of the character's abilities which match the category requirements.
+	 * @param character The character to be tested.
+	 * @param categoryName The name of the required category, null if any category will be matched.
+	 * @param category The category to be matched
+	 * @return A list of categories matching.
+	 */
+	private static List<Ability> buildAbilityList(final PlayerCharacter character, String categoryName, AbilityCategory category)
+	{
+		final List<Ability> abilityList = new ArrayList<Ability>();
+		if (character != null)
+		{
+			Collection<AbilityCategory> allCats = SettingsHandler.getGame().getAllAbilityCategories();
+			if (categoryName == null)
+			{
+				for (AbilityCategory aCat : allCats)
+				{
+					abilityList.addAll(character.getAggregateAbilityList(aCat));
+				}
+			}
+			else
+			{
+				for (AbilityCategory aCat : allCats)
+				{
+					if (aCat.getAbilityCategory().equals(categoryName))
+					{
+						abilityList.addAll(character.getAggregateAbilityList(aCat));
+					}
+				}
+			}
+		}
+		return abilityList;
+	}
+
+	/**
+	 * @param countMults
+	 * @param runningTotal
+	 * @param cType
+	 * @param selectedList
+	 * @return int
+	 */
+	private static int subKeySpell(final boolean countMults, int runningTotal,
+		final String cType, final List selectedList)
+	{
+		int returnTotal = runningTotal;
+		for (Object aObj : selectedList)
+		{
+			final Spell sp;
+			String spellKey = null;
+			if (aObj instanceof PObject)
+			{
+				spellKey = ((PObject) aObj).getKeyName();
+			}
+			else
+			{
+				spellKey = aObj.toString();
+			}
+			sp = Globals.getSpellKeyed(spellKey);
+			if (sp == null)
+			{
+				continue;
+			}
+			if (sp.isType(cType))
+			{
+				returnTotal++;
+				if (!countMults)
+				{
+					break;
+				}
+			}
+		}
+		return returnTotal;
+	}
+
+	/**
+	 * @param countMults
+	 * @param runningTotal
+	 * @param cType
+	 * @param selectedList
+	 * @return int
+	 */
+	private static int subKeyDomain(final boolean countMults, int runningTotal,
+		final String cType, final List selectedList)
+	{
+		int returnTotal = runningTotal;
+		for (Object aObj : selectedList)
+		{
+			final Domain dom;
+			dom = Globals.getDomainKeyed(aObj.toString());
+			if (dom == null)
+			{
+				continue;
+			}
+			if (dom.isType(cType))
+			{
+				returnTotal++;
+				if (!countMults)
+				{
+					break;
+				}
+			}
+		}
+		return returnTotal;
+	}
+
+	/**
+	 * @param countMults
+	 * @param runningTotal
+	 * @param cType
+	 * @param selectedList
+	 * @return int
+	 */
+	private static int subKeyWeaponProf(final boolean countMults, int runningTotal,
+		final String cType, final List selectedList)
+	{
+		int returnTotal = runningTotal;
+		for (Object aObj : selectedList)
+		{
+			final WeaponProf wp;
+			wp = Globals.getWeaponProfKeyed(aObj.toString());
+			if (wp == null)
+			{
+				continue;
+			}
+			final Equipment eq;
+			eq = EquipmentList.getEquipmentKeyed(wp.getKeyName());
+			if (eq == null)
+			{
+				continue;
+			}
+			if (eq.isType(cType))
+			{
+				returnTotal++;
+				if (!countMults)
+				{
+					break;
+				}
+			}
+		}
+		return returnTotal;
+	}
+
+	/**
+	 * @param countMults
+	 * @param runningTotal
+	 * @param cType
+	 * @param selectedList
+	 * @return int
+	 */
+	private static int subKeySkill(final boolean countMults, int runningTotal,
+		final String cType, final List selectedList)
+	{
+		int returnTotal = runningTotal;
+		for (Object aObj : selectedList)
+		{
+			final Skill sk;
+			sk = Globals.getSkillKeyed(aObj.toString());
+			if (sk == null)
+			{
+				continue;
+			}
+			if (sk.isType(cType))
+			{
+				returnTotal++;
+				if (!countMults)
+				{
+					break;
+				}
+			}
+		}
+		return returnTotal;
 	}
 	
 }
