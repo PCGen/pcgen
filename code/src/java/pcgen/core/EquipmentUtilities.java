600793547c4126467a367e3f9bcef72f1da4f0c9
nuance <nuance@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2007-10-27 21:56:03 +0000

    Refactor some code to simplify and remove duplication.
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@4443 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- code/src/java/pcgen/core/EquipmentUtilities.java
+++ code/src/java/pcgen/core/EquipmentUtilities.java
@@ -26,9 +26,12 @@
  */
 package pcgen.core;
 
-import java.util.*;
-
 import pcgen.core.utils.CoreUtility;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 /**
  * @author Bryan McRoberts <merton_monk@users.sourceforge.net>
@@ -42,78 +45,79 @@ public final class EquipmentUtilities
 		//Don't allow instantiation of utility class
 	}
 
+	private static Comparator<Equipment> equipmentComparator = new Comparator<Equipment>()
+	{
+		private int compareInts(final int obj1Index, final int obj2Index)
+		{
+			if (obj1Index > obj2Index)
+			{
+				return 1;
+			}
+			else if (obj1Index < obj2Index)
+			{
+				return -1;
+			}
+			else
+			{
+				return 0;
+			}
+		}
+
+		public int compare(final Equipment obj1, final Equipment obj2)
+		{
+			int o1i = obj1.getOutputIndex();
+			int o2i = obj2.getOutputIndex();
+
+			// Force unset items (index of 0) to appear at the end
+			o1i = (o1i == 0) ? 999 : o1i;
+			o2i = (o2i == 0) ? 999 : o2i;
+
+			final int result1 = compareInts(o1i, o2i);
+
+			if (result1 != 0)
+			{
+				return result1;
+			}
+
+			final int result2 = compareInts(obj1.getOutputSubindex(), obj2.getOutputSubindex());
+
+			if (result2 != 0)
+			{
+				return result2;
+			}
+
+			final int result3 = obj1.getName().compareToIgnoreCase(obj2.getName());
+
+			if (result3 != 0)
+			{
+				return result3;
+			}
+
+			return obj1.getParentName().compareToIgnoreCase(obj2.getParentName());
+		}
+
+		public boolean equals(final Equipment obj)
+		{
+			return false;
+		}
+
+		public int hashCode()
+		{
+			return 0;
+		}
+	};
+
 	/**
 	 * Merge the equipment list
-	 * @param aList
-	 * @param merge
+	 *
+	 * @param aList the list of Equipment
+	 * @param merge The type of merge to perform
+	 *
 	 * @return merged list
 	 */
 	public static List<Equipment> mergeEquipmentList(final List<Equipment> aList, final int merge)
 	{
-		Collections.sort(aList, new Comparator<Equipment>()
-		{
-			public int compare(final Equipment obj1, final Equipment obj2)
-			{
-				int e1 = obj1.getOutputIndex();
-				int obj2Index = obj2.getOutputIndex();
-
-				// Force unset items (index of 0) to appear at the end
-				if (e1 == 0)
-				{
-					e1 = 999;
-				}
-
-				if (obj2Index == 0)
-				{
-					obj2Index = 999;
-				}
-
-				if (e1 > obj2Index)
-				{
-					return 1;
-				}
-				else if (e1 < obj2Index)
-				{
-					return -1;
-				}
-				else
-				{
-					int sub1 = obj1.getOutputSubindex();
-					int obj2Subindex = obj2.getOutputSubindex();
-
-					if (sub1 > obj2Subindex)
-					{
-						return 1;
-					}
-					else if (sub1 < obj2Subindex)
-					{
-						return -1;
-					}
-					else
-					{
-						if (obj1.getName().compareToIgnoreCase(
-								obj2.getName()) == 0)
-						{
-							return obj1.getParentName()
-									.compareToIgnoreCase(
-											obj2.getParentName());
-						}
-						return obj1.getName()
-								.compareToIgnoreCase(obj2.getName());
-					}
-				}
-			}
-
-			public boolean equals(final Equipment obj)
-			{
-				return false;
-			}
-
-			public int hashCode()
-			{
-				return 0;
-			}
-		});
+		Collections.sort(aList, equipmentComparator);
 
 		// no merging, just sorting
 		if (merge == Constants.MERGE_NONE)
@@ -121,135 +125,69 @@ public final class EquipmentUtilities
 			return aList;
 		}
 
-		final ArrayList<Equipment> eq1List = new ArrayList<Equipment>();
-		final ArrayList<Equipment> eq2List = new ArrayList<Equipment>();
-		final ArrayList<Equipment> mergeList = new ArrayList<Equipment>();
+		final List<Equipment> workingList = new ArrayList<Equipment>();
 
 		// create a temporary list to merge with
-		for ( Equipment tempEq : aList )
+		for (final Equipment tempEq : aList)
 		{
-			final Equipment eq = tempEq.clone();
-			eq1List.add(eq);
-			eq2List.add(eq);
+			workingList.add(tempEq.clone());
 		}
 
-		// merge like equipment within same container
-		if (merge == Constants.MERGE_LOCATION)
+		int endIndex = workingList.size();
+
+		for (int i = 0; i < endIndex; i++)
 		{
-			for ( Equipment eq1 : eq1List )
+			final Equipment eq1 = workingList.get(i);
+			double eQty = eq1.qty();
+
+			for (int j = i + 1; j < endIndex; j++)
 			{
-				double eQty = eq1.qty();
-				boolean found = false;
+				final Equipment eq2 = workingList.get(j);
 
-				for (int i = 0; i < eq2List.size(); i++)
+				// no container merge or Temporary Bonus generated equipment must not merge
+				if (eq1.isContainer() || eq1.isType("TEMPORARY") || eq2.isType("TEMPORARY"))
 				{
-					final Equipment eq2 = eq2List.get(i);
+					continue;
+				}
 
-					if (eq1 == eq2)
+				if (eq1.getName().equals(eq2.getName()))
+				{
+					// merge all like equipment together
+					if (merge == Constants.MERGE_ALL ||
+
+					    // merge like equipment within same container
+					    (merge == Constants.MERGE_LOCATION
+					     && (eq1.getLocation() == eq2.getLocation())
+					     && eq1.getParentName().equals(eq2.getParentName())))
 					{
-						eq2List.remove(eq2);
-						found = true;
-						i--;
-					}
-					else if (eq1.isContainer())
-					{
-						// no container merge
-						continue;
-					}
-					else if (eq1.isType("TEMPORARY") || eq2.isType("TEMPORARY"))
-					{
-						// Temporary Bonus generated equipment must not merge
-						continue;
-					}
-					else if (eq1.getName().equals(eq2.getName())
-							&& (eq1.getLocation() == eq2.getLocation())
-							&& eq1.getParentName().equals(eq2.getParentName()))
-					{
-						eq2List.remove(eq2);
+						workingList.remove(eq2);
 						eQty += eq2.qty();
-						found = true;
-						i--;
+						endIndex--;
 					}
-				}
-
-				if (found)
-				{
-					eq1.setQty(eQty);
-					mergeList.add(eq1);
 				}
 			}
 
-			return mergeList;
+			eq1.setQty(eQty);
 		}
 
-		// merge all like equipment together
-		if (merge == Constants.MERGE_ALL)
-		{
-			for ( Equipment eq1 : eq1List )
-			{
-				double eQty = 0.0;
-				boolean found = false;
-
-				for (int i = 0; i < eq2List.size(); i++)
-				{
-					final Equipment eq2 = eq2List.get(i);
-
-					if (eq1.getName().equals(eq2.getName()))
-					{
-						if (eq1.isContainer())
-						{
-							// no container merge
-							found = true;
-						}
-						else if (eq1.isType("TEMPORARY")
-								|| eq2.isType("TEMPORARY"))
-						{
-							// Temporary Bonus generated equipment must not
-							// merge
-							found = true;
-						}
-						else
-						{
-							eq2List.remove(eq2);
-							eQty += eq2.qty();
-							found = true;
-							i--;
-						}
-					}
-				}
-
-				if (eQty <= 0.0)
-				{
-					eQty = eq1.qty();
-				}
-
-				if (found)
-				{
-					eq1.setQty(eQty);
-					mergeList.add(eq1);
-				}
-			}
-
-			return mergeList;
-		}
-
-		return null;
+		return workingList;
 	}
 
 	/**
 	 * filters a list of equipment to remove all equipment of a given type
+	 *
 	 * @param aList the list to remove
-	 * @param type the type to remove
+	 * @param type  the type to remove
+	 *
 	 * @return a new list containing objects which aren't the specified type
 	 */
 	public static List<Equipment> removeEqType(final List<Equipment> aList, final String type)
 	{
 		final List<Equipment> aArrayList = new ArrayList<Equipment>();
 
-		for ( Equipment eq : aList )
+		for (final Equipment eq : aList)
 		{
-			if (type.equalsIgnoreCase("CONTAINED")
-					&& (eq.getParent() != null))
+			if ("CONTAINED".equalsIgnoreCase(type) && (eq.getParent() != null))
 			{
 				continue;
 			}
@@ -264,17 +202,19 @@ public final class EquipmentUtilities
 	}
 
 	/**
-	 * Filters a list of equipment, returns a new list which only has the item
-	 * of equipment that matched type
-	 * @param aList the list of equipment to filter
+	 * Filters a list of equipment, returns a new list which only has the item of equipment
+	 * that matched type
+	 *
+	 * @param aList   the list of equipment to filter
 	 * @param aString the type of object to return
+	 *
 	 * @return a new list of objects which are all of type aString
 	 */
 	public static List<Equipment> removeNotEqType(final List<Equipment> aList, final String aString)
 	{
 		final List<Equipment> aArrayList = new ArrayList<Equipment>();
 
-		for ( Equipment eq : aList )
+		for (Equipment eq : aList)
 		{
 			if (eq.typeStringContains(aString))
 			{
@@ -286,18 +226,18 @@ public final class EquipmentUtilities
 	}
 
 	/**
-	 * Adds a String to a name, for example, adding "Longsword" to "Weapon
-	 * Specialisation" gives "Weapon Specialisation (Longsword)"
+	 * Adds a String to a name, for example, adding "Longsword" to "Weapon Specialisation"
+	 * gives "Weapon Specialisation (Longsword)"
 	 *
-	 * @param   aName    The Name to add to
-	 * @param   aString  The string to add
+	 * @param aName   The Name to add to
+	 * @param aString The string to add
 	 *
-	 * @return  The modified name
+	 * @return The modified name
 	 */
 	static String appendToName(final String aName, final String aString)
 	{
 		final StringBuffer aBuf = new StringBuffer(aName);
-		final int          iLen = aBuf.length() - 1;
+		final int iLen = aBuf.length() - 1;
 
 		if (aBuf.charAt(iLen) == ')')
 		{
@@ -317,9 +257,9 @@ public final class EquipmentUtilities
 	/**
 	 * Extracts the choiceless form of a name, for example, with all choices removed
 	 *
-	 * @param   aName
+	 * @param aName
 	 *
-	 * @return  the name with sub-choices stripped from it
+	 * @return the name with sub-choices stripped from it
 	 */
 	public static String removeChoicesFromName(String aName)
 	{
@@ -330,33 +270,34 @@ public final class EquipmentUtilities
 
 
 	/**
-	 * Takes a string of the form "foo (bar, baz)", populates the array
-	 * with ["bar", "baz"] and returns foo.  All strings returned by this
-	 * function have had leading.trailing whitespace removed.
+	 * Takes a string of the form "foo (bar, baz)", populates the array with ["bar", "baz"]
+	 * and returns foo.  All strings returned by this function have had leading.trailing
+	 * whitespace removed.
 	 *
-	 * @param name
-	 * @param specifics
+	 * @param name      The full name with stuff in parenthesis
+	 * @param specifics a list which will contain the specifics after the operation has
+	 *                  completed
 	 *
 	 * @return the name with sub-choices stripped from it
 	 */
-	public static String getUndecoratedName(final String name, ArrayList<String> specifics) {
+	public static String getUndecoratedName(final String name, final ArrayList<String> specifics)
+	{
 
-		String altName = removeChoicesFromName(name);
+		final String altName = removeChoicesFromName(name);
 
 		specifics.clear();
-		int start = name.indexOf('(') + 1;
-		int end   = name.lastIndexOf(')');
+		final int start = name.indexOf('(') + 1;
+		final int end = name.lastIndexOf(')');
 
-		if (start >= 0 && end > start) {
+		if (start >= 0 && end > start)
+		{
 
 			// we want what is inside the outermost parenthesis.
-			String subName = name.substring(start, end);
+			final String subName = name.substring(start, end);
 
 			specifics.addAll(CoreUtility.split(subName, ','));
-
 		}
 
 		return altName;
 	}
-
 }
