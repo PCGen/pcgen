8ae19b93c8a854055f5b8f54ef659b17f5b2b5ad
jdempsey <jdempsey@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2007-11-18 12:13:00 +0000

    Complete tests - deprecated tokens and all kits tokens
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@4528 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- code/src/test/plugin/PluginBuildTest.java
+++ code/src/test/plugin/PluginBuildTest.java
@@ -36,7 +36,7 @@ import pcgen.PCGenTestCase;
 
 /**
  * <code>PluginBuildTest</code> verifies that the pluginbuild.xml file has all 
- * required data. As a result this unit tets is a bit different in structure to 
+ * required data. As a result this unit test is a bit different in structure to 
  * a normal test.  
  *
  * Last Editor: $Author$
@@ -47,6 +47,10 @@ import pcgen.PCGenTestCase;
  */
 public class PluginBuildTest extends PCGenTestCase
 {
+	/**
+	 * Array of exceptions to normal names. Each entry is a pair of
+	 * Java source file name and JAR file name. 
+	 */
 	String [][] exceptions = new String[][] {
 		{ "EqBuilderSpell", "EqBuilder.Spell" },
 		{ "GetVar", "Var" },
@@ -63,13 +67,11 @@ public class PluginBuildTest extends PCGenTestCase
 	{
 		return new TestSuite(PluginBuildTest.class);
 	}
-
-	/**
-	 * Test that all required lst-choose plugins are defined.
-	 * @throws Exception
-	 */
 	
-	public void testBonusPlugins() throws Exception
+	/**
+	 * Check for the presence of all 'bonus' token plugins.
+	 */
+	public void testBonusPlugins()
 	{
 		String jarPrefix = "BonusToken-";
 		File sourceFolder = new File("code/src/java/plugin/bonustokens");
@@ -77,38 +79,85 @@ public class PluginBuildTest extends PCGenTestCase
 		checkPluginJars(jarPrefix, sourceFolder, jarFolder);
 	}
 	
-	//TODO: Deprecated tokens don't match the standard format as they are all mixed in
-	// As a result this still needs to be tackled.
+	/**
+	 * Check for the presence of all deprecated token plugins.
+	 */
+	public void testDeprecatedPlugins()
+	{
+		String jarPrefix = "^[a-zA-Z]*-DEPRECATED-";
+		String sourceSuffix = "Lst";
+		File sourceFolder = new File("code/src/java/plugin/lsttokens/deprecated");
+		File jarFolder[] = new File[]{new File("plugins/lstplugins"), new File("plugins/preplugins")};
+		assertTrue("Source folder " + sourceFolder.getAbsolutePath() + " should be a directory", sourceFolder.isDirectory());
+		String[] sources = sourceFolder.list();
+		List<String> srcList = new ArrayList<String>();
+		srcList.addAll(Arrays.asList(sources));
+		srcList.remove("PreDefaultMonsterTester.java");
+		srcList.remove("PreDefaultMonsterWriter.java");
+		sources = (String[]) srcList.toArray(sources);
+		String [][] exceptions = new String[][] {
+			{ "AddInit", "Init" },
+			{ "AddSpecial", "Special" },
+			{ "AddSpellLevel", "SpellLevel" },
+			{ "ClassBab", "Bab" },
+			{ "PreDefaultMonsterParser", "PreDefaultMonster" },
+			{ "RaceAc", "Ac" },
+			{ "SkillList", "Choose" }
+		};
+		checkPluginJarsByRegex(jarPrefix, jarFolder, sourceSuffix, sources, exceptions);
+	}
 
-	public void testExportPlugins() throws Exception
+	/**
+	 * Check for the presence of all export token plugins.
+	 */
+	public void testExportPlugins()
 	{
 		String jarPrefix = "ExportToken-";
 		File sourceFolder = new File("code/src/java/plugin/exporttokens");
 		File jarFolder = new File("plugins/outputplugins");
 		checkPluginJars(jarPrefix, sourceFolder, jarFolder);
 	}
-	public void testJepPlugins() throws Exception
+	
+	/**
+	 * Check for the presence of all JEP command plugins.
+	 */
+	public void testJepPlugins()
 	{
 		String jarPrefix = "JepCommand-";
 		File sourceFolder = new File("code/src/java/plugin/jepcommands");
 		File jarFolder = new File("plugins/jepplugins");
 		checkPluginJars(jarPrefix, sourceFolder, jarFolder, "Command");
 	}
-	public void testLstAbilityPlugins() throws Exception
+	
+	/**
+	 * Check for the presence of all ability
+	 * token parsing plugins.
+	 */
+	public void testLstAbilityPlugins()
 	{
 		String jarPrefix = "AbilityLstToken-";
 		File sourceFolder = new File("code/src/java/plugin/lsttokens/ability");
 		File jarFolder = new File("plugins/lstplugins");
 		checkPluginJars(jarPrefix, sourceFolder, jarFolder);
 	}
-	public void testLstAddPlugins() throws Exception
+	
+	/**
+	 * Check for the presence of all 'add'
+	 * token parsing plugins.
+	 */
+	public void testLstAddPlugins()
 	{
 		String jarPrefix = "AddLstToken-";
 		File sourceFolder = new File("code/src/java/plugin/lsttokens/add");
 		File jarFolder = new File("plugins/lstplugins");
 		checkPluginJars(jarPrefix, sourceFolder, jarFolder);
 	}
-	public void testLstAutoPlugins() throws Exception
+	
+	/**
+	 * Check for the presence of all 'automatic'
+	 * token parsing plugins.
+	 */
+	public void testLstAutoPlugins()
 	{
 		String jarPrefix = "AutoLstToken-";
 		File sourceFolder = new File("code/src/java/plugin/lsttokens/auto");
@@ -116,129 +165,263 @@ public class PluginBuildTest extends PCGenTestCase
 		checkPluginJars(jarPrefix, sourceFolder, jarFolder);
 	}
 	
-	//TODO: BaseKitLstToken
-	
-	public void testLstCampaignPlugins() throws Exception
+	/**
+	 * Check for the presence of all campaign
+	 * token parsing plugins.
+	 */
+	public void testLstCampaignPlugins()
 	{
 		String jarPrefix = "CampaignLstToken-";
 		File sourceFolder = new File("code/src/java/plugin/lsttokens/campaign");
 		File jarFolder = new File("plugins/lstplugins");
 		checkPluginJars(jarPrefix, sourceFolder, jarFolder);
 	}
-	public void testLstChoosePlugins() throws Exception
+	
+	/**
+	 * Check for the presence of all choose
+	 * token parsing plugins.
+	 */
+	public void testLstChoosePlugins()
 	{
 		String jarPrefix = "ChooseToken-";
 		File sourceFolder = new File("code/src/java/plugin/lsttokens/choose");
 		File jarFolder = new File("plugins/lstplugins");
 		checkPluginJars(jarPrefix, sourceFolder, jarFolder);
 	}
-	public void testLstClassPlugins() throws Exception
+	
+	/**
+	 * Check for the presence of all class
+	 * token parsing plugins.
+	 */
+	public void testLstClassPlugins()
 	{
 		String jarPrefix = "ClassLstToken-";
 		File sourceFolder = new File("code/src/java/plugin/lsttokens/pcclass");
 		File jarFolder = new File("plugins/lstplugins");
 		checkPluginJars(jarPrefix, sourceFolder, jarFolder);
 	}
-	public void testLstCompanionmodPlugins() throws Exception
+	
+	/**
+	 * Check for the presence of all companion mod
+	 * token parsing plugins.
+	 */
+	public void testLstCompanionModPlugins()
 	{
 		String jarPrefix = "CompanionModLstToken-";
 		File sourceFolder = new File("code/src/java/plugin/lsttokens/companionmod");
 		File jarFolder = new File("plugins/lstplugins");
 		checkPluginJars(jarPrefix, sourceFolder, jarFolder);
 	}
-	public void testLstDietyPlugins() throws Exception
+	
+	/**
+	 * Check for the presence of all deity
+	 * token parsing plugins.
+	 */
+	public void testLstDietyPlugins()
 	{
 		String jarPrefix = "DeityLstToken-";
 		File sourceFolder = new File("code/src/java/plugin/lsttokens/deity");
 		File jarFolder = new File("plugins/lstplugins");
 		checkPluginJars(jarPrefix, sourceFolder, jarFolder);
 	}
-	public void testLstDomainPlugins() throws Exception
+	
+	/**
+	 * Check for the presence of all domain
+	 * token parsing plugins.
+	 */
+	public void testLstDomainPlugins()
 	{
 		String jarPrefix = "DomainLstToken-";
 		File sourceFolder = new File("code/src/java/plugin/lsttokens/domain");
 		File jarFolder = new File("plugins/lstplugins");
 		checkPluginJars(jarPrefix, sourceFolder, jarFolder);
 	}
-	public void testLstEquipmentPlugins() throws Exception
+	
+	/**
+	 * Check for the presence of all equipment
+	 * token parsing plugins.
+	 */
+	public void testLstEquipmentPlugins()
 	{
 		String jarPrefix = "EquipmentLstToken-";
 		File sourceFolder = new File("code/src/java/plugin/lsttokens/equipment");
 		File jarFolder = new File("plugins/lstplugins");
 		checkPluginJars(jarPrefix, sourceFolder, jarFolder);
 	}
-	public void testLstEquipmentmodifierPlugins() throws Exception
+	
+	/**
+	 * Check for the presence of all equipment modifier
+	 * token parsing plugins.
+	 */
+	public void testLstEquipmentModifierPlugins()
 	{
 		String jarPrefix = "EquipmentModifierLstToken-";
 		File sourceFolder = new File("code/src/java/plugin/lsttokens/equipmentmodifier");
 		File jarFolder = new File("plugins/lstplugins");
 		checkPluginJars(jarPrefix, sourceFolder, jarFolder);
 	}
-	public void testLstGlobalPlugins() throws Exception
+	
+	/**
+	 * Check for the presence of all global
+	 * token parsing plugins.
+	 */
+	public void testLstGlobalPlugins()
 	{
 		String jarPrefix = "LstToken-";
 		File sourceFolder = new File("code/src/java/plugin/lsttokens");
 		File jarFolder = new File("plugins/lstplugins");
 		checkPluginJars(jarPrefix, sourceFolder, jarFolder, "Lst");
 	}
-	public void testLstKitPlugins() throws Exception
+	
+	/**
+	 * Check for the presence of all kit
+	 * token parsing plugins.
+	 */
+	public void testLstKitPlugins()
 	{
 		String jarPrefix = "KitLstToken-";
 		File sourceFolder = new File("code/src/java/plugin/lsttokens/kit");
 		File jarFolder = new File("plugins/lstplugins");
 		checkPluginJars(jarPrefix, sourceFolder, jarFolder);
+
+		jarPrefix = "BaseKitLstToken-";
+		sourceFolder = new File("code/src/java/plugin/lsttokens/kit/basekit");
+		checkPluginJars(jarPrefix, sourceFolder, jarFolder);
+
+		jarPrefix = "KitAbilityLstToken-";
+		sourceFolder = new File("code/src/java/plugin/lsttokens/kit/ability");
+		checkPluginJars(jarPrefix, sourceFolder, jarFolder);
+
+		jarPrefix = "KitClassLstToken-";
+		sourceFolder = new File("code/src/java/plugin/lsttokens/kit/clazz");
+		checkPluginJars(jarPrefix, sourceFolder, jarFolder);
+
+		jarPrefix = "KitDeityLstToken-";
+		sourceFolder = new File("code/src/java/plugin/lsttokens/kit/deity");
+		checkPluginJars(jarPrefix, sourceFolder, jarFolder);
+
+		jarPrefix = "KitFundsLstToken-";
+		sourceFolder = new File("code/src/java/plugin/lsttokens/kit/funds");
+		checkPluginJars(jarPrefix, sourceFolder, jarFolder);
+
+		jarPrefix = "KitGearLstToken-";
+		sourceFolder = new File("code/src/java/plugin/lsttokens/kit/gear");
+		checkPluginJars(jarPrefix, sourceFolder, jarFolder);
+
+		jarPrefix = "KitLevelAbilityLstToken-";
+		sourceFolder = new File("code/src/java/plugin/lsttokens/kit/levelability");
+		checkPluginJars(jarPrefix, sourceFolder, jarFolder);
+
+		jarPrefix = "KitProfLstToken-";
+		sourceFolder = new File("code/src/java/plugin/lsttokens/kit/prof");
+		checkPluginJars(jarPrefix, sourceFolder, jarFolder);
+
+		jarPrefix = "KitSkillLstToken-";
+		sourceFolder = new File("code/src/java/plugin/lsttokens/kit/skill");
+		checkPluginJars(jarPrefix, sourceFolder, jarFolder);
+
+		jarPrefix = "KitSpellsLstToken-";
+		sourceFolder = new File("code/src/java/plugin/lsttokens/kit/spells");
+		checkPluginJars(jarPrefix, sourceFolder, jarFolder);
+
+		jarPrefix = "KitStartpackLstToken-";
+		sourceFolder = new File("code/src/java/plugin/lsttokens/kit/startpack");
+		checkPluginJars(jarPrefix, sourceFolder, jarFolder);
+
+		jarPrefix = "KitTableLstToken-";
+		sourceFolder = new File("code/src/java/plugin/lsttokens/kit/table");
+		checkPluginJars(jarPrefix, sourceFolder, jarFolder);
 	}
-	//TODO: Rest of kit plugins
-	public void testLstRacePlugins() throws Exception
+	
+	/**
+	 * Check for the presence of all race
+	 * token parsing plugins.
+	 */
+	public void testLstRacePlugins()
 	{
 		String jarPrefix = "RaceLstToken-";
 		File sourceFolder = new File("code/src/java/plugin/lsttokens/race");
 		File jarFolder = new File("plugins/lstplugins");
 		checkPluginJars(jarPrefix, sourceFolder, jarFolder);
 	}
-	public void testLstSkillPlugins() throws Exception
+	
+	/**
+	 * Check for the presence of all skill
+	 * token parsing plugins.
+	 */
+	public void testLstSkillPlugins()
 	{
 		String jarPrefix = "SkillLstToken-";
 		File sourceFolder = new File("code/src/java/plugin/lsttokens/skill");
 		File jarFolder = new File("plugins/lstplugins");
 		checkPluginJars(jarPrefix, sourceFolder, jarFolder);
 	}
-	public void testLstSpellPlugins() throws Exception
+	
+	/**
+	 * Check for the presence of all spell
+	 * token parsing plugins.
+	 */
+	public void testLstSpellPlugins()
 	{
 		String jarPrefix = "SpellLstToken-";
 		File sourceFolder = new File("code/src/java/plugin/lsttokens/spell");
 		File jarFolder = new File("plugins/lstplugins");
 		checkPluginJars(jarPrefix, sourceFolder, jarFolder);
 	}
-	public void testLstSubclassPlugins() throws Exception
+	
+	/**
+	 * Check for the presence of all sub class
+	 * token parsing plugins.
+	 */
+	public void testLstSubclassPlugins()
 	{
 		String jarPrefix = "SubClassLstToken-";
 		File sourceFolder = new File("code/src/java/plugin/lsttokens/subclass");
 		File jarFolder = new File("plugins/lstplugins");
 		checkPluginJars(jarPrefix, sourceFolder, jarFolder);
 	}
-	public void testLstSubstitutionlevelPlugins() throws Exception
+	
+	/**
+	 * Check for the presence of all substitution level
+	 * token parsing plugins.
+	 */
+	public void testLstSubstitutionlevelPlugins()
 	{
 		String jarPrefix = "SubstitutionLevelLstToken-";
 		File sourceFolder = new File("code/src/java/plugin/lsttokens/substitutionlevel");
 		File jarFolder = new File("plugins/lstplugins");
 		checkPluginJars(jarPrefix, sourceFolder, jarFolder);
 	}
-	public void testLstTemplatePlugins() throws Exception
+	
+	/**
+	 * Check for the presence of all template
+	 * token parsing plugins.
+	 */
+	public void testLstTemplatePlugins()
 	{
 		String jarPrefix = "TemplateLstToken-";
 		File sourceFolder = new File("code/src/java/plugin/lsttokens/template");
 		File jarFolder = new File("plugins/lstplugins");
 		checkPluginJars(jarPrefix, sourceFolder, jarFolder);
 	}
-	public void testLstWeaponprofPlugins() throws Exception
+	
+	/**
+	 * Check for the presence of all weapon proficiency
+	 * token parsing plugins.
+	 */
+	public void testLstWeaponProfPlugins()
 	{
 		String jarPrefix = "WeaponProfLstToken-";
 		File sourceFolder = new File("code/src/java/plugin/lsttokens/weaponprof");
 		File jarFolder = new File("plugins/lstplugins");
 		checkPluginJars(jarPrefix, sourceFolder, jarFolder);
 	}
-	public void testPrePlugins() throws Exception
+	
+	/**
+	 * Check for the presence of all prerequisite
+	 * token parsing plugins.
+	 */
+	public void testPrePlugins()
 	{
 		String jarPrefix = "PreToken-";
 		File sourceFolder = new File("code/src/java/plugin/pretokens/parser");
@@ -249,9 +432,14 @@ public class PluginBuildTest extends PCGenTestCase
 		srcList.addAll(Arrays.asList(sources));
 		srcList.remove("PreSkillTotalParser.java");
 		sources = (String[]) srcList.toArray(sources);
-		checkPluginJars(jarPrefix, sourceFolder, jarFolder, "Parser", sources);
+		checkPluginJars(jarPrefix, jarFolder, "Parser", sources);
 	}
-	public void testSystemLstplugins() throws Exception
+	
+	/**
+	 * Check for the presence of all system (gamemode, miscinfo etc) file
+	 * token parsing plugins.
+	 */
+	public void testSystemLstPlugins()
 	{
 		String jarPrefix = "GameModeLstToken-";
 		File sourceFolder = new File("code/src/java/plugin/lsttokens/gamemode");
@@ -341,9 +529,13 @@ public class PluginBuildTest extends PCGenTestCase
 	}
 
 	/**
-	 * @param jarPrefix
-	 * @param sourceFolder
-	 * @param jarFolder
+	 * Verify that all non-excluded java files are represented by a jar 
+	 * file of the same name. An exceptions list is used to track jars 
+	 * which are know to have an name varying from the standard.
+	 *   
+	 * @param jarPrefix The text that the jar file name is expected to start with. 
+	 * @param sourceFolder The folder containing the source files.
+	 * @param jarFolder The folder containing the JAR files.
 	 */
 	private void checkPluginJars(String jarPrefix, File sourceFolder,
 		File jarFolder)
@@ -351,15 +543,35 @@ public class PluginBuildTest extends PCGenTestCase
 		checkPluginJars(jarPrefix, sourceFolder, jarFolder, "");
 	}
 
+	/**
+	 * Verify that all non-excluded java files are represented by a jar 
+	 * file of the same name. An exceptions list is used to track jars 
+	 * which are know to have an name varying from the standard.
+	 *   
+	 * @param jarPrefix The text that the jar file name is expected to start with. 
+	 * @param sourceFolder The folder containing the source files.
+	 * @param jarFolder The folder containing the JAR files.
+	 * @param classSuffix The optional suffix on the class name that should be ignored.
+	 */
 	private void checkPluginJars(String jarPrefix, File sourceFolder,
 		File jarFolder, String classSuffix)
 	{
 		assertTrue("Source folder " + sourceFolder.getAbsolutePath() + " should be a directory", sourceFolder.isDirectory());
 		String[] sources = sourceFolder.list();
-		checkPluginJars(jarPrefix, sourceFolder, jarFolder, classSuffix, sources);
+		checkPluginJars(jarPrefix, jarFolder, classSuffix, sources);
 	}
 	
-	private void checkPluginJars(String jarPrefix, File sourceFolder,
+	/**
+	 * Verify that all non-excluded java files are represented by a jar 
+	 * file of the same name. An exceptions list is used to track jars 
+	 * which are know to have an name varying from the standard.
+	 *   
+	 * @param jarPrefix The text that the jar file name is expected to start with. 
+	 * @param jarFolder The folder containing the JAR files.
+	 * @param classSuffix The optional suffix on the class name that should be ignored.
+	 * @param sources The array of names of java source files.
+	 */
+	private void checkPluginJars(String jarPrefix, 
 		File jarFolder, String classSuffix, String[] sources)
 	{
 		assertTrue("Jar folder " + jarFolder.getAbsolutePath() + " should be a directory", jarFolder.isDirectory());
@@ -390,6 +602,63 @@ public class PluginBuildTest extends PCGenTestCase
 				}
 
 				testString = jarPrefix + testString + ".jar";
+				testString = testString.toLowerCase();
+				assertTrue("Jar for " + sources[i] + " should be present in jars list", jarSet.contains(testString));
+			}
+		}
+	}
+	
+	/**
+	 * Verify that all non-excluded java files are represented by a jar 
+	 * file of the same name. An exceptions list is used to track jars 
+	 * which are know to have an name varying from the standard.
+	 *   
+	 * @param jarPrefix The regex text that the jar file name is expected to start with. 
+	 * @param jarFolder The folder containing the JAR files.
+	 * @param classSuffix The optional suffix on the class name that should be ignored.
+	 * @param sources The array of names of java source files.
+	 * @param exceptions The list of known exceptions to the naming standards.
+	 */
+	private void checkPluginJarsByRegex(String jarRegexPrefix,
+		File jarFolder[], String classSuffix, String[] sources,
+		String[][] exceptions)
+	{
+		for (File folder : jarFolder)
+		{
+			assertTrue("Jar folder " + folder.getAbsolutePath() + " should be a directory", folder.isDirectory());
+		}
+		Set<String> jarSet = new HashSet<String>();
+		for (File folder : jarFolder)
+		{
+			String[] jars = folder.list();
+			String jarRegexPattern = jarRegexPrefix + ".*";
+			for (int i = 0; i < jars.length; i++)
+			{
+				if (jars[i].matches(jarRegexPattern))
+				{
+					String jarStr = jars[i].replaceFirst(jarRegexPrefix, "");
+					jarSet.add(jarStr.toLowerCase());
+				}
+			}
+		}
+		for (int i = 0; i < sources.length; i++)
+		{
+			if (sources[i] != null && sources[i].endsWith(".java"))
+			{
+				//String testString = jarPrefix + sources[i].substring(0, sources[i].length()-5);
+				String testString = sources[i];
+				testString = testString.replaceAll(".java", "");
+				testString = testString.replaceAll("Token", "");
+				if (classSuffix.length() > 0)
+				{
+					testString = testString.replaceAll(classSuffix, "");
+				}
+				for (String exception[] : exceptions)
+				{
+					testString = testString.replaceAll(exception[0], exception[1]);
+				}
+
+				testString = testString + ".jar";
 				testString = testString.toLowerCase();
 				assertTrue("Jar for " + sources[i] + " should be present in jars list", jarSet.contains(testString));
 			}
