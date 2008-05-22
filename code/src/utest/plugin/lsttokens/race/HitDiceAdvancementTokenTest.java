f45c33e87019fc61bc92abb3aebdd400b6c1ede1
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2008-05-22 03:19:38 +0000

    Tests for converted Race Tokens
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@6341 ef53c05b-3124-4d59-ae7d-fe3273f42be7

---
+++ code/src/utest/plugin/lsttokens/race/HitDiceAdvancementTokenTest.java
@@ -0,0 +1,147 @@
+/*
+ * Copyright (c) 2007 Tom Parker <thpr@users.sourceforge.net>
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+package plugin.lsttokens.race;
+
+import org.junit.Test;
+
+import pcgen.core.Race;
+import pcgen.persistence.PersistenceLayerException;
+import pcgen.rules.persistence.CDOMLoader;
+import pcgen.rules.persistence.CDOMTokenLoader;
+import pcgen.rules.persistence.token.CDOMPrimaryToken;
+import plugin.lsttokens.testsupport.AbstractTokenTestCase;
+
+public class HitDiceAdvancementTokenTest extends AbstractTokenTestCase<Race>
+{
+
+	static HitdiceadvancementToken token = new HitdiceadvancementToken();
+	static CDOMTokenLoader<Race> loader = new CDOMTokenLoader<Race>(Race.class);
+
+	@Override
+	public Class<Race> getCDOMClass()
+	{
+		return Race.class;
+	}
+
+	@Override
+	public CDOMLoader<Race> getLoader()
+	{
+		return loader;
+	}
+
+	@Override
+	public CDOMPrimaryToken<Race> getToken()
+	{
+		return token;
+	}
+
+	@Test
+	public void testInvalidEmpty() throws PersistenceLayerException
+	{
+		assertFalse(token.parse(primaryContext, primaryProf, ""));
+		assertNoSideEffects();
+	}
+
+	// @Test
+	// public void testInvalidTooManyValues() throws PersistenceLayerException
+	// {
+	// assertFalse(token.parse(primaryContext, primaryProf,
+	// "1,2,3,4,5,6,7,8,9,0"));
+	// }
+
+	@Test
+	public void testInvalidEmptyValue1() throws PersistenceLayerException
+	{
+		assertFalse(token.parse(primaryContext, primaryProf, ",2,3,4"));
+		assertNoSideEffects();
+	}
+
+	@Test
+	public void testInvalidEmptyValue2() throws PersistenceLayerException
+	{
+		assertFalse(token.parse(primaryContext, primaryProf, "1,,3,4"));
+		assertNoSideEffects();
+	}
+
+	@Test
+	public void testInvalidEmptyValueLast() throws PersistenceLayerException
+	{
+		assertFalse(token.parse(primaryContext, primaryProf, "1,2,3,"));
+		assertNoSideEffects();
+	}
+
+	@Test
+	public void testInvalidNegativeValue() throws PersistenceLayerException
+	{
+		assertFalse(token.parse(primaryContext, primaryProf, "-1,2,3"));
+		assertNoSideEffects();
+	}
+
+	@Test
+	public void testInvalidDecreasingValue() throws PersistenceLayerException
+	{
+		assertFalse(token.parse(primaryContext, primaryProf, "5,3,8"));
+		assertNoSideEffects();
+	}
+
+	@Test
+	public void testInvalidEmbeddedSplat() throws PersistenceLayerException
+	{
+		assertFalse(token.parse(primaryContext, primaryProf, "5,*,8"));
+		assertNoSideEffects();
+	}
+
+	@Test
+	public void testInvalidNaN() throws PersistenceLayerException
+	{
+		assertFalse(token.parse(primaryContext, primaryProf, "5,N"));
+		assertNoSideEffects();
+	}
+
+	@Test
+	public void testInvalidTooMuchSplat() throws PersistenceLayerException
+	{
+		assertFalse(token.parse(primaryContext, primaryProf, "5,8*"));
+		assertNoSideEffects();
+	}
+
+	@Test
+	public void testInvalidTooMuchAfterSplat() throws PersistenceLayerException
+	{
+		assertFalse(token.parse(primaryContext, primaryProf, "5,*8"));
+		assertNoSideEffects();
+	}
+
+	@Test
+	public void testRoundRobinSingle() throws PersistenceLayerException
+	{
+		this.runRoundRobin("1");
+	}
+
+	@Test
+	public void testRoundRobinSimple() throws PersistenceLayerException
+	{
+		this.runRoundRobin("1,2,3");
+	}
+
+	@Test
+	public void testRoundRobinComplex() throws PersistenceLayerException
+	{
+		this.runRoundRobin("5,7,9,*");
+	}
+}
---
+++ code/src/utest/plugin/lsttokens/race/LegsTokenTest.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2007 Tom Parker <thpr@users.sourceforge.net>
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+package plugin.lsttokens.race;
+
+import org.junit.Test;
+
+import pcgen.cdom.enumeration.IntegerKey;
+import pcgen.core.Race;
+import pcgen.rules.persistence.CDOMLoader;
+import pcgen.rules.persistence.CDOMTokenLoader;
+import pcgen.rules.persistence.token.CDOMPrimaryToken;
+import plugin.lsttokens.testsupport.AbstractIntegerTokenTestCase;
+
+public class LegsTokenTest extends AbstractIntegerTokenTestCase<Race>
+{
+
+	static LegsToken token = new LegsToken();
+	static CDOMTokenLoader<Race> loader = new CDOMTokenLoader<Race>(Race.class);
+
+	@Override
+	public Class<Race> getCDOMClass()
+	{
+		return Race.class;
+	}
+
+	@Override
+	public CDOMLoader<Race> getLoader()
+	{
+		return loader;
+	}
+
+	@Override
+	public CDOMPrimaryToken<Race> getToken()
+	{
+		return token;
+	}
+
+	@Override
+	public IntegerKey getIntegerKey()
+	{
+		return IntegerKey.LEGS;
+	}
+
+	@Override
+	public boolean isNegativeAllowed()
+	{
+		return false;
+	}
+
+	@Override
+	public boolean isZeroAllowed()
+	{
+		return true;
+	}
+
+	@Override
+	public boolean isPositiveAllowed()
+	{
+		return true;
+	}
+
+	@Test
+	public void dummyTest()
+	{
+		// Just to get Eclipse to recognize this as a JUnit 4.0 Test Case
+	}
+
+}
