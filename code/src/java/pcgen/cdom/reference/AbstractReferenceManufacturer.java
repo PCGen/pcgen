43017f6ba5225ce6cfdbc2f515e0b538ba2990b1
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2008-06-13 01:59:17 +0000

    cleanup
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@6559 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- code/src/java/pcgen/cdom/reference/AbstractReferenceManufacturer.java
+++ code/src/java/pcgen/cdom/reference/AbstractReferenceManufacturer.java
@@ -4,10 +4,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.TreeMap;
 import java.util.Map.Entry;
 
