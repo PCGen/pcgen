456af6ca98b56f8329e95c17ec61a27d4dc4a348
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2008-06-15 22:44:48 +0000

    Remove old ADD syntax
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@6658 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- code/src/java/plugin/lsttokens/AddLst.java
+++ code/src/java/plugin/lsttokens/AddLst.java
@@ -28,131 +28,25 @@ public class AddLst implements GlobalLstToken
 
 	public boolean parse(PObject obj, String value, int anInt)
 	{
-		String key;
-		if (value.startsWith("SPECIAL"))
+		int barLoc = value.indexOf(Constants.PIPE);
+		if (barLoc == -1)
 		{
-			key = "SPECIAL";
+			Logging.errorPrint("Invalid " + getTokenName() + " syntax: "
+					+ value + " ... must have a PIPE");
+			return false;
 		}
-		else if (value.startsWith("FEAT"))
+		else if (barLoc == 0)
 		{
-			key = "FEAT";
+			Logging.errorPrint("Invalid " + getTokenName() + " syntax: "
+					+ value + " ... cannot start with a PIPE");
+			return false;
 		}
-		else if (value.startsWith("INIT"))
+		String key = value.substring(0, barLoc);
+		String contents = value.substring(barLoc + 1);
+		if (contents == null || contents.length() == 0)
 		{
-			key = "INIT";
-		}
-		else if (value.startsWith("VFEAT"))
-		{
-			key = "VFEAT";
-		}
-		else if (value.startsWith("ABILITY"))
-		{
-			key = "ABILITY";
-		}
-		else if (value.startsWith("VABILITY"))
-		{
-			key = "VABILITY";
-		}
-		else if (value.startsWith("CLASSSKILLS"))
-		{
-			key = "CLASSSKILLS";
-		}
-		else if (value.startsWith("WEAPONBONUS"))
-		{
-			key = "WEAPONBONUS";
-			Logging.errorPrint("ADD:LIST has been deprecated, please use a "
-				+ "combination of CHOOSE:WEAPONPROF and BONUS:WEAPONPROF");
-		}
-		else if (value.startsWith("EQUIP"))
-		{
-			key = "EQUIP";
-		}
-		else if (value.startsWith("LIST"))
-		{
-			key = "LIST";
-			Logging.errorPrint("ADD:LIST has been deprecated");
-		}
-		else if (value.startsWith("Language"))
-		{
-			Logging.deprecationPrint("Use of lower-case Language "
-				+ "in ADD is deprecated. Use upper-case LANGUAGE");
-			key = "LANGUAGE";
-		}
-		else if (value.startsWith("LANGUAGE"))
-		{
-			key = "LANGUAGE";
-		}
-		else if (value.startsWith("SKILL"))
-		{
-			key = "SKILL";
-		}
-		else if (value.startsWith("SPELLCASTER"))
-		{
-			key = "SPELLCASTER";
-		}
-		else if (value.startsWith("SPELLLEVEL"))
-		{
-			key = "SPELLLEVEL";
-		}
-		else if (value.startsWith("SA"))
-		{
-			key = "SA";
-		}
-		else
-		{
-			Logging
-				.deprecationPrint("Invalid ADD: Token encountered or lack of a SUBTOKEN for ADD:SA is deprecated.");
-			Logging.deprecationPrint("Please use ADD:SA|name|[count|]X,X");
-			Logging.deprecationPrint("  Offending Token is: ADD:" + value);
-			key = "SA";
-		}
-		String contents;
-		int keyLength = key.length();
-		if (key.equals("SA"))
-		{
-			if (value.indexOf(Constants.PIPE) == -1)
-			{
-				obj.addAddList(anInt, value);
-				return true;
-			}
-			contents = value;
-		}
-		else
-		{
-			if (key.equals("FEAT") && value.equals("FEAT"))
-			{
-				Logging.deprecationPrint("ADD:FEAT "
-					+ "should not be used with no parameters");
-				Logging.deprecationPrint("  This usage is deprecated");
-				Logging
-					.deprecationPrint("  Please use BONUS:FEAT|POOL|1 instead");
-				return obj.addBonusList("FEAT|POOL|1");
-			}
-			contents = value.substring(keyLength + 1);
-			if (value.charAt(keyLength) == '(')
-			{
-				Logging
-					.deprecationPrint("ADD: syntax with parenthesis is deprecated.");
-				Logging.deprecationPrint("Please use ADD:" + key + "|... in ADD:"+ value);
-				obj.addAddList(anInt, value);
-				return true;
-			}
-			else if (key.equals("SPELLLEVEL"))
-			{
-				if (contents.charAt(keyLength) == ':')
-				{
-					Logging.deprecationPrint("Invalid ADD:SPELLLEVEL Syntax: "
-						+ value);
-					Logging.deprecationPrint("Please use ADD:SPELLLEVEL|...");
-					obj.addAddList(anInt, value);
-					return true;
-				}
-			}
-		}
-		if (value.charAt(keyLength) != '|')
-		{
-			Logging.errorPrint("Invalid ADD: Syntax: " + value);
-			Logging.errorPrint("Please use ADD:" + key + "|...");
+			Logging.errorPrint("Invalid " + getTokenName() + " syntax: "
+					+ value + " ... cannot end with a PIPE");
 			return false;
 		}
 		// Guaranteed to be the new syntax here...
