6dc3c676013adc35b3276ba0ef4de68f84f04c6a
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2008-06-22 18:13:38 +0000

    Move code Technically, this creates a tangle, but getting the size of PlayerCharacter down is good and worth that risk, for now
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@6826 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- code/src/java/pcgen/cdom/inst/ObjectCache.java
+++ code/src/java/pcgen/cdom/inst/ObjectCache.java
@@ -1,7 +1,18 @@
 package pcgen.cdom.inst;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeSet;
+
+import pcgen.cdom.base.AssociatedPrereqObject;
 import pcgen.cdom.base.CDOMObject;
+import pcgen.cdom.base.CDOMReference;
 import pcgen.cdom.enumeration.ListKey;
+import pcgen.core.PlayerCharacter;
+import pcgen.core.Vision;
+import pcgen.core.prereq.PrereqHandler;
+import pcgen.util.enumeration.VisionType;
 
 public class ObjectCache extends CDOMObject
 {
@@ -15,6 +26,67 @@ public class ObjectCache extends CDOMObject
 	public void initializeListFor(ListKey<?> lk)
 	{
 		listChar.initializeListFor(lk);
+	}
+
+	public void initializeVisionCache(PlayerCharacter pc)
+	{
+		initializeListFor(ListKey.VISION_CACHE);
+		Map<VisionType, Integer> map = new HashMap<VisionType, Integer>();
+		for (CDOMObject cdo : pc.getCDOMObjectList())
+		{
+			Collection<CDOMReference<Vision>> mods = cdo
+					.getListMods(Vision.VISIONLIST);
+			if (mods == null)
+			{
+				continue;
+			}
+			for (CDOMReference<Vision> ref : mods)
+			{
+				Collection<AssociatedPrereqObject> assoc = cdo
+						.getListAssociations(Vision.VISIONLIST, ref);
+				for (AssociatedPrereqObject apo : assoc)
+				{
+					if (PrereqHandler.passesAll(apo.getPrerequisiteList(), pc,
+							null))
+					{
+						for (Vision v : ref.getContainedObjects())
+						{
+							VisionType visType = v.getType();
+							int a = pc.getVariableValue(v.getDistance(), "")
+									.intValue();
+							Integer current = map.get(visType);
+							if (current == null || current < a)
+							{
+								map.put(visType, a);
+							}
+						}
+					}
+				}
+			}
+		}
+
+		/*
+		 * parse through the global list of vision tags and see if this PC has
+		 * any BONUS:VISION tags which will create a new visionMap entry, and
+		 * add any BONUS to existing entries in the map
+		 */
+		for (VisionType vType : VisionType.getAllVisionTypes())
+		{
+			final int aVal = (int) pc.getTotalBonusTo("VISION", vType
+					.toString());
+
+			if (aVal > 0)
+			{
+				Integer current = map.get(vType);
+				map.put(vType, aVal + (current == null ? 0 : current));
+			}
+		}
+		TreeSet<Vision> set = new TreeSet<Vision>();
+		for (Map.Entry<VisionType, Integer> me : map.entrySet())
+		{
+			set.add(new Vision(me.getKey(), me.getValue().toString()));
+		}
+		addAllToListFor(ListKey.VISION_CACHE, set);
 	}
 
 }
--- code/src/java/pcgen/core/PlayerCharacter.java
+++ code/src/java/pcgen/core/PlayerCharacter.java
@@ -7641,63 +7641,7 @@ public final class PlayerCharacter extends Observable implements Cloneable,
 	{
 		if (!cache.containsListFor(ListKey.VISION_CACHE))
 		{
-			cache.initializeListFor(ListKey.VISION_CACHE);
-			Map<VisionType, Integer> map = new HashMap<VisionType, Integer>();
-			for (CDOMObject cdo : getCDOMObjectList())
-			{
-				Collection<CDOMReference<Vision>> mods = cdo
-						.getListMods(Vision.VISIONLIST);
-				if (mods == null)
-				{
-					continue;
-				}
-				for (CDOMReference<Vision> ref : mods)
-				{
-					Collection<AssociatedPrereqObject> assoc = cdo
-							.getListAssociations(Vision.VISIONLIST, ref);
-					for (AssociatedPrereqObject apo : assoc)
-					{
-						if (PrereqHandler.passesAll(apo.getPrerequisiteList(),
-								this, null))
-						{
-							for (Vision v : ref.getContainedObjects())
-							{
-								VisionType visType = v.getType();
-								int a = getVariableValue(v.getDistance(), "")
-										.intValue();
-								Integer current = map.get(visType);
-								if (current == null || current < a)
-								{
-									map.put(visType, a);
-								}
-							}
-						}
-					}
-				}
-			}
-
-			/*
-			 * parse through the global list of vision tags and see if this PC
-			 * has any BONUS:VISION tags which will create a new visionMap
-			 * entry, and add any BONUS to existing entries in the map
-			 */
-			for (VisionType vType : VisionType.getAllVisionTypes())
-			{
-				final int aVal = (int) getTotalBonusTo("VISION", vType
-						.toString());
-
-				if (aVal > 0)
-				{
-					Integer current = map.get(vType);
-					map.put(vType, aVal + (current == null ? 0 : current));
-				}
-			}
-			TreeSet<Vision> set = new TreeSet<Vision>();
-			for (Map.Entry<VisionType, Integer> me : map.entrySet())
-			{
-				set.add(new Vision(me.getKey(), me.getValue().toString()));
-			}
-			cache.addAllToListFor(ListKey.VISION_CACHE, set);
+			cache.initializeVisionCache(this);
 		}
 		return cache.getListFor(ListKey.VISION_CACHE);
 	}
@@ -12684,7 +12628,7 @@ public final class PlayerCharacter extends Observable implements Cloneable,
 		return lvl;
 	}
 
-	private List<? extends CDOMObject> getCDOMObjectList()
+	public List<? extends CDOMObject> getCDOMObjectList()
 	{
 		List<CDOMObject> list = new ArrayList<CDOMObject>();
 		for (PObject po : getPObjectList())
