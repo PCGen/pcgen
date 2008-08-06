496475a9b4a20e534c045b821215bf6317c6bb1b
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2008-08-06 21:40:53 +0000

    tests
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@7400 ef53c05b-3124-4d59-ae7d-fe3273f42be7

---
+++ code/src/utest/plugin/lsttokens/equipment/BaseItemTokenTest.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2007 Tom Parker <thpr@users.sourceforge.net>
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+package plugin.lsttokens.equipment;
+
+import pcgen.cdom.enumeration.ObjectKey;
+import pcgen.core.Equipment;
+import pcgen.rules.persistence.CDOMLoader;
+import pcgen.rules.persistence.token.CDOMPrimaryToken;
+import plugin.lsttokens.testsupport.AbstractItemTokenTestCase;
+import plugin.lsttokens.testsupport.CDOMTokenLoader;
+
+public class BaseItemTokenTest extends
+		AbstractItemTokenTestCase<Equipment, Equipment>
+{
+
+	static BaseitemToken token = new BaseitemToken();
+	static CDOMTokenLoader<Equipment> loader = new CDOMTokenLoader<Equipment>(
+			Equipment.class);
+
+	@Override
+	public Class<Equipment> getCDOMClass()
+	{
+		return Equipment.class;
+	}
+
+	@Override
+	public CDOMLoader<Equipment> getLoader()
+	{
+		return loader;
+	}
+
+	@Override
+	public CDOMPrimaryToken<Equipment> getToken()
+	{
+		return token;
+	}
+
+	@Override
+	public Class<Equipment> getTargetClass()
+	{
+		return Equipment.class;
+	}
+
+	@Override
+	public boolean isClearLegal()
+	{
+		return false;
+	}
+
+	@Override
+	public ObjectKey<?> getObjectKey()
+	{
+		return ObjectKey.BASE_ITEM;
+	}
+}
---
+++ code/src/utest/plugin/lsttokens/equipment/SizeTokenTest.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright (c) 2007 Tom Parker <thpr@users.sourceforge.net>
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+package plugin.lsttokens.equipment;
+
+import java.net.URISyntaxException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import pcgen.core.Equipment;
+import pcgen.core.SizeAdjustment;
+import pcgen.persistence.PersistenceLayerException;
+import pcgen.rules.persistence.CDOMLoader;
+import pcgen.rules.persistence.token.CDOMPrimaryToken;
+import plugin.lsttokens.testsupport.AbstractTokenTestCase;
+import plugin.lsttokens.testsupport.CDOMTokenLoader;
+
+public class SizeTokenTest extends AbstractTokenTestCase<Equipment>
+{
+
+	static SizeToken token = new SizeToken();
+	static CDOMTokenLoader<Equipment> loader = new CDOMTokenLoader<Equipment>(
+			Equipment.class);
+
+	@Override
+	public Class<Equipment> getCDOMClass()
+	{
+		return Equipment.class;
+	}
+
+	@Override
+	public CDOMLoader<Equipment> getLoader()
+	{
+		return loader;
+	}
+
+	@Override
+	public CDOMPrimaryToken<Equipment> getToken()
+	{
+		return token;
+	}
+
+	@Override
+	@Before
+	public void setUp() throws PersistenceLayerException, URISyntaxException
+	{
+		super.setUp();
+		SizeAdjustment ps = primaryContext.ref.constructCDOMObject(
+				SizeAdjustment.class, "Small");
+		ps.setAbbreviation("S");
+		primaryContext.ref.registerAbbreviation(ps, "S");
+		SizeAdjustment pm = primaryContext.ref.constructCDOMObject(
+				SizeAdjustment.class, "Medium");
+		pm.setAbbreviation("M");
+		primaryContext.ref.registerAbbreviation(pm, "M");
+		SizeAdjustment ss = secondaryContext.ref.constructCDOMObject(
+				SizeAdjustment.class, "Small");
+		ss.setAbbreviation("S");
+		secondaryContext.ref.registerAbbreviation(ss, "S");
+		SizeAdjustment sm = secondaryContext.ref.constructCDOMObject(
+				SizeAdjustment.class, "Medium");
+		sm.setAbbreviation("M");
+		secondaryContext.ref.registerAbbreviation(sm, "M");
+	}
+
+	@Test
+	public void testInvalidNotASize()
+	{
+		assertFalse(token.parse(primaryContext, primaryProf, "W"));
+		assertNoSideEffects();
+	}
+
+	@Test
+	public void testRoundRobinS() throws PersistenceLayerException
+	{
+		runRoundRobin("S");
+	}
+
+	@Test
+	public void testRoundRobinM() throws PersistenceLayerException
+	{
+		runRoundRobin("M");
+	}
+
+}
