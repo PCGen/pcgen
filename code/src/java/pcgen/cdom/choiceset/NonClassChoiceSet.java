9191be87a4c49a13bac674b121e6b82ef303a8ad
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2008-10-30 21:38:32 +0000

    Supporting work for [ 2210198 ] ADD:CLASSSKILLS|1|CROSSCLASSSKILLS broken
    Issue#: 2210198
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@8250 ef53c05b-3124-4d59-ae7d-fe3273f42be7

---
+++ code/src/java/pcgen/cdom/choiceset/NonClassChoiceSet.java
@@ -0,0 +1,179 @@
+/*
+ * Copyright 2006 (C) Tom Parker <thpr@users.sourceforge.net>
+ * 
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * 
+ * Created on October 29, 2006.
+ * 
+ * Current Ver: $Revision: 1111 $ Last Editor: $Author: boomer70 $ Last Edited:
+ * $Date: 2006-06-22 21:22:44 -0400 (Thu, 22 Jun 2006) $
+ */
+package pcgen.cdom.choiceset;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+import pcgen.cdom.base.CDOMReference;
+import pcgen.cdom.base.Constants;
+import pcgen.cdom.base.PrimitiveChoiceSet;
+import pcgen.cdom.reference.ReferenceUtilities;
+import pcgen.core.PlayerCharacter;
+import pcgen.core.Skill;
+
+/**
+ * A NonClassChoiceSet contains references to Skills. It will only return those
+ * that are not Class Skils to the given PlayerCharacter
+ * 
+ * The base contents of a NonClassChoiceSet is defined at construction of the
+ * NonClassChoiceSet.
+ * 
+ */
+public class NonClassChoiceSet implements PrimitiveChoiceSet<Skill>
+{
+
+	/**
+	 * The underlying Set of CDOMReferences that contain the objects in this
+	 * NonClassChoiceSet
+	 */
+	private final Set<CDOMReference<Skill>> set;
+
+	/**
+	 * Constructs a new NonClassChoiceSet which contains the Set of objects
+	 * contained within the given CDOMReferences. The CDOMReferences do not need
+	 * to be resolved at the time of construction of the NonClassChoiceSet.
+	 * 
+	 * This constructor is reference-semantic, meaning that ownership of the
+	 * Collection provided to this constructor is not transferred. Modification
+	 * of the Collection (after this constructor completes) does not result in
+	 * modifying the ReferenceChoiceSet, and the ReferenceChoiceSet will not
+	 * modify the given Collection.
+	 * 
+	 * @param col
+	 *            A Collection of CDOMReferences which define the Set of objects
+	 *            contained within the NonClassChoiceSet
+	 * @throws IllegalArgumentException
+	 *             if the given Collection is null or empty.
+	 */
+	public NonClassChoiceSet(Collection<? extends CDOMReference<Skill>> col)
+	{
+		super();
+		if (col == null)
+		{
+			throw new IllegalArgumentException(
+					"Choice Collection cannot be null");
+		}
+		if (col.isEmpty())
+		{
+			throw new IllegalArgumentException(
+					"Choice Collection cannot be empty");
+		}
+		set = new HashSet<CDOMReference<Skill>>(col);
+	}
+
+	/**
+	 * Returns a representation of this NonClassChoiceSet, suitable for storing
+	 * in an LST file.
+	 */
+	public String getLSTformat()
+	{
+		Set<CDOMReference<Skill>> sortedSet = new TreeSet<CDOMReference<Skill>>(
+				ReferenceUtilities.REFERENCE_SORTER);
+		sortedSet.addAll(set);
+		return ReferenceUtilities.joinLstFormat(sortedSet, Constants.COMMA);
+	}
+
+	/**
+	 * The class of object this NonClassChoiceSet contains.
+	 * 
+	 * The behavior of this method is undefined if the CDOMReference objects
+	 * provided during the construction of this NonClassChoiceSet are not yet
+	 * resolved.
+	 * 
+	 * @return The class of object this NonClassChoiceSet contains.
+	 */
+	public Class<Skill> getChoiceClass()
+	{
+		return Skill.class;
+	}
+
+	/**
+	 * Returns a Set containing the Skills that are not already Class Skills for
+	 * the given PlayerCharacter, which are also Skills this NonClassChoiceSet
+	 * contains.
+	 * 
+	 * The behavior of this method is undefined if the CDOMReference objects
+	 * provided during the construction of this NonClassChoiceSet are not yet
+	 * resolved.
+	 * 
+	 * This method is reference-semantic, meaning that ownership of the Set
+	 * returned by this method will be transferred to the calling object.
+	 * Modification of the returned Set should not result in modifying the
+	 * NonClassChoiceSet, and modifying the NonClassChoiceSet after the Set is
+	 * returned should not modify the Set.
+	 * 
+	 * @return A Set containing the Objects which this NonClassChoiceSet
+	 *         contains.
+	 */
+	public Set<Skill> getSet(PlayerCharacter pc)
+	{
+		Set<Skill> returnSet = new HashSet<Skill>();
+		for (CDOMReference<Skill> ref : set)
+		{
+			for (Skill sk : ref.getContainedObjects())
+			{
+				if (!pc.isClassSkill(sk))
+				{
+					returnSet.add(sk);
+				}
+			}
+		}
+		return returnSet;
+	}
+
+	/**
+	 * Returns the consistent-with-equals hashCode for this NonClassChoiceSet
+	 * 
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode()
+	{
+		return set.size();
+	}
+
+	/**
+	 * Returns true if this NonClassChoiceSet is equal to the given Object.
+	 * Equality is defined as being another NonClassChoiceSet object with equal
+	 * underlying contents.
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object o)
+	{
+		if (o == this)
+		{
+			return true;
+		}
+		if (o instanceof NonClassChoiceSet)
+		{
+			NonClassChoiceSet other = (NonClassChoiceSet) o;
+			return set.equals(other.set);
+		}
+		return false;
+	}
+}
