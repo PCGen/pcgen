a9416676373b2f751c9beea86d6dfe88bf2aa031
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2008-11-02 00:51:47 +0000

    [ 2214399 ] KEY in ability object causes error message KEY and CATEGORY are interrelated, and only checking in one caused problems for Abilities.  This is a hack until KEY is properly rebuilt - this bug only impacts Abilities as those are the only ones imported before completeObject is called. Issue#: 2214399
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@8297 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- code/src/java/plugin/lsttokens/KeyLst.java
+++ code/src/java/plugin/lsttokens/KeyLst.java
@@ -4,6 +4,8 @@
  */
 package plugin.lsttokens;
 
+import pcgen.core.Ability;
+import pcgen.core.Globals;
 import pcgen.core.PObject;
 import pcgen.persistence.lst.GlobalLstToken;
 
@@ -21,6 +23,10 @@ public class KeyLst implements GlobalLstToken
 
 	public boolean parse(PObject obj, String value, int anInt)
 	{
+		if (obj instanceof Ability)
+		{
+			Globals.getContext().ref.reassociateKey(value, obj);
+		}
 		obj.setKeyName(value);
 		return true;
 	}
