df1d3662b4e36485a3bab27c26a0080abc18ecc4
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2008-12-31 02:18:10 +0000

    test for REMOVE token
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@8742 ef53c05b-3124-4d59-ae7d-fe3273f42be7

---
+++ code/src/utest/plugin/lsttokens/remove/FeatTokenTest.java
@@ -0,0 +1,179 @@
+/*
+ * 
+ * Copyright (c) 2007 Tom Parker <thpr@users.sourceforge.net>
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+package plugin.lsttokens.remove;
+
+import org.junit.Test;
+
+import pcgen.cdom.base.CDOMObject;
+import pcgen.core.Ability;
+import pcgen.core.AbilityCategory;
+import pcgen.core.PCClass;
+import pcgen.core.PCTemplate;
+import pcgen.persistence.PersistenceLayerException;
+import pcgen.rules.context.LoadContext;
+import pcgen.rules.persistence.CDOMLoader;
+import pcgen.rules.persistence.token.CDOMPrimaryToken;
+import pcgen.rules.persistence.token.CDOMSecondaryToken;
+import plugin.lsttokens.RemoveLst;
+import plugin.lsttokens.testsupport.AbstractAddTokenTestCase;
+import plugin.lsttokens.testsupport.CDOMTokenLoader;
+
+public class FeatTokenTest extends
+		AbstractAddTokenTestCase<CDOMObject, Ability>
+{
+
+	static RemoveLst token = new RemoveLst();
+	static FeatToken subtoken = new FeatToken();
+	static CDOMTokenLoader<CDOMObject> loader = new CDOMTokenLoader<CDOMObject>(
+			CDOMObject.class);
+
+	@Override
+	public String getAllString()
+	{
+		return "ANY";
+	}
+
+	@Override
+	public Class<PCTemplate> getCDOMClass()
+	{
+		return PCTemplate.class;
+	}
+
+	@Override
+	public CDOMLoader<CDOMObject> getLoader()
+	{
+		return loader;
+	}
+
+	@Override
+	public CDOMPrimaryToken<CDOMObject> getToken()
+	{
+		return token;
+	}
+
+	@Override
+	public CDOMSecondaryToken<?> getSubToken()
+	{
+		return subtoken;
+	}
+
+	@Override
+	public Class<Ability> getTargetClass()
+	{
+		return Ability.class;
+	}
+
+	@Override
+	public boolean isAllLegal()
+	{
+		return true;
+	}
+
+	@Override
+	public boolean isTypeLegal()
+	{
+		return true;
+	}
+
+	@Test
+	public void testEmpty()
+	{
+		// Just to get Eclipse to recognize this as a JUnit 4.0 Test Case
+	}
+
+	@Override
+	public boolean allowsParenAsSub()
+	{
+		return true;
+	}
+
+	@Override
+	protected void construct(LoadContext loadContext, String one)
+	{
+		Ability obj = loadContext.ref.constructCDOMObject(Ability.class, one);
+		loadContext.ref.reassociateCategory(AbilityCategory.FEAT, obj);
+	}
+
+	@Test
+	public void testRoundRobinClass() throws PersistenceLayerException
+	{
+		primaryContext.ref.constructCDOMObject(PCClass.class, "Fighter");
+		secondaryContext.ref.constructCDOMObject(PCClass.class, "Fighter");
+		construct(primaryContext, "TestWP1");
+		construct(primaryContext, "TestWP2");
+		construct(primaryContext, "TestWP3");
+		construct(secondaryContext, "TestWP1");
+		construct(secondaryContext, "TestWP2");
+		construct(secondaryContext, "TestWP3");
+		runRoundRobin(getSubTokenName() + '|' + "CLASS.Fighter,TestWP1"
+				+ getJoinCharacter() + "TestWP2" + getJoinCharacter()
+				+ "TestWP3");
+	}
+
+	@Test
+	public void testRoundRobinClassValue() throws PersistenceLayerException
+	{
+		primaryContext.ref.constructCDOMObject(PCClass.class, "Fighter");
+		secondaryContext.ref.constructCDOMObject(PCClass.class, "Fighter");
+		construct(primaryContext, "TestWP1");
+		construct(primaryContext, "TestWP2");
+		construct(primaryContext, "TestWP3");
+		construct(secondaryContext, "TestWP1");
+		construct(secondaryContext, "TestWP2");
+		construct(secondaryContext, "TestWP3");
+		runRoundRobin(getSubTokenName() + '|' + "CLASS.Fighter,TestWP1"
+				+ getJoinCharacter() + "TestWP2" + getJoinCharacter()
+				+ "TestWP3");
+	}
+
+	@Override
+	public boolean allowsFormula()
+	{
+		return true;
+	}
+
+	@Test
+	public void testInvalidInputClassUnbuilt() throws PersistenceLayerException
+	{
+		assertTrue(parse(getSubTokenName() + '|' + "CLASS=Fighter"));
+		assertFalse(primaryContext.ref.validate());
+	}
+
+	@Test
+	public void testInvalidInputDoubleEquals() throws PersistenceLayerException
+	{
+		assertTrue(parse(getSubTokenName() + '|' + "CLASS==Fighter"));
+		assertFalse(primaryContext.ref.validate());
+	}
+
+	@Test
+	public void testInvalidInputOnlyClass() throws PersistenceLayerException
+	{
+		assertFalse(parse(getSubTokenName() + '|' + "CLASS="));
+		assertNoSideEffects();
+	}
+
+	@Test
+	public void testInvalidInputMultTarget() throws PersistenceLayerException
+	{
+		assertFalse(parse(getSubTokenName() + '|' + "TestWP1(Foo,Bar)"
+				+ getJoinCharacter() + "TestWP2"));
+		assertNoSideEffects();
+	}
+
+}
