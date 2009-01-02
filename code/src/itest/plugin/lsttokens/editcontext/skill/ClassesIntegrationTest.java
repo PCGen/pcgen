88e609cb7a80d13adc70950ffe5994e45bbfca63
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2009-01-02 02:41:29 +0000

    skill integration tests
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@8804 ef53c05b-3124-4d59-ae7d-fe3273f42be7

---
+++ code/src/itest/plugin/lsttokens/editcontext/skill/ClassesIntegrationTest.java
@@ -0,0 +1,168 @@
+/*
+ * Copyright (c) 2007 Tom Parker <thpr@users.sourceforge.net>
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+package plugin.lsttokens.editcontext.skill;
+
+import java.net.URISyntaxException;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import pcgen.cdom.list.ClassSkillList;
+import pcgen.core.Skill;
+import pcgen.persistence.PersistenceLayerException;
+import pcgen.rules.persistence.CDOMLoader;
+import pcgen.rules.persistence.token.CDOMPrimaryToken;
+import plugin.lsttokens.editcontext.testsupport.AbstractIntegrationTestCase;
+import plugin.lsttokens.editcontext.testsupport.TestContext;
+import plugin.lsttokens.skill.ClassesToken;
+import plugin.lsttokens.testsupport.CDOMTokenLoader;
+import plugin.lsttokens.testsupport.TokenRegistration;
+import plugin.pretokens.parser.PreClassParser;
+
+public class ClassesIntegrationTest extends AbstractIntegrationTestCase<Skill>
+{
+
+	static ClassesToken token = new ClassesToken();
+	static CDOMTokenLoader<Skill> loader = new CDOMTokenLoader<Skill>(
+			Skill.class);
+
+	private static boolean classSetUpFired = false;
+
+	@BeforeClass
+	public static final void ltClassSetUp() throws PersistenceLayerException
+	{
+		TokenRegistration.register(new PreClassParser());
+		classSetUpFired = true;
+	}
+
+	@Override
+	@Before
+	public final void setUp() throws PersistenceLayerException,
+			URISyntaxException
+	{
+		super.setUp();
+		if (!classSetUpFired)
+		{
+			ltClassSetUp();
+		}
+	}
+
+	@Override
+	public Class<Skill> getCDOMClass()
+	{
+		return Skill.class;
+	}
+
+	@Override
+	public CDOMLoader<Skill> getLoader()
+	{
+		return loader;
+	}
+
+	@Override
+	public CDOMPrimaryToken<Skill> getToken()
+	{
+		return token;
+	}
+
+	@Test
+	public void testRoundRobinSimple() throws PersistenceLayerException
+	{
+		verifyCleanStart();
+		primaryContext.ref.constructCDOMObject(ClassSkillList.class, "Wizard");
+		secondaryContext.ref
+				.constructCDOMObject(ClassSkillList.class, "Wizard");
+		primaryContext.ref
+				.constructCDOMObject(ClassSkillList.class, "Sorcerer");
+		secondaryContext.ref.constructCDOMObject(ClassSkillList.class,
+				"Sorcerer");
+		primaryContext.ref.constructCDOMObject(ClassSkillList.class, "Cleric");
+		secondaryContext.ref
+				.constructCDOMObject(ClassSkillList.class, "Cleric");
+		TestContext tc = new TestContext();
+		commit(testCampaign, tc, "Wizard");
+		commit(modCampaign, tc, "Cleric|Sorcerer");
+		completeRoundRobin(tc);
+	}
+
+	@Test
+	public void testRoundRobinAddNot() throws PersistenceLayerException
+	{
+		verifyCleanStart();
+		primaryContext.ref.constructCDOMObject(ClassSkillList.class, "Wizard");
+		secondaryContext.ref
+				.constructCDOMObject(ClassSkillList.class, "Wizard");
+		primaryContext.ref
+				.constructCDOMObject(ClassSkillList.class, "Sorcerer");
+		secondaryContext.ref.constructCDOMObject(ClassSkillList.class,
+				"Sorcerer");
+		TestContext tc = new TestContext();
+		commit(testCampaign, tc, "Wizard");
+		commit(modCampaign, tc, "ALL|!Sorcerer");
+		completeRoundRobin(tc);
+	}
+
+	@Test
+	public void testRoundRobinOverridePre() throws PersistenceLayerException
+	{
+		verifyCleanStart();
+		primaryContext.ref.constructCDOMObject(ClassSkillList.class, "Wizard");
+		secondaryContext.ref
+				.constructCDOMObject(ClassSkillList.class, "Wizard");
+		primaryContext.ref
+				.constructCDOMObject(ClassSkillList.class, "Sorcerer");
+		secondaryContext.ref.constructCDOMObject(ClassSkillList.class,
+				"Sorcerer");
+		TestContext tc = new TestContext();
+		commit(testCampaign, tc, "ALL|!Sorcerer|!Wizard");
+		commit(modCampaign, tc, "Sorcerer");
+		completeRoundRobin(tc);
+	}
+
+	@Test
+	public void testRoundRobinNoSet() throws PersistenceLayerException
+	{
+		verifyCleanStart();
+		primaryContext.ref
+				.constructCDOMObject(ClassSkillList.class, "Sorcerer");
+		secondaryContext.ref.constructCDOMObject(ClassSkillList.class,
+				"Sorcerer");
+		TestContext tc = new TestContext();
+		emptyCommit(testCampaign, tc);
+		commit(modCampaign, tc, "Sorcerer");
+		completeRoundRobin(tc);
+	}
+
+	@Test
+	public void testRoundRobinNoReset() throws PersistenceLayerException
+	{
+		verifyCleanStart();
+		primaryContext.ref.constructCDOMObject(ClassSkillList.class, "Wizard");
+		secondaryContext.ref
+				.constructCDOMObject(ClassSkillList.class, "Wizard");
+		primaryContext.ref
+				.constructCDOMObject(ClassSkillList.class, "Sorcerer");
+		secondaryContext.ref.constructCDOMObject(ClassSkillList.class,
+				"Sorcerer");
+		TestContext tc = new TestContext();
+		commit(testCampaign, tc, "Sorcerer|Wizard");
+		emptyCommit(modCampaign, tc);
+		completeRoundRobin(tc);
+	}
+}
---
+++ code/src/itest/plugin/lsttokens/editcontext/skill/KeystatIntegrationTest.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright (c) 2007 Tom Parker <thpr@users.sourceforge.net>
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+package plugin.lsttokens.editcontext.skill;
+
+import java.net.URISyntaxException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import pcgen.core.PCStat;
+import pcgen.core.Skill;
+import pcgen.persistence.PersistenceLayerException;
+import pcgen.rules.persistence.CDOMLoader;
+import pcgen.rules.persistence.token.CDOMPrimaryToken;
+import plugin.lsttokens.editcontext.testsupport.AbstractIntegrationTestCase;
+import plugin.lsttokens.editcontext.testsupport.TestContext;
+import plugin.lsttokens.skill.KeystatToken;
+import plugin.lsttokens.testsupport.CDOMTokenLoader;
+
+public class KeystatIntegrationTest extends AbstractIntegrationTestCase<Skill>
+{
+
+	static KeystatToken token = new KeystatToken();
+	static CDOMTokenLoader<Skill> loader = new CDOMTokenLoader<Skill>(
+			Skill.class);
+
+	@Override
+	@Before
+	public void setUp() throws PersistenceLayerException, URISyntaxException
+	{
+		super.setUp();
+		PCStat ps = primaryContext.ref.constructCDOMObject(PCStat.class,
+				"Strength");
+		primaryContext.ref.registerAbbreviation(ps, "STR");
+		PCStat ss = secondaryContext.ref.constructCDOMObject(PCStat.class,
+				"Strength");
+		secondaryContext.ref.registerAbbreviation(ss, "STR");
+		PCStat pi = primaryContext.ref.constructCDOMObject(PCStat.class,
+				"Intelligence");
+		primaryContext.ref.registerAbbreviation(pi, "INT");
+		PCStat si = secondaryContext.ref.constructCDOMObject(PCStat.class,
+				"Intelligence");
+		secondaryContext.ref.registerAbbreviation(si, "INT");
+	}
+
+	@Override
+	public Class<Skill> getCDOMClass()
+	{
+		return Skill.class;
+	}
+
+	@Override
+	public CDOMLoader<Skill> getLoader()
+	{
+		return loader;
+	}
+
+	@Override
+	public CDOMPrimaryToken<Skill> getToken()
+	{
+		return token;
+	}
+
+	@Test
+	public void testRoundRobinOne() throws PersistenceLayerException
+	{
+		verifyCleanStart();
+		TestContext tc = new TestContext();
+		commit(testCampaign, tc, "STR");
+		commit(modCampaign, tc, "INT");
+		completeRoundRobin(tc);
+	}
+
+	@Test
+	public void testRoundRobinNoSet() throws PersistenceLayerException
+	{
+		verifyCleanStart();
+		TestContext tc = new TestContext();
+		emptyCommit(testCampaign, tc);
+		commit(modCampaign, tc, "INT");
+		completeRoundRobin(tc);
+	}
+
+	@Test
+	public void testRoundRobinNoReset() throws PersistenceLayerException
+	{
+		verifyCleanStart();
+		TestContext tc = new TestContext();
+		commit(testCampaign, tc, "STR");
+		emptyCommit(modCampaign, tc);
+		completeRoundRobin(tc);
+	}
+}
