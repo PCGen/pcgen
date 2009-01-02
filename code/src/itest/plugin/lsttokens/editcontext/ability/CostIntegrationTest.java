e018cab8551fd5c48159815b6f5d8b90ed130b68
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2009-01-02 02:47:06 +0000

    ability integration tests
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@8811 ef53c05b-3124-4d59-ae7d-fe3273f42be7

---
+++ code/src/itest/plugin/lsttokens/editcontext/ability/CostIntegrationTest.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2007 Tom Parker <thpr@users.sourceforge.net>
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+package plugin.lsttokens.editcontext.ability;
+
+import pcgen.core.Ability;
+import pcgen.rules.persistence.CDOMLoader;
+import pcgen.rules.persistence.token.CDOMPrimaryToken;
+import plugin.lsttokens.ability.CostToken;
+import plugin.lsttokens.editcontext.testsupport.AbstractBigDecimalIntegrationTestCase;
+import plugin.lsttokens.testsupport.CDOMTokenLoader;
+
+public class CostIntegrationTest extends
+		AbstractBigDecimalIntegrationTestCase<Ability>
+{
+
+	static CostToken token = new CostToken();
+	static CDOMTokenLoader<Ability> loader = new CDOMTokenLoader<Ability>(
+			Ability.class);
+
+	@Override
+	public Class<Ability> getCDOMClass()
+	{
+		return Ability.class;
+	}
+
+	@Override
+	public CDOMLoader<Ability> getLoader()
+	{
+		return loader;
+	}
+
+	@Override
+	public CDOMPrimaryToken<Ability> getToken()
+	{
+		return token;
+	}
+
+	@Override
+	public boolean isNegativeAllowed()
+	{
+		return false;
+	}
+
+	@Override
+	public boolean isPositiveAllowed()
+	{
+		return true;
+	}
+
+	@Override
+	public boolean isZeroAllowed()
+	{
+		return true;
+	}
+}
---
+++ code/src/itest/plugin/lsttokens/editcontext/ability/ModifyfeatchoiceIntegrationTest.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright (c) 2007 Tom Parker <thpr@users.sourceforge.net>
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+package plugin.lsttokens.editcontext.ability;
+
+import org.junit.Test;
+
+import pcgen.core.Ability;
+import pcgen.core.AbilityCategory;
+import pcgen.rules.context.LoadContext;
+import pcgen.rules.persistence.CDOMLoader;
+import pcgen.rules.persistence.token.CDOMPrimaryToken;
+import plugin.lsttokens.ability.ModifyfeatchoiceToken;
+import plugin.lsttokens.editcontext.testsupport.AbstractListIntegrationTestCase;
+import plugin.lsttokens.testsupport.CDOMTokenLoader;
+
+public class ModifyfeatchoiceIntegrationTest extends
+		AbstractListIntegrationTestCase<Ability, Ability>
+{
+
+	static ModifyfeatchoiceToken token = new ModifyfeatchoiceToken();
+	static CDOMTokenLoader<Ability> loader = new CDOMTokenLoader<Ability>(Ability.class);
+
+	@Override
+	public Class<Ability> getCDOMClass()
+	{
+		return Ability.class;
+	}
+
+	@Override
+	public CDOMLoader<Ability> getLoader()
+	{
+		return loader;
+	}
+
+	@Override
+	public CDOMPrimaryToken<Ability> getToken()
+	{
+		return token;
+	}
+
+	@Override
+	public Class<Ability> getTargetClass()
+	{
+		return Ability.class;
+	}
+
+	@Override
+	public boolean isTypeLegal()
+	{
+		return false;
+	}
+
+	@Override
+	public char getJoinCharacter()
+	{
+		return '|';
+	}
+
+	@Test
+	public void dummyTest()
+	{
+		// Just to get Eclipse to recognize this as a JUnit 4.0 Test Case
+	}
+
+	@Override
+	public boolean isClearDotLegal()
+	{
+		return false;
+	}
+
+	@Override
+	public boolean isClearLegal()
+	{
+		return false;
+	}
+
+	@Override
+	public boolean isPrereqLegal()
+	{
+		return false;
+	}
+
+	@Override
+	protected void construct(LoadContext loadContext, String one)
+	{
+		Ability obj = loadContext.ref.constructCDOMObject(Ability.class, one);
+		loadContext.ref.reassociateCategory(AbilityCategory.FEAT, obj);
+	}
+
+	@Override
+	public boolean isAllLegal()
+	{
+		return false;
+	}
+}
