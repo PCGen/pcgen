09f4dcde1e551936fcf7675f6fe659511534c9e1
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2009-01-02 02:42:49 +0000

    eqmod integration tests
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@8806 ef53c05b-3124-4d59-ae7d-fe3273f42be7

---
+++ code/src/itest/plugin/lsttokens/editcontext/equipmentmodifier/CostIntegrationTest.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2007 Tom Parker <thpr@users.sourceforge.net>
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+package plugin.lsttokens.editcontext.equipmentmodifier;
+
+import pcgen.core.EquipmentModifier;
+import pcgen.rules.persistence.CDOMLoader;
+import pcgen.rules.persistence.token.CDOMPrimaryToken;
+import plugin.lsttokens.editcontext.testsupport.AbstractFormulaIntegrationTestCase;
+import plugin.lsttokens.equipmentmodifier.CostToken;
+import plugin.lsttokens.testsupport.CDOMTokenLoader;
+
+public class CostIntegrationTest extends
+		AbstractFormulaIntegrationTestCase<EquipmentModifier>
+{
+
+	static CostToken token = new CostToken();
+	static CDOMTokenLoader<EquipmentModifier> loader = new CDOMTokenLoader<EquipmentModifier>(
+			EquipmentModifier.class);
+
+	@Override
+	public Class<EquipmentModifier> getCDOMClass()
+	{
+		return EquipmentModifier.class;
+	}
+
+	@Override
+	public CDOMLoader<EquipmentModifier> getLoader()
+	{
+		return loader;
+	}
+
+	@Override
+	public CDOMPrimaryToken<EquipmentModifier> getToken()
+	{
+		return token;
+	}
+
+	@Override
+	public boolean isNegativeAllowed()
+	{
+		return false;
+	}
+}
