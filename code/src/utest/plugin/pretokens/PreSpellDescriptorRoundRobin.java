443e15dc5cf42841051b36f171c46809e2a40a31
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2009-01-02 04:31:14 +0000

    1910516 related PRESPELL* round robin tests
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@8822 ef53c05b-3124-4d59-ae7d-fe3273f42be7

---
+++ code/src/utest/plugin/pretokens/PreSpellDescriptorRoundRobin.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2008 Tom Parker <thpr@users.sourceforge.net>
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+package plugin.pretokens;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import junit.textui.TestRunner;
+import plugin.lsttokens.testsupport.TokenRegistration;
+import plugin.pretokens.parser.PreSpellDescriptorParser;
+import plugin.pretokens.writer.PreSpellDescriptorWriter;
+
+public class PreSpellDescriptorRoundRobin extends AbstractRankedRoundRobin
+{
+
+	public static void main(String args[])
+	{
+		TestRunner.run(PreSpellDescriptorRoundRobin.class);
+	}
+
+	/**
+	 * @return Test
+	 */
+	public static Test suite()
+	{
+		return new TestSuite(PreSpellDescriptorRoundRobin.class);
+	}
+
+	@Override
+	protected void setUp() throws Exception
+	{
+		super.setUp();
+		TokenRegistration.register(new PreSpellDescriptorParser());
+		TokenRegistration.register(new PreSpellDescriptorWriter());
+	}
+
+	@Override
+	public String getBaseString()
+	{
+		return "SPELLDESCRIPTOR";
+	}
+
+	@Override
+	public boolean isAnyAllowed()
+	{
+		return true;
+	}
+
+	@Override
+	public boolean isTypeAllowed()
+	{
+		return false;
+	}
+
+}
---
+++ code/src/utest/plugin/pretokens/PreSpellSchoolRoundRobin.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2008 Tom Parker <thpr@users.sourceforge.net>
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+package plugin.pretokens;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import junit.textui.TestRunner;
+import plugin.lsttokens.testsupport.TokenRegistration;
+import plugin.pretokens.parser.PreSpellSchoolParser;
+import plugin.pretokens.writer.PreSpellSchoolWriter;
+
+public class PreSpellSchoolRoundRobin extends AbstractRankedRoundRobin
+{
+
+	public static void main(String args[])
+	{
+		TestRunner.run(PreSpellSchoolRoundRobin.class);
+	}
+
+	/**
+	 * @return Test
+	 */
+	public static Test suite()
+	{
+		return new TestSuite(PreSpellSchoolRoundRobin.class);
+	}
+
+	@Override
+	protected void setUp() throws Exception
+	{
+		super.setUp();
+		TokenRegistration.register(new PreSpellSchoolParser());
+		TokenRegistration.register(new PreSpellSchoolWriter());
+	}
+
+	@Override
+	public String getBaseString()
+	{
+		return "SPELLSCHOOL";
+	}
+
+	@Override
+	public boolean isAnyAllowed()
+	{
+		return true;
+	}
+
+	@Override
+	public boolean isTypeAllowed()
+	{
+		return false;
+	}
+}
