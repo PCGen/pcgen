4268551647cc993e0b3117261bfc4124ed5d4b0e
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2009-01-02 02:36:28 +0000

    license info
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@8798 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- code/src/utest/plugin/pretokens/PreSARoundRobin.java
+++ code/src/utest/plugin/pretokens/PreSARoundRobin.java
@@ -1,24 +1,19 @@
 /*
- * PreAgeSetRoundRobin.java
- * Copyright 2008 (C) James Dempsey <jdempsey@users.sourceforge.net>
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- *
- * Created on 1/04/2008
- *
- * $Id: PreAgeSetRoundRobin.java 8147 2008-10-19 19:45:26Z thpr $
+ * Copyright (c) 2008 Tom Parker <thpr@users.sourceforge.net>
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
  */
 package plugin.pretokens;
 
@@ -29,16 +24,6 @@ import plugin.lsttokens.testsupport.TokenRegistration;
 import plugin.pretokens.parser.PreSpecialAbilityParser;
 import plugin.pretokens.writer.PreSpecialAbilityWriter;
 
-/**
- * <code>PreAgeSetRoundRobin</code> verifies that preageset tags can be 
- * read and written.
- *
- * Last Editor: $Author: thpr $
- * Last Edited: $Date: 2008-10-19 15:45:26 -0400 (Sun, 19 Oct 2008) $
- *
- * @author James Dempsey <jdempsey@users.sourceforge.net>
- * @version $Revision: 8147 $
- */
 public class PreSARoundRobin extends AbstractBasicRoundRobin
 {
 	
