a5335dce7198b4138f4d85005ee8ec9fa3cefce0
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2009-01-03 19:40:08 +0000

    additional PRE round robin test
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@8871 ef53c05b-3124-4d59-ae7d-fe3273f42be7

---
+++ code/src/utest/plugin/pretokens/PreClassLevelMaxRobin.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2008 Tom Parker <thpr@users.sourceforge.net>
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+package plugin.pretokens;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import junit.textui.TestRunner;
+import plugin.lsttokens.testsupport.TokenRegistration;
+import plugin.pretokens.parser.PreClassLevelMaxParser;
+import plugin.pretokens.writer.PreClassWriter;
+
+public class PreClassLevelMaxRobin extends AbstractPreRoundRobin
+{
+	public static void main(String args[])
+	{
+		TestRunner.run(PreClassLevelMaxRobin.class);
+	}
+
+	/**
+	 * @return Test
+	 */
+	public static Test suite()
+	{
+		return new TestSuite(PreClassLevelMaxRobin.class);
+	}
+
+	@Override
+	protected void setUp() throws Exception
+	{
+		super.setUp();
+		TokenRegistration.register(new PreClassLevelMaxParser());
+		TokenRegistration.register(new PreClassWriter());
+	}
+
+	public void testBasic()
+	{
+		runSimpleRoundRobin("PRECLASSLEVELMAX:1,Barbarian=1",
+				"!PRECLASS:1,Barbarian=2");
+	}
+
+	public void testNegated()
+	{
+		runSimpleRoundRobin("!PRECLASSLEVELMAX:1,Barbarian=1",
+				"PRECLASS:1,Barbarian=2");
+	}
+
+	public void testSimple()
+	{
+		runSimpleRoundRobin("PRECLASSLEVELMAX:1,Fighter=1,SPELLCASTER=2",
+				"PREMULT:1,[!PRECLASS:1,Fighter=2],[!PRECLASS:1,SPELLCASTER=3]");
+	}
+
+	public void testTwo()
+	{
+		runSimpleRoundRobin("PRECLASSLEVELMAX:2,Fighter=1,SPELLCASTER=2",
+				"!PRECLASS:1,Fighter=2,SPELLCASTER=3");
+	}
+
+}
