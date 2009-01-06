f70e717d52d332ac97f0e8ac374e53d82dc8046d
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2009-01-06 23:30:50 +0000

    Don't unparse situations where there is an embedded [FEAT=x], only when it "starts with" FEAT= I'm hesitant to actually check == 0 as location due to other potential hidden features of CHOOSE (which isn't really at the same quality as most of the other tokens)
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@8903 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- code/src/java/plugin/lsttokens/choose/FeatToken.java
+++ code/src/java/plugin/lsttokens/choose/FeatToken.java
@@ -92,7 +92,8 @@ public class FeatToken implements CDOMSecondaryToken<CDOMObject>
 	{
 		String chooseString = context.getObjectContext().getString(cdo,
 				StringKey.CHOICE_STRING);
-		if (chooseString == null || chooseString.indexOf("FEAT=") == -1)
+		if (chooseString == null || chooseString.indexOf("FEAT=") == -1
+				|| chooseString.indexOf("[FEAT=") != -1)
 		{
 			return null;
 		}
