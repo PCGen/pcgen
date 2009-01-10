1b8b3b0424306fc8d7ae890c5c576edd37eef805
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2009-01-10 02:20:32 +0000

    integration test for ASPECT
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@8950 ef53c05b-3124-4d59-ae7d-fe3273f42be7

---
+++ code/src/itest/plugin/lsttokens/editcontext/ability/AspectIntegrationTest.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2007 Tom Parker <thpr@users.sourceforge.net>
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+package plugin.lsttokens.editcontext.ability;
+
+import org.junit.Test;
+
+import pcgen.core.Ability;
+import pcgen.persistence.PersistenceLayerException;
+import pcgen.rules.persistence.CDOMLoader;
+import pcgen.rules.persistence.token.CDOMPrimaryToken;
+import plugin.lsttokens.ability.AspectToken;
+import plugin.lsttokens.editcontext.testsupport.AbstractIntegrationTestCase;
+import plugin.lsttokens.editcontext.testsupport.TestContext;
+import plugin.lsttokens.testsupport.CDOMTokenLoader;
+
+public class AspectIntegrationTest extends
+		AbstractIntegrationTestCase<Ability>
+{
+	static AspectToken token = new AspectToken();
+	static CDOMTokenLoader<Ability> loader = new CDOMTokenLoader<Ability>(
+			Ability.class);
+
+	@Override
+	public Class<Ability> getCDOMClass()
+	{
+		return Ability.class;
+	}
+
+	@Override
+	public CDOMLoader<Ability> getLoader()
+	{
+		return loader;
+	}
+
+	@Override
+	public CDOMPrimaryToken<Ability> getToken()
+	{
+		return token;
+	}
+
+	@Test
+	public void testRoundRobinSimple() throws PersistenceLayerException
+	{
+		verifyCleanStart();
+		TestContext tc = new TestContext();
+		commit(testCampaign, tc, "Quality Name|Quality Value");
+		commit(modCampaign, tc, "Niederösterreich Quality|Niederösterreich");
+		completeRoundRobin(tc);
+	}
+
+	@Test
+	public void testRoundRobinRemove() throws PersistenceLayerException
+	{
+		verifyCleanStart();
+		TestContext tc = new TestContext();
+		commit(testCampaign, tc, "Niederösterreich Quality|Niederösterreich");
+		commit(modCampaign, tc, "Niederösterreich Quality|Nied");
+		completeRoundRobin(tc);
+	}
+
+	@Test
+	public void testRoundRobinNoSet() throws PersistenceLayerException
+	{
+		verifyCleanStart();
+		TestContext tc = new TestContext();
+		emptyCommit(testCampaign, tc);
+		commit(modCampaign, tc, "QualityName|QualityValue");
+		completeRoundRobin(tc);
+	}
+
+	@Test
+	public void testRoundRobinNoReset() throws PersistenceLayerException
+	{
+		verifyCleanStart();
+		TestContext tc = new TestContext();
+		commit(testCampaign, tc, "QualityName|QualityValue");
+		emptyCommit(modCampaign, tc);
+		completeRoundRobin(tc);
+	}
+
+}
