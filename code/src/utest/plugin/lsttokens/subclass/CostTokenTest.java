76de4f66e73a49f159c11b2d1567dfeb6d702dbf
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2009-02-02 22:19:25 +0000

    additional unit tests
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@9222 ef53c05b-3124-4d59-ae7d-fe3273f42be7

---
+++ code/src/utest/plugin/lsttokens/subclass/CostTokenTest.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2009 Tom Parker <thpr@users.sourceforge.net>
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+package plugin.lsttokens.subclass;
+
+import pcgen.cdom.enumeration.IntegerKey;
+import pcgen.core.SubClass;
+import pcgen.rules.persistence.CDOMLoader;
+import pcgen.rules.persistence.token.CDOMPrimaryToken;
+import plugin.lsttokens.testsupport.AbstractIntegerTokenTestCase;
+import plugin.lsttokens.testsupport.CDOMTokenLoader;
+
+public class CostTokenTest extends AbstractIntegerTokenTestCase<SubClass>
+{
+
+	static CostToken token = new CostToken();
+	static CDOMTokenLoader<SubClass> loader = new CDOMTokenLoader<SubClass>(
+			SubClass.class);
+
+	@Override
+	public Class<SubClass> getCDOMClass()
+	{
+		return SubClass.class;
+	}
+
+	@Override
+	public CDOMLoader<SubClass> getLoader()
+	{
+		return loader;
+	}
+
+	@Override
+	public CDOMPrimaryToken<SubClass> getToken()
+	{
+		return token;
+	}
+
+	@Override
+	public IntegerKey getIntegerKey()
+	{
+		return IntegerKey.COST;
+	}
+
+	@Override
+	public boolean isNegativeAllowed()
+	{
+		return false;
+	}
+
+	@Override
+	public boolean isZeroAllowed()
+	{
+		return true;
+	}
+
+	@Override
+	public boolean isPositiveAllowed()
+	{
+		return true;
+	}
+}
---
+++ code/src/utest/plugin/lsttokens/subclass/ProhibitspellTokenTest.java
@@ -0,0 +1,129 @@
+/*
+ * Copyright (c) 2009 Tom Parker <thpr@users.sourceforge.net>
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+package plugin.lsttokens.subclass;
+
+import org.junit.Test;
+
+import pcgen.core.SubClass;
+import pcgen.persistence.PersistenceLayerException;
+import pcgen.rules.persistence.CDOMLoader;
+import pcgen.rules.persistence.token.CDOMPrimaryToken;
+import plugin.lsttokens.testsupport.AbstractTokenTestCase;
+import plugin.lsttokens.testsupport.CDOMTokenLoader;
+
+public class ProhibitspellTokenTest extends AbstractTokenTestCase<SubClass>
+{
+
+	static ChoiceToken token = new ChoiceToken();
+	static CDOMTokenLoader<SubClass> loader = new CDOMTokenLoader<SubClass>(
+			SubClass.class);
+
+	@Override
+	public Class<SubClass> getCDOMClass()
+	{
+		return SubClass.class;
+	}
+
+	@Override
+	public CDOMLoader<SubClass> getLoader()
+	{
+		return loader;
+	}
+
+	@Override
+	public CDOMPrimaryToken<SubClass> getToken()
+	{
+		return token;
+	}
+
+	@Test
+	public void testInvalidInputEmpty() throws PersistenceLayerException
+	{
+		assertFalse(parse(""));
+		assertNoSideEffects();
+	}
+
+	@Test
+	public void testInvalidInputOnlyType() throws PersistenceLayerException
+	{
+		assertFalse(parse("SCHOOL"));
+		assertNoSideEffects();
+	}
+
+	@Test
+	public void testInvalidInputNoValue() throws PersistenceLayerException
+	{
+		assertFalse(parse("SCHOOL|"));
+		assertNoSideEffects();
+	}
+
+	@Test
+	public void testInvalidInputNoType() throws PersistenceLayerException
+	{
+		assertFalse(parse("|Good"));
+		assertNoSideEffects();
+	}
+
+	@Test
+	public void testInvalidInputLeadingPipe() throws PersistenceLayerException
+	{
+		assertFalse(parse("|SCHOOL|Good"));
+		assertNoSideEffects();
+	}
+
+	@Test
+	public void testInvalidInputTrailingPipe() throws PersistenceLayerException
+	{
+		assertFalse(parse("SCHOOL|Good|"));
+		assertNoSideEffects();
+	}
+
+	@Test
+	public void testInvalidInputDoubleDot() throws PersistenceLayerException
+	{
+		assertFalse(parse("SCHOOL||Good"));
+		assertNoSideEffects();
+	}
+
+	@Test
+	public void testInvalidInputNotAType() throws PersistenceLayerException
+	{
+		assertFalse(parse("NOTATYPE|Good"));
+		assertNoSideEffects();
+	}
+
+	@Test
+	public void testRoundRobinDescriptorSimple()
+			throws PersistenceLayerException
+	{
+		runRoundRobin("DESCRIPTOR|Fire");
+	}
+
+	@Test
+	public void testRoundRobinSchoolSimple() throws PersistenceLayerException
+	{
+		runRoundRobin("SCHOOL|Evocation");
+	}
+
+	@Test
+	public void testRoundRobinSubSchoolSimple()
+			throws PersistenceLayerException
+	{
+		runRoundRobin("SUBSCHOOL|Subsch");
+	}
+}
