ea5566f798d215c6233e967e933f2264e76b1ad8
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2009-02-02 22:40:14 +0000

    additional itests
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@9225 ef53c05b-3124-4d59-ae7d-fe3273f42be7

---
+++ code/src/itest/plugin/lsttokens/editcontext/subclass/ChoiceIntegrationTest.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2009 Tom Parker <thpr@users.sourceforge.net>
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+package plugin.lsttokens.editcontext.subclass;
+
+import java.net.URISyntaxException;
+
+import org.junit.Test;
+
+import pcgen.core.SubClass;
+import pcgen.persistence.PersistenceLayerException;
+import pcgen.rules.persistence.CDOMLoader;
+import pcgen.rules.persistence.token.CDOMPrimaryToken;
+import plugin.lsttokens.editcontext.testsupport.AbstractIntegrationTestCase;
+import plugin.lsttokens.editcontext.testsupport.TestContext;
+import plugin.lsttokens.subclass.ChoiceToken;
+import plugin.lsttokens.testsupport.CDOMTokenLoader;
+
+public class ChoiceIntegrationTest extends
+		AbstractIntegrationTestCase<SubClass>
+{
+
+	static ChoiceToken token = new ChoiceToken();
+	static CDOMTokenLoader<SubClass> loader = new CDOMTokenLoader<SubClass>(
+			SubClass.class);
+
+	@Override
+	public void setUp() throws PersistenceLayerException, URISyntaxException
+	{
+		super.setUp();
+		prefix = "CLASS:";
+	}
+
+	@Override
+	public Class<SubClass> getCDOMClass()
+	{
+		return SubClass.class;
+	}
+
+	@Override
+	public CDOMLoader<SubClass> getLoader()
+	{
+		return loader;
+	}
+
+	@Override
+	public CDOMPrimaryToken<SubClass> getToken()
+	{
+		return token;
+	}
+
+	@Test
+	public void testRoundRobinSimple() throws PersistenceLayerException
+	{
+		verifyCleanStart();
+		TestContext tc = new TestContext();
+		commit(testCampaign, tc, "SCHOOL|Illusion");
+		commit(modCampaign, tc, "DESCRIPTOR|Fear");
+		completeRoundRobin(tc);
+	}
+
+	@Test
+	public void testRoundRobinNoSet() throws PersistenceLayerException
+	{
+		verifyCleanStart();
+		TestContext tc = new TestContext();
+		emptyCommit(testCampaign, tc);
+		commit(modCampaign, tc, "DESCRIPTOR|Fear");
+		completeRoundRobin(tc);
+	}
+
+	@Test
+	public void testRoundRobinNoReset() throws PersistenceLayerException
+	{
+		verifyCleanStart();
+		TestContext tc = new TestContext();
+		commit(testCampaign, tc, "DESCRIPTOR|Fear");
+		emptyCommit(modCampaign, tc);
+		completeRoundRobin(tc);
+	}
+
+}
---
+++ code/src/itest/plugin/lsttokens/editcontext/subclass/CostIntegrationTest.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2009 Tom Parker <thpr@users.sourceforge.net>
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+package plugin.lsttokens.editcontext.subclass;
+
+import pcgen.core.SubClass;
+import pcgen.rules.persistence.CDOMLoader;
+import pcgen.rules.persistence.token.CDOMPrimaryToken;
+import plugin.lsttokens.editcontext.testsupport.AbstractIntegerIntegrationTestCase;
+import plugin.lsttokens.subclass.CostToken;
+import plugin.lsttokens.testsupport.CDOMTokenLoader;
+
+public class CostIntegrationTest extends
+		AbstractIntegerIntegrationTestCase<SubClass>
+{
+
+	static CostToken token = new CostToken();
+	static CDOMTokenLoader<SubClass> loader = new CDOMTokenLoader<SubClass>(
+			SubClass.class);
+
+	@Override
+	public Class<SubClass> getCDOMClass()
+	{
+		return SubClass.class;
+	}
+
+	@Override
+	public CDOMLoader<SubClass> getLoader()
+	{
+		return loader;
+	}
+
+	@Override
+	public CDOMPrimaryToken<SubClass> getToken()
+	{
+		return token;
+	}
+
+	@Override
+	public boolean isNegativeAllowed()
+	{
+		return false;
+	}
+
+	@Override
+	public boolean isZeroAllowed()
+	{
+		return true;
+	}
+
+	@Override
+	public boolean isPositiveAllowed()
+	{
+		return true;
+	}
+
+	@Override
+	public boolean doesOverwrite()
+	{
+		return true;
+	}
+}
