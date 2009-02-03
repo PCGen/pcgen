a6b95d67f1c6be23857f8544e37ce9e27e742f5c
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2009-02-03 01:01:31 +0000

    additional itests
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@9236 ef53c05b-3124-4d59-ae7d-fe3273f42be7

---
+++ code/src/itest/plugin/lsttokens/editcontext/ServesAsIntegrationTest.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright (c) 2009 Tom Parker <thpr@users.sourceforge.net>
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+package plugin.lsttokens.editcontext;
+
+import org.junit.Test;
+
+import pcgen.cdom.base.CDOMObject;
+import pcgen.core.Skill;
+import pcgen.rules.persistence.CDOMLoader;
+import pcgen.rules.persistence.token.CDOMPrimaryToken;
+import plugin.lsttokens.ServesAsToken;
+import plugin.lsttokens.editcontext.testsupport.AbstractListIntegrationTestCase;
+import plugin.lsttokens.testsupport.CDOMTokenLoader;
+
+public class ServesAsIntegrationTest extends
+		AbstractListIntegrationTestCase<CDOMObject, Skill>
+{
+
+	static ServesAsToken token = new ServesAsToken();
+	static CDOMTokenLoader<CDOMObject> loader = new CDOMTokenLoader<CDOMObject>(
+			CDOMObject.class);
+
+	@Override
+	public String getPrefix()
+	{
+		return "SKILL|";
+	}
+
+	@Override
+	public Class<Skill> getCDOMClass()
+	{
+		return Skill.class;
+	}
+
+	@Override
+	public CDOMLoader<CDOMObject> getLoader()
+	{
+		return loader;
+	}
+
+	@Override
+	public CDOMPrimaryToken<CDOMObject> getToken()
+	{
+		return token;
+	}
+
+	@Override
+	public Class<Skill> getTargetClass()
+	{
+		return Skill.class;
+	}
+
+	@Override
+	public boolean isTypeLegal()
+	{
+		return false;
+	}
+
+	@Override
+	public char getJoinCharacter()
+	{
+		return '|';
+	}
+
+	@Test
+	public void dummyTest()
+	{
+		// Just to get Eclipse to recognize this as a JUnit 4.0 Test Case
+	}
+
+	@Override
+	public boolean isClearDotLegal()
+	{
+		return false;
+	}
+
+	@Override
+	public boolean isClearLegal()
+	{
+		return false;
+	}
+
+	@Override
+	public boolean isPrereqLegal()
+	{
+		return false;
+	}
+
+	@Override
+	public boolean isAllLegal()
+	{
+		return false;
+	}
+}
---
+++ code/src/itest/plugin/lsttokens/editcontext/SourceWebIntegrationTest.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2009 Tom Parker <thpr@users.sourceforge.net>
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+package plugin.lsttokens.editcontext;
+
+import pcgen.cdom.base.CDOMObject;
+import pcgen.core.Deity;
+import pcgen.rules.persistence.CDOMLoader;
+import pcgen.rules.persistence.token.CDOMPrimaryToken;
+import plugin.lsttokens.SourcewebLst;
+import plugin.lsttokens.editcontext.testsupport.AbstractStringIntegrationTestCase;
+import plugin.lsttokens.testsupport.CDOMTokenLoader;
+
+public class SourceWebIntegrationTest extends
+		AbstractStringIntegrationTestCase<CDOMObject>
+{
+
+	static SourcewebLst token = new SourcewebLst();
+	static CDOMTokenLoader<CDOMObject> loader = new CDOMTokenLoader<CDOMObject>(
+			CDOMObject.class);
+
+	@Override
+	public Class<Deity> getCDOMClass()
+	{
+		return Deity.class;
+	}
+
+	@Override
+	public CDOMLoader<CDOMObject> getLoader()
+	{
+		return loader;
+	}
+
+	@Override
+	public CDOMPrimaryToken<CDOMObject> getToken()
+	{
+		return token;
+	}
+
+	@Override
+	public boolean isClearLegal()
+	{
+		return false;
+	}
+}
