e7140687602ff32017b4a249ae2e03506ed4f972
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2009-02-03 01:50:56 +0000

    additional itests
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@9243 ef53c05b-3124-4d59-ae7d-fe3273f42be7

---
+++ code/src/itest/plugin/lsttokens/editcontext/DrIntegrationTest.java
@@ -0,0 +1,145 @@
+/*
+ * Copyright (c) 2009 Tom Parker <thpr@users.sourceforge.net>
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+package plugin.lsttokens.editcontext;
+
+import org.junit.Test;
+
+import pcgen.cdom.base.CDOMObject;
+import pcgen.core.Ability;
+import pcgen.persistence.PersistenceLayerException;
+import pcgen.rules.persistence.CDOMLoader;
+import pcgen.rules.persistence.token.CDOMPrimaryToken;
+import plugin.lsttokens.DrLst;
+import plugin.lsttokens.editcontext.testsupport.AbstractIntegrationTestCase;
+import plugin.lsttokens.editcontext.testsupport.TestContext;
+import plugin.lsttokens.testsupport.CDOMTokenLoader;
+
+public class DrIntegrationTest extends AbstractIntegrationTestCase<CDOMObject>
+{
+	static DrLst token = new DrLst();
+	static CDOMTokenLoader<CDOMObject> loader = new CDOMTokenLoader<CDOMObject>(
+			CDOMObject.class);
+
+	@Override
+	public Class<Ability> getCDOMClass()
+	{
+		return Ability.class;
+	}
+
+	@Override
+	public CDOMLoader<CDOMObject> getLoader()
+	{
+		return loader;
+	}
+
+	@Override
+	public CDOMPrimaryToken<CDOMObject> getToken()
+	{
+		return token;
+	}
+
+	@Test
+	public void testRoundRobinSimple() throws PersistenceLayerException
+	{
+		verifyCleanStart();
+		TestContext tc = new TestContext();
+		commit(testCampaign, tc, "10/+1 and Silver");
+		commit(modCampaign, tc, "10/+1");
+		completeRoundRobin(tc);
+	}
+
+	@Test
+	public void testRoundRobinRemove() throws PersistenceLayerException
+	{
+		verifyCleanStart();
+		TestContext tc = new TestContext();
+		commit(testCampaign, tc, "10/+1");
+		commit(modCampaign, tc, "10/+1 and Silver");
+		completeRoundRobin(tc);
+	}
+
+	@Test
+	public void testRoundRobinNoSet() throws PersistenceLayerException
+	{
+		verifyCleanStart();
+		TestContext tc = new TestContext();
+		emptyCommit(testCampaign, tc);
+		commit(modCampaign, tc, "10/+1 and Silver");
+		completeRoundRobin(tc);
+	}
+
+	@Test
+	public void testRoundRobinNoReset() throws PersistenceLayerException
+	{
+		verifyCleanStart();
+		TestContext tc = new TestContext();
+		commit(testCampaign, tc, "10/+1");
+		emptyCommit(modCampaign, tc);
+		completeRoundRobin(tc);
+	}
+
+	@Test
+	public void testRoundRobinClearBase() throws PersistenceLayerException
+	{
+		verifyCleanStart();
+		TestContext tc = new TestContext();
+		commit(testCampaign, tc, ".CLEAR");
+		commit(modCampaign, tc, "10/+1");
+		completeRoundRobin(tc);
+	}
+
+	@Test
+	public void testRoundRobinClearMod() throws PersistenceLayerException
+	{
+		verifyCleanStart();
+		TestContext tc = new TestContext();
+		commit(testCampaign, tc, "10/+1 and Silver");
+		commit(modCampaign, tc, ".CLEAR");
+		completeRoundRobin(tc);
+	}
+
+	@Test
+	public void testRoundRobinClearBoth() throws PersistenceLayerException
+	{
+		verifyCleanStart();
+		TestContext tc = new TestContext();
+		commit(testCampaign, tc, ".CLEAR");
+		commit(modCampaign, tc, ".CLEAR");
+		completeRoundRobin(tc);
+	}
+
+	@Test
+	public void testRoundRobinNoSetClear() throws PersistenceLayerException
+	{
+		verifyCleanStart();
+		TestContext tc = new TestContext();
+		emptyCommit(testCampaign, tc);
+		commit(modCampaign, tc, ".CLEAR");
+		completeRoundRobin(tc);
+	}
+
+	@Test
+	public void testRoundRobinNoResetClear() throws PersistenceLayerException
+	{
+		verifyCleanStart();
+		TestContext tc = new TestContext();
+		commit(testCampaign, tc, ".CLEAR");
+		emptyCommit(modCampaign, tc);
+		completeRoundRobin(tc);
+	}
+}
---
+++ code/src/itest/plugin/lsttokens/editcontext/MoveCloneIntegrationTest.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) 2009 Tom Parker <thpr@users.sourceforge.net>
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+package plugin.lsttokens.editcontext;
+
+import org.junit.Test;
+
+import pcgen.cdom.base.CDOMObject;
+import pcgen.core.Ability;
+import pcgen.persistence.PersistenceLayerException;
+import pcgen.rules.persistence.CDOMLoader;
+import pcgen.rules.persistence.token.CDOMPrimaryToken;
+import plugin.lsttokens.MovecloneLst;
+import plugin.lsttokens.editcontext.testsupport.AbstractIntegrationTestCase;
+import plugin.lsttokens.editcontext.testsupport.TestContext;
+import plugin.lsttokens.testsupport.CDOMTokenLoader;
+
+public class MoveCloneIntegrationTest extends
+		AbstractIntegrationTestCase<CDOMObject>
+{
+	static MovecloneLst token = new MovecloneLst();
+	static CDOMTokenLoader<CDOMObject> loader = new CDOMTokenLoader<CDOMObject>(
+			CDOMObject.class);
+
+	@Override
+	public Class<Ability> getCDOMClass()
+	{
+		return Ability.class;
+	}
+
+	@Override
+	public CDOMLoader<CDOMObject> getLoader()
+	{
+		return loader;
+	}
+
+	@Override
+	public CDOMPrimaryToken<CDOMObject> getToken()
+	{
+		return token;
+	}
+
+	@Test
+	public void testRoundRobinSimple() throws PersistenceLayerException
+	{
+		verifyCleanStart();
+		TestContext tc = new TestContext();
+		commit(testCampaign, tc, "Normal,Fly,30");
+		commit(modCampaign, tc, "Swim,Fly,20");
+		completeRoundRobin(tc);
+	}
+
+	@Test
+	public void testRoundRobinRemove() throws PersistenceLayerException
+	{
+		verifyCleanStart();
+		TestContext tc = new TestContext();
+		commit(testCampaign, tc, "Swim,Fly,20");
+		commit(modCampaign, tc, "Swim,Fly,35");
+		completeRoundRobin(tc);
+	}
+
+	@Test
+	public void testRoundRobinNoSet() throws PersistenceLayerException
+	{
+		verifyCleanStart();
+		TestContext tc = new TestContext();
+		emptyCommit(testCampaign, tc);
+		commit(modCampaign, tc, "Swim,Fly,20");
+		completeRoundRobin(tc);
+	}
+
+	@Test
+	public void testRoundRobinNoReset() throws PersistenceLayerException
+	{
+		verifyCleanStart();
+		TestContext tc = new TestContext();
+		commit(testCampaign, tc, "Swim,Fly,20");
+		emptyCommit(modCampaign, tc);
+		completeRoundRobin(tc);
+	}
+
+}
