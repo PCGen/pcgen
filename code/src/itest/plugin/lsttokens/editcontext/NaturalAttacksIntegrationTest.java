af5090e4500b26dee430daad20e60197c34f1539
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2009-03-07 22:38:38 +0000

    NaturalAttacks Integration Test
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@9612 ef53c05b-3124-4d59-ae7d-fe3273f42be7

---
+++ code/src/itest/plugin/lsttokens/editcontext/NaturalAttacksIntegrationTest.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright (c) 2009 Tom Parker <thpr@users.sourceforge.net>
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+package plugin.lsttokens.editcontext;
+
+import java.net.URISyntaxException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import pcgen.cdom.base.CDOMObject;
+import pcgen.core.Ability;
+import pcgen.persistence.PersistenceLayerException;
+import pcgen.rules.persistence.CDOMLoader;
+import pcgen.rules.persistence.token.CDOMPrimaryToken;
+import plugin.bonustokens.Weapon;
+import plugin.lsttokens.NaturalattacksLst;
+import plugin.lsttokens.editcontext.testsupport.AbstractIntegrationTestCase;
+import plugin.lsttokens.editcontext.testsupport.TestContext;
+import plugin.lsttokens.testsupport.CDOMTokenLoader;
+
+public class NaturalAttacksIntegrationTest extends
+		AbstractIntegrationTestCase<CDOMObject>
+{
+	static NaturalattacksLst token = new NaturalattacksLst();
+	static CDOMTokenLoader<CDOMObject> loader = new CDOMTokenLoader<CDOMObject>(
+			CDOMObject.class);
+
+	@Override
+	@Before
+	public void setUp() throws PersistenceLayerException, URISyntaxException
+	{
+		super.setUp();
+		addBonus("WEAPON", Weapon.class);
+	}
+
+	@Override
+	public Class<Ability> getCDOMClass()
+	{
+		return Ability.class;
+	}
+
+	@Override
+	public CDOMLoader<CDOMObject> getLoader()
+	{
+		return loader;
+	}
+
+	@Override
+	public CDOMPrimaryToken<CDOMObject> getToken()
+	{
+		return token;
+	}
+
+	@Test
+	public void testRoundRobinSimple() throws PersistenceLayerException
+	{
+		verifyCleanStart();
+		TestContext tc = new TestContext();
+		commit(testCampaign, tc,
+				"Claw,Weapon.Natural.Melee.Piercing.Slashing,1,1d4");
+		commit(modCampaign, tc,
+				"Bite,Weapon.Natural.Melee.Piercing.Slashing,*2,1d4");
+		completeRoundRobin(tc);
+	}
+
+	@Test
+	public void testRoundRobinNoSet() throws PersistenceLayerException
+	{
+		verifyCleanStart();
+		TestContext tc = new TestContext();
+		emptyCommit(testCampaign, tc);
+		commit(modCampaign, tc,
+				"Claw,Weapon.Natural.Melee.Piercing.Slashing,1,1d4");
+		completeRoundRobin(tc);
+	}
+
+	@Test
+	public void testRoundRobinNoReset() throws PersistenceLayerException
+	{
+		verifyCleanStart();
+		TestContext tc = new TestContext();
+		commit(testCampaign, tc,
+				"Claw,Weapon.Natural.Melee.Piercing.Slashing,1,1d4");
+		emptyCommit(modCampaign, tc);
+		completeRoundRobin(tc);
+	}
+
+}
