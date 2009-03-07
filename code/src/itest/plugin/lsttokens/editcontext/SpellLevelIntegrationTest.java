621536b6562ff083180e332ef8b5dcaba1f5f1f5
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2009-03-07 22:46:46 +0000

    SPELLLEVEL and SPELLKNOWN integration test
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@9614 ef53c05b-3124-4d59-ae7d-fe3273f42be7

---
+++ code/src/itest/plugin/lsttokens/editcontext/SpellLevelIntegrationTest.java
@@ -0,0 +1,134 @@
+/*
+ * Copyright (c) 2009 Tom Parker <thpr@users.sourceforge.net>
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+package plugin.lsttokens.editcontext;
+
+import java.net.URISyntaxException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import pcgen.cdom.base.CDOMObject;
+import pcgen.cdom.list.ClassSpellList;
+import pcgen.cdom.list.DomainSpellList;
+import pcgen.core.Ability;
+import pcgen.core.spell.Spell;
+import pcgen.persistence.PersistenceLayerException;
+import pcgen.rules.persistence.CDOMLoader;
+import pcgen.rules.persistence.token.CDOMPrimaryToken;
+import plugin.lsttokens.SpelllevelLst;
+import plugin.lsttokens.editcontext.testsupport.AbstractIntegrationTestCase;
+import plugin.lsttokens.editcontext.testsupport.TestContext;
+import plugin.lsttokens.testsupport.CDOMTokenLoader;
+import plugin.lsttokens.testsupport.TokenRegistration;
+import plugin.pretokens.parser.PreClassParser;
+import plugin.pretokens.parser.PreRaceParser;
+import plugin.pretokens.writer.PreClassWriter;
+import plugin.pretokens.writer.PreRaceWriter;
+
+public class SpellLevelIntegrationTest extends
+		AbstractIntegrationTestCase<CDOMObject>
+{
+	static SpelllevelLst token = new SpelllevelLst();
+	static CDOMTokenLoader<CDOMObject> loader = new CDOMTokenLoader<CDOMObject>(
+			CDOMObject.class);
+
+	PreClassParser preclass = new PreClassParser();
+	PreClassWriter preclasswriter = new PreClassWriter();
+	PreRaceParser prerace = new PreRaceParser();
+	PreRaceWriter preracewriter = new PreRaceWriter();
+
+	@Override
+	@Before
+	public void setUp() throws PersistenceLayerException, URISyntaxException
+	{
+		super.setUp();
+		TokenRegistration.register(preclass);
+		TokenRegistration.register(prerace);
+		TokenRegistration.register(preclasswriter);
+		TokenRegistration.register(preracewriter);
+	}
+
+	@Override
+	public Class<Ability> getCDOMClass()
+	{
+		return Ability.class;
+	}
+
+	@Override
+	public CDOMLoader<CDOMObject> getLoader()
+	{
+		return loader;
+	}
+
+	@Override
+	public CDOMPrimaryToken<CDOMObject> getToken()
+	{
+		return token;
+	}
+
+	@Test
+	public void testRoundRobinSimple() throws PersistenceLayerException
+	{
+		primaryContext.ref.constructCDOMObject(Spell.class, "Fireball");
+		secondaryContext.ref.constructCDOMObject(Spell.class, "Fireball");
+		primaryContext.ref.constructCDOMObject(ClassSpellList.class, "Cleric");
+		secondaryContext.ref
+				.constructCDOMObject(ClassSpellList.class, "Cleric");
+		primaryContext.ref.constructCDOMObject(Spell.class, "Bless");
+		secondaryContext.ref.constructCDOMObject(Spell.class, "Bless");
+		primaryContext.ref.constructCDOMObject(ClassSpellList.class, "Wizard");
+		secondaryContext.ref
+				.constructCDOMObject(ClassSpellList.class, "Wizard");
+		verifyCleanStart();
+		TestContext tc = new TestContext();
+		commit(testCampaign, tc, "CLASS|Wizard=3|Bless");
+		commit(modCampaign, tc, "CLASS|Cleric=2|Fireball|PRECLASS:1,Fighter=2");
+		completeRoundRobin(tc);
+	}
+
+	@Test
+	public void testRoundRobinNoSet() throws PersistenceLayerException
+	{
+		primaryContext.ref.constructCDOMObject(Spell.class, "Fireball");
+		secondaryContext.ref.constructCDOMObject(Spell.class, "Fireball");
+		verifyCleanStart();
+		TestContext tc = new TestContext();
+		emptyCommit(testCampaign, tc);
+		commit(modCampaign, tc,
+				"CLASS|SPELLCASTER.Arcane=2|Fireball|PRECLASS:1,Fighter=2");
+		completeRoundRobin(tc);
+	}
+
+	@Test
+	public void testRoundRobinNoReset() throws PersistenceLayerException
+	{
+		primaryContext.ref.constructCDOMObject(Spell.class, "Fireball");
+		secondaryContext.ref.constructCDOMObject(Spell.class, "Fireball");
+		primaryContext.ref.constructCDOMObject(Spell.class, "Lightning Bolt");
+		secondaryContext.ref.constructCDOMObject(Spell.class, "Lightning Bolt");
+		primaryContext.ref.constructCDOMObject(DomainSpellList.class, "Fire");
+		secondaryContext.ref.constructCDOMObject(DomainSpellList.class, "Fire");
+		verifyCleanStart();
+		TestContext tc = new TestContext();
+		commit(testCampaign, tc,
+				"DOMAIN|Fire=2|Fireball,Lightning Bolt|PRECLASS:1,Fighter=2");
+		emptyCommit(modCampaign, tc);
+		completeRoundRobin(tc);
+	}
+
+}
