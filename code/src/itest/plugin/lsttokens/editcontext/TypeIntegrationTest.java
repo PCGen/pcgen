44d31893641099503f3adc4057f840de2bc55e23
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2009-03-07 19:35:42 +0000

    TYPE integration test
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@9601 ef53c05b-3124-4d59-ae7d-fe3273f42be7

---
+++ code/src/itest/plugin/lsttokens/editcontext/TypeIntegrationTest.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2009 Tom Parker <thpr@users.sourceforge.net>
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+package plugin.lsttokens.editcontext;
+
+import org.junit.Test;
+
+import pcgen.cdom.base.CDOMObject;
+import pcgen.core.Domain;
+import pcgen.rules.persistence.CDOMLoader;
+import pcgen.rules.persistence.token.CDOMPrimaryToken;
+import plugin.lsttokens.TypeLst;
+import plugin.lsttokens.editcontext.testsupport.AbstractTypeSafeListIntegrationTestCase;
+import plugin.lsttokens.testsupport.CDOMTokenLoader;
+
+public class TypeIntegrationTest extends
+		AbstractTypeSafeListIntegrationTestCase<CDOMObject>
+{
+
+	static TypeLst token = new TypeLst();
+	static CDOMTokenLoader<CDOMObject> loader = new CDOMTokenLoader<CDOMObject>(
+			CDOMObject.class);
+
+	@Override
+	public Class<Domain> getCDOMClass()
+	{
+		return Domain.class;
+	}
+
+	@Override
+	public CDOMLoader<CDOMObject> getLoader()
+	{
+		return loader;
+	}
+
+	@Override
+	public CDOMPrimaryToken<CDOMObject> getToken()
+	{
+		return token;
+	}
+
+	@Override
+	public char getJoinCharacter()
+	{
+		return '.';
+	}
+
+	@Test
+	public void dummyTest()
+	{
+		// Just to get Eclipse to recognize this as a JUnit 4.0 Test Case
+	}
+
+	@Override
+	public boolean isClearDotLegal()
+	{
+		return false;
+	}
+
+	@Override
+	public boolean isClearLegal()
+	{
+		return true;
+	}
+
+	@Override
+	public Object getConstant(String string)
+	{
+		return string;
+	}
+
+	@Override
+	protected boolean requiresPreconstruction()
+	{
+		return false;
+	}
+
+}
