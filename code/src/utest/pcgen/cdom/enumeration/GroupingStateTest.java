5eafabafca1482d40be9239d8b10c7867d61b4d8
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2009-04-04 16:49:58 +0000

    Tests for GroupingState
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@9830 ef53c05b-3124-4d59-ae7d-fe3273f42be7

---
+++ code/src/utest/pcgen/cdom/enumeration/GroupingStateTest.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2009 Tom Parker <thpr@users.sourceforge.net>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+package pcgen.cdom.enumeration;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+public class GroupingStateTest extends TestCase
+{
+
+	@Test
+	public void testAddConsistent()
+	{
+		for (GroupingState gs1 : GroupingState.values())
+		{
+			for (GroupingState gs2 : GroupingState.values())
+			{
+				assertEquals(gs1.add(gs2), gs2.add(gs1));
+			}
+		}
+	}
+
+	@Test
+	public void testInvalidImmutable()
+	{
+		GroupingState invalid = GroupingState.INVALID;
+		for (GroupingState gs2 : GroupingState.values())
+		{
+			assertEquals(invalid.add(gs2), invalid);
+			assertEquals(gs2.add(invalid), invalid);
+		}
+	}
+
+	@Test
+	public void testEmptyWeak()
+	{
+		GroupingState empty = GroupingState.EMPTY;
+		for (GroupingState gs2 : GroupingState.values())
+		{
+			assertEquals(empty.add(gs2), gs2);
+			assertEquals(gs2.add(empty), gs2);
+		}
+	}
+}
