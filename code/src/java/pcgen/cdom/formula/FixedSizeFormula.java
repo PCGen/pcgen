ecfc28b96851b06e181f7deeb8e6c4461419326f
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2009-07-19 19:51:47 +0000

    minor change for consistency
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@10176 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- code/src/java/pcgen/cdom/formula/FixedSizeFormula.java
+++ code/src/java/pcgen/cdom/formula/FixedSizeFormula.java
@@ -18,7 +18,6 @@
 package pcgen.cdom.formula;
 
 import pcgen.base.formula.Formula;
-import pcgen.cdom.enumeration.StringKey;
 import pcgen.core.Equipment;
 import pcgen.core.PlayerCharacter;
 import pcgen.core.SizeAdjustment;
@@ -66,7 +65,7 @@ public class FixedSizeFormula implements Formula
 	@Override
 	public String toString()
 	{
-		return size.get(StringKey.ABB);
+		return size.getAbbreviation();
 	}
 
 	/**
