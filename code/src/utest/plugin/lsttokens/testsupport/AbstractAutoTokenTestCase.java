78e5f91b17bc1ccea7882deaf88a197db23a8cb9
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2009-08-26 01:43:51 +0000

    Additional tests (unparse) for AUTO: tokens
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@10303 ef53c05b-3124-4d59-ae7d-fe3273f42be7

---
+++ code/src/utest/plugin/lsttokens/testsupport/AbstractAutoTokenTestCase.java
@@ -0,0 +1,322 @@
+/*
+ * Copyright (c) 2007 Tom Parker <thpr@users.sourceforge.net>
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+package plugin.lsttokens.testsupport;
+
+import java.net.URISyntaxException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import pcgen.cdom.base.CDOMObject;
+import pcgen.cdom.base.ChooseResultActor;
+import pcgen.cdom.enumeration.ListKey;
+import pcgen.cdom.reference.CDOMDirectSingleRef;
+import pcgen.cdom.reference.CDOMSingleRef;
+import pcgen.core.PCTemplate;
+import pcgen.persistence.PersistenceLayerException;
+import pcgen.rules.persistence.CDOMLoader;
+import pcgen.rules.persistence.token.CDOMPrimaryToken;
+import plugin.lsttokens.AutoLst;
+import plugin.pretokens.parser.PreRaceParser;
+import plugin.pretokens.writer.PreRaceWriter;
+
+public abstract class AbstractAutoTokenTestCase<TC extends CDOMObject> extends
+		AbstractAddTokenTestCase<CDOMObject, TC>
+{
+	static AutoLst token = new AutoLst();
+	static CDOMTokenLoader<CDOMObject> loader = new CDOMTokenLoader<CDOMObject>(
+			CDOMObject.class);
+	PreRaceParser prerace = new PreRaceParser();
+	PreRaceWriter preracewriter = new PreRaceWriter();
+
+	@Override
+	@Before
+	public void setUp() throws PersistenceLayerException, URISyntaxException
+	{
+		super.setUp();
+		TokenRegistration.register(prerace);
+		TokenRegistration.register(preracewriter);
+	}
+
+	@Override
+	public char getJoinCharacter()
+	{
+		return '|';
+	}
+
+	@Override
+	public Class<PCTemplate> getCDOMClass()
+	{
+		return PCTemplate.class;
+	}
+
+	@Override
+	public CDOMLoader<CDOMObject> getLoader()
+	{
+		return loader;
+	}
+
+	@Override
+	public CDOMPrimaryToken<CDOMObject> getToken()
+	{
+		return token;
+	}
+
+	@Override
+	public boolean isTypeLegal()
+	{
+		return true;
+	}
+
+	@Override
+	public boolean allowsParenAsSub()
+	{
+		return false;
+	}
+
+	@Override
+	public boolean allowsFormula()
+	{
+		return false;
+	}
+
+	protected abstract void loadAllReference();
+
+	protected abstract ChooseResultActor getActor();
+
+	protected abstract void loadProf(CDOMSingleRef<TC> ref);
+
+	protected abstract void loadTypeProf(String... types);
+
+	@Test
+	public void testInvalidEmptyPre() throws PersistenceLayerException
+	{
+		construct(primaryContext, "TestWP1");
+		assertFalse(parse(getSubTokenName() + '|' + "TestWP1[]"));
+		assertNoSideEffects();
+	}
+
+	@Test
+	public void testInvalidEmptyPre2() throws PersistenceLayerException
+	{
+		construct(primaryContext, "TestWP1");
+		assertFalse(parse(getSubTokenName() + '|' + "TestWP1["));
+		assertNoSideEffects();
+	}
+
+	@Test
+	public void testInvalidEmptyPre3() throws PersistenceLayerException
+	{
+		construct(primaryContext, "TestWP1");
+		boolean parse = parse(getSubTokenName() + '|' + "TestWP1]");
+		if (parse)
+		{
+			assertFalse(primaryContext.ref.validate(null));
+		}
+		else
+		{
+			assertNoSideEffects();
+		}
+	}
+
+	@Test
+	public void testInvalidMismatchedBracket() throws PersistenceLayerException
+	{
+		construct(primaryContext, "TestWP1");
+		assertFalse(parse(getSubTokenName() + '|' + "TestWP1[PRERACE:Dwarf"));
+		assertNoSideEffects();
+	}
+
+	@Test
+	public void testInvalidTrailingAfterBracket()
+			throws PersistenceLayerException
+	{
+		construct(primaryContext, "TestWP1");
+		assertFalse(parse(getSubTokenName() + '|' + "TestWP1[PRERACE:Dwarf]Hi"));
+		assertNoSideEffects();
+	}
+
+	@Test
+	public void testRoundRobinOnePre() throws PersistenceLayerException
+	{
+		construct(primaryContext, "TestWP1");
+		construct(primaryContext, "TestWP2");
+		construct(secondaryContext, "TestWP1");
+		construct(secondaryContext, "TestWP2");
+		runRoundRobin(getSubTokenName() + '|' + "TestWP1[PRERACE:1,Dwarf]");
+	}
+
+	@Test
+	public void testRoundRobinDupeTwoPrereqs() throws PersistenceLayerException
+	{
+		construct(primaryContext, "TestWP1");
+		construct(primaryContext, "TestWP2");
+		construct(secondaryContext, "TestWP1");
+		construct(secondaryContext, "TestWP2");
+		runRoundRobin(getSubTokenName() + '|' + "TestWP1[PRERACE:1,Dwarf]",
+				getSubTokenName() + '|' + "TestWP1[PRERACE:1,Human]");
+	}
+
+	@Test
+	public void testRoundRobinAllPlusWithPrereqLegal()
+			throws PersistenceLayerException
+	{
+		if (isAllLegal())
+		{
+			construct(primaryContext, "TestWP1");
+			construct(secondaryContext, "TestWP1");
+			runRoundRobin(getSubTokenName() + '|' + "ALL[PRERACE:1,Dwarf]",
+					getSubTokenName() + '|' + "TestWP1[PRERACE:1,Human]");
+		}
+	}
+
+	@Test
+	public void testRoundRobinAllIndivPrereq() throws PersistenceLayerException
+	{
+		construct(primaryContext, "TestWP1");
+		construct(secondaryContext, "TestWP1");
+		assertFalse(parse(getSubTokenName() + '|'
+				+ "TestWP1|ALL[PRERACE:Dwarf]"));
+		assertNoSideEffects();
+	}
+
+	@Test
+	public void testRoundRobinAllDoublePrereq()
+			throws PersistenceLayerException
+	{
+		if (isAllLegal())
+		{
+			runRoundRobin(getSubTokenName() + '|' + "ALL[PRERACE:1,Dwarf]",
+					getSubTokenName() + '|' + "ALL[PRERACE:1,Human]");
+		}
+	}
+
+	@Test
+	public void testInvalidAllPlusAllPrereqIllegal()
+			throws PersistenceLayerException
+	{
+		assertFalse(parse(getSubTokenName() + '|' + "ALL[PRERACE:Dwarf]|ALL"));
+		assertNoSideEffects();
+	}
+
+	@Test
+	public void testInvalidAllPlusListIllegal()
+			throws PersistenceLayerException
+	{
+		if (isAllLegal())
+		{
+			assertFalse(parse(getSubTokenName() + '|' + "%LIST|ALL"));
+			assertNoSideEffects();
+		}
+	}
+
+	@Test
+	public void testInvalidInputBadPrerequisite()
+			throws PersistenceLayerException
+	{
+		construct(primaryContext, "TestWP1");
+		construct(secondaryContext, "TestWP1");
+		assertFalse(parse(getSubTokenName() + '|' + "TestWP1[PREFOO:1,Human]"));
+		assertNoSideEffects();
+	}
+
+	@Test
+	public void testUnparseSingle() throws PersistenceLayerException
+	{
+		TC wp1 = construct(primaryContext, "TestWP1");
+		CDOMSingleRef<TC> ref = CDOMDirectSingleRef.getRef(wp1);
+		loadProf(ref);
+		String[] unparsed = getToken().unparse(primaryContext, primaryProf);
+		expectSingle(unparsed, getSubTokenName() + '|' + "TestWP1");
+	}
+
+	@Test
+	public void testUnparseType() throws PersistenceLayerException
+	{
+		loadTypeProf("Foo", "Bar");
+		String[] unparsed = getToken().unparse(primaryContext, primaryProf);
+		expectSingle(unparsed, getSubTokenName() + '|' + getTypePrefix()
+				+ "TYPE=Bar.Foo");
+	}
+
+	@Test
+	public void testUnparseSingleAll() throws PersistenceLayerException
+	{
+		if (isAllLegal())
+		{
+			TC wp1 = construct(primaryContext, "TestWP1");
+			CDOMSingleRef<TC> ref = CDOMDirectSingleRef.getRef(wp1);
+			loadProf(ref);
+			loadAllReference();
+			assertBadUnparse();
+		}
+	}
+
+	@Test
+	public void testUnparseAll() throws PersistenceLayerException
+	{
+		if (isAllLegal())
+		{
+			loadAllReference();
+			String[] unparsed = getToken().unparse(primaryContext, primaryProf);
+			expectSingle(unparsed, getSubTokenName() + '|' + "ALL");
+		}
+	}
+
+	@Test
+	public void testRoundRobinList() throws PersistenceLayerException
+	{
+		runRoundRobin(getSubTokenName() + '|' + "%LIST");
+	}
+
+	@Test
+	public void testRoundRobinListPre() throws PersistenceLayerException
+	{
+		runRoundRobin(getSubTokenName() + '|' + "%LIST[PRERACE:1,Dwarf]");
+	}
+
+	@Test
+	public void testUnparseListAll() throws PersistenceLayerException
+	{
+		if (isAllLegal())
+		{
+			primaryProf.addToListFor(ListKey.CHOOSE_ACTOR, getActor());
+			loadAllReference();
+			assertBadUnparse();
+		}
+	}
+
+	@Test
+	public void testUnparseTypeAll() throws PersistenceLayerException
+	{
+		if (isAllLegal())
+		{
+			loadTypeProf("Foo", "Bar");
+			loadAllReference();
+			assertBadUnparse();
+		}
+	}
+
+	@Test
+	public void testUnparseList() throws PersistenceLayerException
+	{
+		primaryProf.addToListFor(ListKey.CHOOSE_ACTOR, getActor());
+		String[] unparsed = getToken().unparse(primaryContext, primaryProf);
+		expectSingle(unparsed, getSubTokenName() + '|' + "%LIST");
+	}
+}
