40709ba66e07f6926fea5b45ba4a627501649aa0
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2009-09-19 18:00:44 +0000

    XPFacet unit test
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@10563 ef53c05b-3124-4d59-ae7d-fe3273f42be7

---
+++ code/src/utest/pcgen/cdom/facet/XPFacetTest.java
@@ -0,0 +1,191 @@
+/*
+ * Copyright (c) 2009 Tom Parker <thpr@users.sourceforge.net>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+package pcgen.cdom.facet;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+import pcgen.cdom.enumeration.CharID;
+
+public class XPFacetTest extends TestCase
+{
+	private CharID id = new CharID();
+	private CharID altid = new CharID();
+	private XPFacet facet;
+	private int adjustment;
+	private Map<Integer, Integer> minXP;
+
+	@Override
+	protected void setUp() throws Exception
+	{
+		super.setUp();
+		facet = getMockFacet();
+		minXP = new HashMap<Integer, Integer>();
+		minXP.put(1, 0);
+		minXP.put(2, 1000);
+	}
+
+	@Test
+	public void testEarnedXPUnsetZero()
+	{
+		assertEquals(0, facet.getEarnedXP(id));
+	}
+
+	@Test
+	public void testEarnedXPSetGet()
+	{
+		facet.setEarnedXP(id, 250);
+		assertEquals(250, facet.getEarnedXP(id));
+		facet.setEarnedXP(id, 2500);
+		assertEquals(2500, facet.getEarnedXP(id));
+	}
+
+	@Test
+	public void testEarnedXPSetZeroValid()
+	{
+		facet.setEarnedXP(id, 0);
+		assertEquals(0, facet.getEarnedXP(id));
+	}
+
+	@Test
+	public void testEarnedXPSetNegative()
+	{
+		facet.setEarnedXP(id, -250);
+		/*
+		 * TODO Some form of error here?
+		 */
+	}
+
+	@Test
+	public void testEarnedXPDiffPC()
+	{
+		facet.setEarnedXP(id, 2500);
+		assertEquals(0, facet.getEarnedXP(altid));
+	}
+
+	@Test
+	public void testGetTotalXPUnsetZero()
+	{
+		assertEquals(0, facet.getEarnedXP(id));
+		assertEquals(0, facet.getXP(id));
+	}
+
+	@Test
+	public void testGetTotalXP()
+	{
+		facet.setEarnedXP(id, 2500);
+		assertEquals(2500, facet.getXP(id));
+	}
+
+	@Test
+	public void testSetXP()
+	{
+		facet.setXP(id, 2500);
+		assertEquals(2500, facet.getEarnedXP(id));
+		assertEquals(2500, facet.getXP(id));
+	}
+
+	@Test
+	public void testSetXPZeroValid()
+	{
+		facet.setXP(id, 0);
+		assertEquals(0, facet.getEarnedXP(id));
+		assertEquals(0, facet.getXP(id));
+	}
+
+	@Test
+	public void testSetXPNegativeInvalid()
+	{
+		facet.setXP(id, -100);
+		/*
+		 * TODO there is some form of error here?
+		 */
+	}
+
+	@Test
+	public void testSetXPWithLA()
+	{
+		adjustment = 3; // Means equivalent of level 4, since PC's start at
+						// Level 1
+		minXP.put(4, 2000);
+		facet.setXP(id, 2500);
+		assertEquals(500, facet.getEarnedXP(id));
+		assertEquals(2500, facet.getXP(id));
+	}
+
+	@Test
+	public void testSetXPNexativeEarnedXPWithLA()
+	{
+		adjustment = 2;
+		minXP.put(3, 2000);
+		facet.setXP(id, 1500);
+		assertEquals(0, facet.getEarnedXP(id));
+		assertEquals(2000, facet.getXP(id));
+		/*
+		 * TODO Need to detect this error!
+		 */
+	}
+
+	@Test
+	public void testSetXPZeroEarnedXPWithLA()
+	{
+		adjustment = 2;
+		minXP.put(3, 2000);
+		facet.setXP(id, 2000);
+		assertEquals(0, facet.getEarnedXP(id));
+		assertEquals(2000, facet.getXP(id));
+	}
+
+	public XPFacet getMockFacet() throws SecurityException,
+		NoSuchFieldException, IllegalArgumentException, IllegalAccessException
+	{
+		XPFacet f = new XPFacet();
+		Field field = XPFacet.class.getDeclaredField("levelFacet");
+		field.setAccessible(true);
+		LevelFacet fakeFacet = new LevelFacet()
+		{
+
+			@Override
+			public int getLevelAdjustment(CharID id)
+			{
+				return adjustment;
+			}
+
+		};
+		field.set(f, fakeFacet);
+		field = XPFacet.class.getDeclaredField("levelTableFacet");
+		field.setAccessible(true);
+		LevelTableFacet fakeTableFacet = new LevelTableFacet()
+		{
+
+			@Override
+			public int minXPForLevel(int level, CharID id)
+			{
+				return minXP.get(level);
+			}
+
+		};
+		field.set(f, fakeTableFacet);
+		return f;
+	}
+}
