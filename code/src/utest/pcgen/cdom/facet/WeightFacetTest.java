7175586d8c4698a085d55f7225505b8441f7e1ad
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2009-09-19 04:19:15 +0000

    Unit Test for WeightFacet
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@10560 ef53c05b-3124-4d59-ae7d-fe3273f42be7

---
+++ code/src/utest/pcgen/cdom/facet/WeightFacetTest.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2009 Tom Parker <thpr@users.sourceforge.net>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+package pcgen.cdom.facet;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+import pcgen.cdom.enumeration.CharID;
+
+public class WeightFacetTest extends TestCase
+{
+	private CharID id = new CharID();
+	private CharID altid = new CharID();
+	private WeightFacet facet = new WeightFacet();
+
+	@Test
+	public void testWeightUnsetZero()
+	{
+		assertEquals(0, facet.getWeight(id));
+	}
+
+	@Test
+	public void testWeightSetGet()
+	{
+		facet.setWeight(id, 250);
+		assertEquals(250, facet.getWeight(id));
+		facet.setWeight(id, 225);
+		assertEquals(225, facet.getWeight(id));
+	}
+
+	@Test
+	public void testWeightSetZeroValid()
+	{
+		facet.setWeight(id, 0);
+		assertEquals(0, facet.getWeight(id));
+	}
+
+	@Test
+	public void testWeightSetNegative()
+	{
+		facet.setWeight(id, -250);
+		/*
+		 * TODO Some form of error here?
+		 */
+	}
+
+	@Test
+	public void testWeightDiffPC()
+	{
+		facet.setWeight(id, 250);
+		assertEquals(0, facet.getWeight(altid));
+	}
+
+	@Test
+	public void testRemoveWeight()
+	{
+		facet.setWeight(id, 25);
+		assertEquals(25, facet.getWeight(id));
+		facet.removeWeight(id);
+		assertEquals(0, facet.getWeight(id));
+	}
+
+	@Test
+	public void testRemoveAltWeight()
+	{
+		facet.setWeight(id, 25);
+		assertEquals(25, facet.getWeight(id));
+		facet.removeWeight(altid);
+		assertEquals(25, facet.getWeight(id));
+	}
+
+}
