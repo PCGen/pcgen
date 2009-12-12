e27d7e9784d2471cc9a709c8f4d365f4158c0aa5
jdempsey <jdempsey@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2009-12-12 23:09:06 +0000

    Fix bug: HTM Exporter Showing htm tags instead of converting Issue#: 2913021
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@11185 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- code/src/java/pcgen/io/exporttoken/EqToken.java
+++ code/src/java/pcgen/io/exporttoken/EqToken.java
@@ -45,6 +45,7 @@ import pcgen.core.PlayerCharacter;
 import pcgen.core.SettingsHandler;
 import pcgen.core.analysis.OutputNameFormatting;
 import pcgen.io.ExportHandler;
+import pcgen.io.FileAccess;
 import pcgen.util.BigDecimalHelper;
 
 /**
@@ -238,7 +239,7 @@ public class EqToken extends Token
 		if ((temp >= 0) && (temp < eqList.size()))
 		{
 			Equipment eq = eqList.get(temp);
-			retString = getEqToken(pc, eq, tempString, aTok);
+			retString = FileAccess.filterString(getEqToken(pc, eq, tempString, aTok));
 
 			// Starting EQ.%.NAME.MAGIC,befTrue,aftTrue,befFalse,aftFalse treatment
 			if (!"".equals(bFilter))
@@ -295,7 +296,7 @@ public class EqToken extends Token
 	@Override
 	public boolean isEncoded()
 	{
-		return true;
+		return false;
 	}
 
 	/**
