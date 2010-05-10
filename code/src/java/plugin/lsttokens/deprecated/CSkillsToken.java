73d90f9254657a9690eefaac4ec37fe7b6ac787a
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2010-05-10 01:50:31 +0000

    Deprecate CHOOSE:CSKILLS in favor of CHOOSE:SKILL
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@11791 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- code/src/java/plugin/lsttokens/deprecated/CSkillsToken.java
+++ code/src/java/plugin/lsttokens/deprecated/CSkillsToken.java
@@ -15,16 +15,17 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
-package plugin.lsttokens.choose;
+package plugin.lsttokens.deprecated;
 
 import pcgen.cdom.base.CDOMObject;
-import pcgen.cdom.enumeration.StringKey;
 import pcgen.rules.context.LoadContext;
 import pcgen.rules.persistence.token.CDOMSecondaryToken;
 import pcgen.rules.persistence.token.ErrorParsingWrapper;
 import pcgen.rules.persistence.token.ParseResult;
+import pcgen.util.Logging;
 
-public class CSkillsToken extends ErrorParsingWrapper<CDOMObject> implements CDOMSecondaryToken<CDOMObject>
+public class CSkillsToken extends ErrorParsingWrapper<CDOMObject> implements
+		CDOMSecondaryToken<CDOMObject>
 {
 
 	public String getTokenName()
@@ -38,40 +39,16 @@ public class CSkillsToken extends ErrorParsingWrapper<CDOMObject> implements CDO
 	}
 
 	public ParseResult parseToken(LoadContext context, CDOMObject obj,
-		String value)
+			String value)
 	{
-		if (value == null)
-		{
-			// No args - legal
-			context.obj.put(obj, StringKey.CHOICE_STRING, getTokenName());
-			return ParseResult.SUCCESS;
-		}
-		return new ParseResult.Fail("CHOOSE:" + getTokenName()
-				+ " will ignore arguments: " + value);
+		Logging.deprecationPrint("CHOOSE:CSKILLS has been deprecated,"
+				+ "please use CHOOSE:SKILL|CLASS");
+		return context.processSubToken(obj, "CHOOSE", "SKILL", "CLASS");
 	}
 
 	public String[] unparse(LoadContext context, CDOMObject cdo)
 	{
-		String chooseString = context.getObjectContext().getString(cdo,
-				StringKey.CHOICE_STRING);
-		if (chooseString == null)
-		{
-			return null;
-		}
-		String returnString;
-		if (getTokenName().equals(chooseString))
-		{
-			returnString = "";
-		}
-		else
-		{
-			if (chooseString.indexOf(getTokenName() + '|') != 0)
-			{
-				return null;
-			}
-			returnString = chooseString.substring(getTokenName().length() + 1);
-		}
-		return new String[] { returnString };
+		return null;
 	}
 
 	public Class<CDOMObject> getTokenClass()
