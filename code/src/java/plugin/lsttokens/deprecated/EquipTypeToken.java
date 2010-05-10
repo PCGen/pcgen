c5c8a84668ff5a08f9f3812c6bcdfa3ded25e6ac
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2010-05-10 01:27:14 +0000

    Deprecate CHOOSE:EQUIPTYPE in favor of CHOOSE:EQUIPMENT|TYPE=x
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@11788 ef53c05b-3124-4d59-ae7d-fe3273f42be7

---
+++ code/src/java/plugin/lsttokens/deprecated/EquipTypeToken.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2007 (C) Thomas Parker <thpr@users.sourceforge.net>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+package plugin.lsttokens.deprecated;
+
+import pcgen.cdom.base.CDOMObject;
+import pcgen.rules.context.LoadContext;
+import pcgen.rules.persistence.token.CDOMSecondaryToken;
+import pcgen.rules.persistence.token.ErrorParsingWrapper;
+import pcgen.rules.persistence.token.ParseResult;
+import pcgen.util.Logging;
+
+public class EquipTypeToken extends ErrorParsingWrapper<CDOMObject> implements
+		CDOMSecondaryToken<CDOMObject>
+{
+
+	public String getTokenName()
+	{
+		return "EQUIPTYPE";
+	}
+
+	public String getParentToken()
+	{
+		return "CHOOSE";
+	}
+
+	public ParseResult parseToken(LoadContext context, CDOMObject obj,
+			String value)
+	{
+		Logging.deprecationPrint("CHOOSE:EQUIPTYPE has been deprecated,"
+				+ "please use CHOOSE:EQUIPMENT|TYPE=x");
+		return context.processSubToken(obj, getTokenName(), "EQUIPMENT",
+				"TYPE=" + value);
+	}
+
+	public String[] unparse(LoadContext context, CDOMObject cdo)
+	{
+		return null;
+	}
+
+	public Class<CDOMObject> getTokenClass()
+	{
+		return CDOMObject.class;
+	}
+}
