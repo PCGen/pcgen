547d3f5ccccf4d6d390a838fe0b6d0e865078613
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2010-05-10 01:54:01 +0000

    Deprecate CHOOSE:SKILLS in favor of CHOOSE:SKILL|PC
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@11792 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- code/src/java/pcgen/core/chooser/ChooserUtilities.java
+++ code/src/java/pcgen/core/chooser/ChooserUtilities.java
@@ -446,7 +446,6 @@ public class ChooserUtilities
 		classLookup.put("MISC",                 MiscChoiceManager.class.getName());
 		classLookup.put("STRING",               MiscChoiceManager.class.getName());
 		classLookup.put("PROFICIENCY",          ProficiencyChoiceManager.class.getName());
-		classLookup.put("SKILLS",               SkillsChoiceManager.class.getName());
 		classLookup.put("SPELLLEVEL",           SpellLevelChoiceManager.class.getName());
 		classLookup.put("SPELLLIST",            SpellListChoiceManager.class.getName());
 		classLookup.put("SPELLS",               SpellsChoiceManager.class.getName());
--- code/src/java/plugin/lsttokens/deprecated/SkillsToken.java
+++ code/src/java/plugin/lsttokens/deprecated/SkillsToken.java
@@ -15,17 +15,17 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
-package plugin.lsttokens.choose;
+package plugin.lsttokens.deprecated;
 
 import pcgen.cdom.base.CDOMObject;
-import pcgen.cdom.enumeration.StringKey;
 import pcgen.rules.context.LoadContext;
 import pcgen.rules.persistence.token.CDOMSecondaryToken;
-import pcgen.rules.persistence.token.ComplexParseResult;
 import pcgen.rules.persistence.token.ErrorParsingWrapper;
 import pcgen.rules.persistence.token.ParseResult;
+import pcgen.util.Logging;
 
-public class SkillsToken extends ErrorParsingWrapper<CDOMObject> implements CDOMSecondaryToken<CDOMObject>
+public class SkillsToken extends ErrorParsingWrapper<CDOMObject> implements
+		CDOMSecondaryToken<CDOMObject>
 {
 
 	public String getTokenName()
@@ -39,45 +39,16 @@ public class SkillsToken extends ErrorParsingWrapper<CDOMObject> implements CDOM
 	}
 
 	public ParseResult parseToken(LoadContext context, CDOMObject obj,
-		String value)
+			String value)
 	{
-		ParseResult pr = ParseResult.SUCCESS;
-		if (value != null)
-		{
-			ComplexParseResult cpr = new ComplexParseResult();
-			cpr.addWarningMessage("CHOOSE:" + getTokenName()
-					+ " will ignore arguments: " + value);
-			pr = cpr;
-		}
-		// No args - legal
-		StringBuilder sb = new StringBuilder();
-		sb.append(getTokenName());
-		context.obj.put(obj, StringKey.CHOICE_STRING, sb.toString());
-		return pr;
+		Logging.deprecationPrint("CHOOSE:SKILLS has been deprecated,"
+				+ "please use CHOOSE:SKILL|PC");
+		return context.processSubToken(obj, "CHOOSE", "SKILL", "PC");
 	}
 
 	public String[] unparse(LoadContext context, CDOMObject cdo)
 	{
-		String chooseString = context.getObjectContext().getString(cdo,
-				StringKey.CHOICE_STRING);
-		if (chooseString == null)
-		{
-			return null;
-		}
-		String returnString;
-		if (getTokenName().equals(chooseString))
-		{
-			returnString = "";
-		}
-		else
-		{
-			if (chooseString.indexOf(getTokenName() + '|') != 0)
-			{
-				return null;
-			}
-			returnString = chooseString.substring(getTokenName().length() + 1);
-		}
-		return new String[] { returnString };
+		return null;
 	}
 
 	public Class<CDOMObject> getTokenClass()
