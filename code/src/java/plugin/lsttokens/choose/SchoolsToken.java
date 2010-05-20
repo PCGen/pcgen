3746b1417832c61df90bb373049d0e54930cc263
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2010-05-20 23:27:12 +0000

    CHOOSE:SCHOOLS does not unparse correctly  was not correctly implemented according to the specs on the Wiki Issue#: CODE-37
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@11914 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- code/src/java/plugin/lsttokens/choose/SchoolsToken.java
+++ code/src/java/plugin/lsttokens/choose/SchoolsToken.java
@@ -18,14 +18,18 @@
 package plugin.lsttokens.choose;
 
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.StringTokenizer;
 
 import pcgen.cdom.base.BasicChooseInformation;
 import pcgen.cdom.base.CDOMObject;
 import pcgen.cdom.base.ChooseInformation;
 import pcgen.cdom.base.ChooseSelectionActor;
+import pcgen.cdom.base.Constants;
 import pcgen.cdom.base.PersistentChoiceActor;
 import pcgen.cdom.base.PrimitiveChoiceSet;
+import pcgen.cdom.choiceset.SimpleChoiceSet;
 import pcgen.cdom.enumeration.AssociationListKey;
 import pcgen.cdom.enumeration.GroupingState;
 import pcgen.cdom.enumeration.ListKey;
@@ -33,16 +37,19 @@ import pcgen.cdom.enumeration.ObjectKey;
 import pcgen.core.PlayerCharacter;
 import pcgen.core.SettingsHandler;
 import pcgen.rules.context.LoadContext;
+import pcgen.rules.persistence.token.AbstractToken;
 import pcgen.rules.persistence.token.CDOMSecondaryToken;
 import pcgen.rules.persistence.token.ComplexParseResult;
 import pcgen.rules.persistence.token.ErrorParsingWrapper;
 import pcgen.rules.persistence.token.ParseResult;
+import pcgen.util.Logging;
 
-public class SchoolsToken extends ErrorParsingWrapper<CDOMObject> implements
+public class SchoolsToken extends AbstractToken implements
 		CDOMSecondaryToken<CDOMObject>, PrimitiveChoiceSet<String>,
 		PersistentChoiceActor<String>
 {
 
+	@Override
 	public String getTokenName()
 	{
 		return "SCHOOLS";
@@ -54,29 +61,108 @@ public class SchoolsToken extends ErrorParsingWrapper<CDOMObject> implements
 	}
 
 	public ParseResult parseToken(LoadContext context, CDOMObject obj,
-		String value)
+			String value)
 	{
-		if (value != null)
+		if (value == null)
+		{
+			// No args - deprecated
+			Logging.deprecationPrint("CHOOSE:" + getTokenName()
+					+ " with no argument has been deprecated");
+			value = "ALL";
+		}
+		if (value.indexOf('[') != -1 || value.indexOf(']') != -1)
+		{
+			return new ParseResult.Fail(getParentToken() + ":" + getTokenName()
+					+ " may not contain brackets: " + value);
+		}
+		int pipeLoc = value.indexOf('|');
+		String activeValue;
+		String title;
+		if (pipeLoc == -1)
+		{
+			activeValue = value;
+			title = getDefaultTitle();
+		}
+		else
+		{
+			String titleString = value.substring(pipeLoc + 1);
+			if (titleString.startsWith("TITLE="))
+			{
+				title = titleString.substring(6);
+				if (title.startsWith("\""))
+				{
+					title = title.substring(1, title.length() - 1);
+				}
+				activeValue = value.substring(0, pipeLoc);
+			}
+			else
+			{
+				activeValue = value;
+				title = getDefaultTitle();
+			}
+		}
+		PrimitiveChoiceSet<String> pcs;
+		if (Constants.LST_ALL.equals(activeValue))
+		{
+			pcs = this;
+		}
+		else
+		{
+			if (hasIllegalSeparator('|', activeValue))
+			{
+				return ParseResult.INTERNAL_ERROR;
+			}
+			StringTokenizer st = new StringTokenizer(activeValue, "|");
+			HashSet<String> set = new HashSet<String>();
+			while (st.hasMoreTokens())
+			{
+				String tok = st.nextToken();
+				if (Constants.LST_ALL.equals(tok))
+				{
+					return new ParseResult.Fail(
+							"Error, Found ALL and individual items while parsing "
+									+ getTokenName());
+				}
+				if (!set.add(tok))
+				{
+					return new ParseResult.Fail("Error, Found item: " + tok
+							+ " twice while parsing " + getTokenName());
+				}
+			}
+			if (set.isEmpty())
+			{
+				return new ParseResult.Fail("No items in set.");
+			}
+			pcs = new SimpleChoiceSet<String>(set, Constants.PIPE);
+		}
+
+		if (!pcs.getGroupingState().isValid())
 		{
 			ComplexParseResult cpr = new ComplexParseResult();
-			cpr.addWarningMessage("CHOOSE:" + getTokenName()
-				+ " will ignore arguments: " + value);
+			cpr.addErrorMessage("Invalid combination of objects was used in: "
+					+ activeValue);
+			cpr.addErrorMessage("  Check that ALL is not combined");
+			cpr
+					.addErrorMessage("  Check that a key is not joined with AND (,)");
 			return cpr;
 		}
-		// No args - legal
-		ChooseInformation<String> tc =
-				new BasicChooseInformation<String>(getTokenName(), this);
-		tc.setTitle("School Choice");
+		ChooseInformation<String> tc = new BasicChooseInformation<String>(
+				getTokenName(), pcs);
+		tc.setTitle(title);
 		tc.setChoiceActor(this);
 		context.obj.put(obj, ObjectKey.CHOOSE_INFO, tc);
 		return ParseResult.SUCCESS;
 	}
 
+	private String getDefaultTitle()
+	{
+		return "School Choice";
+	}
+
 	public String[] unparse(LoadContext context, CDOMObject cdo)
 	{
-		ChooseInformation<?> tc =
-				context.getObjectContext()
-					.getObject(cdo, ObjectKey.CHOOSE_INFO);
+		ChooseInformation<?> tc = context.getObjectContext().getObject(cdo,
+				ObjectKey.CHOOSE_INFO);
 		if (tc == null)
 		{
 			return null;
@@ -95,7 +181,7 @@ public class SchoolsToken extends ErrorParsingWrapper<CDOMObject> implements
 		// sb.append("|TITLE=");
 		// sb.append(title);
 		// }
-		return new String[]{sb.toString()};
+		return new String[] { sb.toString() };
 	}
 
 	public Class<CDOMObject> getTokenClass()
@@ -115,7 +201,7 @@ public class SchoolsToken extends ErrorParsingWrapper<CDOMObject> implements
 
 	public String getLSTformat(boolean useAny)
 	{
-		return getTokenName();
+		return "ALL";
 	}
 
 	public Collection<String> getSet(PlayerCharacter pc)
@@ -136,8 +222,8 @@ public class SchoolsToken extends ErrorParsingWrapper<CDOMObject> implements
 	public void removeChoice(PlayerCharacter pc, CDOMObject owner, String choice)
 	{
 		pc.removeAssoc(owner, AssociationListKey.CHOOSE_SCHOOL, choice);
-		List<ChooseSelectionActor<?>> actors =
-				owner.getListFor(ListKey.NEW_CHOOSE_ACTOR);
+		List<ChooseSelectionActor<?>> actors = owner
+				.getListFor(ListKey.NEW_CHOOSE_ACTOR);
 		if (actors != null)
 		{
 			for (ChooseSelectionActor ca : actors)
@@ -148,7 +234,7 @@ public class SchoolsToken extends ErrorParsingWrapper<CDOMObject> implements
 	}
 
 	public void restoreChoice(PlayerCharacter pc, CDOMObject owner,
-		String choice)
+			String choice)
 	{
 		pc.addAssoc(owner, AssociationListKey.CHOOSE_SCHOOL, choice);
 	}
@@ -161,8 +247,8 @@ public class SchoolsToken extends ErrorParsingWrapper<CDOMObject> implements
 	public void applyChoice(CDOMObject owner, String choice, PlayerCharacter pc)
 	{
 		restoreChoice(pc, owner, choice);
-		List<ChooseSelectionActor<?>> actors =
-				owner.getListFor(ListKey.NEW_CHOOSE_ACTOR);
+		List<ChooseSelectionActor<?>> actors = owner
+				.getListFor(ListKey.NEW_CHOOSE_ACTOR);
 		if (actors != null)
 		{
 			for (ChooseSelectionActor ca : actors)
@@ -174,14 +260,19 @@ public class SchoolsToken extends ErrorParsingWrapper<CDOMObject> implements
 	}
 
 	private void applyChoice(CDOMObject owner, String st, PlayerCharacter pc,
-		ChooseSelectionActor<String> ca)
+			ChooseSelectionActor<String> ca)
 	{
 		ca.applyChoice(owner, st, pc);
 	}
 
 	public List<String> getCurrentlySelected(CDOMObject owner,
-		PlayerCharacter pc)
+			PlayerCharacter pc)
 	{
 		return pc.getAssocList(owner, AssociationListKey.CHOOSE_SCHOOL);
+	}
+
+	public boolean parse(LoadContext context, CDOMObject obj, String value)
+	{
+		return ErrorParsingWrapper.parseToken(this, context, obj, value);
 	}
 }
