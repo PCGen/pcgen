c432152e85f28eeafbce52892cc069217a7d25a3
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2010-07-10 13:16:30 +0000

    Fix: LST Converter can get stuck at Game Mode Issue#: CODE-156
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@12500 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- code/src/java/pcgen/rules/context/GameReferenceContext.java
+++ code/src/java/pcgen/rules/context/GameReferenceContext.java
@@ -115,4 +115,17 @@ public class GameReferenceContext extends AbstractReferenceContext
 		throw new UnsupportedOperationException(
 				"GameReferenceContext cannot mod objects");
 	}
+
+	@Override
+	public <T extends CDOMObject> boolean hasManufacturer(Class<T> cl)
+	{
+		return false;
+	}
+
+	@Override
+	protected <T extends CDOMObject & CategorizedCDOMObject<T>> boolean hasManufacturer(
+			Class<T> cl, Category<T> cat)
+	{
+		return false;
+	}
 }
--- code/src/java/pcgen/rules/context/RuntimeReferenceContext.java
+++ code/src/java/pcgen/rules/context/RuntimeReferenceContext.java
@@ -166,4 +166,17 @@ public class RuntimeReferenceContext extends AbstractReferenceContext
 		return obj;
 	}
 
+	@Override
+	public <T extends CDOMObject> boolean hasManufacturer(Class<T> cl)
+	{
+		return map.containsKey(cl);
+	}
+
+	@Override
+	protected <T extends CDOMObject & CategorizedCDOMObject<T>> boolean hasManufacturer(
+			Class<T> cl, Category<T> cat)
+	{
+		return catmap.containsKey(cl, cat);
+	}
+
 }
