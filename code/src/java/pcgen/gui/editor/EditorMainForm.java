cdcc8bae55c2a9a74efc881ee1f7a709edc6ac66
jdempsey <jdempsey@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2010-07-12 13:18:58 +0000

    Fix bug: Domain Editor: Weapon prof not saved
    Issue#: CODE-172
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@12530 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- code/src/java/pcgen/gui/editor/EditorMainForm.java
+++ code/src/java/pcgen/gui/editor/EditorMainForm.java
@@ -1060,8 +1060,8 @@ public final class EditorMainForm extends JDialog
 			}
 			if (selList.length() > 0)
 			{
-				context.unconditionallyProcess(thisPObject, "AUTO:WEAPONPROF",
-						selList.toString());
+				context.unconditionallyProcess(thisPObject, "AUTO",
+						"WEAPONPROF|"+selList.toString());
 			}
 
 			sel = pnlWeapons.getSelectedList2();
@@ -2181,7 +2181,7 @@ public final class EditorMainForm extends JDialog
 				{
 					if (s.startsWith("WEAPONPROF|"))
 					{
-						autoWeap.add(s);
+						autoWeap.add(s.substring(11));
 					}
 				}
 			}
