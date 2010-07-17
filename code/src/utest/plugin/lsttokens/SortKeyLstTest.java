8aec499035cbe5a9f9367dd5a7178d25ac8bf67b
jdempsey <jdempsey@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2010-07-17 22:40:32 +0000

    Add SORTKEY to control output sorting (global)
    Issue#: NEWTAG-5
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@12609 ef53c05b-3124-4d59-ae7d-fe3273f42be7

---
+++ code/src/utest/plugin/lsttokens/SortKeyLstTest.java
@@ -0,0 +1,74 @@
+/*
+ * SortKeyLstTest.java
+ * Copyright James Dempsey, 2010
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * Created on 18/07/2010 8:37:13 AM
+ *
+ * $Id$
+ */
+package plugin.lsttokens;
+
+import pcgen.cdom.base.CDOMObject;
+import pcgen.cdom.enumeration.StringKey;
+import pcgen.core.PCTemplate;
+import pcgen.rules.persistence.CDOMLoader;
+import pcgen.rules.persistence.token.CDOMPrimaryToken;
+import plugin.lsttokens.testsupport.AbstractGlobalStringTokenTestCase;
+import plugin.lsttokens.testsupport.CDOMTokenLoader;
+
+/**
+ * The Class <code>SortKeyLstTest</code> is a round robin test for the SORTKEY tag. 
+ * It checks that the parse and unparse are working correctly and that a parsed 
+ * result can be unparsed back to the original input.  
+ *
+ * <br/>
+ * Last Editor: $Author$
+ * Last Edited: $Date$
+ * 
+ * @author James Dempsey <jdempsey@users.sourceforge.net>
+ * @version $Revision$
+ */
+public class SortKeyLstTest extends AbstractGlobalStringTokenTestCase
+{
+	static CDOMPrimaryToken<CDOMObject> token = new SortKeyLst();
+	static CDOMTokenLoader<PCTemplate> loader = new CDOMTokenLoader<PCTemplate>(
+			PCTemplate.class);
+
+	@Override
+	public CDOMLoader<PCTemplate> getLoader()
+	{
+		return loader;
+	}
+
+	@Override
+	public Class<PCTemplate> getCDOMClass()
+	{
+		return PCTemplate.class;
+	}
+
+	@Override
+	public CDOMPrimaryToken<CDOMObject> getToken()
+	{
+		return token;
+	}
+
+	@Override
+	public StringKey getStringKey()
+	{
+		return StringKey.SORT_KEY;
+	}
+}
