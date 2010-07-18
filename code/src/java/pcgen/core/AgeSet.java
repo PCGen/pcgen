67ed90c4c01d25dc0af7a2458eec038f7d3ffccc
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2010-07-18 01:29:25 +0000

    licenses
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@12613 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- code/src/java/pcgen/core/AgeSet.java
+++ code/src/java/pcgen/core/AgeSet.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2010 Tom Parker <thpr@users.sourceforge.net>
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
 package pcgen.core;
 
 import java.util.ArrayList;
@@ -8,6 +24,9 @@ import pcgen.cdom.base.Constants;
 import pcgen.cdom.base.TransitionChoice;
 import pcgen.core.bonus.BonusObj;
 
+/**
+ * This represents the AGESET entries in the BioSettings Game Mode file
+ */
 public class AgeSet
 {
 
