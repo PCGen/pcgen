9bb745fa74700e0bd97e7f65554b25ca71c48cd1
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2010-08-09 20:38:44 +0000

    Add additional token tests
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@12763 ef53c05b-3124-4d59-ae7d-fe3273f42be7

---
+++ code/src/utest/plugin/lsttokens/domain/CCSkillTokenTest.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright (c) 2010 Tom Parker <thpr@users.sourceforge.net>
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+package plugin.lsttokens.domain;
+
+import org.junit.Test;
+
+import pcgen.core.Domain;
+import pcgen.core.Skill;
+import pcgen.persistence.PersistenceLayerException;
+import pcgen.rules.persistence.CDOMLoader;
+import pcgen.rules.persistence.token.CDOMPrimaryToken;
+import plugin.lsttokens.testsupport.AbstractListTokenTestCase;
+import plugin.lsttokens.testsupport.CDOMTokenLoader;
+
+public class CCSkillTokenTest extends AbstractListTokenTestCase<Domain, Skill>
+{
+
+	@Override
+	public char getJoinCharacter()
+	{
+		return '|';
+	}
+
+	@Override
+	public Class<Skill> getTargetClass()
+	{
+		return Skill.class;
+	}
+
+	@Override
+	public boolean isTypeLegal()
+	{
+		return true;
+	}
+
+	@Override
+	public boolean isAllLegal()
+	{
+		return true;
+	}
+
+	@Override
+	public boolean isClearDotLegal()
+	{
+		return true;
+	}
+
+	@Override
+	public boolean isClearLegal()
+	{
+		return true;
+	}
+
+	static CDOMPrimaryToken<Domain> token = new CcskillToken();
+	static CDOMTokenLoader<Domain> loader =
+			new CDOMTokenLoader<Domain>(Domain.class);
+
+	@Override
+	public CDOMLoader<Domain> getLoader()
+	{
+		return loader;
+	}
+
+	@Override
+	public Class<Domain> getCDOMClass()
+	{
+		return Domain.class;
+	}
+
+	@Override
+	public CDOMPrimaryToken<Domain> getToken()
+	{
+		return token;
+	}
+
+	public void testRoundRobinList() throws PersistenceLayerException
+	{
+		runRoundRobin("LIST");
+	}
+
+	public void testAllowClearList() throws PersistenceLayerException
+	{
+		assertTrue(parse(".CLEAR.LIST"));
+	}
+
+	@Test
+	public void testRoundRobinPattern() throws PersistenceLayerException
+	{
+		runRoundRobin("Pattern%");
+	}
+
+	@Test
+	public void testRoundRobinListPlus() throws PersistenceLayerException
+	{
+		construct(primaryContext, "Foo");
+		construct(secondaryContext, "Foo");
+		runRoundRobin("Foo", "LIST");
+	}
+
+	@Override
+	public boolean allowDups()
+	{
+		return false;
+	}
+
+}
