16ecef39154540100e073872a89cc329c574758b
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2010-08-12 20:38:47 +0000

    Correct consolidation facets
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@12778 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- code/src/java/pcgen/cdom/facet/CharacterConsolidationFacet.java
+++ code/src/java/pcgen/cdom/facet/CharacterConsolidationFacet.java
@@ -18,44 +18,24 @@
 package pcgen.cdom.facet;
 
 import pcgen.cdom.base.CDOMObject;
-import pcgen.cdom.enumeration.CharID;
 
 /**
  * This is a transition class, designed to allow things to be taken out of
  * PlayerCharacter while a transition is made to a sytem where abilities are
  * added in a forward manner, rather than a loop.
  */
-public class CharacterConsolidationFacet implements
+public class CharacterConsolidationFacet extends
+		AbstractSourcedListFacet<CDOMObject> implements
 		DataFacetChangeListener<CDOMObject>
 {
 
-	private final CDOMObjectBridge bridge = CDOMObjectBridge.getInstance();
-
-	public void add(CharID id, CDOMObject obj, Object source)
-	{
-		bridge.add(id, obj, source);
-	}
-
-	public void remove(CharID id, CDOMObject obj, Object source)
-	{
-		bridge.remove(id, obj, source);
-	}
-
-	public void addDataFacetChangeListener(
-			DataFacetChangeListener<? super CDOMObject> listener)
-	{
-		bridge.addDataFacetChangeListener(listener);
-	}
-
 	public void dataAdded(DataFacetChangeEvent<CDOMObject> dfce)
 	{
-		CDOMObject cdo = dfce.getCDOMObject();
-		add(dfce.getCharID(), cdo, dfce.getSource());
+		add(dfce.getCharID(), dfce.getCDOMObject(), dfce.getSource());
 	}
 
 	public void dataRemoved(DataFacetChangeEvent<CDOMObject> dfce)
 	{
-		CDOMObject cdo = dfce.getCDOMObject();
-		remove(dfce.getCharID(), cdo, dfce.getSource());
+		remove(dfce.getCharID(), dfce.getCDOMObject(), dfce.getSource());
 	}
 }
