5cb096e55198515c0188af0c7ade5e7dc1be4876
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2010-09-25 20:33:26 +0000

    Additional Qualifier tests
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@13153 ef53c05b-3124-4d59-ae7d-fe3273f42be7

---
+++ code/src/utest/plugin/qualifier/pcclass/PCQualifierTokenTest.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2010 Tom Parker <thpr@users.sourceforge.net>
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+package plugin.qualifier.pcclass;
+
+import java.net.URISyntaxException;
+
+import pcgen.core.PCClass;
+import pcgen.persistence.PersistenceLayerException;
+import pcgen.rules.persistence.token.CDOMSecondaryToken;
+import plugin.lsttokens.choose.ClassToken;
+import plugin.lsttokens.testsupport.AbstractPCQualifierTokenTestCase;
+import plugin.lsttokens.testsupport.TokenRegistration;
+import plugin.lsttokens.testsupport.TransparentPlayerCharacter;
+
+public class PCQualifierTokenTest extends
+		AbstractPCQualifierTokenTestCase<PCClass>
+{
+
+	static ClassToken subtoken = new ClassToken();
+
+	private static final plugin.qualifier.pcclass.PCToken PC_TOKEN =
+			new plugin.qualifier.pcclass.PCToken();
+
+	@Override
+	public void setUp() throws PersistenceLayerException, URISyntaxException
+	{
+		super.setUp();
+		TokenRegistration.register(PC_TOKEN);
+	}
+
+	@Override
+	public CDOMSecondaryToken<?> getSubToken()
+	{
+		return subtoken;
+	}
+
+	@Override
+	public Class<PCClass> getTargetClass()
+	{
+		return PCClass.class;
+	}
+
+	@Override
+	protected boolean allowsNotQualifier()
+	{
+		return true;
+	}
+
+	@Override
+	protected void addToPCSet(TransparentPlayerCharacter pc, PCClass item)
+	{
+		pc.classSet.add(item);
+	}
+}
---
+++ code/src/utest/plugin/qualifier/pobject/QualifiedQualifierTokenTest.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2010 Tom Parker <thpr@users.sourceforge.net>
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+package plugin.qualifier.pobject;
+
+import java.net.URISyntaxException;
+
+import pcgen.cdom.base.CDOMObject;
+import pcgen.core.Race;
+import pcgen.persistence.PersistenceLayerException;
+import pcgen.rules.persistence.CDOMLoader;
+import pcgen.rules.persistence.token.CDOMPrimaryToken;
+import pcgen.rules.persistence.token.CDOMSecondaryToken;
+import plugin.lsttokens.ChooseLst;
+import plugin.lsttokens.choose.RaceToken;
+import plugin.lsttokens.testsupport.AbstractQualifierTokenTestCase;
+import plugin.lsttokens.testsupport.CDOMTokenLoader;
+import plugin.lsttokens.testsupport.TokenRegistration;
+
+public class QualifiedQualifierTokenTest extends
+		AbstractQualifierTokenTestCase<CDOMObject, Race>
+{
+
+	static ChooseLst token = new ChooseLst();
+	static RaceToken subtoken = new RaceToken();
+	static CDOMTokenLoader<CDOMObject> loader = new CDOMTokenLoader<CDOMObject>(
+			CDOMObject.class);
+
+	private static final plugin.qualifier.pobject.QualifiedToken QUALIFIED_TOKEN = new plugin.qualifier.pobject.QualifiedToken();
+
+	public QualifiedQualifierTokenTest()
+	{
+		super("QUALIFIED", null, true);
+	}
+
+	@Override
+	public void setUp() throws PersistenceLayerException, URISyntaxException
+	{
+		super.setUp();
+		TokenRegistration.register(QUALIFIED_TOKEN);
+	}
+
+	@Override
+	public CDOMSecondaryToken<?> getSubToken()
+	{
+		return subtoken;
+	}
+
+	@Override
+	public Class<Race> getTargetClass()
+	{
+		return Race.class;
+	}
+
+	@Override
+	public Class<Race> getCDOMClass()
+	{
+		return Race.class;
+	}
+
+	@Override
+	public CDOMLoader<CDOMObject> getLoader()
+	{
+		return loader;
+	}
+
+	@Override
+	public CDOMPrimaryToken<CDOMObject> getToken()
+	{
+		return token;
+	}
+
+	@Override
+	protected boolean allowsNotQualifier()
+	{
+		return true;
+	}
+
+}
