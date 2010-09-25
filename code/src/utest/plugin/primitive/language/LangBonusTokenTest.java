291c84074e95516b53cacdbe51ca5b3f2da9ecbd
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2010-09-25 20:32:14 +0000

    Additional primitive tests
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@13149 ef53c05b-3124-4d59-ae7d-fe3273f42be7

---
+++ code/src/utest/plugin/primitive/language/LangBonusTokenTest.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2010 Tom Parker <thpr@users.sourceforge.net>
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+package plugin.primitive.language;
+
+import java.net.URISyntaxException;
+
+import pcgen.cdom.base.CDOMObject;
+import pcgen.core.Language;
+import pcgen.persistence.PersistenceLayerException;
+import pcgen.rules.persistence.CDOMLoader;
+import pcgen.rules.persistence.token.CDOMPrimaryToken;
+import pcgen.rules.persistence.token.CDOMSecondaryToken;
+import plugin.lsttokens.ChooseLst;
+import plugin.lsttokens.choose.LangToken;
+import plugin.lsttokens.testsupport.AbstractPrimitiveTokenTestCase;
+import plugin.lsttokens.testsupport.CDOMTokenLoader;
+import plugin.lsttokens.testsupport.TokenRegistration;
+
+public class LangBonusTokenTest extends
+		AbstractPrimitiveTokenTestCase<CDOMObject, Language>
+{
+
+	static ChooseLst token = new ChooseLst();
+	static LangToken subtoken = new LangToken();
+	static CDOMTokenLoader<CDOMObject> loader = new CDOMTokenLoader<CDOMObject>(
+			CDOMObject.class);
+	private static final LangBonusToken LANGBONUS_TOKEN = new LangBonusToken();
+
+	public LangBonusTokenTest()
+	{
+		super("LANGBONUS", null);
+	}
+
+	@Override
+	public void setUp() throws PersistenceLayerException, URISyntaxException
+	{
+		super.setUp();
+		TokenRegistration.register(LANGBONUS_TOKEN);
+	}
+
+	@Override
+	public CDOMSecondaryToken<?> getSubToken()
+	{
+		return subtoken;
+	}
+
+	@Override
+	public Class<Language> getTargetClass()
+	{
+		return Language.class;
+	}
+
+	@Override
+	public Class<Language> getCDOMClass()
+	{
+		return Language.class;
+	}
+
+	@Override
+	public CDOMLoader<CDOMObject> getLoader()
+	{
+		return loader;
+	}
+
+	@Override
+	public CDOMPrimaryToken<CDOMObject> getToken()
+	{
+		return token;
+	}
+
+}
---
+++ code/src/utest/plugin/primitive/pobject/FeatTokenTest.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2010 Tom Parker <thpr@users.sourceforge.net>
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+package plugin.primitive.pobject;
+
+import java.net.URISyntaxException;
+
+import pcgen.cdom.base.CDOMObject;
+import pcgen.core.Ability;
+import pcgen.core.AbilityCategory;
+import pcgen.core.Deity;
+import pcgen.persistence.PersistenceLayerException;
+import pcgen.rules.context.LoadContext;
+import pcgen.rules.persistence.CDOMLoader;
+import pcgen.rules.persistence.token.CDOMPrimaryToken;
+import pcgen.rules.persistence.token.CDOMSecondaryToken;
+import plugin.lsttokens.ChooseLst;
+import plugin.lsttokens.choose.DeityToken;
+import plugin.lsttokens.testsupport.AbstractPrimitiveTokenTestCase;
+import plugin.lsttokens.testsupport.CDOMTokenLoader;
+import plugin.lsttokens.testsupport.TokenRegistration;
+
+public class FeatTokenTest extends
+		AbstractPrimitiveTokenTestCase<CDOMObject, Ability>
+{
+
+	static ChooseLst token = new ChooseLst();
+	static DeityToken subtoken = new DeityToken();
+	static CDOMTokenLoader<CDOMObject> loader = new CDOMTokenLoader<CDOMObject>(
+			CDOMObject.class);
+
+	private static final FeatToken FEAT_TOKEN = new FeatToken();
+
+	public FeatTokenTest()
+	{
+		super("FEAT", "Fast Feet");
+	}
+
+	@Override
+	public void setUp() throws PersistenceLayerException, URISyntaxException
+	{
+		super.setUp();
+		TokenRegistration.register(FEAT_TOKEN);
+	}
+
+	@Override
+	public CDOMSecondaryToken<?> getSubToken()
+	{
+		return subtoken;
+	}
+
+	@Override
+	public Class<Ability> getTargetClass()
+	{
+		return Ability.class;
+	}
+
+	@Override
+	public Class<Deity> getCDOMClass()
+	{
+		return Deity.class;
+	}
+
+	@Override
+	public CDOMLoader<CDOMObject> getLoader()
+	{
+		return loader;
+	}
+
+	@Override
+	public CDOMPrimaryToken<CDOMObject> getToken()
+	{
+		return token;
+	}
+
+	@Override
+	protected void construct(LoadContext loadContext, String one)
+	{
+		Ability obj = loadContext.ref.constructCDOMObject(Ability.class, one);
+		loadContext.ref.reassociateCategory(AbilityCategory.FEAT, obj);
+	}
+
+}
