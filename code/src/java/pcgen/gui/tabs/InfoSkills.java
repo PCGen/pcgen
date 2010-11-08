d0db2e8ad7077d8e0328564aadd4c1ee14053772
jdempsey <jdempsey@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2010-11-08 11:06:10 +0000

    Fix debug output: SEVERE AWT-EventQueue-1 InfoSkills:1273 Saved splitpane loc of 621
    Issue#: CODE-378
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@13805 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- code/src/java/pcgen/gui/tabs/InfoSkills.java
+++ code/src/java/pcgen/gui/tabs/InfoSkills.java
@@ -1271,7 +1271,6 @@ public class InfoSkills extends FilterAdapterPanel implements CharacterInfoTab
 		{
 			splitPane.setDividerLocation(s);
 			SettingsHandler.setPCGenOption("InfoSkills.splitPane", s); //$NON-NLS-1$
-			Logging.errorPrint("Saved splitpane loc of " + s);
 		}
 
 		if (t > 0)
