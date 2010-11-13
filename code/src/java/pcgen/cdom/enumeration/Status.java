3a535cd56ff32ce5853ecc20ad30db3ef7590a5f
zaister <zaister@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2010-11-13 21:29:49 +0000

    Corrected copyright date and author for new java files.
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@13830 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- code/src/java/pcgen/cdom/enumeration/Status.java
+++ code/src/java/pcgen/cdom/enumeration/Status.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2007 (C) Tom Parker <thpr@users.sourceforge.net>
+ * Copyright (c) 2010 Stefan Radermacher <zaister@users.sourceforge.net>
  * 
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
--- code/src/java/plugin/lsttokens/campaign/StatusToken.java
+++ code/src/java/plugin/lsttokens/campaign/StatusToken.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008 Tom Parker <thpr@users.sourceforge.net>
+ * Copyright (c) 2010 Stefan Radermacher <zaister@users.sourceforge.net>
  * 
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
