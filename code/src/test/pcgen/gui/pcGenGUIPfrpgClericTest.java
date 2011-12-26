5a1620f33f265e7d6b13a43ce5c3ec2d0bbcf3c8
jdempsey <jdempsey@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2011-12-26 00:01:06 +0000

    Add a pathfinder character test
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@15689 ef53c05b-3124-4d59-ae7d-fe3273f42be7

---
+++ code/src/test/pcgen/gui/pcGenGUIPfrpgClericTest.java
@@ -0,0 +1,49 @@
+package pcgen.gui;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * Tests a Unit Test Case of a pathfinder cleric.
+ * See the PCG file for details
+ */
+@SuppressWarnings("nls")
+public class pcGenGUIPfrpgClericTest extends pcGenGUITestCase
+{
+
+	/**
+	 * 
+	 */
+	public pcGenGUIPfrpgClericTest()
+	{
+		// Empty Constructor
+	}
+
+	/**
+	 * standard JUnit style constructor
+	 * 
+	 * @param name
+	 */
+	public pcGenGUIPfrpgClericTest(String name)
+	{
+		super(name);
+	}
+
+	/**
+	 * @return A <tt>TestSuite</tt>
+	 */
+	public static Test suite()
+	{
+		return new TestSuite(pcGenGUIPfrpgClericTest.class);
+	}
+
+	/**
+	 * Loads and outputs the character.
+	 * 
+	 * @throws Exception If an error occurs.
+	 */
+	public void testCode() throws Exception
+	{
+		runTest("PFRPGCleric", "Pathfinder_RPG");
+	}
+}
