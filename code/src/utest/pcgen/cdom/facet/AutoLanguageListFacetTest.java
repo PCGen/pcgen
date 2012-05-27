36062f614a26639e321817a9c9e87947aaef3301
soulcatcher <soulcatcher@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2012-05-27 05:21:33 +0000

    hopefully this is the last compiler issue
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@16764 ef53c05b-3124-4d59-ae7d-fe3273f42be7

---
+++ code/src/utest/pcgen/cdom/facet/AutoLanguageListFacetTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2010 Tom Parker <thpr@users.sourceforge.net>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+package pcgen.cdom.facet;
+
+import pcgen.cdom.testsupport.AbstractSourcedListFacetTest;
+import pcgen.core.Language;
+
+public class AutoLanguageListFacetTest extends AbstractSourcedListFacetTest<Language> {
+
+	private AutoLanguageListFacet facet = new AutoLanguageListFacet();
+
+	@Override
+	public void setUp() throws Exception
+	{
+		super.setUp();
+	}
+
+	@Override
+	protected AbstractSourcedListFacet<Language> getFacet()
+	{
+		return facet;
+	}
+
+	public static int n = 0;
+
+	@Override
+	protected Language getObject()
+	{
+		Language wp = new Language();
+		wp.setName("WP" + n++);
+		return wp;
+	}
+}
