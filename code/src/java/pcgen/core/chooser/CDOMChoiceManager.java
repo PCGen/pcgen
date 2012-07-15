d302b1c9cc883292d403e4b0ead9419249706019
jdempsey <jdempsey@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2012-07-15 10:25:51 +0000

    Fix bug: New Chooser - Number of selections always max Issue#: CODE-1530
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@16929 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- code/src/java/pcgen/core/chooser/CDOMChoiceManager.java
+++ code/src/java/pcgen/core/chooser/CDOMChoiceManager.java
@@ -187,7 +187,7 @@ public class CDOMChoiceManager<T> implements ChoiceManagerList<T>
 		int effectiveChoices = Math
 				.min(controller.getPool() + selectedPoolValue,
 						effectiveTotalChoices / choicesPerUnitCost);
-		//effectiveChoices -=  selectedList.size();
+		effectiveChoices -=  selectedList.size();
 		return effectiveChoices;
 	}
 
