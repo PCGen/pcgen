205c2f2129fbd38f4e4d20ce2f0c64622ecc27c2
jdempsey <jdempsey@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2012-09-20 13:16:33 +0000

    Fix bug: CHOOSE:SPELLLEVEL - CLASS=x not parsed Issue#: CODE-1652
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@17384 ef53c05b-3124-4d59-ae7d-fe3273f42be7

---
+++ code/src/java/plugin/primitive/pcclass/ClassToken.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright 2012 (C) James Dempsey
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+package plugin.primitive.pcclass;
+
+import java.util.Collection;
+
+import pcgen.cdom.base.CDOMReference;
+import pcgen.cdom.base.Converter;
+import pcgen.cdom.base.PrimitiveFilter;
+import pcgen.cdom.enumeration.GroupingState;
+import pcgen.cdom.enumeration.StringKey;
+import pcgen.core.PCClass;
+import pcgen.core.PlayerCharacter;
+import pcgen.rules.context.LoadContext;
+import pcgen.rules.persistence.token.PrimitiveToken;
+
+public class ClassToken implements PrimitiveToken<PCClass>, PrimitiveFilter<PCClass>
+{
+	private static final Class<PCClass> PCCLASS_CLASS = PCClass.class;
+	private String pcclass;
+	private CDOMReference<PCClass> allClasses;
+
+	@Override
+	public boolean initialize(LoadContext context, Class<PCClass> cl,
+		String value, String args)
+	{
+		if (args != null)
+		{
+			return false;
+		}
+		pcclass = value;
+		allClasses = context.ref.getCDOMAllReference(PCCLASS_CLASS);
+		return true;
+	}
+
+	@Override
+	public String getTokenName()
+	{
+		return "CLASS";
+	}
+
+	@Override
+	public Class<PCClass> getReferenceClass()
+	{
+		return PCCLASS_CLASS;
+	}
+
+	@Override
+	public String getLSTformat(boolean useAny)
+	{
+		return getTokenName() + "=" + pcclass;
+	}
+
+	@Override
+	public boolean allow(PlayerCharacter pc, PCClass cl)
+	{
+		return pcclass.equals(cl.get(StringKey.KEY_NAME));
+	}
+
+	@Override
+	public GroupingState getGroupingState()
+	{
+		return GroupingState.ANY;
+	}
+
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (obj == this)
+		{
+			return true;
+		}
+		if (obj instanceof ClassToken)
+		{
+			ClassToken other = (ClassToken) obj;
+			return pcclass.equals(other.pcclass);
+		}
+		return false;
+	}
+
+	@Override
+	public int hashCode()
+	{
+		return pcclass == null ? -7 : pcclass.hashCode();
+	}
+
+	@Override
+	public <R> Collection<R> getCollection(PlayerCharacter pc,
+			Converter<PCClass, R> c)
+	{
+		return c.convert(allClasses, this);
+	}
+}
---
+++ code/src/utest/plugin/primitive/pcclass/ClassTokenTest.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2012 James Dempsey
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+package plugin.primitive.pcclass;
+
+import java.net.URISyntaxException;
+
+import pcgen.cdom.base.CDOMObject;
+import pcgen.core.Deity;
+import pcgen.core.PCClass;
+import pcgen.persistence.PersistenceLayerException;
+import pcgen.rules.persistence.CDOMLoader;
+import pcgen.rules.persistence.token.CDOMPrimaryToken;
+import pcgen.rules.persistence.token.CDOMSecondaryToken;
+import plugin.lsttokens.ChooseLst;
+import plugin.lsttokens.choose.ClassToken;
+import plugin.lsttokens.testsupport.AbstractPrimitiveTokenTestCase;
+import plugin.lsttokens.testsupport.CDOMTokenLoader;
+import plugin.lsttokens.testsupport.TokenRegistration;
+
+public class ClassTokenTest extends
+		AbstractPrimitiveTokenTestCase<CDOMObject, PCClass>
+{
+
+	static ChooseLst token = new ChooseLst();
+	static ClassToken subtoken = new ClassToken();
+	static CDOMTokenLoader<CDOMObject> loader = new CDOMTokenLoader<CDOMObject>(
+			CDOMObject.class);
+
+	private static final plugin.primitive.pcclass.ClassToken CLASS_TOKEN = new plugin.primitive.pcclass.ClassToken();
+
+	public ClassTokenTest()
+	{
+		super("CLASS", "Channeler");
+	}
+
+	@Override
+	public void setUp() throws PersistenceLayerException, URISyntaxException
+	{
+		super.setUp();
+		TokenRegistration.register(CLASS_TOKEN);
+	}
+
+	@Override
+	public CDOMSecondaryToken<?> getSubToken()
+	{
+		return subtoken;
+	}
+
+	@Override
+	public Class<PCClass> getTargetClass()
+	{
+		return PCClass.class;
+	}
+
+	@Override
+	public Class<Deity> getCDOMClass()
+	{
+		return Deity.class;
+	}
+
+	@Override
+	public CDOMLoader<CDOMObject> getLoader()
+	{
+		return loader;
+	}
+
+	@Override
+	public CDOMPrimaryToken<CDOMObject> getToken()
+	{
+		return token;
+	}
+
+}
