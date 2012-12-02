c6a1a4f443bfd3b268ac1acd4320fe797094828f
soulcatcher <soulcatcher@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2012-12-02 19:23:15 +0000

    Fixing the license on this old code to reflect the licence grant I made in 2002
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@18530 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- code/src/java/gmgen/plugin/DiceFudge.java
+++ code/src/java/gmgen/plugin/DiceFudge.java
@@ -1,24 +1,24 @@
 /*
- *  DiceFudge.java
+ *  Initiative - A role playing utility to track turns
  *  Copyright (C) 2002 Devon D Jones
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful,
+ *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
- *  The author of this program grants you the ability to use this code
- *  in conjunction with code that is covered under the Open Gaming License
+ *  DiceFudge.java
  *
+ *  Created on January 24, 2002, 11:15 AM
  */
 package gmgen.plugin;
 
