f66f7cfeb104fced5d1cd0fb81df6e1c30896d90
soulcatcher <soulcatcher@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2012-12-02 20:14:45 +0000

    Fixing the license on this old code to reflect the licence grant I made in 2002
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@18533 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- code/src/java/plugin/charactersheet/CharacterSheetPlugin.java
+++ code/src/java/plugin/charactersheet/CharacterSheetPlugin.java
@@ -1,23 +1,19 @@
 /*
- *  plugin.charactersheet - DESCRIPTION OF PACKAGE
  *  Copyright (C) 2003 Devon Jones
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful,
+ *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *
- *  The author of this program grants you the ability to use this code
- *  in conjunction with code that is covered under the Open Gaming License
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
  *  CharacterSheetPlugin.java
  *
