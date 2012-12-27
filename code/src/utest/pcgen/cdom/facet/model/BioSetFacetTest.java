0511e336330cf27a8d2d7a3192f740b95139af2f
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2012-12-27 19:16:43 +0000

    Correct Facet organization (tests) BioSet should be in Model
    Issue#: CODE-1907
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@18822 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- code/src/utest/pcgen/cdom/facet/model/BioSetFacetTest.java
+++ code/src/utest/pcgen/cdom/facet/model/BioSetFacetTest.java
@@ -15,7 +15,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
-package pcgen.cdom.facet;
+package pcgen.cdom.facet.model;
 
 import pcgen.cdom.facet.base.AbstractItemFacet;
 import pcgen.cdom.facet.model.BioSetFacet;
