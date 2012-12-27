8f7e801b1a7405b608c2dab9612916a575ff4f0a
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2012-12-27 19:14:52 +0000

    Template Facet Test
    Issue#: CODE-1943
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@18821 ef53c05b-3124-4d59-ae7d-fe3273f42be7

---
+++ code/src/utest/pcgen/cdom/facet/model/TemplateFacetTest.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2010 Tom Parker <thpr@users.sourceforge.net>
+ * 
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+package pcgen.cdom.facet.model;
+
+import pcgen.cdom.facet.DataFacetChangeListener;
+import pcgen.cdom.facet.base.AbstractSourcedListFacet;
+import pcgen.cdom.testsupport.AbstractConsolidatingFacetTest;
+import pcgen.core.PCTemplate;
+
+public class TemplateFacetTest extends
+		AbstractConsolidatingFacetTest<PCTemplate>
+{
+
+	private TemplateFacet facet = new TemplateFacet();
+
+	@Override
+	protected AbstractSourcedListFacet<PCTemplate> getFacet()
+	{
+		return facet;
+	}
+
+	public static int n = 0;
+
+	@Override
+	protected PCTemplate getObject()
+	{
+		PCTemplate wp = new PCTemplate();
+		wp.setName("WP" + n++);
+		return wp;
+	}
+
+	@Override
+	protected DataFacetChangeListener<PCTemplate> getListener()
+	{
+		return facet;
+	}
+
+}
