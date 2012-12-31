6b45c4b2d3c10e542aad22020fdea8bec3d375bd
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2012-12-31 18:34:00 +0000

    Add Model Integration test for Race/FEAT with target %LIST (e.g. FEAT:MyAbility (%LIST))
    Issue#: CODE-1943
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@18931 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- code/src/itest/tokenmodel/RaceFeatTest.java
+++ code/src/itest/tokenmodel/RaceFeatTest.java
@@ -22,21 +22,28 @@ import java.util.Collection;
 import org.junit.Test;
 
 import pcgen.cdom.enumeration.Nature;
+import pcgen.cdom.enumeration.ObjectKey;
 import pcgen.cdom.facet.base.AbstractListFacet;
 import pcgen.cdom.helper.CategorizedAbilitySelection;
 import pcgen.core.Ability;
 import pcgen.core.AbilityCategory;
 import pcgen.core.Race;
+import pcgen.core.WeaponProf;
+import pcgen.gui2.facade.MockUIDelegate;
 import pcgen.persistence.PersistenceLayerException;
 import pcgen.rules.persistence.token.CDOMToken;
 import pcgen.rules.persistence.token.ParseResult;
+import pcgen.util.chooser.ChooserFactory;
+import plugin.lsttokens.choose.WeaponProficiencyToken;
 import plugin.lsttokens.race.FeatToken;
+import plugin.lsttokens.testsupport.TokenRegistration;
 import tokenmodel.testsupport.AbstractTokenModelTest;
 
 public class RaceFeatTest extends AbstractTokenModelTest
 {
 
 	private static FeatToken token = new FeatToken();
+	private static WeaponProficiencyToken CHOOSE_WP_TOKEN = new WeaponProficiencyToken();
 
 	@Test
 	public void testSimple() throws PersistenceLayerException
@@ -52,31 +59,97 @@ public class RaceFeatTest extends AbstractTokenModelTest
 		finishLoad();
 		assertEquals(0, directAbilityFacet.getCount(id));
 		raceFacet.set(id, source);
-		assertTrue(containsExpected(granted));
+		assertTrue(containsExpected(granted, null));
 		assertEquals(1, directAbilityFacet.getCount(id));
 		raceFacet.remove(id);
 		assertEquals(0, directAbilityFacet.getCount(id));
 	}
 
-	protected boolean containsExpected(Ability granted)
+	@Test
+	public void testList() throws PersistenceLayerException
+	{
+		Race source = create(Race.class, "Source");
+		create(WeaponProf.class, "Longsword");
+		create(WeaponProf.class, "Dagger");
+		Ability granted = createGrantedObject();
+		granted.put(ObjectKey.MULTIPLE_ALLOWED, true);
+		ParseResult result = token.parseToken(context, source, "Granted (%LIST)");
+		if (result != ParseResult.SUCCESS)
+		{
+			result.printMessages();
+			fail("Test Setup Failed");
+		}
+		result =
+				CHOOSE_TOKEN.parseToken(context, source,
+					"WEAPONPROFICIENCY|Longsword");
+		if (result != ParseResult.SUCCESS)
+		{
+			result.printMessages();
+			fail("Test Setup Failed");
+		}
+		result =
+				CHOOSE_TOKEN.parseToken(context, granted,
+					"WEAPONPROFICIENCY|Longsword|Dagger");
+		if (result != ParseResult.SUCCESS)
+		{
+			result.printMessages();
+			fail("Test Setup Failed");
+		}
+		finishLoad();
+		assertEquals(0, directAbilityFacet.getCount(id));
+		raceFacet.set(id, source);
+		assertTrue(containsExpected(granted, "Longsword"));
+		assertEquals(1, directAbilityFacet.getCount(id));
+		raceFacet.remove(id);
+		assertEquals(0, directAbilityFacet.getCount(id));
+	}
+
+	protected boolean containsExpected(Ability granted, String selection)
 	{
 		Collection<CategorizedAbilitySelection> casSet =
 				getTargetFacet().getSet(id);
 		for (CategorizedAbilitySelection cas : casSet)
 		{
-			boolean featExpected =
+			boolean categoryExpected =
 					cas.getAbilityCategory() == AbilityCategory.FEAT;
+			if (!categoryExpected)
+			{
+				System.err.println("Category Mismatch");
+				return false;
+			}
 			boolean abilityExpected =
 					cas.getAbility().equals(
 						context.ref.silentlyGetConstructedCDOMObject(
 							Ability.class, AbilityCategory.FEAT, "Granted"));
-			boolean natureExpected = cas.getNature() == Nature.AUTOMATIC;
-			boolean selectionExpected = cas.getSelection() == null;
-			if (featExpected && abilityExpected && natureExpected
-				&& selectionExpected)
+			if (!abilityExpected)
 			{
-				return true;
+				System.err.println("Ability Mismatch");
+				return false;
 			}
+			boolean natureExpected = cas.getNature() == Nature.AUTOMATIC;
+			if (!natureExpected)
+			{
+				System.err.println("Nature Mismatch");
+				return false;
+			}
+			if (selection == null)
+			{
+				if (cas.getSelection() != null)
+				{
+					System.err.println("Selection Mismatch");
+					return false;
+				}
+			}
+			else
+			{
+				boolean selectionExpected = cas.getSelection().equals(selection);
+				if (!selectionExpected)
+				{
+					System.err.println("Selection Mismatch");
+					return false;
+				}
+			}
+			return true;
 		}
 		return false;
 	}
@@ -98,4 +171,14 @@ public class RaceFeatTest extends AbstractTokenModelTest
 		context.ref.reassociateCategory(AbilityCategory.FEAT, a);
 		return a;
 	}
+
+	@Override
+	protected void setUp() throws Exception
+	{
+		super.setUp();
+		TokenRegistration.register(CHOOSE_WP_TOKEN);
+		ChooserFactory.setDelegate(new MockUIDelegate());
+	}
+	
+	
 }
