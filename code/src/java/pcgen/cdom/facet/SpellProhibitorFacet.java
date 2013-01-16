a46dd1259a769f68b2528fcd3dc1543e98e35f22
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2013-01-16 03:26:34 +0000

    Convert SpellProhibitorFacet to AbstractScopeFacet
    Issue#: CODE-1975
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@19165 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- code/src/java/pcgen/cdom/facet/SpellProhibitorFacet.java
+++ code/src/java/pcgen/cdom/facet/SpellProhibitorFacet.java
@@ -17,19 +17,9 @@
  */
 package pcgen.cdom.facet;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.IdentityHashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
-
-import pcgen.base.util.WrappedMapSet;
 import pcgen.cdom.enumeration.CharID;
 import pcgen.cdom.enumeration.ListKey;
-import pcgen.cdom.facet.base.AbstractStorageFacet;
+import pcgen.cdom.facet.base.AbstractScopeFacet;
 import pcgen.cdom.facet.event.DataFacetChangeEvent;
 import pcgen.cdom.facet.event.DataFacetChangeListener;
 import pcgen.cdom.facet.model.ClassFacet;
@@ -42,7 +32,8 @@ import pcgen.core.SpellProhibitor;
  * 
  * @author Thomas Parker (thpr [at] yahoo.com)
  */
-public class SpellProhibitorFacet extends AbstractStorageFacet implements
+public class SpellProhibitorFacet extends
+		AbstractScopeFacet<PCClass, SpellProhibitor> implements
 		DataFacetChangeListener<PCClass>
 {
 	private ClassFacet classFacet;
@@ -83,113 +74,7 @@ public class SpellProhibitorFacet extends AbstractStorageFacet implements
 	@Override
 	public void dataRemoved(DataFacetChangeEvent<PCClass> dfce)
 	{
-		CacheInfo ci = getInfo(dfce.getCharID());
-		if (ci != null)
-		{
-			ci.removeAll(dfce.getCDOMObject());
-		}
-	}
-
-	private CacheInfo getConstructingInfo(CharID id)
-	{
-		CacheInfo rci = getInfo(id);
-		if (rci == null)
-		{
-			rci = new CacheInfo();
-			setCache(id, rci);
-		}
-		return rci;
-	}
-
-	private CacheInfo getInfo(CharID id)
-	{
-		return (CacheInfo) getCache(id);
-	}
-
-	/**
-	 * CacheInfo is the data structure used by SpellProhibitorFacet to store a
-	 * Player Character's SpellProhibitor Info
-	 */
-	private static class CacheInfo
-	{
-		Map<PCClass, Map<SpellProhibitor, Set<Object>>> map =
-				new HashMap<PCClass, Map<SpellProhibitor, Set<Object>>>();
-
-		public void add(PCClass pcc, SpellProhibitor sp, Object source)
-		{
-			Map<SpellProhibitor, Set<Object>> spMap = map.get(pcc);
-			if (spMap == null)
-			{
-				spMap = new IdentityHashMap<SpellProhibitor, Set<Object>>();
-				map.put(pcc, spMap);
-			}
-			Set<Object> set = spMap.get(sp);
-			if (set == null)
-			{
-				set = new WrappedMapSet<Object>(IdentityHashMap.class);
-				spMap.put(sp, set);
-			}
-			set.add(source);
-		}
-
-		public void removeAll(Object source)
-		{
-			for (Iterator<Map<SpellProhibitor, Set<Object>>> mit =
-					map.values().iterator(); mit.hasNext();)
-			{
-				Map<SpellProhibitor, Set<Object>> spMap = mit.next();
-				for (Iterator<Set<Object>> sit = spMap.values().iterator(); sit
-					.hasNext();)
-				{
-					Set<Object> set = sit.next();
-					if (set.remove(source) && set.isEmpty())
-					{
-						sit.remove();
-					}
-				}
-				if (spMap.isEmpty())
-				{
-					mit.remove();
-				}
-			}
-		}
-
-		public boolean contains(PCClass pcc, SpellProhibitor sp)
-		{
-			Map<SpellProhibitor, Set<Object>> spMap = map.get(pcc);
-			return (spMap != null) && spMap.containsKey(sp);
-		}
-
-		public Collection<SpellProhibitor> getSet(PCClass pcc)
-		{
-			Map<SpellProhibitor, Set<Object>> skMap = map.get(pcc);
-			if (skMap == null)
-			{
-				return Collections.emptyList();
-			}
-			return new ArrayList<SpellProhibitor>(skMap.keySet());
-		}
-	}
-
-	private void add(CharID id, PCClass pcc, SpellProhibitor sp, Object source)
-	{
-		getConstructingInfo(id).add(pcc, sp, source);
-	}
-
-	public Collection<SpellProhibitor> getSet(CharID id, PCClass pcc)
-	{
-		CacheInfo ci = getInfo(id);
-		if (ci == null)
-		{
-			return Collections.emptyList();
-		}
-		return ci.getSet(pcc);
-	}
-
-	public boolean contains(CharID id, PCClass pcc, SpellProhibitor sp)
-	{
-		CacheInfo ci = getInfo(id);
-		return ci != null && ci.contains(pcc, sp);
+		removeAllFromSource(dfce.getCharID(), dfce.getCDOMObject());
 	}
 
 	public void setClassFacet(ClassFacet classFacet)
@@ -206,52 +91,5 @@ public class SpellProhibitorFacet extends AbstractStorageFacet implements
 	public void init()
 	{
 		classFacet.addDataFacetChangeListener(this);
-	}
-
-	/**
-	 * Copies the contents of the SpellProhibitorFacet from one Player Character
-	 * to another Player Character, based on the given CharIDs representing
-	 * those Player Characters.
-	 * 
-	 * This is a method in SpellProhibitorFacet in order to avoid exposing the
-	 * mutable Map object to other classes. This should not be inlined, as the
-	 * Map is internal information to SpellProhibitorFacet and should not be
-	 * exposed to other classes.
-	 * 
-	 * Note also the copy is a one-time event and no references are maintained
-	 * between the Player Characters represented by the given CharIDs (meaning
-	 * once this copy takes place, any change to the SpellProhibitorFacet of one
-	 * Player Character will only impact the Player Character where the
-	 * SpellProhibitorFacet was changed).
-	 * 
-	 * @param source
-	 *            The CharID representing the Player Character from which the
-	 *            information should be copied
-	 * @param copy
-	 *            The CharID representing the Player Character to which the
-	 *            information should be copied
-	 */
-	@Override
-	public void copyContents(CharID source, CharID copy)
-	{
-		CacheInfo rci = getInfo(source);
-		if (rci != null)
-		{
-			CacheInfo copyci = getConstructingInfo(copy);
-			for (Map.Entry<PCClass, Map<SpellProhibitor, Set<Object>>> clme : rci.map
-				.entrySet())
-			{
-				PCClass pcc = clme.getKey();
-				for (Map.Entry<SpellProhibitor, Set<Object>> spme : clme
-					.getValue().entrySet())
-				{
-					SpellProhibitor sp = spme.getKey();
-					for (Object spsource : spme.getValue())
-					{
-						copyci.add(pcc, sp, spsource);
-					}
-				}
-			}
-		}
 	}
 }
