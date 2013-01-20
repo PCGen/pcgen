294cb766094a53a7d8a3906376b5e8b8e889fd4e
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2013-01-20 20:07:02 +0000

    Required update with AbstractToken getTokenName() as public
    Issue#: CODE-1906
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@19199 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- code/src/java/plugin/converter/FavClassConvertPlugin.java
+++ code/src/java/plugin/converter/FavClassConvertPlugin.java
@@ -150,7 +150,7 @@ public class FavClassConvertPlugin extends AbstractToken implements
 	}
 
 	@Override
-	protected String getTokenName()
+	public String getTokenName()
 	{
 		return getProcessedToken();
 	}
