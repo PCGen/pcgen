cf8cf7ac7e88e861f0516969b52fb07349e1fea2
henkslaaf2 <henkslaaf2@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2013-11-03 17:21:08 +0000

    [NEWTAG-327] [NEWTAG-391] Add SOURCELINK tag
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@22029 ef53c05b-3124-4d59-ae7d-fe3273f42be7

---
+++ code/src/java/plugin/lsttokens/SourcelinkLst.java
@@ -0,0 +1,49 @@
+/*
+ * Created on Sep 2, 2005
+ *
+ */
+package plugin.lsttokens;
+
+import pcgen.cdom.base.CDOMObject;
+import pcgen.cdom.enumeration.StringKey;
+import pcgen.core.Campaign;
+import pcgen.persistence.lst.InstallLstToken;
+import pcgen.rules.persistence.token.AbstractStringToken;
+import pcgen.rules.persistence.token.CDOMPrimaryToken;
+
+
+import java.net.URI;
+
+/**
+ * @author djones4
+ *
+ */
+public class SourcelinkLst extends AbstractStringToken<CDOMObject> implements
+        CDOMPrimaryToken<CDOMObject>, InstallLstToken
+{
+
+	@Override
+	public String getTokenName()
+	{
+		return "SOURCELINK";
+	}
+
+	@Override
+	protected StringKey stringKey()
+	{
+		return StringKey.SOURCE_LINK;
+	}
+
+	@Override
+	public Class<CDOMObject> getTokenClass()
+	{
+		return CDOMObject.class;
+	}
+
+	@Override
+	public boolean parse(Campaign campaign, String value, URI sourceURI)
+	{
+		campaign.put(StringKey.SOURCE_LINK, value);
+		return true;
+	}
+}
