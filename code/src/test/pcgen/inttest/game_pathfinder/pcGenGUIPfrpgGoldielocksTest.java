5a38fbfa4d0f1ba24b80697b31c9f9f70cbab12d
jdempsey <jdempsey@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2013-12-04 20:22:30 +0000

    Add a character test for the new dragon data.
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@22517 ef53c05b-3124-4d59-ae7d-fe3273f42be7

---
+++ code/src/test/pcgen/inttest/game_pathfinder/pcGenGUIPfrpgGoldielocksTest.java
@@ -0,0 +1,50 @@
+package pcgen.inttest.game_pathfinder;
+
+import pcgen.inttest.pcGenGUITestCase;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * Tests a Unit Test Case of a pathfinder great wyrm gold dragon.
+ * See the PCG file for details
+ */
+@SuppressWarnings("nls")
+public class pcGenGUIPfrpgGoldielocksTest extends pcGenGUITestCase
+{
+
+	/**
+	 * 
+	 */
+	public pcGenGUIPfrpgGoldielocksTest()
+	{
+		// Empty Constructor
+	}
+
+	/**
+	 * standard JUnit style constructor
+	 * 
+	 * @param name
+	 */
+	public pcGenGUIPfrpgGoldielocksTest(String name)
+	{
+		super(name);
+	}
+
+	/**
+	 * @return A <tt>TestSuite</tt>
+	 */
+	public static Test suite()
+	{
+		return new TestSuite(pcGenGUIPfrpgGoldielocksTest.class);
+	}
+
+	/**
+	 * Loads and outputs the character.
+	 * 
+	 * @throws Exception If an error occurs.
+	 */
+	public void testCode() throws Exception
+	{
+		runTest("pf_goldielocks", "Pathfinder_RPG");
+	}
+}
