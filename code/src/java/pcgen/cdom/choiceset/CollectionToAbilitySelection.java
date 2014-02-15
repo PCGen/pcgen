1a8730c3710970f785ee1f6b601d80401c1b21cd
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2014-02-15 03:23:25 +0000

    Fix: Infinite Loop Produced This is the rebuild necessary in the order of operations to resolve the selections first, then expand them to include subchoices.
    Issue#: CODE-2504
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@23228 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- code/src/java/pcgen/cdom/choiceset/CollectionToAbilitySelection.java
+++ code/src/java/pcgen/cdom/choiceset/CollectionToAbilitySelection.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010 (C) Tom Parker <thpr@users.sourceforge.net>
+ * Copyright 2010-14 (C) Tom Parker <thpr@users.sourceforge.net>
  * 
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
@@ -46,6 +46,8 @@ public class CollectionToAbilitySelection implements
 	
 	private final Category<Ability> category;
 
+	private static Stack<Ability> infiniteLoopDetectionStack = new Stack<Ability>();
+
 	public CollectionToAbilitySelection(Category<Ability> cat, PrimitiveCollection<Ability> coll)
 	{
 		if (cat == null)
@@ -83,7 +85,156 @@ public class CollectionToAbilitySelection implements
 	@Override
 	public Collection<AbilitySelection> getSet(PlayerCharacter pc)
 	{
-		return collection.getCollection(pc, new ExpandingConverter(pc));
+		Collection<AbilityWithChoice> aColl =
+				collection.getCollection(pc, new ExpandingConverter(pc));
+		Set<AbilitySelection> returnSet = new HashSet<AbilitySelection>();
+		for (AbilityWithChoice a : aColl)
+		{
+			processAbility(pc, returnSet, a);
+		}
+		return returnSet;
+	}
+
+	private void processAbility(PlayerCharacter character,
+		Set<AbilitySelection> returnSet, AbilityWithChoice awc)
+	{
+		Ability a = awc.getAbility();
+		if (infiniteLoopDetectionStack.contains(a))
+		{
+			Stack<Ability> current = new Stack<Ability>();
+			current.addAll(infiniteLoopDetectionStack);
+			Logging.errorPrint("Error: Circular Expansion Found: "
+				+ reportCircularExpansion(current));
+			return;
+		}
+		try
+		{
+			infiniteLoopDetectionStack.push(a);
+			if (a.getSafe(ObjectKey.MULTIPLE_ALLOWED))
+			{
+				returnSet.addAll(addMultiplySelectableAbility(character, a,
+					awc.getChoice()));
+			}
+			else
+			{
+				returnSet.add(new AbilitySelection(a, null));
+			}
+		}
+		finally
+		{
+			infiniteLoopDetectionStack.pop();
+		}
+	}
+
+	private Collection<AbilitySelection> addMultiplySelectableAbility(
+		final PlayerCharacter aPC, Ability ability, String subName)
+	{
+		// If already have taken the feat, use it so we can remove
+		// any choices already selected
+		final Ability pcFeat = aPC.getFeatNamed(ability.getKeyName());
+
+		Ability pcability = ability;
+		if (pcFeat != null)
+		{
+			pcability = pcFeat;
+		}
+
+		boolean isPattern = false;
+		String nameRoot = null;
+		if (subName != null)
+		{
+			final int percIdx = subName.indexOf('%');
+
+			if (percIdx > -1)
+			{
+				isPattern = true;
+				nameRoot = subName.substring(0, percIdx);
+			}
+			else if (subName.length() != 0)
+			{
+				nameRoot = subName;
+			}
+		}
+
+		final List<String> availableList = new ArrayList<String>();
+		final List<?> tempAvailList = new ArrayList<Object>();
+		final List<?> tempSelList = new ArrayList<Object>();
+		ChooserUtilities.modChoices(pcability, tempAvailList, tempSelList,
+			false, aPC, true, AbilityCategory.FEAT);
+		// Mod choices may have sent us back weaponprofs, abilities or
+		// strings,
+		// so we have to do a conversion here
+		for (Object o : tempAvailList)
+		{
+			String choice = o.toString();
+			if ("NOCHOICE".equals(choice))
+			{
+				availableList.add("");
+			}
+			else
+			{
+				availableList.add(choice);
+			}
+		}
+
+		// Remove any that don't match
+
+		if (nameRoot != null && nameRoot.length() != 0)
+		{
+			for (int n = availableList.size() - 1; n >= 0; --n)
+			{
+				final String aString = availableList.get(n);
+
+				if (!aString.startsWith(nameRoot))
+				{
+					availableList.remove(n);
+				}
+			}
+
+			// Example: ADD:FEAT(Skill Focus(Craft (Basketweaving))) If you
+			// have no ranks in Craft (Basketweaving), the available list
+			// will
+			// be empty
+			//
+			// Make sure that the specified feat is available, even though
+			// it
+			// does not meet the prerequisite
+
+			if (isPattern && !availableList.isEmpty())
+			{
+				availableList.add(nameRoot);
+			}
+		}
+
+		List<AbilitySelection> returnList =
+				new ArrayList<AbilitySelection>(availableList.size());
+		for (String s : availableList)
+		{
+			returnList.add(new AbilitySelection(pcability, s));
+		}
+		return returnList;
+	}
+
+	private String reportCircularExpansion(Stack<Ability> s)
+	{
+		StringBuilder sb = new StringBuilder(2000);
+		processCircularExpansion(sb, s);
+		sb.append("    which is a circular reference");
+		return sb.toString();
+	}
+
+	private void processCircularExpansion(StringBuilder sb, Stack<Ability> s)
+	{
+		Ability a = s.pop();
+		if (!s.isEmpty())
+		{
+			processCircularExpansion(sb, s);
+			sb.append("     which includes");
+		}
+		sb.append(a.getCDOMCategory()).append(' ').append(a.getKeyName());
+		sb.append(" selects items: ");
+		sb.append(a.get(ObjectKey.CHOOSE_INFO).getLSTformat());
+		sb.append('\n');
 	}
 
 	public Category<Ability> getCategory()
@@ -119,7 +270,7 @@ public class CollectionToAbilitySelection implements
 	}
 
 	public static class ExpandingConverter implements
-			Converter<Ability, AbilitySelection>
+			Converter<Ability, AbilityWithChoice>
 	{
 
 		private final PlayerCharacter character;
@@ -129,189 +280,66 @@ public class CollectionToAbilitySelection implements
 			character = pc;
 		}
 
-		private static Stack<Ability> stack = new Stack<Ability>();
-
 		@Override
-		public Collection<AbilitySelection> convert(CDOMReference<Ability> ref)
+		public Collection<AbilityWithChoice> convert(CDOMReference<Ability> ref)
 		{
-			Set<AbilitySelection> returnSet = new HashSet<AbilitySelection>();
+			Set<AbilityWithChoice> returnSet = new HashSet<AbilityWithChoice>();
 			for (Ability a : ref.getContainedObjects())
 			{
-				if (stack.contains(a))
-				{
-					Stack<Ability> current = new Stack<Ability>();
-					current.addAll(stack);
-					Logging.errorPrint("Error: Circular Expansion Found: "
-						+ reportCircularExpansion(current));
-					continue;
-				}
-				try
-				{
-					stack.push(a);
-					processAbility(ref, returnSet, a);
-				}
-				finally
-				{
-					stack.pop();
-				}
+				processAbility(ref, returnSet, a);
 			}
 			return returnSet;
 		}
 
-		private String reportCircularExpansion(Stack<Ability> s)
-		{
-			StringBuilder sb = new StringBuilder(2000);
-			processCircularExpansion(sb, s);
-			sb.append("    which is a circular reference");
-			return sb.toString();
-		}
-
-		private void processCircularExpansion(StringBuilder sb, Stack<Ability> s)
-		{
-			Ability a = s.pop();
-			if (!s.isEmpty())
-			{
-				processCircularExpansion(sb, s);
-				sb.append("     which includes");
-			}
-			sb.append(a.getCDOMCategory()).append(' ').append(a.getKeyName());
-			sb.append(" selects items: ");
-			sb.append(a.get(ObjectKey.CHOOSE_INFO).getLSTformat());
-			sb.append('\n');
-		}
-
 		private void processAbility(CDOMReference<Ability> ref,
-				Set<AbilitySelection> returnSet, Ability a)
+			Set<AbilityWithChoice> returnSet, Ability a)
 		{
-			if (a.getSafe(ObjectKey.MULTIPLE_ALLOWED))
-			{
-				returnSet.addAll(addMultiplySelectableAbility(character, a, ref
-						.getChoice()));
-			}
-			else
-			{
-				returnSet.add(new AbilitySelection(a, null));
-			}
+			returnSet.add(new AbilityWithChoice(a, ref.getChoice()));
 		}
 
 		@Override
-		public Collection<AbilitySelection> convert(CDOMReference<Ability> ref,
-				PrimitiveFilter<Ability> lim)
+		public Collection<AbilityWithChoice> convert(
+			CDOMReference<Ability> ref, PrimitiveFilter<Ability> lim)
 		{
-			Set<AbilitySelection> returnSet = new HashSet<AbilitySelection>();
+			Set<AbilityWithChoice> returnSet = new HashSet<AbilityWithChoice>();
 			for (Ability a : ref.getContainedObjects())
 			{
 				if (lim.allow(character, a))
 				{
-					if (stack.contains(a))
-					{
-						Stack<Ability> current = new Stack<Ability>();
-						current.addAll(stack);
-						Logging.errorPrint("Error: Circular Expansion Found: "
-							+ reportCircularExpansion(current));
-						continue;
-					}
-					try
-					{
-						stack.push(a);
-						processAbility(ref, returnSet, a);
-					}
-					finally
-					{
-						stack.pop();
-					}
+					processAbility(ref, returnSet, a);
 				}
 			}
 			return returnSet;
 		}
+	}
 
-		private Collection<AbilitySelection> addMultiplySelectableAbility(
-				final PlayerCharacter aPC, Ability ability, String subName)
+	/*
+	 * A custom object, NOT an AbilitySelection. The reasoning is that
+	 * AbilitySelection does enforcement of MULT:YES to allow/require choices
+	 * and (1) We can't guarantee we have that here as all expansions may be
+	 * legal (2) We don't want to compromise on the tight enforcement by
+	 * AbilitySelection
+	 */
+	private static class AbilityWithChoice
+	{
+
+		private final Ability ability;
+		private final String choice;
+
+		public AbilityWithChoice(Ability a, String c)
 		{
-			// If already have taken the feat, use it so we can remove
-			// any choices already selected
-			final Ability pcFeat = aPC.getFeatNamed(ability.getKeyName());
+			ability = a;
+			choice = c;
+		}
 
-			Ability pcability = ability;
-			if (pcFeat != null)
-			{
-				pcability = pcFeat;
-			}
+		public Ability getAbility()
+		{
+			return ability;
+		}
 
-			boolean isPattern = false;
-			String nameRoot = null;
-			if (subName != null)
-			{
-				final int percIdx = subName.indexOf('%');
-
-				if (percIdx > -1)
-				{
-					isPattern = true;
-					nameRoot = subName.substring(0, percIdx);
-				}
-				else if (subName.length() != 0)
-				{
-					nameRoot = subName;
-				}
-			}
-
-			final List<String> availableList = new ArrayList<String>();
-			final List<?> tempAvailList = new ArrayList<Object>();
-			final List<?> tempSelList = new ArrayList<Object>();
-			ChooserUtilities.modChoices(pcability, tempAvailList, tempSelList,
-					false, aPC, true, AbilityCategory.FEAT);
-			// Mod choices may have sent us back weaponprofs, abilities or
-			// strings,
-			// so we have to do a conversion here
-			for (Object o : tempAvailList)
-			{
-				String choice = o.toString();
-				if ("NOCHOICE".equals(choice))
-				{
-					availableList.add("");
-				}
-				else
-				{
-					availableList.add(choice);
-				}
-			}
-
-			// Remove any that don't match
-
-			if (nameRoot != null && nameRoot.length() != 0)
-			{
-				for (int n = availableList.size() - 1; n >= 0; --n)
-				{
-					final String aString = availableList.get(n);
-
-					if (!aString.startsWith(nameRoot))
-					{
-						availableList.remove(n);
-					}
-				}
-
-				// Example: ADD:FEAT(Skill Focus(Craft (Basketweaving))) If you
-				// have no ranks in Craft (Basketweaving), the available list
-				// will
-				// be empty
-				//
-				// Make sure that the specified feat is available, even though
-				// it
-				// does not meet the prerequisite
-
-				if (isPattern && !availableList.isEmpty())
-				{
-					availableList.add(nameRoot);
-				}
-			}
-
-			List<AbilitySelection> returnList = new ArrayList<AbilitySelection>(
-					availableList.size());
-			for (String s : availableList)
-			{
-				returnList.add(new AbilitySelection(pcability, s));
-			}
-			return returnList;
+		public String getChoice()
+		{
+			return choice;
 		}
 
 	}
