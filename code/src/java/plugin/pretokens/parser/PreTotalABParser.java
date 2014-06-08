651a6bdc2fd4f80e5c03bdca5d7bd367eaf959ba
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2014-06-08 02:53:34 +0000

    Implement: PRETOTALAB
    Issue#: NEWTAG-451
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@24066 ef53c05b-3124-4d59-ae7d-fe3273f42be7

---
+++ code/src/java/plugin/pretokens/parser/PreTotalABParser.java
@@ -0,0 +1,91 @@
+/*
+ * derived from PreAttackParser.java
+ *
+ * Copyright 2003 (C) Chris Ward <frugal@purplewombat.co.uk>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.       See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * Created on 17-Dec-2003
+ *
+ * Current Ver: $Revision: 18181 $
+ *
+ * Last Editor: $Author: javydreamercsw $
+ *
+ * Last Edited: $Date: 2012-11-06 14:50:30 -0500 (Tue, 06 Nov 2012) $
+ *
+ */
+package plugin.pretokens.parser;
+
+import pcgen.core.prereq.Prerequisite;
+import pcgen.persistence.PersistenceLayerException;
+import pcgen.persistence.lst.prereq.AbstractPrerequisiteParser;
+import pcgen.persistence.lst.prereq.PrerequisiteParserInterface;
+import pcgen.util.Logging;
+
+/**
+ * A prerequisite parser class that handles the parsing of pre attack tokens.
+ *
+ */
+public class PreTotalABParser extends AbstractPrerequisiteParser implements
+		PrerequisiteParserInterface
+{
+	/**
+	 * Get the type of prerequisite handled by this token.
+	 * @return the type of prerequisite handled by this token.
+	 */
+    @Override
+	public String[] kindsHandled()
+	{
+		return new String[]{"TOTALAB"};
+	}
+
+	/**
+	 * Parse the pre req list
+	 *
+	 * @param kind The kind of the prerequisite (less the "PRE" prefix)
+	 * @param formula The body of the prerequisite.
+	 * @param invertResult Whether the prerequisite should invert the result.
+	 * @param overrideQualify
+	 *           if set true, this prerequisite will be enforced in spite
+	 *           of any "QUALIFY" tag that may be present.
+	 * @return PreReq
+	 * @throws PersistenceLayerException
+	 */
+	@Override
+	public Prerequisite parse(String kind,
+	                          String formula,
+	                          boolean invertResult,
+	                          boolean overrideQualify) throws PersistenceLayerException
+	{
+		Prerequisite prereq = super.parse(kind, formula, invertResult, overrideQualify);
+
+		try
+		{
+			prereq.setOperand(Integer.toString(Integer.parseInt(formula)));
+		}
+		catch (NumberFormatException nfe)
+		{
+			Logging.errorPrint("Badly formed PRETOTALAB attribute: '" + formula
+				+ "' assuming '1'");
+			prereq.setOperand("1");
+		}
+
+		if (invertResult)
+		{
+			prereq.setOperator(prereq.getOperator().invert());
+		}
+		return prereq;
+	}
+}
---
+++ code/src/java/plugin/pretokens/test/PreTotalABTester.java
@@ -0,0 +1,89 @@
+/*
+ * derived from PreAttack.java
+ * Copyright 2001 (C) Bryan McRoberts <merton_monk@yahoo.com>
+ * Copyright 2003 (C) Chris Ward <frugal@purplewombat.co.uk>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	   See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * Created on November 28, 2003
+ *
+ * Current Ver: $Revision: 18181 $
+ * Last Editor: $Author: javydreamercsw $
+ * Last Edited: $Date: 2012-11-06 14:50:30 -0500 (Tue, 06 Nov 2012) $
+ *
+ */
+package plugin.pretokens.test;
+
+import pcgen.cdom.base.CDOMObject;
+import pcgen.core.PlayerCharacter;
+import pcgen.core.prereq.AbstractPrerequisiteTest;
+import pcgen.core.prereq.Prerequisite;
+import pcgen.core.prereq.PrerequisiteException;
+import pcgen.core.prereq.PrerequisiteTest;
+import pcgen.system.LanguageBundle;
+
+public class PreTotalABTester extends AbstractPrerequisiteTest implements PrerequisiteTest
+{
+
+	/* (non-Javadoc)
+	 * @see pcgen.core.prereq.PrerequisiteTest#passes(pcgen.core.PlayerCharacter)
+	 */
+	@Override
+	public int passes(
+		final Prerequisite prereq,
+		final PlayerCharacter character,
+		CDOMObject source)
+		throws PrerequisiteException
+	{
+		int runningTotal;
+		int att = character.baseAttackBonus();
+		att += character.getTotalBonusTo("COMBAT", "EPICAB");
+
+		try
+		{
+			final int anInt = Integer.parseInt(prereq.getOperand());
+			runningTotal = prereq.getOperator().compare(att, anInt);
+		}
+		catch (NumberFormatException exc)
+		{
+			throw new PrerequisiteException(LanguageBundle.getFormattedString(
+				"PreAttack.error.badly_formed_attribute", prereq.getOperand())); //$NON-NLS-1$
+		}
+
+		return countedTotal(prereq, runningTotal);
+	}
+
+	/**
+	 * Get the type of prerequisite handled by this token.
+	 * @return the type of prerequisite handled by this token.
+	 */
+    @Override
+	public String kindHandled()
+	{
+		return "TOTALAB"; //$NON-NLS-1$
+	}
+
+	/* (non-Javadoc)
+	 * @see pcgen.core.prereq.PrerequisiteTest#toHtmlString(pcgen.core.prereq.Prerequisite)
+	 */
+	@Override
+	public String toHtmlString(final Prerequisite prereq)
+	{
+		return LanguageBundle
+			.getFormattedString(
+				"PreAttack.toHtml", new Object[]{prereq.getOperator().toDisplayString(), prereq.getOperand()}); //$NON-NLS-1$ //$NON-NLS-2$
+	}
+
+}
