da0e43cebce09f20fbe33fa46b907c1f119798d0
jdempsey <jdempsey@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2014-06-22 04:37:11 +0000

    Create a script to convert old export sheets to FreeMarker Issue#: CODE-2624
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@24268 ef53c05b-3124-4d59-ae7d-fe3273f42be7

---
+++ code/convfreemarker.groovy
@@ -0,0 +1,120 @@
+/**
+ * This script converts a PCGen output sheet to FreeMarker. Note it is expected 
+ * that manual tidy-up will be required afterwards.
+ *
+ * Usage: groovy convfreemarker.groovy sheetname [outputtemplatename]
+ * 
+ * Author: James Dempsey on 22 June 2014
+ */
+/*
+ * Copyright 2014 (C) James Dempsey
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * Created on 22/06/2014 10:55:00 AM
+ *
+ * $Id: aa $
+ */
+
+import groovy.xml.StreamingMarkupBuilder
+import java.util.regex.Matcher
+import groovy.xml.XmlUtil
+
+// Check parameters
+def usage = "Usage: groovy convfreemarker.groovy sheetname [outputtemplatename]"
+if (args.length == 0) {
+	println "Insufficient arguments."
+	println usage
+	System.exit(1)
+}
+if (args.length > 2) {
+	println "Too many arguments."
+	println usage
+	System.exit(1)
+}
+
+// Set up our file names, default output name is inputname with .ftl appended.
+def source = args[0]
+def target = source+'.ftl'
+if (args.length > 1) {
+	target = args[1]
+}
+
+def pcgenExportFile = new File(source)
+
+def freemarkerFile = new File(target)
+freemarkerFile.write('<#ftl encoding="UTF-8" strip_whitespace=true >\r\n')
+
+// Process each line in the inout file, writing the updated line to output
+def linenum = 0;
+pcgenExportFile.eachLine {
+	linenum++
+
+    def line = it
+    def origLine = it
+
+    		
+    // FOR loops - static values
+    // |FOR,%range,0,5,1,0|
+    // becomes <@loop from=0 to=5 ; range , range_has_next >
+    line = line.replaceAll("\\|FOR,\\%([a-zA-Z0-9]+),([0-9]+),([0-9]+),1,0\\|", /<@loop from=$2 to=$3 ; $1 , $1_has_next>/)
+
+    // FOR loops - dynamic values
+    // |FOR,%class,0,COUNT[CLASSES]-1,1,0|
+    // becomes <@loop from=0 to=pcvar('COUNT[CLASSES]-1') ; class , class_has_next >
+    line = line.replaceAll('\\|FOR,\\%([a-zA-Z0-9]+),([0-9]+),([-_+=.,% A-Za-z0-9"()\\[\\]]+),1,0\\|', /<@loop from=$2 to=pcvar('$3') ; $1 , $1_has_next>/)
+    // |FOR,%spelllevelcount,COUNT[SPELLSINBOOK.%class.%spellbook.%level],COUNT[SPELLSINBOOK.%class.%spellbook.%level],1,0|
+    line = line.replaceAll('\\|FOR,\\%([a-zA-Z0-9]+),([-_+=.% A-Za-z0-9"()\\[\\]]+),([-_+=.,% A-Za-z0-9"()\\[\\]]+),1,0\\|', /<@loop from=pcvar('$2') to=pcvar('$3') ; $1 , $1_has_next>/)
+    line = line.replaceAll("\\|ENDFOR\\|", '</@loop>')
+    
+    // If tests
+    // |IIF(%spelllevelcount:0)|
+    line = line.replaceAll("\\|IIF\\(%([A-Za-z0-9]+):([0-9]+)\\)\\|", '<#if ($1 == $2)>')
+    // |IIF(WEAPON.%weap.CATEGORY:BOTH)|
+    line = line.replaceAll("\\|IIF\\(([A-Za-z0-9%.]+):([A-Za-z0-9]+)\\)\\|", '<#if (pcstring("$1") == "$2")>')
+    // |IIF(countdistinct("ABILITIES";"NAME=Turn Undead")>0)|
+    line = line.replaceAll('\\|IIF\\(([A-Za-z0-9;=_ "\\[\\]\\(\\)]+)>([0-9]+)\\)\\|', /<#if (pcvar('$1') > $2)>/)
+    // |IIF(countdistinct("ABILITIES";"NAME=Turn Undead")==0)|
+    line = line.replaceAll('\\|IIF\\(([A-Za-z0-9;=_ "\\[\\]\\(\\)]+)==([0-9]+)\\)\\|', /<#if (pcvar('$1') == $2)>/)
+    line = line.replaceAll("\\|ELSE\\|", "<#else>")
+    line = line.replaceAll("\\|ENDIF\\|", '</#if>')
+    
+    // Filter tags - uggh
+    // |%VAR.RageTimes.GTEQ.1|
+    line = line.replaceAll("\\|%([^\\|]+)\\.GTEQ\\.([0-9]+)\\|", '<#if (pcvar("$1") >= $2) >')
+    // |%BARBARIAN=1|
+    line = line.replaceAll("\\|%([^\\|]+)=([0-9]+)\\|", '<#if (pcvar("$1") >= $2) >')
+    line = line.replaceAll("\\|%\\|", '</#if>')
+    
+    // General tags
+    def replaceStr = Matcher.quoteReplacement('${pcstring(') + "'\$1')}"; 
+    line = line.replaceAll("\\|([_ A-Za-z0-9.%/*\\(\\)]+)\\|", replaceStr)
+   
+    // Loop variables
+    replaceStr = Matcher.quoteReplacement('${') + "\$1}"; 
+    line = line.replaceAll("%([a-zA-Z0-9]+)", replaceStr)
+
+    if (line ==~ ".*\\|.*") {
+    	// If we couldn't conert everything report the line and revert back to the original content
+    	println "Unable to convert line ${linenum}: ${origLine}"
+    	line = origLine
+    }
+    
+	// Output the revised file
+	freemarkerFile.append(line+"\r\n")
+}
+
+println ""
+println "Output new template to ${freemarkerFile}"
