8690126f6d32118452ae1befe861dda6d07330dd
thpr <thpr@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2014-06-29 02:49:15 +0000

    Implement: Add BASESIZE=x Primitive for RACE in CHOOSE
    Issue#: NEWTAG-39
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@24330 ef53c05b-3124-4d59-ae7d-fe3273f42be7

---
+++ code/src/java/plugin/primitive/race/BaseSizeToken.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2014 (C) Thomas Parker <thpr@users.sourceforge.net>
+ * 
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+package plugin.primitive.race;
+
+import java.util.Collection;
+
+import pcgen.base.formula.Formula;
+import pcgen.cdom.base.CDOMReference;
+import pcgen.cdom.base.Converter;
+import pcgen.cdom.base.PrimitiveFilter;
+import pcgen.cdom.enumeration.FormulaKey;
+import pcgen.cdom.enumeration.GroupingState;
+import pcgen.core.PlayerCharacter;
+import pcgen.core.Race;
+import pcgen.core.SizeAdjustment;
+import pcgen.rules.context.LoadContext;
+import pcgen.rules.persistence.token.PrimitiveToken;
+
+public class BaseSizeToken implements PrimitiveToken<Race>,
+		PrimitiveFilter<Race>
+{
+	private static final Class<Race> RACE_CLASS = Race.class;
+	private SizeAdjustment size;
+	private CDOMReference<Race> allRaces;
+
+	@Override
+	public boolean initialize(LoadContext context, Class<Race> cl,
+		String value, String args)
+	{
+		if (args != null)
+		{
+			return false;
+		}
+		size =
+				context.getReferenceContext().getAbbreviatedObject(
+					SizeAdjustment.class, value);
+		allRaces =
+				context.getReferenceContext().getCDOMAllReference(RACE_CLASS);
+		return true;
+	}
+
+	@Override
+	public String getTokenName()
+	{
+		return "BASESIZE";
+	}
+
+	@Override
+	public Class<Race> getReferenceClass()
+	{
+		return RACE_CLASS;
+	}
+
+	@Override
+	public String getLSTformat(boolean useAny)
+	{
+		return getTokenName() + "=" + size.getAbbreviation();
+	}
+
+	@Override
+	public boolean allow(PlayerCharacter pc, Race race)
+	{
+		Formula raceSize = race.get(FormulaKey.SIZE);
+		return size.getAbbreviation().equals(raceSize.toString());
+	}
+
+	@Override
+	public GroupingState getGroupingState()
+	{
+		return GroupingState.ANY;
+	}
+
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (obj == this)
+		{
+			return true;
+		}
+		if (obj instanceof BaseSizeToken)
+		{
+			BaseSizeToken other = (BaseSizeToken) obj;
+			return size.equals(other.size);
+		}
+		return false;
+	}
+
+	@Override
+	public int hashCode()
+	{
+		return size == null ? -7 : size.hashCode();
+	}
+
+	@Override
+	public <R> Collection<R> getCollection(PlayerCharacter pc,
+		Converter<Race, R> c)
+	{
+		return c.convert(allRaces, this);
+	}
+}
---
+++ code/src/utest/plugin/primitive/race/BaseSizeTokenTest.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2010 Tom Parker <thpr@users.sourceforge.net>
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+package plugin.primitive.race;
+
+import java.net.URISyntaxException;
+
+import pcgen.cdom.base.CDOMObject;
+import pcgen.core.Race;
+import pcgen.core.SizeAdjustment;
+import pcgen.persistence.PersistenceLayerException;
+import pcgen.rules.persistence.CDOMLoader;
+import pcgen.rules.persistence.token.CDOMPrimaryToken;
+import pcgen.rules.persistence.token.CDOMSecondaryToken;
+import plugin.lsttokens.ChooseLst;
+import plugin.lsttokens.choose.RaceToken;
+import plugin.lsttokens.testsupport.AbstractPrimitiveTokenTestCase;
+import plugin.lsttokens.testsupport.CDOMTokenLoader;
+import plugin.lsttokens.testsupport.TokenRegistration;
+
+public class BaseSizeTokenTest extends
+		AbstractPrimitiveTokenTestCase<CDOMObject, Race>
+{
+
+	static ChooseLst token = new ChooseLst();
+	static RaceToken subtoken = new RaceToken();
+	static CDOMTokenLoader<CDOMObject> loader = new CDOMTokenLoader<CDOMObject>(
+			CDOMObject.class);
+
+	private static final BaseSizeToken BASESIZE_TOKEN = new BaseSizeToken();
+
+	public BaseSizeTokenTest()
+	{
+		super("BASESIZE", "S");
+	}
+
+	@Override
+	public void setUp() throws PersistenceLayerException, URISyntaxException
+	{
+		super.setUp();
+		TokenRegistration.register(BASESIZE_TOKEN);
+		SizeAdjustment small =
+				primaryContext.getReferenceContext().constructNowIfNecessary(
+					SizeAdjustment.class, "Small");
+		primaryContext.getReferenceContext().registerAbbreviation(small, "S");
+		small =
+				secondaryContext.getReferenceContext().constructNowIfNecessary(
+					SizeAdjustment.class, "Small");
+		secondaryContext.getReferenceContext().registerAbbreviation(small, "S");
+	}
+
+	@Override
+	public CDOMSecondaryToken<?> getSubToken()
+	{
+		return subtoken;
+	}
+
+	@Override
+	public Class<Race> getTargetClass()
+	{
+		return Race.class;
+	}
+
+	@Override
+	public Class<Race> getCDOMClass()
+	{
+		return Race.class;
+	}
+
+	@Override
+	public CDOMLoader<CDOMObject> getLoader()
+	{
+		return loader;
+	}
+
+	@Override
+	public CDOMPrimaryToken<CDOMObject> getToken()
+	{
+		return token;
+	}
+
+}
