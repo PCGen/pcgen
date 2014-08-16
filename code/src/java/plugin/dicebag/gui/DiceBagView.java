94ea70e27afe0c320387129a83df7642f6d5fbf2
jdempsey <jdempsey@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2014-08-16 22:15:56 +0000

    Missing license information on many source files - Add LGPL header to files Ross M. Lodge has contributed Issue#: CODE-1849
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@24899 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- code/src/java/plugin/dicebag/gui/DiceBagView.java
+++ code/src/java/plugin/dicebag/gui/DiceBagView.java
@@ -1,29 +1,19 @@
 /*
- *  $Id$
+ * Copyright 2003 (C) Ross M. Lodge
  *
- *  gmgen.plugin.dicebag - DESCRIPTION OF PACKAGE
- *  Copyright (C) 2003 RossLodge
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *
- *  The author of this program grants you the ability to use this code
- *  in conjunction with code that is covered under the Open Gaming License
- *
- *  DiceBagView.java
- *
- *  Created on Oct 17, 2003, 2:54:09 PM
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 package plugin.dicebag.gui;
 
