eb778a8c9e83c022ba2e903999f859e1754cc4bd
jdempsey <jdempsey@ef53c05b-3124-4d59-ae7d-fe3273f42be7>
2014-08-16 01:57:34 +0000

    Missing license information on many source files - Add LGPL header to files released by Devon Jones to the PCGen project from GMGen. License was confirmed in email to PCGen BoD group by Devon. Issue#: CODE-1849
    git-svn-id: svn+ssh://svn.code.sf.net/p/pcgen/code/Trunk@24888 ef53c05b-3124-4d59-ae7d-fe3273f42be7

--- code/src/java/pcgen/core/doomsdaybook/HyphenRule.java
+++ code/src/java/pcgen/core/doomsdaybook/HyphenRule.java
@@ -1,9 +1,23 @@
 /*
- * SpaceRule.java
+ * Copyright 2003 (C) Devon Jones
  *
- * Created on April 25, 2003, 2:20 PM
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * $Id$
  */
-package pcgen.core.doomsdaybook;
+ package pcgen.core.doomsdaybook;
 
 import java.util.ArrayList;
 
--- code/src/java/plugin/pcgtracker/gui/PCGTrackerView.java
+++ code/src/java/plugin/pcgtracker/gui/PCGTrackerView.java
@@ -1,9 +1,23 @@
 /*
- * PCGTrackerView.java
+ * Copyright 2003 (C) Devon Jones
  *
- * Created on June 25, 2003, 5:08 PM
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * $Id$
  */
-package plugin.pcgtracker.gui;
+ package plugin.pcgtracker.gui;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
