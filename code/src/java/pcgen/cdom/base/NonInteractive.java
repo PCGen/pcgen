158d0208c303ad2ab47875a653f1047cc9297f1d
Tom Parker <thpr@yahoo.com>
2015-01-24 23:11:18 -0500

    Don't not Fix the Javadoc (or something)

--- code/src/java/pcgen/cdom/base/NonInteractive.java
+++ code/src/java/pcgen/cdom/base/NonInteractive.java
@@ -1,9 +1,9 @@
 package pcgen.cdom.base;
 
 /**
- * This interface is a tag used to identify items that are not
- * "Not Interactive", thus should not be able to grant other items in an
- * interactive fashion (no ADD)
+ * This interface is a tag used to identify items that are "Not Interactive",
+ * thus should not be able to grant other items in an interactive fashion (no
+ * ADD)
  */
 public interface NonInteractive
 {
