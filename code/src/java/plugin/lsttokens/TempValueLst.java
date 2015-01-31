33ccc6b849250ac5256b40ebf5b0ab9f2ddc3cb6
Tom Parker <thpr@yahoo.com>
2015-01-30 22:21:25 -0500

    NEWTAG-478 Docs

--- code/src/java/plugin/lsttokens/TempValueLst.java
+++ code/src/java/plugin/lsttokens/TempValueLst.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2015 (C) Thomas Parker <thpr@users.sourceforge.net>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
 package plugin.lsttokens;
 
 import java.util.StringTokenizer;
@@ -10,6 +27,10 @@ import pcgen.rules.persistence.token.AbstractTokenWithSeparator;
 import pcgen.rules.persistence.token.CDOMPrimaryToken;
 import pcgen.rules.persistence.token.ParseResult;
 
+/**
+ * TempValueLst handles the TEMPVALUE token, which is used to select the value
+ * for a TEMPBONUS when it is using %LIST
+ */
 public class TempValueLst extends AbstractTokenWithSeparator<CDOMObject>
 		implements CDOMPrimaryToken<CDOMObject>
 {
