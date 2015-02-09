03ad21fb445b00935696dee95bbe015dffd7a797
Tom Parker <thpr@yahoo.com>
2015-02-08 20:34:26 -0500

    NEWTAG-477 -Rebase fixes -Doc update

--- code/src/java/plugin/primitive/pobject/AbilityToken.java
+++ code/src/java/plugin/primitive/pobject/AbilityToken.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010 (C) Thomas Parker <thpr@users.sourceforge.net>
+ * Copyright 2010-15 (C) Thomas Parker <thpr@users.sourceforge.net>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
