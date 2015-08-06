9f75514dfba05e55526551254b0983e7e01ba83c
Tom Parker <thpr@yahoo.com>
2015-08-05 22:10:44 -0400

    Set up Mode Model Capabilities Set up UnitSet Model Capabilities

---
+++ code/src/java/pcgen/output/factory/UnitSetModelFactory.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2015 (C) Tom Parker <thpr@users.sourceforge.net>
+ * 
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+package pcgen.output.factory;
+
+import pcgen.core.GameMode;
+import pcgen.output.base.ModeModelFactory;
+import pcgen.output.model.UnitSetModel;
+
+/**
+ * An UnitSetModelFactory is a ModeModelFactory that produces an UnitSetModel
+ * for the current Game Mode
+ */
+public class UnitSetModelFactory implements ModeModelFactory
+{
+	/**
+	 * @see pcgen.output.base.ModeModelFactory#generate()
+	 */
+	@Override
+	public UnitSetModel generate(GameMode mode)
+	{
+		return new UnitSetModel(mode.getUnitSet());
+	}
+}
