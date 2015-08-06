c59dd202fe5bb5004e51fbccc744c0b2e106a6dc
Tom Parker <thpr@yahoo.com>
2015-08-06 12:18:02 -0400

    NEWTAG-487 Deprecate BIRTHDAY output token

--- code/src/java/plugin/exporttokens/deprecated/BirthdayToken.java
+++ code/src/java/plugin/exporttokens/deprecated/BirthdayToken.java
@@ -23,7 +23,7 @@
  * Last Edited: $Date$
  *
  */
-package plugin.exporttokens;
+package plugin.exporttokens.deprecated;
 
 import pcgen.core.display.CharacterDisplay;
 import pcgen.io.ExportHandler;
