c439ca834e9174025a8151d248d71e7d83d23eaa
Tom Parker <thpr@yahoo.com>
2015-08-05 23:31:23 -0400

    Undo mis-formatting of comments

--- code/src/java/plugin/lsttokens/SortKeyLst.java
+++ code/src/java/plugin/lsttokens/SortKeyLst.java
@@ -1,15 +1,24 @@
 /**
- * SortKeyLst.java Copyright 2010 (C) James Dempsey This library is free
- * software; you can redistribute it and/or modify it under the terms of the GNU
- * Lesser General Public License as published by the Free Software Foundation;
- * either version 2.1 of the License, or (at your option) any later version.
- * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details. You should have received a copy of the GNU Lesser General Public
+ * SortKeyLst.java
+ * Copyright 2010 (C) James Dempsey
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- * Created on 28/05/2010 10:02:14 PM $Id$
+ *
+ * Created on 28/05/2010 10:02:14 PM
+ *
+ * $Id$
  */
 package plugin.lsttokens;
 
@@ -30,9 +39,12 @@ import pcgen.rules.persistence.token.PostValidationToken;
 import pcgen.util.Logging;
 
 /**
- * The Class <code>SortKeyLst</code> implements the global SORTKEY tag, which
- * allows items to be sorted in a custom manner. <br/>
- * Last Editor: $Author$ Last Edited: $Date$
+ * The Class <code>SortKeyLst</code> implements the global SORTKEY tag, which 
+ * allows items to be sorted in a custom manner.
+ *
+ * <br/>
+ * Last Editor: $Author$
+ * Last Edited: $Date$
  * 
  * @author James Dempsey <jdempsey@users.sourceforge.net>
  * @version $Revision$
