a951df8dbdac3fddb99956f32bdb121db5e78d73
Tom Parker <thpr@yahoo.com>
2015-08-15 15:16:06 -0400

    Tests and minor bug fix

---
+++ code/src/utest/plugin/primitive/skill/UseUntrainedTokenTest.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2010 Tom Parker <thpr@users.sourceforge.net>
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+package plugin.primitive.skill;
+
+import java.net.URISyntaxException;
+
+import pcgen.cdom.base.CDOMObject;
+import pcgen.core.Race;
+import pcgen.core.Skill;
+import pcgen.persistence.PersistenceLayerException;
+import pcgen.rules.persistence.CDOMLoader;
+import pcgen.rules.persistence.token.CDOMPrimaryToken;
+import pcgen.rules.persistence.token.CDOMSecondaryToken;
+import plugin.lsttokens.ChooseLst;
+import plugin.lsttokens.choose.SkillToken;
+import plugin.lsttokens.testsupport.AbstractPrimitiveTokenTestCase;
+import plugin.lsttokens.testsupport.CDOMTokenLoader;
+import plugin.lsttokens.testsupport.TokenRegistration;
+
+public class UseUntrainedTokenTest extends
+		AbstractPrimitiveTokenTestCase<CDOMObject, Skill>
+{
+	static ChooseLst token = new ChooseLst();
+	static SkillToken subtoken = new SkillToken();
+	static CDOMTokenLoader<CDOMObject> loader = new CDOMTokenLoader<CDOMObject>();
+
+	private static final UseUntrainedToken USEUNTRAINED_TOKEN = new UseUntrainedToken();
+
+	public UseUntrainedTokenTest()
+	{
+		super("USEUNTRAINED", null);
+	}
+
+	@Override
+	public void setUp() throws PersistenceLayerException, URISyntaxException
+	{
+		super.setUp();
+		construct(primaryContext, "Skill1");
+		construct(secondaryContext, "Skill1");
+		TokenRegistration.register(USEUNTRAINED_TOKEN);
+	}
+
+	@Override
+	public CDOMSecondaryToken<?> getSubToken()
+	{
+		return subtoken;
+	}
+
+	@Override
+	public Class<Skill> getTargetClass()
+	{
+		return Skill.class;
+	}
+
+	@Override
+	public Class<Race> getCDOMClass()
+	{
+		return Race.class;
+	}
+
+	@Override
+	public CDOMLoader<CDOMObject> getLoader()
+	{
+		return loader;
+	}
+
+	@Override
+	public CDOMPrimaryToken<CDOMObject> getToken()
+	{
+		return token;
+	}
+}
