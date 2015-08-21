6361a8b9606b909276a3a70ce9834a1e035e2066
Stefan Radermacher <stefan@zaister.de>
2015-08-21 22:40:21 +0200

    NEWTAG-560
    Add javadoc and license header; remove unused import.

--- code/src/java/plugin/lsttokens/equipment/VisibleToken.java
+++ code/src/java/plugin/lsttokens/equipment/VisibleToken.java
@@ -1,6 +1,22 @@
+/*
+ * Copyright 2015 (C) Stefan Radermacher
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
 package plugin.lsttokens.equipment;
 
-import pcgen.cdom.base.Constants;
 import pcgen.cdom.enumeration.ObjectKey;
 import pcgen.core.Equipment;
 import pcgen.rules.context.LoadContext;
@@ -10,10 +26,15 @@ import pcgen.rules.persistence.token.ComplexParseResult;
 import pcgen.rules.persistence.token.ParseResult;
 import pcgen.util.enumeration.Visibility;
 
+/**
+ * VisibleToken handles the processing of the VISIBLE tag in the
+ * definition of an Equipment object.
+ * 
+ * @author Stefan Radermacher
+ */
 public class VisibleToken extends AbstractNonEmptyToken<Equipment>
 	implements CDOMPrimaryToken<Equipment>
 {
-
 	@Override
 	public String getTokenName()
 	{
@@ -94,6 +115,4 @@ public class VisibleToken extends AbstractNonEmptyToken<Equipment>
 	{
 		return Equipment.class;
 	}
-
 }
-
