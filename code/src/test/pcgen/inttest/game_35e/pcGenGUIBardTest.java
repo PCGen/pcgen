adfb4926e8eecb4c15d2ac11c8999081ca25b86e
LegacyKing <drew0500@yahoo.com>
2015-09-07 22:27:00 -0700

    CODE-2898 Update active unit tests to use FTL Engine, and add missing License Headers

--- code/src/test/pcgen/inttest/game_35e/pcGenGUIBardTest.java
+++ code/src/test/pcgen/inttest/game_35e/pcGenGUIBardTest.java
@@ -1,6 +1,28 @@
+/*
+ * pcGenGUIBardTest.java
+ * Copyright 2015 (C) Andrew Maitland <amaitland@users.sourceforge.net>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * Created on 09/07/2015
+ *
+ * $Id$
+ */
 package pcgen.inttest.game_35e;
 
-import pcgen.inttest.pcGenGUITestCase;
+import pcgen.inttest.PcgenFtlTestCase;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
@@ -9,7 +31,7 @@ import junit.framework.TestSuite;
  * abilities that are conditionally granted via variables and skill ranks.
  */
 @SuppressWarnings("nls")
-public class pcGenGUIBardTest extends pcGenGUITestCase
+public class pcGenGUIBardTest extends PcgenFtlTestCase
 {
 
 	/**
@@ -17,7 +39,7 @@ public class pcGenGUIBardTest extends pcGenGUITestCase
 	 */
 	public pcGenGUIBardTest()
 	{
-		// Empty Constructor
+		super("35e_bard");
 	}
 
 	/**
--- code/src/test/pcgen/inttest/game_35e/pcGenGUIQPsiCrystalTest.java
+++ code/src/test/pcgen/inttest/game_35e/pcGenGUIQPsiCrystalTest.java
@@ -1,6 +1,28 @@
+/*
+ * pcGenGUIQPsiCrystalTest.java
+ * Copyright 2015 (C) Andrew Maitland <amaitland@users.sourceforge.net>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * Created on 09/07/2015
+ *
+ * $Id$
+ */
 package pcgen.inttest.game_35e;
 
-import pcgen.inttest.pcGenGUITestCase;
+import pcgen.inttest.PcgenFtlTestCase;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
@@ -9,7 +31,7 @@ import junit.framework.TestSuite;
  * See the PCG file for details
  */
 @SuppressWarnings("nls")
-public class pcGenGUIQPsiCrystalTest extends pcGenGUITestCase
+public class pcGenGUIQPsiCrystalTest extends PcgenFtlTestCase
 {
 
 	/**
@@ -17,7 +39,7 @@ public class pcGenGUIQPsiCrystalTest extends pcGenGUITestCase
 	 */
 	public pcGenGUIQPsiCrystalTest()
 	{
-		// Empty Constructor
+		super("35e_psicrystal");
 	}
 
 	/**
