85d6ed69a51591593a2c1a702b26d06ee35901d5
LegacyKing <drew0500@yahoo.com>
2015-12-08 11:43:47 -0800

    add proper spacing

--- installers/win-installer/gendatalist.pl
+++ installers/win-installer/gendatalist.pl
@@ -117,7 +117,7 @@ open SCRIPT, ">$script_file " or die "can't open $script_file  $!";
 foreach $dirname (@basedirlist)
 {
 	# Skip some folders we don't wish to offer
-	if ($dirname eq 'homebrew' ||$dirname eq 'readme.md' || $dirname eq 'zen_test' || $dirname eq 'customsources')
+	if ($dirname eq 'homebrew' || $dirname eq 'readme.md' || $dirname eq 'zen_test' || $dirname eq 'customsources')
 	{
 		next;
 	}
@@ -136,7 +136,7 @@ foreach $dirname (@basedirlist)
 NAME:	foreach $filename (@nondots)
 	{
 		# Skip some folders we don't wish to distribute
-		if ($filename eq 'homebrew' ||$filename eq 'readme.md' || $filename eq 'pcgen_test_advanced' || $filename =~ /zen_test.*/)
+		if ($filename eq 'homebrew' || $filename eq 'readme.md' || $filename eq 'pcgen_test_advanced' || $filename =~ /zen_test.*/)
 		{
 			next NAME;
 		}
