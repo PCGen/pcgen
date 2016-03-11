64684259bb517afc71976565a01d8abcc7c90d6e
thpr <thpr@yahoo.com>
2016-03-11 07:40:05 -0500

    Initial

---
+++ code/src/java/pcgen/base/format/dice/DiceFormat.java
@@ -0,0 +1,75 @@
+package pcgen.base.format.dice;
+
+import pcgen.base.util.BasicIndirect;
+import pcgen.base.util.BasicObjectContainer;
+import pcgen.base.util.FormatManager;
+import pcgen.base.util.Indirect;
+import pcgen.base.util.ObjectContainer;
+
+public class DiceFormat implements FormatManager<Dice>
+{
+
+	@Override
+	public Dice convert(String inputStr)
+	{
+		int dLoc = inputStr.indexOf("d");
+		if (dLoc == -1)
+		{
+			throw new IllegalArgumentException(
+				inputStr + " is not valid for Dice");
+		}
+		int quantity;
+		if (dLoc == 0)
+		{
+			quantity = 1;
+		}
+		else
+		{
+			quantity = Integer.parseInt(inputStr.substring(0, dLoc));
+		}
+		int sides = Integer.parseInt(inputStr.substring(dLoc + 1));
+		Die d = new Die(sides);
+		return new Dice(quantity, d);
+	}
+
+	@Override
+	public Indirect<Dice> convertIndirect(String inputStr)
+	{
+		return new BasicIndirect<>(this, convert(inputStr));
+	}
+
+	@Override
+	public String unconvert(Dice d)
+	{
+		StringBuilder sb = new StringBuilder();
+		sb.append(d.getQuantity());
+		sb.append('d');
+		sb.append(d.getDie().getSides());
+		return sb.toString();
+	}
+
+	@Override
+	public Class<Dice> getManagedClass()
+	{
+		return Dice.class;
+	}
+
+	@Override
+	public String getIdentifierType()
+	{
+		return "DICE";
+	}
+
+	@Override
+	public ObjectContainer<Dice> convertObjectContainer(String inputStr)
+	{
+		return new BasicObjectContainer<>(this, convert(inputStr));
+	}
+
+	@Override
+	public FormatManager<?> getComponentManager()
+	{
+		return null;
+	}
+
+}
