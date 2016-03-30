f336c1533ccdb010714f502660d7db58e8f1179b
Tom Parker <thpr@yahoo.com>
2016-03-29 22:51:13 -0400

    Minor Cleanup

--- code/src/java/plugin/lsttokens/gamemode/codecontrol/AltersACToken.java
+++ code/src/java/plugin/lsttokens/gamemode/codecontrol/AltersACToken.java
@@ -21,7 +21,7 @@ import pcgen.cdom.inst.CodeControl;
 import pcgen.cdom.util.CControl;
 import pcgen.rules.persistence.token.AbstractStringStoringToken;
 
-public class AltersAC extends AbstractStringStoringToken<CodeControl>
+public class AltersACToken extends AbstractStringStoringToken<CodeControl>
 {
 	@Override
 	public String getTokenName()
