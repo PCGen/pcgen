ecd8e47ec57938a3e5201c1e3807da0cdffdff62
Tom Parker <thpr@yahoo.com>
2016-09-05 22:05:53 -0400

    updates

--- code/src/java/pcgen/base/format/dice/Dice.java
+++ code/src/java/pcgen/base/format/dice/Dice.java
@@ -1,4 +1,23 @@
+/*
+ * Copyright (c) 2016 Tom Parker <thpr@users.sourceforge.net>
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
 package pcgen.base.format.dice;
+
+import java.util.Objects;
 
 public class Dice
 {
@@ -12,12 +31,8 @@ public class Dice
 			throw new IllegalArgumentException(
 				"Quantity cannot be < 0 in Dice");
 		}
-		if (die == null)
-		{
-			throw new IllegalArgumentException("Die cannot be null in Dice");
-		}
 		this.quantity = quantity;
-		this.die = die;
+		this.die = Objects.requireNonNull(die);
 	}
 
 	public Die getDie()
