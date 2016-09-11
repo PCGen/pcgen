8920be8bd3eb8dbb65ea74ec4763f488e9e58359
Eitan Adler <lists@eitanadler.com>
2016-09-10 19:13:43 -0700

    add contracts

--- code/src/test/pcgen/PCGenTestCase.java
+++ code/src/test/pcgen/PCGenTestCase.java
@@ -19,6 +19,7 @@ import pcgen.util.testchecker.CompareGreaterThan;
 import pcgen.util.testchecker.CompareNull;
 
 import junit.framework.TestCase;
+import org.jetbrains.annotations.Contract;
 
 /**
  * Test case base for PCGen.  This addresses a common bug with JUnit whereby
@@ -151,41 +152,49 @@ public abstract class PCGenTestCase extends TestCase
 		}
 	}
 
+	@Contract("_ -> !null")
 	public static CompareEqualString strEq(final String s)
 	{
 		return new CompareEqualString(s);
 	}
 
+	@Contract("_ -> !null")
 	public static CompareEqualObject eq(final Object operand)
 	{
 		return new CompareEqualObject(operand);
 	}
 
+	@Contract("_ -> !null")
 	public static CompareEqualBoolean eq(final boolean bo)
 	{
 		return new CompareEqualBoolean(bo);
 	}
 
+	@Contract("_ -> !null")
 	public static CompareEqualInt eq(final int operand)
 	{
 		return new CompareEqualInt(operand);
 	}
 
+	@Contract("_, _ -> !null")
 	public static CompareDeadband eq(final double operand, final double error)
 	{
 		return new CompareDeadband(operand, error);
 	}
 
+	@Contract(" -> !null")
 	protected static CompareNull eqnull()
 	{
 		return new CompareNull();
 	}
 
+	@Contract("_ -> !null")
 	public static BoolNot not(final TestChecker c)
 	{
 		return new BoolNot(c);
 	}
 
+	@Contract("_ -> !null")
 	protected static CompareGreaterThan gt(Comparable operand)
 	{
 		return new CompareGreaterThan(operand);
