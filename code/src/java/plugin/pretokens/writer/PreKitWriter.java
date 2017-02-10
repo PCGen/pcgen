2bfb6872a3c8f0dbec075b329b1ce650c0bcfab9
Eitan Adler <lists@eitanadler.com>
2017-02-09 16:52:49 -0800

    remove @author, updated on, created on, and other useless remarks

--- code/src/java/plugin/pretokens/writer/PreKitWriter.java
+++ code/src/java/plugin/pretokens/writer/PreKitWriter.java
@@ -16,9 +16,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
- * Created on 27/10/2013
  *
- * $Id$
  */
 package plugin.pretokens.writer;
 
@@ -34,7 +32,6 @@ import pcgen.persistence.lst.output.prereq.PrerequisiteWriterInterface;
 /**
  * Writes PREKIT token
  * 
- * @author James Dempsey &lt;jdempsey@users.sourceforge.net&gt;
  */
 public class PreKitWriter extends AbstractPrerequisiteWriter implements
 		PrerequisiteWriterInterface
