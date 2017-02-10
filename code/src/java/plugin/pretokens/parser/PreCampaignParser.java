d595dfa5542fe9cc979620ae14e97c3cfbb17504
Eitan Adler <lists@eitanadler.com>
2017-02-09 16:52:47 -0800

    remove @author, updated on, created on, and other useless remarks

--- code/src/java/plugin/pretokens/parser/PreCampaignParser.java
+++ code/src/java/plugin/pretokens/parser/PreCampaignParser.java
@@ -16,9 +16,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
- * Created on 12/07/2008 12:03:09
  *
- * $Id: $
  */
 package plugin.pretokens.parser;
 
--- code/src/java/plugin/pretokens/parser/PreKitParser.java
+++ code/src/java/plugin/pretokens/parser/PreKitParser.java
@@ -16,9 +16,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
- * Created on 27/10/2013
  *
- * $Id$
  */
 package plugin.pretokens.parser;
 
@@ -28,7 +26,6 @@ import pcgen.persistence.lst.prereq.PrerequisiteParserInterface;
 /**
  * A prerequisite parser class that handles the parsing of pre kit tokens.
  *
- * @author James Dempsey &lt;jdempsey@users.sourceforge.net&gt;
  */
 public class PreKitParser extends AbstractPrerequisiteListParser implements
 		PrerequisiteParserInterface
