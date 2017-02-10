f86824f789cb01ab23b9488af4727c73a5be935b
Eitan Adler <lists@eitanadler.com>
2017-02-09 16:52:46 -0800

    remove @author, updated on, created on, and other useless remarks

--- code/src/java/plugin/lsttokens/kit/spells/CountToken.java
+++ code/src/java/plugin/lsttokens/kit/spells/CountToken.java
@@ -16,9 +16,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
- * Created on March 6, 2006
  *
- * Current Ver: $Revision$
  */
 
 package plugin.lsttokens.kit.spells;
