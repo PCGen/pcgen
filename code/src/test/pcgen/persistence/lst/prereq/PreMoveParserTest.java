0ec2f2b797c87f1a64014b073d01c588067b2e7d
Eitan Adler <lists@eitanadler.com>
2017-02-09 16:52:52 -0800

    remove @author, updated on, created on, and other useless remarks

--- code/src/test/pcgen/persistence/lst/prereq/PreMoveParserTest.java
+++ code/src/test/pcgen/persistence/lst/prereq/PreMoveParserTest.java
@@ -1,5 +1,4 @@
 /*
- * Created on 22-Dec-2003
  * 
  * To change the template for this generated file go to Window - Preferences - Java - Code Generation - Code and
  * Comments
@@ -15,7 +14,6 @@ import pcgen.core.prereq.Prerequisite;
 import plugin.pretokens.parser.PreMoveParser;
 
 /**
- * @author Valued Customer
  * 
  * To change the template for this generated type comment go to Window - Preferences - Java - Code Generation - Code
  * and Comments
