23c38783b00ba1c564d84a038ed7e68f7eff313a
Eitan Adler <lists@eitanadler.com>
2017-02-09 16:52:37 -0800

    remove @author, updated on, created on, and other useless remarks

--- code/src/java/pcgen/gui2/util/SimpleTextIcon.java
+++ code/src/java/pcgen/gui2/util/SimpleTextIcon.java
@@ -16,7 +16,6 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  * 
- * Created on Jun 17, 2008, 4:24:42 PM
  */
 package pcgen.gui2.util;
 
@@ -35,7 +34,6 @@ import javax.swing.Icon;
  * <br>
  * <br>
  * *Note* The SimpleTextIcon does not treat html text specially so it will be displayed verbatim.
- * @author Connor Petty &lt;cpmeister@users.sourceforge.net&gt;
  */
 public class SimpleTextIcon implements Icon
 {
--- code/src/java/pcgen/gui2/util/table/SortableTableModel.java
+++ code/src/java/pcgen/gui2/util/table/SortableTableModel.java
@@ -16,7 +16,6 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  * 
- * Created on Feb 22, 2008, 3:26:34 PM
  */
 
 package pcgen.gui2.util.table;
@@ -26,7 +25,6 @@ import pcgen.gui2.util.SortableModel;
 
 /**
  *
- * @author Connor Petty &lt;mistercpp2000@gmail.com&gt;
  */
 public interface SortableTableModel extends TableModel, SortableModel
 {
