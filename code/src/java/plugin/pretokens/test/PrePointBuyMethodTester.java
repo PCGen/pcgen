4df60b59dfa8d842a4ec2920b6da412fe9c61bf3
Eitan Adler <lists@eitanadler.com>
2017-02-09 16:52:48 -0800

    remove @author, updated on, created on, and other useless remarks

--- code/src/java/plugin/pretokens/test/PrePointBuyMethodTester.java
+++ code/src/java/plugin/pretokens/test/PrePointBuyMethodTester.java
@@ -16,9 +16,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
- * Created on September 16, 2005
  *
- * $Id$
  */
 package plugin.pretokens.test;
 
@@ -34,7 +32,6 @@ import pcgen.system.LanguageBundle;
 /**
  * {@code PrePointBuyMethod}.
  *
- * @author Greg Bingleman &lt;byngl@hotmail.com&gt;
  */
 public class PrePointBuyMethodTester extends AbstractDisplayPrereqTest implements PrerequisiteTest
 {
