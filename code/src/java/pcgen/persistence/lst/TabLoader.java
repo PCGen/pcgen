d7044efe1531e4319984f35447c50c310d17a5be
Eitan Adler <lists@eitanadler.com>
2017-02-09 16:52:39 -0800

    remove @author, updated on, created on, and other useless remarks

--- code/src/java/pcgen/persistence/lst/TabLoader.java
+++ code/src/java/pcgen/persistence/lst/TabLoader.java
@@ -16,9 +16,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
- * Created on 01/10/2013
  *
- * $Id$
  */
 package pcgen.persistence.lst;
 
@@ -34,7 +32,6 @@ import pcgen.util.enumeration.Tab;
  * TabLoader loads TAB lines from the miscinfo.lst file for a game mode.
  * 
  * 
- * @author James Dempsey &lt;jdempsey@users.sourceforge.net&gt;
  */
 public class TabLoader extends SimpleLoader<TabInfo>
 {
