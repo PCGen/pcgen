59d2996aec94fa215de68272023d190c7f8cb38e
Eitan Adler <lists@eitanadler.com>
2017-02-11 23:07:13 -0800

    clone not supported

--- code/src/java/plugin/notes/gui/NotesTreeNode.java
+++ code/src/java/plugin/notes/gui/NotesTreeNode.java
@@ -827,8 +827,6 @@ public class NotesTreeNode implements MutableTreeNode, DocumentListener
 
 			num++;
 		}
-
-		return null;
 	}
 
 	/** Create a new child named "New Node (n)", and create it's directory.
