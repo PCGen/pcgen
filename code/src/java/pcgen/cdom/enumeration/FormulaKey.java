d23cd5effff279f3c44c2c57449e72502380637e
Eitan Adler <lists@eitanadler.com>
2017-02-14 23:15:04 -0800

    maybe fix shit

--- code/src/java/pcgen/cdom/enumeration/FormulaKey.java
+++ code/src/java/pcgen/cdom/enumeration/FormulaKey.java
@@ -43,7 +43,7 @@ public class FormulaKey implements TypeSafeConstant
 	/**
 	 * This Map contains the mappings from Strings to the Type Safe Constant
 	 */
-	private static CaseInsensitiveMap<FormulaKey> typeMap = new CaseInsensitiveMap<>();
+	private static final CaseInsensitiveMap<FormulaKey> typeMap = new CaseInsensitiveMap<>();
 
 	/**
 	 * This is used to provide a unique ordinal to each constant in this class
