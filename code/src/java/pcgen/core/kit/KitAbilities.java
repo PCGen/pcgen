f98a34500d77d356ee8143c00e8a32c9df722e52
Eitan Adler <lists@eitanadler.com>
2017-02-20 14:03:51 -0800

    per pmd avoid creating BigDecimal where it isn't needed

--- code/src/java/pcgen/core/kit/KitAbilities.java
+++ code/src/java/pcgen/core/kit/KitAbilities.java
@@ -219,7 +219,7 @@ public final class KitAbilities extends BaseKit
 				// Need to pay for it first
 				if (free)
 				{
-					aPC.adjustAbilities(category, new BigDecimal(1));
+					aPC.adjustAbilities(category, BigDecimal.ONE);
 				}
 			}
 			if (ability.getCost() > aPC.getAvailableAbilityPool(category).doubleValue())
@@ -255,7 +255,7 @@ public final class KitAbilities extends BaseKit
 			if (isFree())
 			{
 				AbilityCategory category = catRef.get();
-				aPC.adjustAbilities(category, new BigDecimal(1));
+				aPC.adjustAbilities(category, BigDecimal.ONE);
 			}
 		}
 	}
