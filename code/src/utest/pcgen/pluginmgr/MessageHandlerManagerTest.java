be7b1659999e932cc4ac451a03b1c27dee32f155
Eitan Adler <lists@eitanadler.com>
2017-02-25 11:26:44 -0800

    Formatting

--- code/src/utest/pcgen/pluginmgr/MessageHandlerManagerTest.java
+++ code/src/utest/pcgen/pluginmgr/MessageHandlerManagerTest.java
@@ -1,5 +1,4 @@
 /*
- * MessageHandlerManagerTest.java
  * Copyright James Dempsey, 2014
  *
  * This library is free software; you can redistribute it and/or
@@ -15,24 +14,19 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- *
- *
  */
 package pcgen.pluginmgr;
+
+import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
-import org.junit.Test;
-
 /**
- * The Class <code>MessageHandlerManagerTest</code> checks that MessageHandlerManager is
+ * The Class {@code MessageHandlerManagerTest} checks that MessageHandlerManager is
  * working correctly.
- *
- * <br/>
- * 
  */
 
 public class MessageHandlerManagerTest
