437e32a33c63ee3158f9aa1e191910b288ea0372
Eitan Adler <grimreaper@users.noreply.github.com>
2017-03-08 09:47:28 -0800

    Revert "for loop does not loop"

--- code/src/java/pcgen/io/PCGVer2Creator.java
+++ code/src/java/pcgen/io/PCGVer2Creator.java
@@ -33,6 +33,8 @@ import java.util.Set;
 import java.util.StringTokenizer;
 import java.util.TreeSet;
 
+import org.apache.commons.lang3.StringUtils;
+
 import pcgen.base.lang.StringUtil;
 import pcgen.cdom.base.CDOMList;
 import pcgen.cdom.base.CDOMListObject;
@@ -97,8 +99,6 @@ import pcgen.system.PCGenPropBundle;
 import pcgen.util.FileHelper;
 import pcgen.util.Logging;
 import pcgen.util.StringPClassUtil;
-
-import org.apache.commons.lang3.StringUtils;
 
 /**
  * {@code PCGVer2Creator}<br>
@@ -850,9 +850,12 @@ public final class PCGVer2Creator
 			List<? extends SpecialAbility> salist = charDisplay.getUserSpecialAbilityList(pcClass);
 			if (salist != null)
 			{
-				SpecialAbility sa = salist.get(0);
-				specials.put(pcClass.getKeyName() + IOConstants.TAG_SA + 0, sa
-						.getKeyName());
+				for (SpecialAbility sa : salist)
+				{
+					specials.put(pcClass.getKeyName() + IOConstants.TAG_SA + 0, sa
+							.getKeyName());
+					break;
+				}
 			}
 
 			for (BonusObj save : thePC.getSaveableBonusList(pcClass))
