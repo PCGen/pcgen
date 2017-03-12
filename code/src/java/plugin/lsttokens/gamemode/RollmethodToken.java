cb2e6031a9392664d5c64613133cea9d4238bfba
Eitan Adler <lists@eitanadler.com>
2017-03-11 19:43:59 -0800

    Remove extranious '*' from javadocs (id: 9:6167)

--- code/src/java/plugin/lsttokens/gamemode/RollmethodToken.java
+++ code/src/java/plugin/lsttokens/gamemode/RollmethodToken.java
@@ -15,9 +15,6 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- *
- *
- *
  */
 package plugin.lsttokens.gamemode;
 
@@ -31,7 +28,6 @@ import pcgen.util.Logging;
 
 /**
  * {@code RollmethodToken}
- *
  */
 public class RollmethodToken implements GameModeLstToken
 {
