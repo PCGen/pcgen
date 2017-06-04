548d0aeb03164b4e879462d1df0bcfaabb2b0f54
Eitan Adler <grimreaper@users.noreply.github.com>
2017-06-04 14:33:56 -0700

    Remove extranious '*' from javadocs (id: 7:24869) (#2675)

--- code/src/java/plugin/exporttokens/SpellListTypeToken.java
+++ code/src/java/plugin/exporttokens/SpellListTypeToken.java
@@ -15,9 +15,6 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- *
- *
- *
  */
 package plugin.exporttokens;
 
@@ -30,8 +27,6 @@ import pcgen.io.exporttoken.SpellListToken;
 /**
  * {@code SpellListTypeToken} outputs the type (i.e. Arcane or
  * Divine) of the specified spell class.
- *
- *
  */
 
 public class SpellListTypeToken extends SpellListToken
