9623c40a965603b21ad98ad9158f51b3811432c3
Eitan Adler <grimreaper@users.noreply.github.com>
2017-06-04 16:45:44 -0700

    include root cause in exception (#2569)

--- code/gradle/releaseUtils.groovy
+++ code/gradle/releaseUtils.groovy
@@ -75,7 +75,7 @@ void updateVersionProperty(String newVersion) {
 			try {
 				project.ant.replace(file: propFile, token: "${prop}=${oldVersion}", value: "${prop}=${newVersion}", failOnNoReplacements: true)
 			} catch (org.apache.tools.ant.BuildException be) {
-				throw new GradleException("Unable to update version property. Please check file permissions, and ensure property is in \"${prop}=${newVersion}\" format.")
+				throw new GradleException("Unable to update version property. Please check file permissions, and ensure property is in \"${prop}=${newVersion}\" format.", be)
 			}
 		}
 	}
