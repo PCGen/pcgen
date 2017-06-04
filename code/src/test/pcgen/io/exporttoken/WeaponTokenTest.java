8056c43030638f75372e86e87093677368cec3be
Eitan Adler <grimreaper@users.noreply.github.com>
2017-06-04 14:39:40 -0700

    Remove extranious '*' from javadocs (id: 13:26585) (#2680)

--- code/src/test/pcgen/io/exporttoken/WeaponTokenTest.java
+++ code/src/test/pcgen/io/exporttoken/WeaponTokenTest.java
@@ -15,9 +15,6 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- *
- *
- *
  */
 package pcgen.io.exporttoken;
 
@@ -66,8 +63,6 @@ import pcgen.util.TestHelper;
 /**
  * <code>WeaponTokenTest</code> contains tests to verify that the
  * WEAPON token is working correctly.
- *
- *
  */
 
 public class WeaponTokenTest extends AbstractCharacterTestCase
