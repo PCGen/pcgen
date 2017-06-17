8a033a635bf1459a2286a2c3cee3bed538b52266
Eitan Adler <lists@eitanadler.com>
2017-06-17 12:00:00 -0700

    don't weaken to number

--- code/src/java/pcgen/core/PlayerCharacter.java
+++ code/src/java/pcgen/core/PlayerCharacter.java
@@ -8440,7 +8440,7 @@ public class PlayerCharacter implements Cloneable, VariableContainer
 		return getDescription(Collections.singletonList(pobj));
 	}
 
-	public String getDescription(List<? extends Object> objList)
+	public String getDescription(List<?> objList)
 	{
 		if (objList.isEmpty())
 		{
