ec9582b54ad9fcce2dbd01c8ac2a17f525cc5741
Eitan Adler <lists@eitanadler.com>
2017-06-17 12:00:00 -0700

    clone and private

--- code/src/java/pcgen/core/EquipmentModifier.java
+++ code/src/java/pcgen/core/EquipmentModifier.java
@@ -43,7 +43,7 @@ import pcgen.util.Delta;
 /**
  * Definition and games rules for an equipment modifier.
  */
-public final class EquipmentModifier extends PObject implements Comparable<Object>, EquipModFacade
+public final class EquipmentModifier extends PObject implements Comparable<Object>, EquipModFacade, Cloneable
 {
 	private static final String PERCENT_CHOICE_PATTERN = Pattern
 								.quote(Constants.LST_PERCENT_CHOICE);
