e391617219fc096e02d04e37cb7a1c48da932784
Eitan Adler <lists@eitanadler.com>
2017-06-17 12:00:00 -0700

    remove complexity from PCGenTestCase

--- code/src/test/pcgen/PCGenTestCase.java
+++ code/src/test/pcgen/PCGenTestCase.java
@@ -81,50 +81,6 @@ public abstract class PCGenTestCase extends TestCase
 	{
 		super(name);
 	}
-	
-	/**
-	 * Fixes {@link TestCase#runBare()} to not swallow a throwable from {@link
-	 * #runTest()} if {@link #tearDown()} also throws.
-	 *
-	 * @throws Throwable
-	 */
-	@Override
-	public void runBare() throws Throwable
-	{
-		setUp();
-
-		Throwable thrown = null;
-
-		try
-		{
-			runTest();
-		}
-
-		catch (final Throwable t)
-		{
-			thrown = t;
-
-		}
-
-		finally
-		{
-			try
-			{
-				tearDown();
-
-			}
-
-			finally
-			{
-				if (thrown != null)
-				{
-					// Replace any tear down exception with
-					// unit test exception
-					throw thrown;
-				}
-			}
-		}
-	}
 
 	protected void is(final Object something, final TestChecker matches)
 	{
