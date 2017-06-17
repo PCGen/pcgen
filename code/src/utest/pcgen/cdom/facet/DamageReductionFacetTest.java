d8c65ddd1fe342d731140d3e04bcf5137cd60f23
Eitan Adler <lists@eitanadler.com>
2017-06-17 12:00:00 -0700

    remove an unused test class

--- code/src/utest/pcgen/cdom/facet/DamageReductionFacetTest.java
+++ code/src/utest/pcgen/cdom/facet/DamageReductionFacetTest.java
@@ -1,5 +1,4 @@
 /*
- *
  * Copyright 2006 (C) Aaron Divinsky <boomer70@yahoo.com>
  *
  * This library is free software; you can redistribute it and/or
@@ -41,7 +40,7 @@ import pcgen.core.Race;
 public class DamageReductionFacetTest extends
 		AbstractExtractingFacetTest<CDOMObject, DamageReduction>
 {
-	private DamageReductionFacet facet = new DamageReductionFacet();
+	private final DamageReductionFacet facet = new DamageReductionFacet();
 	private DamageReduction[] target;
 	private CDOMObject[] source;
 
