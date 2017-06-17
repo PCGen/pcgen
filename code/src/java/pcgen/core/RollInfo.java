6a51d6d668a9d06c8ca47ec6e5cd5b9b6716ed95
Eitan Adler <lists@eitanadler.com>
2017-06-17 12:00:00 -0700

    append char

--- code/src/java/pcgen/core/RollInfo.java
+++ code/src/java/pcgen/core/RollInfo.java
@@ -325,7 +325,7 @@ public final class RollInfo
 			buf.append(times);
 		}
 
-		buf.append("d").append(sides);
+		buf.append('d').append(sides);
 
 		while (keepList != null) // let break work
 		{
@@ -377,7 +377,7 @@ public final class RollInfo
 
 			if ((p > 0) && (i == times))
 			{
-				buf.append("\\").append(p);
+				buf.append('\\').append(p);
 
 				break;
 			}
@@ -403,13 +403,13 @@ public final class RollInfo
 
 			if ((p > 0) && (i == times))
 			{
-				buf.append("/").append((times - p));
+				buf.append('/').append((times - p));
 
 				break;
 			}
 
 			// Finally, we have a list
-			buf.append("|");
+			buf.append('|');
 
 			boolean first = true;
 
@@ -426,7 +426,7 @@ public final class RollInfo
 				}
 				else
 				{
-					buf.append(",");
+					buf.append(',');
 				}
 
 				buf.append(i + 1);
@@ -435,31 +435,31 @@ public final class RollInfo
 
 		if (rerollBelow != Integer.MIN_VALUE)
 		{
-			buf.append("m").append(rerollBelow);
+			buf.append('m').append(rerollBelow);
 		}
 
 		if (rerollAbove != Integer.MAX_VALUE)
 		{
-			buf.append("M").append(rerollAbove);
+			buf.append('M').append(rerollAbove);
 		}
 
 		if (modifier > 0)
 		{
-			buf.append("+").append(modifier);
+			buf.append('+').append(modifier);
 		}
 		else if (modifier < 0)
 		{
-			buf.append("-").append(-modifier);
+			buf.append('-').append(-modifier);
 		}
 
 		if (totalFloor != Integer.MIN_VALUE)
 		{
-			buf.append("t").append(totalFloor);
+			buf.append('t').append(totalFloor);
 		}
 
 		if (totalCeiling != Integer.MAX_VALUE)
 		{
-			buf.append("T").append(totalCeiling);
+			buf.append('T').append(totalCeiling);
 		}
 
 		return buf.toString();
