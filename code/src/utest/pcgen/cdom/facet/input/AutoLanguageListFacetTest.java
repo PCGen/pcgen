9e6856cc96ad9654ec00035b89b37d5d864d7ae3
Eitan Adler <lists@eitanadler.com>
2017-06-17 12:00:00 -0700

    remove empty test methods from test classes

--- code/src/utest/pcgen/cdom/facet/input/AutoLanguageListFacetTest.java
+++ code/src/utest/pcgen/cdom/facet/input/AutoLanguageListFacetTest.java
@@ -27,12 +27,6 @@ public class AutoLanguageListFacetTest extends AbstractSourcedListFacetTest<Lang
 	private AutoLanguageListFacet facet = new AutoLanguageListFacet();
 
 	@Override
-	public void setUp() throws Exception
-	{
-		super.setUp();
-	}
-
-	@Override
 	protected AbstractSourcedListFacet<CharID, Language> getFacet()
 	{
 		return facet;
