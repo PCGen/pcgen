b1bcdab7fcbad4855004675c1352da1abccec20c
Eitan Adler <lists@eitanadler.com>
2017-06-17 12:00:00 -0700

    Replace custom code in MiscUtils with standard Java code

--- code/src/java/gmgen/util/MiscUtilities.java
+++ code/src/java/gmgen/util/MiscUtilities.java
@@ -8,13 +8,11 @@
 package gmgen.util;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import javax.swing.JOptionPane;
-
-import pcgen.core.Globals;
 
 /**
  *  Misc Utilities, all static.  Will create and store a private static instance
@@ -24,19 +22,13 @@ public final class MiscUtilities
 {
 	private MiscUtilities()
 	{
-		//Do not allow instantiation of utility class
 	}
 
-	/**
-	 * Copy a file
-	 * @param from_file
-	 * @param to_file
-	 * @throws IOException
-	 */
-	public static void copy(File from_file, File to_file)
-		throws IOException
+	public static void copy(File from_file, File requested_to_file)
+			throws IOException
 	{
-		// First make sure the source file exists, is a file, and is readable.
+		File to_file = requested_to_file;
+
 		if (!from_file.exists())
 		{
 			throw new IOException("FileCopy: no such source file: " + from_file.getPath());
@@ -69,10 +61,10 @@ public final class MiscUtilities
 				throw new IOException("FileCopy: destination file is unwriteable: " + to_file.getPath());
 			}
 
-			// Ask whether to overwrite it
 			int choice = JOptionPane.showConfirmDialog(null,
-				    "Overwrite existing file " + to_file.getPath(), "File Exists", JOptionPane.YES_NO_OPTION,
-				    JOptionPane.QUESTION_MESSAGE);
+					"Overwrite existing file " + to_file.getPath(), "File Exists", JOptionPane.YES_NO_OPTION,
+					JOptionPane.QUESTION_MESSAGE
+			);
 
 			if (choice != JOptionPane.YES_OPTION)
 			{
@@ -81,17 +73,17 @@ public final class MiscUtilities
 		}
 		else
 		{
-			// if file doesn't exist, check if directory exists and is writeable.
+			// If file doesn't exist, check if directory exists and is writeable.
 			// If getParent() returns null, then the directory is the current dir.
 			// so look up the user.dir system property to find out what that is.
 			String parent = to_file.getParent(); // Get the destination directory
 
 			if (parent == null)
 			{
-				parent = Globals.getDefaultPath(); // or CWD
+				parent = Paths.get(".").toAbsolutePath() .toString();
 			}
 
-			File dir = new File(parent); // Convert it to a file.
+			File dir = new File(parent);
 
 			if (!dir.exists())
 			{
@@ -109,51 +101,6 @@ public final class MiscUtilities
 			}
 		}
 
-		// If we've gotten this far, then everything is okay.
-		// So we copy the file, a buffer of bytes at a time.
-		FileInputStream from = null; // Stream to read from source
-		FileOutputStream to = null; // Stream to write to destination
-
-		try
-		{
-			from = new FileInputStream(from_file); // Create input stream
-			to = new FileOutputStream(to_file); // Create output stream
-
-			byte[] buffer = new byte[4096]; // A buffer to hold file contents
-			int bytes_read; // How many bytes in buffer
-
-			while ((bytes_read = from.read(buffer)) != -1)
-			{ // Read bytes until EOF
-				to.write(buffer, 0, bytes_read); //   write bytes
-			}
-		}
-
-		// Always close the streams, even if exceptions were thrown
-		finally
-		{
-			if (from != null)
-			{
-				try
-				{
-					from.close();
-				}
-				catch (IOException e)
-				{
-					//TODO: Should this really be ignored?
-				}
-			}
-
-			if (to != null)
-			{
-				try
-				{
-					to.close();
-				}
-				catch (IOException e)
-				{
-					//TODO: Should this really be ignored?
-				}
-			}
-		}
+		Files.copy(from_file.toPath(), to_file.toPath());
 	}
 }
