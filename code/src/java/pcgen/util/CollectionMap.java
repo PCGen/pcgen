eae23d68ef3aabbf1f6fc70c6bf4e331e6b4d143
Eitan Adler <lists@eitanadler.com>
2017-06-20 12:00:00 -0700

    pull out overridden abstract functions that remain abstract

--- code/src/java/pcgen/util/CollectionMap.java
+++ code/src/java/pcgen/util/CollectionMap.java
@@ -1,5 +1,4 @@
 /*
- *
  * Copyright 2007 (C) Connor Petty <mistercpp2000@gmail.com>
  *
  * This library is free software; you can redistribute it and/or
@@ -15,17 +14,12 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- *
- *
- * To change this template, choose Tools | Template Manager
- * and open the template in the editor.
  */
 
 package pcgen.util;
 
 import java.util.Collection;
 import java.util.Map;
-
 
 public interface CollectionMap<K, V, C extends Collection<V>> extends Map<K, C>{
     /**
@@ -51,36 +45,11 @@ public interface CollectionMap<K, V, C extends Collection<V>> extends Map<K, C>{
      */
     @Override
     C put(K key, C collection);
-    
+
     boolean add(K key, V value);
     boolean addAll(K key, Collection<? extends V> values);
     boolean containsValue(Object key, Object value);
-    /**
-     * Removes the mapping for a key from this map if it is present
-     * (optional operation).   More formally, if this map contains a mapping
-     * from key <tt>k</tt> to collection <tt>v</tt> such that
-     * {@code (key==null ?  k==null : key.equals(k))}, that mapping
-     * is removed.  (The map can contain at most one such mapping.)
-     *
-     * <p>Returns the collection to which this map previously associated the key,
-     * or <tt>null</tt> if the map contained no mapping for the key.
-     *
-     * <p>The map will not contain a mapping for the specified key once the
-     * call returns.
-     *
-     * @param key key whose mapping is to be removed from the map
-     * @return the previous collection associated with <tt>key</tt>, or
-     *         <tt>null</tt> if there was no mapping for <tt>key</tt>.
-     * @throws UnsupportedOperationException if the <tt>remove</tt> operation
-     *         is not supported by this map
-     * @throws ClassCastException if the key is of an inappropriate type for
-     *         this map (optional)
-     * @throws NullPointerException if the specified key is null and this
-     *         map does not permit null keys (optional)
-     */
-    @Override
-    C remove(Object key);
-    
+
     boolean remove(Object key, Object value);
     boolean removeAll(Object key, Collection<?> c);
     boolean retainAll(Object key, Collection<?> c);
