71fbee3bbd2abfd8479fbdc873ff5c5ecef8b899
Tom Parker <thpr@yahoo.com>
2017-07-20 17:13:04 -0400

    Cleanup unused imports

--- code/src/utest/selectionactor/testsupport/AbstractSelectionActorTest.java
+++ code/src/utest/selectionactor/testsupport/AbstractSelectionActorTest.java
@@ -31,9 +31,6 @@ import pcgen.testsupport.AbstractCharacterUsingTestCase;
 import compare.InequalityTesterInst;
 import org.junit.Test;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
 public abstract class AbstractSelectionActorTest<T extends CDOMObject> extends AbstractCharacterUsingTestCase
 {
 
