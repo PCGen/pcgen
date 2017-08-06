23a1a3aa5e8b437f9afcb9d9125621308bbebf3b
Tom Parker <thpr@yahoo.com>
2017-08-05 23:28:26 -0400

    update

--- PCGen-Formula/code/src/java/pcgen/base/formula/base/DependencyManager.java
+++ PCGen-Formula/code/src/java/pcgen/base/formula/base/DependencyManager.java
@@ -1,17 +1,19 @@
 /*
  * Copyright 2014-16 (C) Tom Parker <thpr@users.sourceforge.net>
  * 
- * This library is free software; you can redistribute it and/or modify it under the terms
- * of the GNU Lesser General Public License as published by the Free Software Foundation;
- * either version 2.1 of the License, or (at your option) any later version.
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
  * 
- * This library is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
- * PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
  * 
- * You should have received a copy of the GNU Lesser General Public License along with
- * this library; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
- * Suite 330, Boston, MA 02111-1307 USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 package pcgen.base.formula.base;
 
@@ -22,6 +24,7 @@ import java.util.Map;
 import java.util.Objects;
 
 import pcgen.base.util.TypedKey;
+import pcgen.base.util.WriteOnceReadManyList;
 
 /**
  * A DependencyManager is a class to capture Formula dependencies.
@@ -33,15 +36,13 @@ public class DependencyManager
 {
 
 	/**
-	 * A NonNullValueKey used for a location to write messages while processing
-	 * dependencies.
+	 * A TypedKey used for a location to write messages while processing dependencies.
 	 */
 	public static final TypedKey<List<String>> LOG =
 			new TypedKey<List<String>>();
 
 	/**
-	 * A ValidatingKey used for storing the FormulaManager contained in this
-	 * DependencyManager
+	 * A TypedKey used for storing the FormulaManager contained in this DependencyManager
 	 */
 	public static final TypedKey<FormulaManager> FMANAGER =
 			new TypedKey<FormulaManager>();
@@ -66,8 +67,7 @@ public class DependencyManager
 	/**
 	 * A TypedKey used for storing the (static) Variables contained in this DependencyManager.
 	 */
-	public static final TypedKey<ArrayList<VariableID<?>>> VARIABLES =
-			new TypedKey<ArrayList<VariableID<?>>>();
+	public static final TypedKey<VariableList> VARIABLES = new TypedKey<VariableList>();
 
 	/**
 	 * A TypedKey used for determining how encountered variables are processed. This can
@@ -84,7 +84,7 @@ public class DependencyManager
 
 	private DependencyManager()
 	{
-		
+		//Null constructor for getWith
 	}
 
 	/**
@@ -93,8 +93,7 @@ public class DependencyManager
 	public DependencyManager(FormulaManager formulaManager)
 	{
 		map.put(FMANAGER, formulaManager);
-		//TODO Make this write only?
-		map.put(LOG, new WriteOnceReadManyList(new ArrayList<>()));
+		map.put(LOG, new WriteOnceReadManyList<>(new ArrayList<>()));
 	}
 
 	/**
@@ -116,7 +115,7 @@ public class DependencyManager
 	}
 
 	/**
-	 * Returns the value of the DependencyManager for the given ValidatingKey.
+	 * Returns the value of the DependencyManager for the given TypedKey.
 	 * 
 	 * Note that this method will not throw an error if the DependencyManager is empty. It
 	 * will simply return the "Default Value" for the given TypeKey. Note null is a legal
@@ -124,7 +123,7 @@ public class DependencyManager
 	 * 
 	 * @param key
 	 *            The TypeKey for which the value should be returned
-	 * @return The value of the DependencyManager for the given ValidatingKey
+	 * @return The value of the DependencyManager for the given TypedKey
 	 */
 	public <T> T get(TypedKey<T> key)
 	{
