e550c3345c76d808de1a8b2bf2c66c812de49c41
Tom Parker <thpr@yahoo.com>
2017-08-05 23:33:52 -0400

    Add docs

--- PCGen-Formula/code/src/java/pcgen/base/formula/base/VariableList.java
+++ PCGen-Formula/code/src/java/pcgen/base/formula/base/VariableList.java
@@ -1,12 +1,34 @@
+/*
+ * Copyright 2017 (C) Tom Parker <thpr@users.sourceforge.net>
+ * 
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License along with
+ * this library; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+ * Suite 330, Boston, MA 02111-1307 USA
+ */
 package pcgen.base.formula.base;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * A VariableList is a contained list of VariableID objects. It does lazy instantiation
+ * and provides ownership protection to the underlying list.
+ */
 public class VariableList
 {
 
+	/**
+	 * The underlying List of this Variable List.
+	 */
 	private List<VariableID<?>> vars;
 
 	/**
@@ -35,6 +57,12 @@ public class VariableList
 		return vars;
 	}
 
+	/**
+	 * Adds a new VariableID to this VariableList.
+	 * 
+	 * @param varID
+	 *            The VariableID to be added to this VariableList.
+	 */
 	public void add(VariableID<?> varID)
 	{
 		if (vars == null)
