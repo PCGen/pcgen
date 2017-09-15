77587c5051e812d288c6233719bbe9dc6a84a74f
Andrew Maitland <drew0500@yahoo.com>
2017-09-14 23:13:34 -0700

    Revert "updates"
    This reverts commit ca02380e3304a9752596a92e51d0518771e93b9d.

--- code/src/java/pcgen/base/format/dice/Dice.java
+++ code/src/java/pcgen/base/format/dice/Dice.java
@@ -1,23 +1,4 @@
-/*
- * Copyright (c) 2016 Tom Parker <thpr@users.sourceforge.net>
- * 
- * This program is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 2.1 of the License, or (at your option)
- * any later version.
- * 
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
- * 
- * You should have received a copy of the GNU Lesser General Public License
- * along with this library; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
- */
 package pcgen.base.format.dice;
-
-import java.util.Objects;
 
 public class Dice
 {
@@ -31,8 +12,12 @@ public class Dice
 			throw new IllegalArgumentException(
 				"Quantity cannot be < 0 in Dice");
 		}
+		if (die == null)
+		{
+			throw new IllegalArgumentException("Die cannot be null in Dice");
+		}
 		this.quantity = quantity;
-		this.die = Objects.requireNonNull(die);
+		this.die = die;
 	}
 
 	public Die getDie()
