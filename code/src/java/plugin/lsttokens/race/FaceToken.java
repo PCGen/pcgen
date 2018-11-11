443a2c2635158eb4ed984be7a76b81f1bad4a565
Tom Parker <thpr@yahoo.com>
2018-11-11 18:12:58 -0500

    InterfaceToken Cleanup & Enhancement - Consolidated as a bug fix
    Convert FACE tokens to Interface Tokens
    Fix an existing issue with FACE calculation
    Finish conversion of MODIFY tokens, removing the ListKeys

--- code/src/java/plugin/lsttokens/race/FaceToken.java
+++ code/src/java/plugin/lsttokens/race/FaceToken.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008-15 Tom Parker <thpr@users.sourceforge.net>
+ * Copyright (c) 2008-18 Tom Parker <thpr@users.sourceforge.net>
  * 
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
@@ -17,27 +17,25 @@
  */
 package plugin.lsttokens.race;
 
-import java.util.Collection;
-
 import pcgen.base.calculation.FormulaModifier;
 import pcgen.base.math.OrderedPair;
 import pcgen.base.util.FormatManager;
+import pcgen.cdom.base.LimitedVarContainer;
+import pcgen.cdom.base.LimitedVarHolder;
 import pcgen.cdom.content.VarModifier;
-import pcgen.cdom.enumeration.ListKey;
 import pcgen.cdom.formula.scope.PCGenScope;
 import pcgen.cdom.util.CControl;
 import pcgen.cdom.util.ControlUtilities;
-import pcgen.core.Race;
-import pcgen.rules.context.Changes;
 import pcgen.rules.context.LoadContext;
 import pcgen.rules.persistence.token.AbstractNonEmptyToken;
-import pcgen.rules.persistence.token.CDOMPrimaryToken;
+import pcgen.rules.persistence.token.CDOMInterfaceToken;
 import pcgen.rules.persistence.token.ParseResult;
 
 /**
  * Class deals with FACE Token
  */
-public class FaceToken extends AbstractNonEmptyToken<Race> implements CDOMPrimaryToken<Race>
+public class FaceToken extends AbstractNonEmptyToken<LimitedVarHolder>
+		implements CDOMInterfaceToken<LimitedVarContainer, LimitedVarHolder>
 {
 
 	private static final int MOD_PRIORITY = 10;
@@ -50,8 +48,13 @@ public class FaceToken extends AbstractNonEmptyToken<Race> implements CDOMPrimar
 	}
 
 	@Override
-	protected ParseResult parseNonEmptyToken(LoadContext context, Race race, String value)
+	protected ParseResult parseNonEmptyToken(LoadContext context, LimitedVarHolder race, String value)
 	{
+		if (!"TEMPLATE".equals(race.getIdentifier())
+			&& !"RACE".equals(race.getIdentifier()))
+		{
+			return new ParseResult.Fail("FACE: is only supported on Race and Template");
+		}
 		if (ControlUtilities.hasControlToken(context, CControl.FACE))
 		{
 			return new ParseResult.Fail("FACE: LST Token is disabled when FACE: control is used");
@@ -60,6 +63,7 @@ public class FaceToken extends AbstractNonEmptyToken<Race> implements CDOMPrimar
 		{
 			value = value + ',' + 0;
 		}
+		@SuppressWarnings("unchecked")
 		FormatManager<OrderedPair> formatManager =
 				(FormatManager<OrderedPair>) context.getReferenceContext().getFormatManager("ORDEREDPAIR");
 		PCGenScope scope = context.getActiveScope();
@@ -70,8 +74,8 @@ public class FaceToken extends AbstractNonEmptyToken<Race> implements CDOMPrimar
 		}
 		catch (IllegalArgumentException iae)
 		{
-			return new ParseResult.Fail(getTokenName() + " Modifier " + MOD_IDENTIFICATION + " had value " + value
-				+ " but it was not valid: " + iae.getMessage());
+			return new ParseResult.Fail(getTokenName() + " Modifier " + MOD_IDENTIFICATION
+				+ " had value " + value + " but it was not valid: " + iae.getMessage());
 		}
 		modifier.addAssociation("PRIORITY=" + MOD_PRIORITY);
 		OrderedPair pair = modifier.process(null);
@@ -83,7 +87,6 @@ public class FaceToken extends AbstractNonEmptyToken<Race> implements CDOMPrimar
 		{
 			return new ParseResult.Fail(getTokenName() + " had value " + value + " but second item cannot be negative");
 		}
-
 		String varName = CControl.FACE.getDefaultValue();
 		if (!context.getVariableContext().isLegalVariableID(scope, varName))
 		{
@@ -91,15 +94,14 @@ public class FaceToken extends AbstractNonEmptyToken<Race> implements CDOMPrimar
 				+ ", Modified on " + race.getClass().getSimpleName() + ' ' + race.getKeyName());
 		}
 		VarModifier<OrderedPair> vm = new VarModifier<>(varName, scope, modifier);
-		context.getObjectContext().addToList(race, ListKey.MODIFY, vm);
+		race.addModifier(vm);
 		return ParseResult.SUCCESS;
 	}
 
 	@Override
-	public String[] unparse(LoadContext context, Race race)
+	public String[] unparse(LoadContext context, LimitedVarContainer race)
 	{
-		Changes<VarModifier<?>> changes = context.getObjectContext().getListChanges(race, ListKey.MODIFY);
-		Collection<VarModifier<?>> added = changes.getAdded();
+		VarModifier<?>[] added = race.getModifierArray();
 		String face = null;
 		if (added != null)
 		{
@@ -122,9 +124,14 @@ public class FaceToken extends AbstractNonEmptyToken<Race> implements CDOMPrimar
 	}
 
 	@Override
-	public Class<Race> getTokenClass()
+	public Class<LimitedVarHolder> getTokenClass()
 	{
-		return Race.class;
+		return LimitedVarHolder.class;
 	}
 
+	@Override
+	public Class<LimitedVarContainer> getReadInterface()
+	{
+		return LimitedVarContainer.class;
+	}
 }
