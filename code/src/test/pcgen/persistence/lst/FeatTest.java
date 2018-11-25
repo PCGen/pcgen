9d83f846d58f1f437473209bbdc9d4cc51c2c6eb
Eitan Adler <grimreaper@users.noreply.github.com>
2018-11-25 10:10:37 -0800

    [FeatTest] change FeatTest from suite to test (#4470)

--- code/src/test/pcgen/persistence/lst/FeatTest.java
+++ code/src/test/pcgen/persistence/lst/FeatTest.java
@@ -6,9 +6,6 @@ package pcgen.persistence.lst;
 import java.net.URI;
 import java.net.URISyntaxException;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
 import pcgen.base.lang.UnreachableError;
 import pcgen.core.Ability;
 import pcgen.core.Campaign;
@@ -17,43 +14,16 @@ import pcgen.persistence.PersistenceLayerException;
 import pcgen.util.TestHelper;
 import plugin.lsttokens.testsupport.BuildUtilities;
 
+import junit.framework.TestCase;
+import org.junit.Assert;
+
 /**
  * JUnit testcases for {@code pcgen.core.Feat}.
  */
 public class FeatTest extends TestCase
 {
 	/**
-	 * Constructor
-	 * @param name
-	 */
-	public FeatTest(String name)
-	{
-		super(name);
-	}
-
-	/**
-	 * Main, run the test
-	 * @param args
-	 */
-	public static void main(String[] args)
-	{
-		junit.textui.TestRunner.run(FeatTest.class);
-	}
-
-	/**
-	 * Return a new test suite
-	 * @return Test
-	 */
-	public static Test suite()
-	{
-		// quick method, adds all methods beginning with "test"
-		return new TestSuite(FeatTest.class);
-	}
-
-	/**
 	 * Sets up the test case by loading the system plugins.
-	 * 
-	 * @see junit.framework.TestCase#setUp()
 	 */
 	@Override
 	public void setUp() throws Exception
@@ -88,7 +58,7 @@ public class FeatTest extends TestCase
 				Globals.getContext(),
 				alertnessFeat,
 				"Alertness	TYPE:General	DESC:+2 on Listen and Spot checks	BONUS:SKILL|Listen,Spot|2", source);
-		assertEquals("Alertness", alertnessFeat.getKeyName());
+		Assert.assertEquals("Alertness", alertnessFeat.getKeyName());
 	}
 
 	/**
@@ -116,7 +86,7 @@ public class FeatTest extends TestCase
 				"Ambidexterity	PRESTAT:1,DEX=15	PREHANDSEQ:2	TYPE:General.Fighter	"
 			+ "DESC:You ignore all penalties for using your off-hand	BONUS:COMBAT|TOHIT-SECONDARY|4",
 				source);
-		assertEquals("Ambidexterity", ambidexterityFeat.getKeyName());
+		Assert.assertEquals("Ambidexterity", ambidexterityFeat.getKeyName());
 	}
 
 	/**
@@ -145,6 +115,6 @@ public class FeatTest extends TestCase
 				simpleWeaponFeat,
 				"Simple Weapon Proficiency	TYPE:General	DESC:You are proficient with all simple weapons. "
 						+ "Non-proficiency suffers -4 to hit.	ADD:WEAPONPROFS|Simple", source);
-		assertEquals("Simple Weapon Proficiency", simpleWeaponFeat.getKeyName());
+		Assert.assertEquals("Simple Weapon Proficiency", simpleWeaponFeat.getKeyName());
 	}
 }
