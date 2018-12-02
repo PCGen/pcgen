70b5af577e1e6e909ddcdc0215e004d7f6712cc5
Eitan Adler <lists@eitanadler.com>
2018-12-01 22:07:47 -0800

    [ClassTypeTest]  remove unused

--- code/src/test/pcgen/core/ClassTypeTest.java
+++ code/src/test/pcgen/core/ClassTypeTest.java
@@ -8,24 +8,6 @@ import junit.framework.TestCase;
 public class ClassTypeTest extends TestCase
 {
 	/**
-	 * Constructs a new {@code ClassTypeTest}.
-	 */
-	public ClassTypeTest()
-	{
-		// Do Nothing
-	}
-
-	/**
-	 * Constructs a new {@code ClassTypeTest} with the given <var>name</var>.
-	 *
-	 * @param name the test case name
-	 */
-	public ClassTypeTest(final String name)
-	{
-		super(name);
-	}
-
-	/**
 	 * test clone.
 	 */
 	public void testClone()
