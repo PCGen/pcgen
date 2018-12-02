5446fae6650b790a17b19675dbbc2ed49085f2b2
Eitan Adler <lists@eitanadler.com>
2018-12-01 23:16:04 -0800

    [*] Use Collections.removeIf if possible.

--- code/src/java/pcgen/io/exporttoken/WeaponToken.java
+++ code/src/java/pcgen/io/exporttoken/WeaponToken.java
@@ -152,49 +152,25 @@ public class WeaponToken extends Token
 		else if (token.equals("EQUIPPED"))
 		{
 			// remove all weapons which are not equipped from list
-			for (Iterator<Equipment> it = weaponList.iterator(); it.hasNext();)
-			{
-				if (!it.next().isEquipped())
-				{
-					it.remove();
-				}
-			}
+			weaponList.removeIf(equipment -> !equipment.isEquipped());
 			token = aTok.nextToken();
 		}
 		else if (token.equals("NOT_EQUIPPED"))
 		{
 			// remove all weapons which are equipped from list
-			for (Iterator<Equipment> it = weaponList.iterator(); it.hasNext();)
-			{
-				if (it.next().isEquipped())
-				{
-					it.remove();
-				}
-			}
+			weaponList.removeIf(Equipment::isEquipped);
 			token = aTok.nextToken();
 		}
 		else if (token.equals("CARRIED"))
 		{
 			// remove all weapons which are not carried from list
-			for (Iterator<Equipment> it = weaponList.iterator(); it.hasNext();)
-			{
-				if (it.next().numberCarried().intValue() == 0)
-				{
-					it.remove();
-				}
-			}
+			weaponList.removeIf(equipment -> equipment.numberCarried().intValue() == 0);
 			token = aTok.nextToken();
 		}
 		else if (token.equals("NOT_CARRIED"))
 		{
 			// remove all weapons which are carried from list
-			for (Iterator<Equipment> it = weaponList.iterator(); it.hasNext();)
-			{
-				if (it.next().numberCarried().intValue() > 0)
-				{
-					it.remove();
-				}
-			}
+			weaponList.removeIf(equipment -> equipment.numberCarried().intValue() > 0);
 			token = aTok.nextToken();
 		}
 
