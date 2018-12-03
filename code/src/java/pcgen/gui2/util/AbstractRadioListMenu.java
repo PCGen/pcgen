PCGen/pcgen

commit 5ff35d115580f10b9c30b63d75e84ee826e8cbf8

[gui2] Remove unused code from UIPropertyContext (#4484)

code/src/java/pcgen/gui2/util/AbstractRadioListMenu.java
@@ -33,10 +33,10 @@ public abstract class AbstractRadioListMenu<E> extends AbstractListMenu<E> imple
 {
 
 	private final ButtonGroup group = new ButtonGroup();
-	private Map<E, RadioMenuItem> menuMap = new HashMap<>();
-	private E selectedItem = null;
+	private final Map<E, RadioMenuItem> menuMap = new HashMap<>();
+	private final E selectedItem = null;
 
-	public AbstractRadioListMenu(Action action)
+	protected AbstractRadioListMenu(Action action)
 	{
 		super(action);
 	}
@@ -70,17 +70,17 @@ public abstract class AbstractRadioListMenu<E> extends AbstractListMenu<E> imple
 	/**
 	 * Update the menu so that no entries are selected. 
 	 */
-	public void clearSelection()
+	protected void clearSelection()
 	{
 		group.clearSelection();
 	}
 
-	private static class RadioMenuItem extends JRadioButtonMenuItem
+	private static final class RadioMenuItem extends JRadioButtonMenuItem
 	{
 
 		private final Object item;
 
-		public RadioMenuItem(Object item, boolean selected, ItemListener listener)
+		private RadioMenuItem(Object item, boolean selected, ItemListener listener)
 		{
 			super(item.toString(), selected);
 			this.item = item;

code/src/java/pcgen/gui2/InfoGuidePane.java
@@ -51,7 +51,7 @@ import pcgen.system.LanguageBundle;
  * Note: this class extends UIResource so that the component can be added
  * as a child of a JTabbedPane without it becoming a tab
  */
-public class InfoGuidePane extends JComponent implements UIResource
+class InfoGuidePane extends JComponent implements UIResource
 {
 
 	/**
@@ -64,7 +64,7 @@ public class InfoGuidePane extends JComponent implements UIResource
 	private final JEditorPane tipPane;
 	private JPanel mainPanel;
 
-	public InfoGuidePane(PCGenFrame frame, UIContext uiContext)
+	InfoGuidePane(PCGenFrame frame, UIContext uiContext)
 	{
 		this.uiContext = Objects.requireNonNull(uiContext);
 		this.frame = frame;

code/src/java/pcgen/gui2/PCGenStatusBar.java
@@ -49,7 +49,7 @@ public final class PCGenStatusBar extends JPanel
 	private final JProgressBar progressBar;
 	private final JLabel loadStatusLabel;
 
-	public PCGenStatusBar(PCGenFrame frame)
+	PCGenStatusBar(PCGenFrame frame)
 	{
 		this.frame = frame;
 		this.messageLabel = new JLabel();
@@ -85,7 +85,7 @@ public final class PCGenStatusBar extends JPanel
 		return progressBar;
 	}
 
-	public void setSourceLoadErrors(List<LogRecord> errors)
+	void setSourceLoadErrors(List<LogRecord> errors)
 	{
 		if (errors != null && !errors.isEmpty())
 		{
@@ -131,7 +131,7 @@ public final class PCGenStatusBar extends JPanel
 	 * @param task a PCGenTask
 	 * @return a SwingWorker
 	 */
-	public SwingWorker<List<LogRecord>> createWorker(String taskName, PCGenTask task)
+	SwingWorker<List<LogRecord>> createWorker(String taskName, PCGenTask task)
 	{
 		return new StatusWorker(taskName, task, this);
 	}

code/src/java/pcgen/gui2/PCGenToolBar.java
@@ -18,9 +18,9 @@
  */
 package pcgen.gui2;
 
+import javax.swing.Action;
+import javax.swing.JButton;
 import javax.swing.JToolBar;
-
-import pcgen.gui2.util.ToolBarUtilities;
 
 /**
  * The toolbar that is displayed in PCGen's main window. Provides shortcuts to
@@ -31,14 +31,23 @@ import pcgen.gui2.util.ToolBarUtilities;
 public final class PCGenToolBar extends JToolBar
 {
 
-	private final PCGenFrame frame;
 	private final PCGenActionMap actionMap;
 
-	public PCGenToolBar(PCGenFrame frame)
+	PCGenToolBar(PCGenFrame frame)
 	{
-		this.frame = frame;
 		this.actionMap = frame.getActionMap();
 		initComponents();
+	}
+
+	private static JButton createToolBarButton(Action action)
+	{
+		JButton button = new JButton();
+		button.putClientProperty("hideActionText", true);
+		button.setFocusable(false);
+		button.setHorizontalTextPosition(CENTER);
+		button.setVerticalTextPosition(BOTTOM);
+		button.setAction(action);
+		return button;
 	}
 
 	private void initComponents()
@@ -46,17 +55,17 @@ public final class PCGenToolBar extends JToolBar
 		setFloatable(false);
 		setRollover(true);
 
-		add(ToolBarUtilities.createToolBarButton(actionMap.get(PCGenActionMap.NEW_COMMAND)));
-		add(ToolBarUtilities.createToolBarButton(actionMap.get(PCGenActionMap.OPEN_COMMAND)));
-		add(ToolBarUtilities.createToolBarButton(actionMap.get(PCGenActionMap.CLOSE_COMMAND)));
-		add(ToolBarUtilities.createToolBarButton(actionMap.get(PCGenActionMap.SAVE_COMMAND)));
+		add(createToolBarButton(actionMap.get(PCGenActionMap.NEW_COMMAND)));
+		add(createToolBarButton(actionMap.get(PCGenActionMap.OPEN_COMMAND)));
+		add(createToolBarButton(actionMap.get(PCGenActionMap.CLOSE_COMMAND)));
+		add(createToolBarButton(actionMap.get(PCGenActionMap.SAVE_COMMAND)));
 		addSeparator();
 
-		add(ToolBarUtilities.createToolBarButton(actionMap.get(PCGenActionMap.PRINT_COMMAND)));
-		add(ToolBarUtilities.createToolBarButton(actionMap.get(PCGenActionMap.EXPORT_COMMAND)));
+		add(createToolBarButton(actionMap.get(PCGenActionMap.PRINT_COMMAND)));
+		add(createToolBarButton(actionMap.get(PCGenActionMap.EXPORT_COMMAND)));
 		addSeparator();
 
-		add(ToolBarUtilities.createToolBarButton(actionMap.get(PCGenActionMap.PREFERENCES_COMMAND)));
+		add(createToolBarButton(actionMap.get(PCGenActionMap.PREFERENCES_COMMAND)));
 		// addSeparator();
 
 		// add(ToolBarUtilities.createToolBarButton(actionMap.get(PCGenActionMap.HELP_CONTEXT_COMMAND)));

code/src/java/pcgen/gui2/UIContext.java
@@ -32,7 +32,7 @@ public class UIContext
 	/**
 	 * Constructs a new UIContext with an empty source list.
 	 */
-	public UIContext()
+	UIContext()
 	{
 		this.currentSourceSelection = new DefaultReferenceFacade<>();
 	}

code/src/java/pcgen/gui2/UIPropertyContext.java
@@ -25,6 +25,8 @@ import pcgen.cdom.base.Constants;
 import pcgen.facade.core.CharacterFacade;
 import pcgen.system.PropertyContext;
 
+import org.jetbrains.annotations.Nullable;
+
 /**
  * This is a property context which holds UI related user preferences such as
  * screen position and colors.
@@ -109,18 +111,12 @@ public final class UIPropertyContext extends PropertyContext
 		return new Color(Integer.parseInt(prop, 16));
 	}
 
-	public Color getColor(String key, Color defaultValue)
-	{
-		String prop = getProperty(key, Integer.toString(defaultValue.getRGB(), 16));
-		return new Color(Integer.parseInt(prop, 16));
-	}
-
 	public void setColor(String key, Color color)
 	{
 		setProperty(key, Integer.toString(color.getRGB(), 16));
 	}
 
-	public Color initColor(String key, Color defaultValue)
+	private Color initColor(String key, Color defaultValue)
 	{
 		String prop = initProperty(key, Integer.toString(defaultValue.getRGB(), 16));
 		return new Color(Integer.parseInt(prop, 16));
@@ -129,11 +125,6 @@ public final class UIPropertyContext extends PropertyContext
 	public static Color getCustomItemColor()
 	{
 		return getInstance().getColor(CUSTOM_ITEM_COLOR);
-	}
-
-	public static void setCustomItemColor(Color color)
-	{
-		getInstance().setColor(CUSTOM_ITEM_COLOR, color);
 	}
 
 	public static void setQualifiedColor(Color color)
@@ -246,6 +237,7 @@ public final class UIPropertyContext extends PropertyContext
 		return createFilePropertyKey(character.getFileRef().get(), key);
 	}
 
+	@Nullable
 	static String createFilePropertyKey(File file, String key)
 	{
 		if (file == null)

code/src/java/pcgen/gui2/tabs/CharacterSheetInfoTab.java
@@ -59,8 +59,6 @@ import pcgen.util.enumeration.Tab;
 /**
  * The Class {@code CharacterSheetInfoTab} is a placeholder for the
  * character sheet tab.
- *
- * -0800 (Tue, 22 Feb 2011) $
  */
 public class CharacterSheetInfoTab extends FlippingSplitPane implements CharacterInfoTab, DisplayAwareTab
 {

code/src/java/pcgen/gui2/util/ArrowIcon.java
@@ -24,7 +24,6 @@ import java.awt.Graphics;
 
 import javax.swing.Icon;
 import javax.swing.SwingConstants;
-import javax.swing.UIManager;
 
 /**
  * This class contains a lot of code taken from
@@ -41,20 +40,13 @@ public class ArrowIcon implements Icon
 	private int direction;
 	private int size;
 
-	public ArrowIcon(int direction, int size, Color shadow, Color darkShadow, Color highlight)
+	ArrowIcon(int direction, int size, Color shadow, Color darkShadow, Color highlight)
 	{
-		super();
 		this.shadow = shadow;
 		this.darkShadow = darkShadow;
 		this.highlight = highlight;
 		this.direction = direction;
 		this.size = size;
-	}
-
-	public ArrowIcon(int direction, int size)
-	{
-		this(direction, size, UIManager.getColor("controlShadow"), UIManager.getColor("controlDkShadow"),
-			UIManager.getColor("controlLtHighlight"));
 	}
 
 	@Override

commit a58cd6b2f5da6997af49d9d73613b57c9043249c

[gui2] Make use of selectedItem in AbstractRadioListMenu
This was noticed, but not broken in #4484. Previously, selectedItem was
always null: now it changes. The impact is not clear to me (I test a few
usages of it and found no difference) but it seems this is what was
intended.

code/src/java/pcgen/gui2/util/AbstractRadioListMenu.java
@@ -34,7 +34,8 @@ public abstract class AbstractRadioListMenu<E> extends AbstractListMenu<E> imple
 
 	private final ButtonGroup group = new ButtonGroup();
 	private final Map<E, RadioMenuItem> menuMap = new HashMap<>();
-	private final E selectedItem = null;
+	@SuppressWarnings("FieldHasSetterButNoGetter")
+	private E selectedItem = null;
 
 	protected AbstractRadioListMenu(Action action)
 	{
@@ -44,7 +45,7 @@ public abstract class AbstractRadioListMenu<E> extends AbstractListMenu<E> imple
 	@Override
 	protected JMenuItem createMenuItem(E item, int index)
 	{
-		RadioMenuItem menuItem = new RadioMenuItem(item, item == selectedItem, this);
+		RadioMenuItem<E> menuItem = new RadioMenuItem<>(item, item == selectedItem, this);
 		group.add(menuItem);
 		menuMap.put(item, menuItem);
 		return menuItem;
@@ -65,6 +66,7 @@ public abstract class AbstractRadioListMenu<E> extends AbstractListMenu<E> imple
 		{
 			menuItem.setSelected(true);
 		}
+		selectedItem = item;
 	}
 
 	/**
@@ -73,14 +75,15 @@ public abstract class AbstractRadioListMenu<E> extends AbstractListMenu<E> imple
 	protected void clearSelection()
 	{
 		group.clearSelection();
+		selectedItem = null;
 	}
 
-	private static final class RadioMenuItem extends JRadioButtonMenuItem
+	private static final class RadioMenuItem<E> extends JRadioButtonMenuItem
 	{
 
-		private final Object item;
+		private final E item;
 
-		private RadioMenuItem(Object item, boolean selected, ItemListener listener)
+		private RadioMenuItem(E item, boolean selected, ItemListener listener)
 		{
 			super(item.toString(), selected);
 			this.item = item;