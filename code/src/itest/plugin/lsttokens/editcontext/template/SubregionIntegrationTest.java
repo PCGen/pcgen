b810722ab0329af9e1506e78e346d956eb8b885f
Tom <Tom@192.168.1.103>
2018-12-03 21:02:51 -0500

    Remove Old Tests

--- code/src/itest/plugin/lsttokens/editcontext/template/SubregionIntegrationTest.java
+++ code/src/itest/plugin/lsttokens/editcontext/template/SubregionIntegrationTest.java
@@ -21,11 +21,9 @@ import org.junit.Test;
 
 import pcgen.cdom.enumeration.SubRegion;
 import pcgen.core.PCTemplate;
-import pcgen.persistence.PersistenceLayerException;
 import pcgen.rules.persistence.CDOMLoader;
 import pcgen.rules.persistence.token.CDOMPrimaryToken;
 import plugin.lsttokens.editcontext.testsupport.AbstractTypeSafeIntegrationTestCase;
-import plugin.lsttokens.editcontext.testsupport.TestContext;
 import plugin.lsttokens.template.SubregionToken;
 import plugin.lsttokens.testsupport.CDOMTokenLoader;
 
@@ -71,45 +69,4 @@ public class SubregionIntegrationTest extends
 	{
 		// Just to get Eclipse to recognize this as a JUnit 4.0 Test Case
 	}
-
-	@Test
-	public void testRoundRobinSpecialCaseOne() throws PersistenceLayerException
-	{
-		verifyCleanStart();
-		TestContext tc = new TestContext();
-		commit(testCampaign, tc, "YES");
-		commit(modCampaign, tc, "Yarra Valley");
-		completeRoundRobin(tc);
-	}
-
-	@Test
-	public void testRoundRobinSpecialCaseTwo() throws PersistenceLayerException
-	{
-		verifyCleanStart();
-		TestContext tc = new TestContext();
-		commit(testCampaign, tc, "Yarra Valley");
-		commit(modCampaign, tc, "YES");
-		completeRoundRobin(tc);
-	}
-
-	@Test
-	public void testRoundRobinSpecialNoSet() throws PersistenceLayerException
-	{
-		verifyCleanStart();
-		TestContext tc = new TestContext();
-		emptyCommit(testCampaign, tc);
-		commit(modCampaign, tc, "YES");
-		completeRoundRobin(tc);
-	}
-
-	@Test
-	public void testRoundRobinSpecialNoReset() throws PersistenceLayerException
-	{
-		verifyCleanStart();
-		TestContext tc = new TestContext();
-		commit(testCampaign, tc, "YES");
-		emptyCommit(modCampaign, tc);
-		completeRoundRobin(tc);
-	}
-
 }
