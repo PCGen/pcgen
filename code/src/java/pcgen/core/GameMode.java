0ac92d1a9b197c4de3e77815228292b6877efd8f
Eitan Adler <lists@eitanadler.com>
2018-12-07 19:08:33 -0800

    Simplify string empty check

--- code/src/java/pcgen/core/GameMode.java
+++ code/src/java/pcgen/core/GameMode.java
@@ -2359,7 +2359,7 @@ public final class GameMode implements Comparable<Object>, GameModeFacade
 	 */
 	public String getDefaultXPTableName()
 	{
-		if (defaultXPTableName == null || defaultXPTableName.equals("") || !xpTableNames.contains(defaultXPTableName))
+		if (defaultXPTableName == null || defaultXPTableName.isEmpty() || !xpTableNames.contains(defaultXPTableName))
 		{
 			if (xpTableNames.isEmpty())
 			{
@@ -2407,7 +2407,7 @@ public final class GameMode implements Comparable<Object>, GameModeFacade
 	 */
 	public String getDefaultCharacterType()
 	{
-		if (defaultCharacterType == null || defaultCharacterType.equals("")
+		if (defaultCharacterType == null || defaultCharacterType.isEmpty()
 			|| !characterTypeList.contains(defaultCharacterType))
 		{
 			if (characterTypeList.isEmpty())
