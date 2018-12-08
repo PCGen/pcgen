PCGen/pcgen

commit 832c919751d7d41638ae1c1b20978f8dfb5ea72e

Cleanup - mostly eliminated unnecessary casts

code/src/java/pcgen/gui2/facade/CharacterLevelsFacadeImpl.java
@@ -357,10 +357,7 @@ public class CharacterLevelsFacadeImpl extends AbstractListFacade<CharacterLevel
 		{
 			final String classKeyName = charDisplay.getLevelInfoClassKeyName(getLevelIndex(level));
 			PCClass aClass = theCharacter.getClassKeyed(classKeyName);
-			if (skill instanceof Skill)
-			{
-				return theCharacter.getSkillCostForClass((Skill) skill, aClass);
-			}
+			return theCharacter.getSkillCostForClass(skill, aClass);
 		}
 
 		return null;
@@ -390,36 +387,23 @@ public class CharacterLevelsFacadeImpl extends AbstractListFacade<CharacterLevel
 	@Override
 	public int getSkillModifier(CharacterLevelFacade level, Skill skill)
 	{
-		if (skill instanceof Skill)
-		{
-			return SkillModifier.modifier((Skill) skill, theCharacter);
-		}
-		return 0;
+		return SkillModifier.modifier(skill, theCharacter);
 	}
 
 	@Override
 	public float getSkillRanks(CharacterLevelFacade level, Skill skill)
 	{
 		// TODO Ranks aren't stored by level - have compromised by returning the total. Further discussion needed 
-		if (skill instanceof Skill)
-		{
-			return SkillRankControl.getTotalRank(theCharacter, (Skill) skill);
-		}
-		return 0;
+		return SkillRankControl.getTotalRank(theCharacter, skill);
 	}
 
 	@Override
 	public int getSkillTotal(CharacterLevelFacade level, Skill skill)
 	{
 		// TODO Ranks aren't stored by level - have compromised by returning the total. Further discussion needed 
-		if (skill instanceof Skill)
-		{
-			Float ranks = SkillRankControl.getTotalRank(theCharacter, (Skill) skill);
-			Integer mods = SkillModifier.modifier((Skill) skill, theCharacter);
-			return mods.intValue() + ranks.intValue();
-		}
-
-		return 0;
+		Float ranks = SkillRankControl.getTotalRank(theCharacter, skill);
+		Integer mods = SkillModifier.modifier(skill, theCharacter);
+		return mods.intValue() + ranks.intValue();
 	}
 
 	@Override
@@ -427,12 +411,9 @@ public class CharacterLevelsFacadeImpl extends AbstractListFacade<CharacterLevel
 	{
 		SkillBreakdown sb = new SkillBreakdown();
 		// TODO Ranks aren't stored by level - have compromised by returning the total. Further discussion needed 
-		if (skill instanceof Skill)
-		{
-			sb.ranks = SkillRankControl.getTotalRank(theCharacter, (Skill) skill);
-			sb.modifier = SkillModifier.modifier((Skill) skill, theCharacter);
-			sb.total = sb.modifier + (int) sb.ranks;
-		}
+		sb.ranks = SkillRankControl.getTotalRank(theCharacter, skill);
+		sb.modifier = SkillModifier.modifier(skill, theCharacter);
+		sb.total = sb.modifier + (int) sb.ranks;
 		return sb;
 	}
 
@@ -520,24 +501,22 @@ public class CharacterLevelsFacadeImpl extends AbstractListFacade<CharacterLevel
 		final double cost = sc.getCost();
 		double rank = points / cost;
 
-		Skill aSkill = (Skill) skill;
-
-		boolean hasSkill = charDisplay.hasSkill(aSkill);
+		boolean hasSkill = charDisplay.hasSkill(skill);
 		if (!hasSkill)
 		{
-			SkillDisplay.updateSkillsOutputOrder(theCharacter, aSkill);
+			SkillDisplay.updateSkillsOutputOrder(theCharacter, skill);
 		}
 
 		final String classKeyName = charDisplay.getLevelInfoClassKeyName(getLevelIndex(level));
 		PCClass aClass = theCharacter.getClassKeyed(classKeyName);
-		String errMessage = SkillRankControl.modRanks(rank, aClass, false, theCharacter, aSkill);
+		String errMessage = SkillRankControl.modRanks(rank, aClass, false, theCharacter, skill);
 
 		if ("".equals(errMessage)) //$NON-NLS-1$
 		{
 			classLevel.setSkillPointsRemaining(skillPool - points);
 		}
 
-		if (ChooseActivation.hasNewChooseToken(aSkill) && characterFacadeImpl != null)
+		if (ChooseActivation.hasNewChooseToken(skill) && characterFacadeImpl != null)
 		{
 			characterFacadeImpl.postLevellingUpdates();
 		}
@@ -558,11 +537,10 @@ public class CharacterLevelsFacadeImpl extends AbstractListFacade<CharacterLevel
 	@Override
 	public CharacterLevelFacade findNextLevelForSkill(Skill skill, CharacterLevelFacade baseLevel, float newRank)
 	{
-		Skill aSkill = (Skill) skill;
-		SkillCost skillCost = getSkillCost(baseLevel, aSkill);
-		float maxRanks = getMaxRanks(baseLevel, skillCost, isClassSkillForMaxRanks(baseLevel, aSkill));
+		SkillCost skillCost = getSkillCost(baseLevel, skill);
+		float maxRanks = getMaxRanks(baseLevel, skillCost, isClassSkillForMaxRanks(baseLevel, skill));
 
-		float currRank = SkillRankControl.getTotalRank(theCharacter, aSkill);
+		float currRank = SkillRankControl.getTotalRank(theCharacter, skill);
 		if (newRank < currRank)
 		{
 			// 1. Selected level (if class had purchased a rank and is not above max ranks)
@@ -576,7 +554,7 @@ public class CharacterLevelsFacadeImpl extends AbstractListFacade<CharacterLevel
 			// selected level in which the rank to be removed is below max ranks and 
 			// is a class that has bought ranks in the class
 			CharacterLevelFacade levelToRefundSkill =
-					scanForLevelToRefundSkill(aSkill, currRank, (PCClass) getClassTaken(baseLevel));
+					scanForLevelToRefundSkill(skill, currRank, (PCClass) getClassTaken(baseLevel));
 			if (levelToRefundSkill != null)
 			{
 				return levelToRefundSkill;
@@ -585,7 +563,7 @@ public class CharacterLevelsFacadeImpl extends AbstractListFacade<CharacterLevel
 			// 3. Scan from level 1 for first level of any class in which the rank 
 			// to be removed is below max ranks and is a class that has bought 
 			// ranks in the class
-			levelToRefundSkill = scanForLevelToRefundSkill(aSkill, currRank, null);
+			levelToRefundSkill = scanForLevelToRefundSkill(skill, currRank, null);
 			return levelToRefundSkill;
 		}
 
@@ -598,25 +576,25 @@ public class CharacterLevelsFacadeImpl extends AbstractListFacade<CharacterLevel
 		// Check for class cost on this level or higher
 		int baseLevelIndex = getLevelIndex(baseLevel);
 		CharacterLevelFacade levelToBuySkill =
-				scanForwardforLevelToBuySkill(aSkill, newRank, baseLevelIndex, SkillCost.CLASS);
+				scanForwardforLevelToBuySkill(skill, newRank, baseLevelIndex, SkillCost.CLASS);
 		if (levelToBuySkill != null)
 		{
 			return levelToBuySkill;
 		}
 		// Check for class cost on any level
-		levelToBuySkill = scanForwardforLevelToBuySkill(aSkill, newRank, 0, SkillCost.CLASS);
+		levelToBuySkill = scanForwardforLevelToBuySkill(skill, newRank, 0, SkillCost.CLASS);
 		if (levelToBuySkill != null)
 		{
 			return levelToBuySkill;
 		}
 		// Check for any cost on this level or higher
-		levelToBuySkill = scanForwardforLevelToBuySkill(aSkill, newRank, baseLevelIndex, null);
+		levelToBuySkill = scanForwardforLevelToBuySkill(skill, newRank, baseLevelIndex, null);
 		if (levelToBuySkill != null)
 		{
 			return levelToBuySkill;
 		}
 		// Check for any cost on any level
-		levelToBuySkill = scanForwardforLevelToBuySkill(aSkill, newRank, 0, null);
+		levelToBuySkill = scanForwardforLevelToBuySkill(skill, newRank, 0, null);
 
 		return levelToBuySkill;
 	}
@@ -715,7 +693,7 @@ public class CharacterLevelsFacadeImpl extends AbstractListFacade<CharacterLevel
 	 */
 	private boolean classHasRanksIn(Skill skill, ClassFacade pcClass)
 	{
-		Double rank = theCharacter.getSkillRankForClass((Skill) skill, (PCClass) pcClass);
+		Double rank = theCharacter.getSkillRankForClass(skill, (PCClass) pcClass);
 		return (rank != null) && (rank > 0.0d);
 	}
 

code/src/java/pcgen/gui2/facade/CharacterFacadeImpl.java
@@ -33,11 +33,11 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Objects;
 import java.util.Set;
 
 import javax.swing.undo.UndoManager;
 
-import org.apache.commons.lang3.ObjectUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import pcgen.cdom.base.AssociatedPrereqObject;
@@ -346,14 +346,7 @@ public class CharacterFacadeImpl
 		statScoreMap = new HashMap<>();
 		for (PCStat stat : dataSet.getStats())
 		{
-			if (stat instanceof PCStat)
-			{
-				statScoreMap.put(stat, getStatReferenceFacade(stat));
-			}
-			else
-			{
-				statScoreMap.put(stat, new DefaultReferenceFacade<>());
-			}
+			statScoreMap.put(stat, getStatReferenceFacade(stat));
 		}
 
 		File portraitFile = null;
@@ -484,7 +477,7 @@ public class CharacterFacadeImpl
 
 	private WriteableReferenceFacade<Number> getStatReferenceFacade(PCStat stat)
 	{
-		return ChannelCompatibility.getStatScore(theCharacter.getCharID(), (PCStat) stat);
+		return ChannelCompatibility.getStatScore(theCharacter.getCharID(), stat);
 	}
 
 	/**
@@ -1432,9 +1425,9 @@ public class CharacterFacadeImpl
 	@Override
 	public int getModTotal(PCStat stat)
 	{
-		if (stat instanceof PCStat && !charDisplay.isNonAbility((PCStat) stat))
+		if (!charDisplay.isNonAbility(stat))
 		{
-			return theCharacter.getStatModFor((PCStat) stat);
+			return theCharacter.getStatModFor(stat);
 		}
 		return 0;
 	}
@@ -1454,43 +1447,30 @@ public class CharacterFacadeImpl
 	@Override
 	public int getScoreBase(PCStat stat)
 	{
-		if (!(stat instanceof PCStat))
-		{
-			return 0;
-		}
-		return theCharacter.getBaseStatFor((PCStat) stat);
+		return theCharacter.getBaseStatFor(stat);
 	}
 
 	@Override
 	public String getScoreTotalString(PCStat stat)
 	{
-		if (!(stat instanceof PCStat))
-		{
-			return "";
-		}
-		if (charDisplay.isNonAbility((PCStat) stat))
+		if (charDisplay.isNonAbility(stat))
 		{
 			return "*"; //$NON-NLS-1$
 		}
 
-		return SettingsHandler.getGame().getStatDisplayText(theCharacter.getTotalStatFor((PCStat) stat));
+		return SettingsHandler.getGame().getStatDisplayText(theCharacter.getTotalStatFor(stat));
 	}
 
 	@Override
 	public int getScoreRaceBonus(PCStat stat)
 	{
-		if (!(stat instanceof PCStat))
-		{
-			return 0;
-		}
-		PCStat activeStat = (PCStat) stat;
-		if (charDisplay.isNonAbility(activeStat))
+		if (charDisplay.isNonAbility(stat))
 		{
 			return 0;
 		}
 
-		int rBonus = (int) theCharacter.getRaceBonusTo("STAT", activeStat.getKeyName()); //$NON-NLS-1$
-		rBonus += (int) theCharacter.getBonusDueToType("STAT", activeStat.getKeyName(), "RACIAL");
+		int rBonus = (int) theCharacter.getRaceBonusTo("STAT", stat.getKeyName()); //$NON-NLS-1$
+		rBonus += (int) theCharacter.getBonusDueToType("STAT", stat.getKeyName(), "RACIAL");
 
 		return rBonus;
 	}
@@ -1498,20 +1478,15 @@ public class CharacterFacadeImpl
 	@Override
 	public int getScoreOtherBonus(PCStat stat)
 	{
-		if (!(stat instanceof PCStat))
-		{
-			return 0;
-		}
-		PCStat activeStat = (PCStat) stat;
-		if (charDisplay.isNonAbility(activeStat))
+		if (charDisplay.isNonAbility(stat))
 		{
 			return 0;
 		}
 
-		int iRace = (int) theCharacter.getRaceBonusTo("STAT", activeStat.getKeyName()); //$NON-NLS-1$
-		iRace += (int) theCharacter.getBonusDueToType("STAT", activeStat.getKeyName(), "RACIAL");
+		int iRace = (int) theCharacter.getRaceBonusTo("STAT", stat.getKeyName()); //$NON-NLS-1$
+		iRace += (int) theCharacter.getBonusDueToType("STAT", stat.getKeyName(), "RACIAL");
 
-		return theCharacter.getTotalStatFor(activeStat) - theCharacter.getBaseStatFor(activeStat) - iRace;
+		return theCharacter.getTotalStatFor(stat) - theCharacter.getBaseStatFor(stat) - iRace;
 	}
 
 	@Override
@@ -2330,7 +2305,7 @@ public class CharacterFacadeImpl
 		{
 			chooserList.addElement(
 				new LanguageChooserFacadeImpl(this, LanguageBundle.getString("in_sumLangSkill"), //$NON-NLS-1$
-				(Skill) speakLangSkill));
+				speakLangSkill));
 		}
 		return chooserList;
 	}
@@ -2366,7 +2341,7 @@ public class CharacterFacadeImpl
 		}
 		else if (languages.containsElement(lang) && !isAutomatic(lang))
 		{
-			return (Skill) dataSet.getSpeakLanguageSkill();
+			return dataSet.getSpeakLanguageSkill();
 		}
 		return null;
 	}
@@ -2540,7 +2515,7 @@ public class CharacterFacadeImpl
 	public void setHanded(Handed handedness)
 	{
 		this.handedness.set(handedness);
-		theCharacter.setHanded((Handed) handedness);
+		theCharacter.setHanded(handedness);
 	}
 
 	@Override
@@ -3005,10 +2980,9 @@ public class CharacterFacadeImpl
 			for (PCStat stat : statScoreMap.keySet())
 			{
 				WriteableReferenceFacade<Number> score = statScoreMap.get(stat);
-				if (score.get().intValue() < SettingsHandler.getGame().getPurchaseScoreMin(theCharacter)
-					&& stat instanceof PCStat)
+				if (score.get().intValue() < SettingsHandler.getGame().getPurchaseScoreMin(theCharacter))
 				{
-					setStatToPurchaseNeutral((PCStat) stat, score);
+					setStatToPurchaseNeutral(stat, score);
 				}
 			}
 
@@ -3254,7 +3228,7 @@ public class CharacterFacadeImpl
 
 		EquipmentBuilderFacadeImpl builder = new EquipmentBuilderFacadeImpl(newEquip, theCharacter, delegate);
 		CustomEquipResult result = delegate.showCustomEquipDialog(this, builder);
-		if (newEquip != null && result != CustomEquipResult.CANCELLED)
+		if (result != CustomEquipResult.CANCELLED)
 		{
 			dataSet.addEquipment(newEquip);
 		}
@@ -3887,7 +3861,7 @@ public class CharacterFacadeImpl
 		public void set(Rectangle rect)
 		{
 			Rectangle old = get();
-			if (ObjectUtils.equals(old, rect))
+			if (Objects.equals(old, rect))
 			{
 				return;
 			}

code/src/java/pcgen/gui2/facade/SpellBuilderFacadeImpl.java
@@ -373,7 +373,7 @@ public class SpellBuilderFacadeImpl implements SpellBuilderFacade
 				else
 				// must have books
 				{
-					if (!(obj instanceof PCClass) || !obj.getSafe(ObjectKey.SPELLBOOK))
+					if (!obj.getSafe(ObjectKey.SPELLBOOK))
 					{
 						classes.remove(i);
 					}

code/src/java/pcgen/gui2/tabs/equip/UnequippedList.java
@@ -39,7 +39,6 @@ public class UnequippedList extends AbstractListFacade<EquipmentFacade> implemen
 		EquipmentListListener, ListListener<EquipmentFacade>, ReferenceListener<EquipmentSetFacade>
 {
 
-	private final CharacterFacade character;
 	private final EquipmentListFacade purchasedList;
 	private final List<EquipmentFacade> equipmentList;
 	private final Map<EquipmentFacade, Integer> quantityMap;
@@ -48,7 +47,6 @@ public class UnequippedList extends AbstractListFacade<EquipmentFacade> implemen
 
 	public UnequippedList(CharacterFacade character)
 	{
-		this.character = character;
 		this.purchasedList = character.getPurchasedEquipment();
 		equipmentList = new ArrayList<>();
 		quantityMap = new HashMap<>();

code/src/java/pcgen/gui2/util/table/IntegerEditor.java
@@ -68,8 +68,8 @@ public class IntegerEditor extends DefaultCellEditor
 	{
 		super(new JFormattedTextField());
 		ftf = (JFormattedTextField) getComponent();
-		minimum = new Integer(min);
-		maximum = new Integer(max);
+		minimum = Integer.valueOf(min);
+		maximum = Integer.valueOf(max);
 
 		//Set up the editor for the integer cells.
 		integerFormat = NumberFormat.getIntegerInstance();
@@ -138,7 +138,7 @@ public class IntegerEditor extends DefaultCellEditor
 		}
 		else if (o instanceof Number)
 		{
-			return new Integer(((Number) o).intValue());
+			return Integer.valueOf(((Number) o).intValue());
 		}
 		else
 		{

commit 7b2f4a8e41e9f7a3b66e049d29d4d130c1882435

Unbreak character creation
This was broken in 832c919751d ("Cleanup - mostly eliminated unnecessary
casts") which removed a hidden null check. While here sprinkle a bit of
assertions around to avoid this type of issue in the future.

code/src/java/pcgen/core/analysis/SkillRankControl.java
@@ -20,8 +20,7 @@ package pcgen.core.analysis;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import org.apache.commons.lang3.StringUtils;
+import java.util.Objects;
 
 import pcgen.cdom.base.CDOMObjectUtilities;
 import pcgen.cdom.base.PersistentTransitionChoice;
@@ -42,6 +41,9 @@ import pcgen.core.utils.CoreUtility;
 import pcgen.util.Logging;
 import pcgen.util.enumeration.View;
 
+
+import org.apache.commons.lang3.StringUtils;
+
 public final class SkillRankControl
 {
 
@@ -59,6 +61,7 @@ public final class SkillRankControl
 	 */
 	public static Float getTotalRank(PlayerCharacter pc, Skill sk)
 	{
+		Objects.requireNonNull(sk);
 		if (pc == null)
 		{
 			Logging.errorPrint("Asked to get total rank for null character. Location was ", new Throwable());

code/src/java/pcgen/gui2/facade/CharacterLevelsFacadeImpl.java
@@ -22,6 +22,7 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 import pcgen.cdom.base.Constants;
 import pcgen.cdom.enumeration.CharID;
@@ -53,6 +54,9 @@ import pcgen.facade.util.AbstractListFacade;
 import pcgen.system.LanguageBundle;
 import pcgen.util.Logging;
 import pcgen.util.enumeration.Tab;
+
+
+import org.jetbrains.annotations.Nullable;
 
 /**
  * The Class {@code CharacterLevelsFacadeImpl} is an implementation of
@@ -390,19 +394,23 @@ public class CharacterLevelsFacadeImpl extends AbstractListFacade<CharacterLevel
 	}
 
 	@Override
-	public float getSkillRanks(CharacterLevelFacade level, Skill skill)
+	public float getSkillRanks(@Nullable CharacterLevelFacade level, @Nullable Skill skill)
 	{
-		// TODO Ranks aren't stored by level - have compromised by returning the total. Further discussion needed 
+		// TODO Ranks aren't stored by level - have compromised by returning the total. Further discussion needed
+		if (skill == null)
+		{
+			return 0;
+		}
 		return SkillRankControl.getTotalRank(theCharacter, skill);
 	}
 
 	@Override
 	public int getSkillTotal(CharacterLevelFacade level, Skill skill)
 	{
-		// TODO Ranks aren't stored by level - have compromised by returning the total. Further discussion needed 
+		// TODO Ranks aren't stored by level - have compromised by returning the total. Further discussion needed
 		Float ranks = SkillRankControl.getTotalRank(theCharacter, skill);
 		Integer mods = SkillModifier.modifier(skill, theCharacter);
-		return mods.intValue() + ranks.intValue();
+		return mods + ranks.intValue();
 	}
 
 	@Override

code/src/java/pcgen/gui2/tabs/SkillInfoTab.java
@@ -179,6 +179,7 @@ public class SkillInfoTab extends FlippingSplitPane implements CharacterInfoTab,
 	@Override
 	public ModelMap createModels(final CharacterFacade character)
 	{
+		Objects.requireNonNull(character);
 		ModelMap models = new ModelMap();
 
 		ListSelectionModel listModel = new DefaultListSelectionModel();