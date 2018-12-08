c64a0eeb27885e689baa49aa7e837eba8b87c0f0
Eitan Adler <grimreaper@users.noreply.github.com>
2018-12-07 20:05:16 -0800

    Use Collections.singletonList where possible (#4635)

--- code/src/java/pcgen/core/GameMode.java
+++ code/src/java/pcgen/core/GameMode.java
@@ -2248,7 +2248,7 @@ public final class GameMode implements Comparable<Object>, GameModeFacade
 	 */
 	public List<String> getMonsterRoleDefaultList()
 	{
-		return new ArrayList<>(Arrays.asList(monsterRoleDefault));
+		return new ArrayList<>(Collections.singletonList(monsterRoleDefault));
 	}
 
 	/**
