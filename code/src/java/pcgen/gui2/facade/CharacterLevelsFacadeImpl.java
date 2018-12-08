08bdc7e59cd65de420775de5b1527474c0808580
thpr <thpr@users.noreply.github.com>
2018-12-08 09:26:00 -0500

    Update CharacterLevelsFacadeImpl.java

--- code/src/java/pcgen/gui2/facade/CharacterLevelsFacadeImpl.java
+++ code/src/java/pcgen/gui2/facade/CharacterLevelsFacadeImpl.java
@@ -22,7 +22,6 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 import pcgen.cdom.base.Constants;
 import pcgen.cdom.enumeration.CharID;
