01976c0d168533a6bef2d8899ced3136b1674699
Eitan Adler <lists@eitanadler.com>
2018-12-29 12:00:00 -0700

    CODE-3200: add Locale.ENGLISH to computer-read values (id: 13:27782)

--- code/src/java/plugin/pretokens/writer/PreSubClassWriter.java
+++ code/src/java/plugin/pretokens/writer/PreSubClassWriter.java
@@ -20,6 +20,7 @@ package plugin.pretokens.writer;
 
 import java.io.IOException;
 import java.io.Writer;
+import java.util.Locale;
 
 import pcgen.core.prereq.Prerequisite;
 import pcgen.core.prereq.PrerequisiteOperator;
@@ -79,7 +80,7 @@ public class PreSubClassWriter extends AbstractPrerequisiteWriter implements Pre
 			writer.write('!');
 		}
 
-		writer.write("PRE" + kindHandled().toUpperCase() + ':' + (prereq.isOverrideQualify() ? "Q:" : ""));
+		writer.write("PRE" + kindHandled().toUpperCase(Locale.ENGLISH) + ':' + (prereq.isOverrideQualify() ? "Q:" : ""));
 		writer.write(po.equals(PrerequisiteOperator.GTEQ) ? prereq.getOperand() : "1");
 		for (Prerequisite p : prereq.getPrerequisites())
 		{
--- code/src/java/plugin/pretokens/writer/PreTypeWriter.java
+++ code/src/java/plugin/pretokens/writer/PreTypeWriter.java
@@ -20,6 +20,7 @@ package plugin.pretokens.writer;
 
 import java.io.IOException;
 import java.io.Writer;
+import java.util.Locale;
 
 import pcgen.core.prereq.Prerequisite;
 import pcgen.core.prereq.PrerequisiteOperator;
@@ -131,7 +132,7 @@ public class PreTypeWriter extends AbstractPrerequisiteWriter implements Prerequ
 			writer.write('!');
 		}
 
-		writer.write("PRE" + kindHandled().toUpperCase() + ':' + (prereq.isOverrideQualify() ? "Q:" : ""));
+		writer.write("PRE" + kindHandled().toUpperCase(Locale.ENGLISH) + ':' + (prereq.isOverrideQualify() ? "Q:" : ""));
 		writer.write(oper.equals(PrerequisiteOperator.EQ) ? prereq.getOperand() : "1");
 		for (Prerequisite p : prereq.getPrerequisites())
 		{
