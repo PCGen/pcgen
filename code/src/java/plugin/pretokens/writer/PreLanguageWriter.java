48d825f8d3956d6e1bb2b49977b8d117bc0ed4d8
Eitan Adler <grimreaper@users.noreply.github.com>
2018-12-31 14:56:42 -0800

    CODE-3200: add Locale.ENGLISH to computer-read values (id: 11:12590) (#4775)

--- code/src/java/plugin/pretokens/writer/PreLanguageWriter.java
+++ code/src/java/plugin/pretokens/writer/PreLanguageWriter.java
@@ -20,6 +20,7 @@ package plugin.pretokens.writer;
 
 import java.io.IOException;
 import java.io.Writer;
+import java.util.Locale;
 
 import pcgen.core.prereq.Prerequisite;
 import pcgen.core.prereq.PrerequisiteOperator;
@@ -78,7 +79,7 @@ public class PreLanguageWriter extends AbstractPrerequisiteWriter implements Pre
 			writer.write('!');
 		}
 
-		writer.write("PRE" + kindHandled().toUpperCase() + ':' + (prereq.isOverrideQualify() ? "Q:" : ""));
+		writer.write("PRE" + kindHandled().toUpperCase(Locale.ENGLISH) + ':' + (prereq.isOverrideQualify() ? "Q:" : ""));
 		writer.write(po.equals(PrerequisiteOperator.GTEQ) ? prereq.getOperand() : "1");
 		for (Prerequisite p : prereq.getPrerequisites())
 		{
