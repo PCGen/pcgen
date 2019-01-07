26ffcb732d7cbaf937f6501cddb0acdc2f220f60
Eitan Adler <lists@eitanadler.com>
2019-01-06 22:57:59 -0800

    AbstractListFacetTest+friends: convert to JUnuit 5

--- code/src/utest/pcgen/cdom/facet/fact/ChronicleEntryFacetTest.java
+++ code/src/utest/pcgen/cdom/facet/fact/ChronicleEntryFacetTest.java
@@ -17,6 +17,11 @@
  */
 package pcgen.cdom.facet.fact;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
