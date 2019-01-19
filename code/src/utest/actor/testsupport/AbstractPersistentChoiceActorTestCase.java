590ca52e482812c62cb8452d28d69d269471651e
Eitan Adler <lists@eitanadler.com>
2019-01-18 19:54:57 -0800

    part 8

--- code/src/utest/actor/testsupport/AbstractPersistentChoiceActorTestCase.java
+++ code/src/utest/actor/testsupport/AbstractPersistentChoiceActorTestCase.java
@@ -17,8 +17,8 @@
  */
 package actor.testsupport;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import pcgen.cdom.base.Persistent;
 import pcgen.core.Globals;
@@ -26,18 +26,8 @@ import pcgen.core.SettingsHandler;
 import pcgen.rules.context.LoadContext;
 import plugin.lsttokens.testsupport.BuildUtilities;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.fail;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeEach;
 
 public abstract class AbstractPersistentChoiceActorTestCase<T>
 {
