b2e98197bfa9c7a26eb6bb6f10e4fabc37d17200
Eitan Adler <lists@eitanadler.com>
2019-02-03 10:13:42 -0800

    unbreak

--- code/src/test/pcgen/core/EquipmentTest.java
+++ code/src/test/pcgen/core/EquipmentTest.java
@@ -22,6 +22,9 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.not;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.math.BigDecimal;
