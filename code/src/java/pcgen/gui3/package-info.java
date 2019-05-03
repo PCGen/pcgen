d2963d07c281ad2659c61fe76c6ea27cbdd0a7cc
Eitan Adler <lists@eitanadler.com>
2019-05-02 23:04:03 -0700

    [PCGenFrame] Use JavaFx browser for "simple" HTML content.

---
+++ code/src/java/pcgen/gui3/package-info.java
@@ -0,0 +1,9 @@
+/*
+ * Contains code for a JavaFX based UI.
+ * Includes transition code to switch from Swing.
+ */
+@DefaultAnnotation(NonNull.class)
+package pcgen.gui3;
+
+import edu.umd.cs.findbugs.annotations.DefaultAnnotation;
+import edu.umd.cs.findbugs.annotations.NonNull;
