21c27b076fd62a5bd69b057c3a09ab69b78fb33c
Eitan Adler <grimreaper@users.noreply.github.com>
2019-05-06 19:49:29 -0700

    [PCGenFrame] Use JavaFx browser for "simple" HTML content. (#5442)

---
+++ code/src/java/pcgen/gui3/package-info.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2019 (C) Eitan Adler <lists@eitanadler.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.     See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+/*
+ * Contains code for a JavaFX based UI.
+ * Includes transition code to switch from Swing.
+ */
+package pcgen.gui3;
