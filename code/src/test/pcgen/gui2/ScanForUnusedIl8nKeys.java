704b7d10000a32ffc1a0cdd7e3c66441bdf358f3
Tom <Tom@DESKTOP-NUKKUHT>
2019-05-07 22:27:34 -0500

    Remove unused import

--- code/src/test/pcgen/gui2/ScanForUnusedIl8nKeys.java
+++ code/src/test/pcgen/gui2/ScanForUnusedIl8nKeys.java
@@ -31,7 +31,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.Clock;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collection;
