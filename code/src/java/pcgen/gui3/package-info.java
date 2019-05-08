021e3fe0b519c050b10dad33088a942b729f190b
Eitan Adler <grimreaper@users.noreply.github.com>
2019-05-07 20:28:09 -0700

    [gui] make new gui3 code stricter w.r.t. code standards (#5484)
    - Make non-null the default

--- code/src/java/pcgen/gui3/package-info.java
+++ code/src/java/pcgen/gui3/package-info.java
@@ -20,4 +20,9 @@
  * Contains code for a JavaFX based UI.
  * Includes transition code to switch from Swing.
  */
+
+@DefaultAnnotation(NonNull.class)
 package pcgen.gui3;
+
+import edu.umd.cs.findbugs.annotations.DefaultAnnotation;
+import edu.umd.cs.findbugs.annotations.NonNull;
