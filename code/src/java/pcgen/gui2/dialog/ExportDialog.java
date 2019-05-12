PCGen/pcgen

commit 96dcdde641c62b43c4750fdc9cb52cbc8e35f83a

reduce reliance on commons-io
This is useful since most of what we're using commos-io
for is actually availble in native jdk

code/src/java/pcgen/gui2/dialog/ExportDialog.java
@@ -22,16 +22,20 @@ import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Container;
 import java.awt.Desktop;
+import java.awt.Window;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FilenameFilter;
 import java.io.IOException;
 import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
 
 import javax.swing.BorderFactory;
 import javax.swing.Box;
@@ -71,11 +75,8 @@ import pcgen.system.ConfigurationSettings;
 import pcgen.system.PCGenSettings;
 import pcgen.util.Logging;
 
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.filefilter.FileFilterUtils;
 import org.apache.commons.io.filefilter.IOFileFilter;
-import org.apache.commons.io.filefilter.SuffixFileFilter;
-import org.apache.commons.io.filefilter.TrueFileFilter;
 import org.apache.commons.lang3.BooleanUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.SystemUtils;
@@ -97,7 +98,7 @@ public final class ExportDialog extends JDialog implements ActionListener, ListS
 
 	public static void showExportDialog(PCGenFrame parent)
 	{
-		ExportDialog dialog = new ExportDialog(parent);
+		Window dialog = new ExportDialog(parent);
 		Utility.setComponentRelativeLocation(parent, dialog);
 		dialog.setVisible(true);
 	}
@@ -472,7 +473,6 @@ public final class ExportDialog extends JDialog implements ActionListener, ListS
 		if (outputSheetDirectory == null)
 		{
 			osDir = new File(ConfigurationSettings.getOutputSheetsDir());
-			outputSheetDirectory = "";
 		}
 		else
 		{
@@ -633,7 +633,7 @@ public final class ExportDialog extends JDialog implements ActionListener, ListS
 	{
 
 		@Override
-		protected Collection<File> doInBackground() throws Exception
+		protected Collection<File> doInBackground() throws IOException
 		{
 			File dir;
 			String outputSheetDirectory = SettingsHandler.getGame().getOutputSheetDirectory();
@@ -641,7 +641,6 @@ public final class ExportDialog extends JDialog implements ActionListener, ListS
 			{
 				Logging.errorPrint("OUTPUTSHEET|DIRECTORY not defined for game mode " + SettingsHandler.getGame());
 				dir = new File(ConfigurationSettings.getOutputSheetsDir());
-				outputSheetDirectory = "";
 			}
 			else
 			{
@@ -651,7 +650,6 @@ public final class ExportDialog extends JDialog implements ActionListener, ListS
 					Logging.errorPrint(
 						"Unable to find game mode outputsheets at " + dir.getCanonicalPath() + ". Trying base.");
 					dir = new File(ConfigurationSettings.getOutputSheetsDir());
-					outputSheetDirectory = "";
 				}
 			}
 			if (!dir.isDirectory())
@@ -659,8 +657,10 @@ public final class ExportDialog extends JDialog implements ActionListener, ListS
 				Logging.errorPrint("Unable to find outputsheets folder at " + dir.getCanonicalPath() + ".");
 				return Collections.emptyList();
 			}
-			IOFileFilter fileFilter = FileFilterUtils.notFileFilter(new SuffixFileFilter(".fo"));
-			return FileUtils.listFiles(dir, fileFilter, TrueFileFilter.INSTANCE);
+			return Files.list(dir.toPath())
+			            .filter(f -> !f.endsWith(".fo"))
+			            .map(Path::toFile)
+					    .collect(Collectors.toList());
 		}
 
 		@Override

code/src/java/pcgen/gui2/dialog/PrintPreviewDialog.java
@@ -437,7 +437,7 @@ public final class PrintPreviewDialog extends JDialog implements ActionListener
 		}
 
 		@Override
-		protected Object[] doInBackground() throws Exception
+		protected Object[] doInBackground()
 		{
 			IOFileFilter pdfFilter = FileFilterUtils.asFileFilter(this);
 			IOFileFilter suffixFilter = FileFilterUtils.notFileFilter(new SuffixFileFilter(".fo"));
@@ -448,8 +448,7 @@ public final class PrintPreviewDialog extends JDialog implements ActionListener
 			File dir = new File(ConfigurationSettings.getOutputSheetsDir());
 			Collection<File> files = FileUtils.listFiles(dir, fileFilter, dirFilter);
 			URI osPath = new File(ConfigurationSettings.getOutputSheetsDir()).toURI();
-			Object[] uriList = files.stream().map(v -> osPath.relativize(v.toURI())).toArray();
-			return uriList;
+			return files.stream().map(v -> osPath.relativize(v.toURI())).toArray();
 		}
 
 		@Override

code/src/java/pcgen/gui2/tabs/CompanionInfoTab.java
@@ -27,6 +27,7 @@ import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -97,8 +98,6 @@ import pcgen.system.LanguageBundle;
 import pcgen.util.Comparators;
 import pcgen.util.Logging;
 import pcgen.util.enumeration.Tab;
-
-import org.apache.commons.io.FileUtils;
 
 /**
  * This component allows a user to manage a character's companions (animal,
@@ -312,10 +311,9 @@ public class CompanionInfoTab extends FlippingSplitPane implements CharacterInfo
 
 		public LoadButtonAndSheetHandler()
 		{
-			File sheet =
-					FileUtils.getFile(
+			File sheet = Path.of(
 						ConfigurationSettings.getPreviewDir(),
-						"companions", "compact_companion.htm"); //$NON-NLS-1$ //$NON-NLS-2$
+						"companions", "compact_companion.htm").toFile(); //$NON-NLS-1$ //$NON-NLS-2$
 			this.sheetSupport = new HtmlSheetSupport(infoPane, sheet.getAbsolutePath());
 			this.selectedRow = -1;
 			this.selectionModel = companionsTable.getSelectionModel();

code/src/java/pcgen/io/PCGIOHandler.java
@@ -24,12 +24,14 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.math.BigDecimal;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
@@ -37,6 +39,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import pcgen.cdom.base.Constants;
 import pcgen.cdom.content.CNAbility;
@@ -294,34 +297,16 @@ public final class PCGIOHandler extends IOHandler
 		createBackupForFile(outFile);
 
 		// Now save the character
-		BufferedWriter bw = null;
 
-		try
+		try (FileWriter fileWriter = new FileWriter(outFile, StandardCharsets.UTF_8);
+		     Writer bw = new BufferedWriter(fileWriter))
 		{
-			FileOutputStream out = new FileOutputStream(outFile);
-			bw = new BufferedWriter(new OutputStreamWriter(out, StandardCharsets.UTF_8));
-			bw.write(pcgString);
-			bw.flush();
-
 			pcToBeWritten.setDirty(false);
+			bw.write(pcgString);
 		}
 		catch (IOException ioe)
 		{
 			Logging.errorPrint("Exception in PCGIOHandler::write", ioe);
-		}
-		finally
-		{
-			try
-			{
-				if (bw != null)
-				{
-					bw.close();
-				}
-			}
-			catch (IOException e)
-			{
-				Logging.errorPrint("Couldn't close file in PCGIOHandler.write", e);
-			}
 		}
 	}
 
@@ -522,9 +507,9 @@ public final class PCGIOHandler extends IOHandler
 	public List<File> readCharacterFileList(File partyFile)
 	{
 		List<String> lines;
-		try
+		try (BufferedReader bufferedReader = new BufferedReader(new FileReader(partyFile, StandardCharsets.UTF_8)))
 		{
-			lines = FileUtils.readLines(partyFile, "UTF-8");
+			lines = bufferedReader.lines().collect(Collectors.toList());
 		}
 		catch (IOException ex)
 		{
@@ -592,34 +577,14 @@ public final class PCGIOHandler extends IOHandler
 	 */
 	public SourceSelectionFacade readSources(File pcgFile)
 	{
-		InputStream in = null;
 
-		try
+		try (InputStream in = new FileInputStream(pcgFile))
 		{
-			in = new FileInputStream(pcgFile);
 			return internalReadSources(in);
 		}
 		catch (IOException ex)
 		{
 			Logging.errorPrint("Exception in IOHandler::read when reading", ex);
-		}
-		finally
-		{
-			if (in != null)
-			{
-				try
-				{
-					in.close();
-				}
-				catch (IOException e)
-				{
-					Logging.errorPrint("Exception in IOHandler::readSources", e);
-				}
-				catch (NullPointerException e)
-				{
-					Logging.errorPrint("Could not create file inputStream IOHandler::readSources", e);
-				}
-			}
 		}
 		return null;
 	}
@@ -633,7 +598,7 @@ public final class PCGIOHandler extends IOHandler
 		// Verify it is ver2
 		boolean isPCGVersion2 = isPCGCersion2(lines);
 
-		final String[] pcgLines = lines.toArray(new String[lines.size()]);
+		final String[] pcgLines = lines.toArray(new String[0]);
 
 		if (isPCGVersion2)
 		{

code/src/java/pcgen/persistence/lst/utils/VariableReport.java
@@ -50,7 +50,6 @@ import pcgen.system.ConfigurationSettings;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
-import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 
 /**
@@ -93,14 +92,9 @@ public class VariableReport
 
 		for (Entry<ReportFormat, String> reportRequest : reportNameMap.entrySet())
 		{
-			Writer file = new FileWriter(new File(reportRequest.getValue()), StandardCharsets.UTF_8);
-			try
+			try (Writer file = new FileWriter(new File(reportRequest.getValue()), StandardCharsets.UTF_8))
 			{
 				outputReport(gameModeVarMap, gameModeVarCountMap, reportRequest.getKey(), file);
-			}
-			finally
-			{
-				IOUtils.closeQuietly(file);
 			}
 		}
 	}

code/src/java/pcgen/system/BatchExporter.java
@@ -26,9 +26,11 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
+import java.nio.file.Path;
 
 import pcgen.cdom.base.Constants;
 import pcgen.core.SettingsHandler;
@@ -47,7 +49,6 @@ import pcgen.persistence.SourceFileLoader;
 import pcgen.util.Logging;
 import pcgen.util.fop.FopTask;
 
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.output.TeeOutputStream;
 import org.apache.commons.lang3.StringUtils;
@@ -215,9 +216,9 @@ public class BatchExporter
 				PCGenSettings.OPTIONS_CONTEXT.initBoolean(PCGenSettings.OPTION_GENERATE_TEMP_FILE_WITH_PDF, false);
 		String outFileName = FilenameUtils.removeExtension(outFile.getAbsolutePath());
 		File tempFile = new File(outFileName + (isTransformTemplate ? ".xml" : ".fo"));
-		try (BufferedOutputStream fileStream = new BufferedOutputStream(new FileOutputStream(outFile));
-				ByteArrayOutputStream byteOutputStream = new ByteArrayOutputStream();
-				OutputStream exportOutput = useTempFile
+		try (OutputStream fileStream = new BufferedOutputStream(new FileOutputStream(outFile));
+		     ByteArrayOutputStream byteOutputStream = new ByteArrayOutputStream();
+		     OutputStream exportOutput = useTempFile
 					//Output to both the byte stream and to the temp file.
 					? new TeeOutputStream(byteOutputStream, new FileOutputStream(tempFile)) : byteOutputStream)
 		{
@@ -225,13 +226,13 @@ public class BatchExporter
 			if (isTransformTemplate)
 			{
 				exportCharacter(character, exportOutput);
-				ByteArrayInputStream inputStream = new ByteArrayInputStream(byteOutputStream.toByteArray());
+				InputStream inputStream = new ByteArrayInputStream(byteOutputStream.toByteArray());
 				task = FopTask.newFopTask(inputStream, templateFile, fileStream);
 			}
 			else
 			{
 				exportCharacter(character, templateFile, exportOutput);
-				ByteArrayInputStream inputStream = new ByteArrayInputStream(byteOutputStream.toByteArray());
+				InputStream inputStream = new ByteArrayInputStream(byteOutputStream.toByteArray());
 				task = FopTask.newFopTask(inputStream, null, fileStream);
 			}
 			character.setDefaultOutputSheet(true, templateFile);
@@ -243,12 +244,7 @@ public class BatchExporter
 				return false;
 			}
 		}
-		catch (final IOException e)
-		{
-			Logging.errorPrint("BatchExporter.exportCharacterToPDF failed", e); //$NON-NLS-1$
-			return false;
-		}
-		catch (final ExportException e)
+		catch (final IOException | ExportException e)
 		{
 			Logging.errorPrint("BatchExporter.exportCharacterToPDF failed", e); //$NON-NLS-1$
 			return false;
@@ -361,12 +357,7 @@ public class BatchExporter
 			}
 			task.run();
 		}
-		catch (final IOException e)
-		{
-			Logging.errorPrint("BatchExporter.exportPartyToPDF failed", e);
-			return false;
-		}
-		catch (final ExportException e)
+		catch (final IOException | ExportException e)
 		{
 			Logging.errorPrint("BatchExporter.exportPartyToPDF failed", e);
 			return false;
@@ -390,11 +381,6 @@ public class BatchExporter
 		{
 			party.export(new ExportHandler(templateFile), bw);
 			return true;
-		}
-		catch (final UnsupportedEncodingException e)
-		{
-			Logging.errorPrint("Unable to create output file " + outFile.getAbsolutePath(), e);
-			return false;
 		}
 		catch (final IOException e)
 		{
@@ -517,8 +503,9 @@ public class BatchExporter
 
 	private static File getXMLTemplate(CharacterFacade character)
 	{
-		File template = FileUtils.getFile(ConfigurationSettings.getSystemsDir(), "gameModes",
-			character.getDataSet().getGameMode().getName(), "base.xml.ftl");
+		Path path = Path.of(ConfigurationSettings.getSystemsDir(), "gameModes",
+				character.getDataSet().getGameMode().getName(), "base.xml.ftl");
+		File template = new File(path.toUri());
 		if (!template.exists())
 		{
 			template = new File(ConfigurationSettings.getOutputSheetsDir(), "base.xml.ftl");

code/src/java/plugin/notes/gui/NotesTreeNode.java
@@ -26,6 +26,8 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Enumeration;
@@ -49,8 +51,6 @@ import gmgen.gui.ExtendedHTMLEditorKit;
 import pcgen.cdom.base.Constants;
 import pcgen.system.LanguageBundle;
 import pcgen.util.Logging;
-
-import org.apache.commons.io.FileUtils;
 
 /**
  * This defines the preferences tree
@@ -890,16 +890,13 @@ public class NotesTreeNode implements MutableTreeNode, DocumentListener
 
 		try
 		{
-			List fileList = ((List) t.getTransferData(DataFlavor.javaFileListFlavor));
+			List<File> fileList = ((List<File>) t.getTransferData(DataFlavor.javaFileListFlavor));
 
-			for (Object aFileList : fileList)
+			for (File origFile : fileList)
 			{
-				File newFile = (File) aFileList;
-
-				if (newFile.exists())
+				if (origFile.exists())
 				{
-
-					FileUtils.copyFile(newFile, new File(dir.getAbsolutePath() + File.separator + newFile.getName()));
+					Files.copy(origFile.toPath(), Path.of(dir.getAbsolutePath(), ((File) origFile).getName()));
 				}
 			}
 		}
@@ -987,17 +984,9 @@ public class NotesTreeNode implements MutableTreeNode, DocumentListener
 
 				if (nodeDir.exists())
 				{
-					for (int i = 0; i < childDirs.size(); i++)
-					{
-						File childDir = childDirs.get(i);
-
-						if (nodeDir.getName().equals(childDir.getName()))
-						{
-							removeDirs.add(childDir);
-
-							continue;
-						}
-					}
+					childDirs.stream()
+					         .filter(childDir -> nodeDir.getName().equals(childDir.getName()))
+					         .forEach(removeDirs::add);
 				}
 				else
 				{
@@ -1005,16 +994,10 @@ public class NotesTreeNode implements MutableTreeNode, DocumentListener
 				}
 			}
 
-			for (File childDir : childDirs)
-			{
-				if (!removeDirs.contains(childDir))
-				{
-					if (include(childDir))
-					{
-						add(new NotesTreeNode(childDir.getName(), childDir, tree));
-					}
-				}
-			}
+			childDirs.stream()
+			         .filter(childDir -> !removeDirs.contains(childDir))
+			         .filter(this::include)
+			         .forEach(childDir -> add(new NotesTreeNode(childDir.getName(), childDir, tree)));
 
 			Enumeration<MutableTreeNode> newNodes = children();
 

code/src/java/plugin/notes/gui/NotesView.java
@@ -21,10 +21,8 @@ package plugin.notes.gui;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Cursor;
-import java.awt.Point;
 import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.Transferable;
-import java.awt.dnd.DropTarget;
 import java.awt.dnd.DropTargetAdapter;
 import java.awt.dnd.DropTargetDragEvent;
 import java.awt.dnd.DropTargetDropEvent;
@@ -37,8 +35,11 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.text.DateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Enumeration;
 import java.util.List;
@@ -89,7 +90,6 @@ import javax.swing.text.html.HTML;
 import javax.swing.tree.DefaultTreeModel;
 import javax.swing.tree.MutableTreeNode;
 import javax.swing.tree.TreeModel;
-import javax.swing.tree.TreePath;
 import javax.swing.tree.TreeSelectionModel;
 import javax.swing.undo.CannotRedoException;
 import javax.swing.undo.CannotUndoException;
@@ -111,8 +111,6 @@ import pcgen.system.LanguageBundle;
 import pcgen.util.Logging;
 import plugin.notes.NotesPlugin;
 
-import org.apache.commons.io.FileUtils;
-
 /**
  *  This class is the main view for the Notes Plugin. Mostof the work is done
  *  here and in the NotesTreeNode Class.
@@ -127,21 +125,12 @@ public class NotesView extends JPanel
 
 	private static final String OPTION_NAME_LASTFILE = NotesPlugin.LOG_NAME + ".LastFile"; //$NON-NLS-1$
 
-	/**  Drop Target for the Edit Area */
-	private DropTarget editAreaDT;
-
-	/**  Drop Target for the File Bar */
-	private DropTarget filesBarDT;
-
-	/**  Drop Target for the Tree */
-	private DropTarget treeDT;
-
 	/**  Insert OL Action for JTextPane */
-	private ExtendedHTMLEditorKit.InsertListAction actionListOrdered =
+	private final Action actionListOrdered =
 			new ExtendedHTMLEditorKit.InsertListAction("InsertOLItem", HTML.Tag.OL);
 
 	/**  Insert UL Action for JTextPane */
-	private ExtendedHTMLEditorKit.InsertListAction actionListUnordered =
+	private final Action actionListUnordered =
 			new ExtendedHTMLEditorKit.InsertListAction("InsertULItem", HTML.Tag.UL);
 
 	// End of variables declaration//GEN-END:variables
@@ -1380,8 +1369,6 @@ public class NotesView extends JPanel
 
 	private void initDnDComponents()
 	{
-		filesBarDT = new DropTarget(filesBar, new DropBarListener());
-		treeDT = new DropTarget(notesTree, new DropTreeListener());
 	}
 
 	private void initEditingComponents()
@@ -1507,7 +1494,7 @@ public class NotesView extends JPanel
 	 *@exception  BadLocationException  if the file does not exist
 	 *@exception  RuntimeException      cause
 	 */
-	private void insertLocalImage(File whatImage) throws IOException, BadLocationException, RuntimeException
+	private void insertLocalImage(File whatImage) throws IOException, BadLocationException
 	{
 		File image = whatImage;
 		if (whatImage == null)
@@ -1518,11 +1505,11 @@ public class NotesView extends JPanel
 			//null possible if user cancelled
 			if (newImage != null && newImage.exists())
 			{
-				image = new File(dir.getAbsolutePath() + File.separator + newImage.getName());
+				image = Path.of(dir.getAbsolutePath(), newImage.getName()).toFile();
 
 				if (!image.exists())
 				{
-					FileUtils.copyFile(newImage, image);
+					Files.copy(newImage.toPath(), image.toPath());
 				}
 			}
 		}
@@ -1608,7 +1595,6 @@ public class NotesView extends JPanel
 			JViewport vp = new JViewport();
 			vp.setView(editor);
 			jScrollPane2.setViewport(vp);
-			editAreaDT = new DropTarget(editor, new DropEditorListener());
 			editor.addCaretListener(this::editorCaretUpdate);
 			editor.addKeyListener(new java.awt.event.KeyListener()
 			{
@@ -1884,7 +1870,7 @@ public class NotesView extends JPanel
 		 *@param  dtde  DropTargetDropEvent
 		 *@return       drop successful or not
 		 */
-		boolean handleDropJavaFileListAsImage(DropTargetDropEvent dtde)
+		private boolean handleDropJavaFileListAsImage(DropTargetDropEvent dtde)
 		{
 			dtde.acceptDrop(dtde.getDropAction());
 
@@ -1899,11 +1885,11 @@ public class NotesView extends JPanel
 				{
 					if (newFile.exists())
 					{
-						File destFile = new File(dir.getAbsolutePath() + File.separator + newFile.getName());
+						File destFile = Path.of(dir.getAbsolutePath(), newFile.getName()).toFile();
 
 						if (!isImageFile(destFile) || !destFile.exists())
 						{
-							FileUtils.copyFile(newFile, destFile);
+							Files.copy(newFile.toPath(), destFile.toPath());
 						}
 
 						editor.setCaretPosition(editor.viewToModel(dtde.getLocation()));
@@ -1926,70 +1912,17 @@ public class NotesView extends JPanel
 		 *
 		 *@param  image  File to insert
 		 */
-		void handleImageDropInsertion(File image)
+		private void handleImageDropInsertion(File image)
 		{
-			for (String s : extsIMG)
+			if (Arrays.stream(extsIMG).anyMatch(s -> image.getName().endsWith(s)))
 			{
-				if (image.getName().endsWith(s))
+				try
 				{
-					try
-					{
-						insertLocalImage(image);
-					}
-					catch (Exception e)
-					{
-						Logging.errorPrint(e.getMessage(), e);
-					}
-
-					break;
-				}
-			}
-		}
-	}
-
-	/**
-	 *  Drop listener for the Tree
-	 */
-	public class DropTreeListener extends DropListener
-	{
-		/**
-		 *  implements drop.if we accept it, pass the event to the currently selected
-		 *  node
-		 *
-		 *@param  dtde  Description of the Parameter
-		 */
-		@Override
-		public void drop(DropTargetDropEvent dtde)
-		{
-			Point p = dtde.getLocation();
-			TreePath path = notesTree.getPathForLocation(p.x, p.y);
-
-			if (path == null)
-			{
-				dtde.rejectDrop();
-
-				return;
-			}
-
-			Object obj = path.getLastPathComponent();
-
-			if (obj instanceof NotesTreeNode)
-			{
-				NotesTreeNode node = (NotesTreeNode) obj;
-
-				if (dtde.isDataFlavorSupported(DataFlavor.javaFileListFlavor))
+					insertLocalImage(image);
+				} catch (Exception e)
 				{
-					dtde.dropComplete(node.handleDropJavaFileList(dtde));
-					refreshTreeNodes();
+					Logging.errorPrint(e.getMessage(), e);
 				}
-				else
-				{
-					dtde.rejectDrop();
-				}
-			}
-			else
-			{
-				dtde.rejectDrop();
 			}
 		}
 	}

commit ca895b40a82f87d43514d3148690f0e16a85051a

ExportDialog: fix exports
This broke in 96dcdde641c62b43c4750fdc9cb52cbc8e35f83a. The previous
implementation was recursive (as the notFileFilter implicitly did NOT
filter out directories). Matching this behavior fixes the issue.

code/src/java/pcgen/gui2/dialog/ExportDialog.java
@@ -657,7 +657,7 @@ public final class ExportDialog extends JDialog implements ActionListener, ListS
 				Logging.errorPrint("Unable to find outputsheets folder at " + dir.getCanonicalPath() + ".");
 				return Collections.emptyList();
 			}
-			return Files.list(dir.toPath())
+			return Files.walk(dir.toPath())
 			            .filter(f -> !f.endsWith(".fo"))
 			            .map(Path::toFile)
 					    .collect(Collectors.toList());