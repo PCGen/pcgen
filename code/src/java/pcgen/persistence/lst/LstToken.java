b194600faaaeb5ecabefb2b0a9314d211dab57d1
Eitan Adler <lists@eitanadler.com>
2019-05-17 00:39:17 -0700

    [*] remove default template comment on class

--- code/src/java/pcgen/persistence/lst/LstToken.java
+++ code/src/java/pcgen/persistence/lst/LstToken.java
@@ -17,10 +17,6 @@
  *
  */
 
-/*
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
- */
 package pcgen.persistence.lst;
 
 public interface LstToken
