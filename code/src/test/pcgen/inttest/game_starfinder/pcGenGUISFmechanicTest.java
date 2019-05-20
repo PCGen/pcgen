55df9cdb268c47df8fd69dc25fbfb0511bb1399f
Andrew Maitland <drew0500@yahoo.com>
2019-05-20 11:56:50 -0700

    Add 2 unit tests for SF (#5691)
    * Add 2 unit tests for SF
    
    * changes between unit tests...

---
+++ code/src/test/pcgen/inttest/game_starfinder/pcGenGUISFmechanicTest.java
@@ -0,0 +1,22 @@
+/*
+ */
+package pcgen.inttest.game_starfinder;
+
+import java.io.IOException;
+
+import pcgen.inttest.PcgenFtlTestCase;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests a Unit Test Case of a starfinder Ysoki level 20 mechanic.
+ * See the PCG file for details
+ */
+public class pcGenGUISFmechanicTest extends PcgenFtlTestCase
+{
+	@Test
+	public void testCode() throws IOException
+	{
+		runTest("sf_mechanic", "Starfinder");
+	}
+}
