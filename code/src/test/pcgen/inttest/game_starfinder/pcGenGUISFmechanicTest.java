5cf385dfab8d1aa4ce3e0fb28a353ddbe062b503
LegacyKing <drew0500@yahoo.com>
2019-05-19 23:49:44 -0700

    changes between unit tests...

--- code/src/test/pcgen/inttest/game_starfinder/pcGenGUISFmechanicTest.java
+++ code/src/test/pcgen/inttest/game_starfinder/pcGenGUISFmechanicTest.java
@@ -2,48 +2,20 @@
  */
 package pcgen.inttest.game_starfinder;
 
-import junit.framework.Test;
-import junit.framework.TestSuite;
+import java.io.IOException;
+
 import pcgen.inttest.PcgenFtlTestCase;
 
+import org.junit.jupiter.api.Test;
+
 /**
- * Tests a Unit Test Case of a pathfinder cleric.
+ * Tests a Unit Test Case of a starfinder Ysoki level 20 mechanic.
  * See the PCG file for details
  */
-@SuppressWarnings("nls")
 public class pcGenGUISFmechanicTest extends PcgenFtlTestCase
 {
-
-
-	public pcGenGUISFmechanicTest()
-	{
-		super("sf_mechanic");
-	}
-
-	/**
-	 * standard JUnit style constructor
-	 * 
-	 * @param name
-	 */
-	public pcGenGUISFmechanicTest(String name)
-	{
-		super(name);
-	}
-
-	/**
-	 * @return A <tt>TestSuite</tt>
-	 */
-	public static Test suite()
-	{
-		return new TestSuite(pcGenGUISFmechanicTest.class);
-	}
-
-	/**
-	 * Loads and outputs the character.
-	 * 
-	 * @throws Exception If an error occurs.
-	 */
-	public void testCode() throws Exception
+	@Test
+	public void testCode() throws IOException
 	{
 		runTest("sf_mechanic", "Starfinder");
 	}
--- code/src/test/pcgen/inttest/game_starfinder/pcGenGUISFsoldierTest.java
+++ code/src/test/pcgen/inttest/game_starfinder/pcGenGUISFsoldierTest.java
@@ -2,48 +2,20 @@
  */
 package pcgen.inttest.game_starfinder;
 
-import junit.framework.Test;
-import junit.framework.TestSuite;
+import java.io.IOException;
+
 import pcgen.inttest.PcgenFtlTestCase;
 
+import org.junit.jupiter.api.Test;
+
 /**
- * Tests a Unit Test Case of a pathfinder cleric.
+ * Tests a Unit Test Case of a starfinder Android soldier level 10.
  * See the PCG file for details
  */
-@SuppressWarnings("nls")
 public class pcGenGUISFsoldierTest extends PcgenFtlTestCase
 {
-
-
-	public pcGenGUISFsoldierTest()
-	{
-		super("sf_soldier");
-	}
-
-	/**
-	 * standard JUnit style constructor
-	 * 
-	 * @param name
-	 */
-	public pcGenGUISFsoldierTest(String name)
-	{
-		super(name);
-	}
-
-	/**
-	 * @return A <tt>TestSuite</tt>
-	 */
-	public static Test suite()
-	{
-		return new TestSuite(pcGenGUISFsoldierTest.class);
-	}
-
-	/**
-	 * Loads and outputs the character.
-	 * 
-	 * @throws Exception If an error occurs.
-	 */
-	public void testCode() throws Exception
+	@Test
+	public void testCode() throws IOException
 	{
 		runTest("sf_soldier", "Starfinder");
 	}
