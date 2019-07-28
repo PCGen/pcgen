f8cb067b47ee08e11e7f9ab968d86e66939b747f
Eitan Adler <lists@eitanadler.com>
2019-07-27 20:58:38 -0700

    [DebugDialog] permit inside to stretch when resized
    Also add test to make sure it loads

---
+++ code/src/utest/pcgen/gui3/dialog/DebugDialogTest.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2019 (C) Eitan Adler <lists@eitanadler.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+package pcgen.gui3.dialog;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import pcgen.system.LanguageBundle;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.api.FxAssert;
+import org.testfx.api.FxRobotInterface;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
+import org.testfx.matcher.base.NodeMatchers;
+
+@ExtendWith(ApplicationExtension.class)
+class DebugDialogTest
+{
+	@Start
+	private void Start(Stage stage) throws IOException
+	{
+		FXMLLoader loader = new FXMLLoader();
+		URL resource = DebugDialogController.class.getResource("DebugDialog.fxml");
+		assert resource != null;
+		loader.setLocation(resource);
+		// initialize the language bundle
+		LanguageBundle.getString("");
+		ResourceBundle bundle = LanguageBundle.getBundle();
+		assert bundle != null;
+		loader.setResources(bundle);
+		Scene scene = loader.load();
+		stage.setScene(scene);
+		stage.show();
+	}
+
+	@Test
+	void test_about_dialog_loads(final FxRobotInterface robot)
+	{
+		FxAssert.verifyThat("#memoryType", NodeMatchers.isVisible());
+	}
+
+
+}
