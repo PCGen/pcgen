bb703c10145dd3b98e0acd5084499827dbd69e02
Mark Green <drmoose94@gmail.com>
2019-12-04 01:34:01 +0000

    Slowtest runs, old checkstyle?

--- code/src/java/plugin/lsttokens/kit/table/ValuesToken.java
+++ code/src/java/plugin/lsttokens/kit/table/ValuesToken.java
@@ -36,156 +36,127 @@ import pcgen.rules.persistence.token.ParseResult;
 /**
  * VALUES token for KitTable
  */
-public class ValuesToken extends AbstractNonEmptyToken<KitTable> implements CDOMPrimaryToken<KitTable>
-{
-	/**
-	 * Gets the name of the tag this class will parse.
-	 *
-	 * @return Name of the tag this class handles
-	 */
-	@Override
-	public String getTokenName()
-	{
-		return "VALUES";
-	}
+public class ValuesToken extends AbstractNonEmptyToken<KitTable> implements CDOMPrimaryToken<KitTable> {
+    /**
+     * Gets the name of the tag this class will parse.
+     *
+     * @return Name of the tag this class handles
+     */
+    @Override
+    public String getTokenName() {
+        return "VALUES";
+    }
 
-	@Override
-	public Class<KitTable> getTokenClass()
-	{
-		return KitTable.class;
-	}
+    @Override
+    public Class<KitTable> getTokenClass() {
+        return KitTable.class;
+    }
 
-	@Override
-	protected ParseResult parseNonEmptyToken(LoadContext context, KitTable kitTable, String value)
-	{
-		ParsingSeparator sep = new ParsingSeparator(value, '|');
-		sep.addGroupingPair('[', ']');
-		sep.addGroupingPair('(', ')');
+    @Override
+    protected ParseResult parseNonEmptyToken(LoadContext context, KitTable kitTable, String value) {
+        ParsingSeparator sep = new ParsingSeparator(value, '|');
+        sep.addGroupingPair('[', ']');
+        sep.addGroupingPair('(', ')');
 
-		while (sep.hasNext())
-		{
-			String thing = sep.next();
-			if (thing.isEmpty())
-			{
-				return new ParseResult.Fail(getTokenName() + " arguments has invalid pipe separator: " + value);
-			}
-			KitGear optionInfo = new KitGear();
-			for (String s : thing.split("[\\[\\]]"))
-			{
-				if (s.isEmpty())
-				{
-					continue;
-				}
-				int colonLoc = s.indexOf(':');
-				if (colonLoc == -1)
-				{
-					return new ParseResult.Fail("Expected colon in Value item: " + s + " within: " + value);
-				}
-				String key = s.substring(0, colonLoc);
-				String thingValue = s.substring(colonLoc + 1);
+        while (sep.hasNext()) {
+            String thing = sep.next();
+            if (thing.isEmpty()) {
+                return new ParseResult.Fail(getTokenName() + " arguments has invalid pipe separator: " + value);
+            }
+            KitGear optionInfo = new KitGear();
+            for (String s : thing.split("[\\[\\]]")) {
+                if (s.isEmpty()) {
+                    continue;
+                }
+                int colonLoc = s.indexOf(':');
+                if (colonLoc == -1) {
+                    return new ParseResult.Fail("Expected colon in Value item: " + s + " within: " + value);
+                }
+                String key = s.substring(0, colonLoc);
+                String thingValue = s.substring(colonLoc + 1);
 
-					boolean passed = context.processToken(optionInfo, key, thingValue);
-					if (!passed)
-					{
-						return new ParseResult.Fail("Failure in token: " + key);
-					}
+                boolean passed = context.processToken(optionInfo, key, thingValue);
+                if (!passed) {
+                    return new ParseResult.Fail("Failure in token: " + key);
+                }
 
 
-			}
-			if (!sep.hasNext())
-			{
-				return new ParseResult.Fail("Odd token count in Value: " + value);
-			}
-			String range = sep.next();
-			ParseResult pr = processRange(kitTable, optionInfo, range);
-			if (!pr.passed())
-			{
-				return new ParseResult.Fail(
-					"Invalid Range in Value: " + range + " within " + value + " report was: " + pr);
-			}
-		}
+            }
+            if (!sep.hasNext()) {
+                return new ParseResult.Fail("Odd token count in Value: " + value);
+            }
+            String range = sep.next();
+            ParseResult pr = processRange(kitTable, optionInfo, range);
+            if (!pr.passed()) {
+                return new ParseResult.Fail(
+                        "Invalid Range in Value: " + range + " within " + value + " report was: " + pr);
+            }
+        }
 
-		return ParseResult.SUCCESS;
-	}
+        return ParseResult.SUCCESS;
+    }
 
-	private ParseResult processRange(KitTable kitTable, KitGear optionInfo, String range)
-	{
-		ParseResult pr = checkSeparatorsAndNonEmpty(',', range);
-		if (!pr.passed())
-		{
-			return pr;
-		}
+    private ParseResult processRange(KitTable kitTable, KitGear optionInfo, String range) {
+        ParseResult pr = checkSeparatorsAndNonEmpty(',', range);
+        if (!pr.passed()) {
+            return pr;
+        }
 
-		ParsingSeparator sep = new ParsingSeparator(range, ',');
-		sep.addGroupingPair('[', ']');
-		sep.addGroupingPair('(', ')');
-		String minString = sep.next();
-		String maxString;
-		if (sep.hasNext())
-		{
-			maxString = sep.next();
-		}
-		else
-		{
-			maxString = range;
-		}
-		if (sep.hasNext())
-		{
-			return new ParseResult.Fail("Expected more than one value in a range, found: " + range);
-		}
-		Formula min = FormulaFactory.getFormulaFor(minString);
-		if (!min.isValid())
-		{
-			return new ParseResult.Fail("Min Formula in " + getTokenName() + " was not valid: " + min.toString());
-		}
-		Formula max = FormulaFactory.getFormulaFor(maxString);
-		if (!max.isValid())
-		{
-			return new ParseResult.Fail("Max Formula in " + getTokenName() + " was not valid: " + max.toString());
-		}
-		kitTable.addGear(optionInfo, min, max);
-		return ParseResult.SUCCESS;
-	}
+        ParsingSeparator sep = new ParsingSeparator(range, ',');
+        sep.addGroupingPair('[', ']');
+        sep.addGroupingPair('(', ')');
+        String minString = sep.next();
+        String maxString;
+        if (sep.hasNext()) {
+            maxString = sep.next();
+        } else {
+            maxString = range;
+        }
+        if (sep.hasNext()) {
+            return new ParseResult.Fail("Expected more than one value in a range, found: " + range);
+        }
+        Formula min = FormulaFactory.getFormulaFor(minString);
+        if (!min.isValid()) {
+            return new ParseResult.Fail("Min Formula in " + getTokenName() + " was not valid: " + min.toString());
+        }
+        Formula max = FormulaFactory.getFormulaFor(maxString);
+        if (!max.isValid()) {
+            return new ParseResult.Fail("Max Formula in " + getTokenName() + " was not valid: " + max.toString());
+        }
+        kitTable.addGear(optionInfo, min, max);
+        return ParseResult.SUCCESS;
+    }
 
-	@Override
-	public String[] unparse(LoadContext context, KitTable kitTable)
-	{
-		StringBuilder sb = new StringBuilder();
-		List<TableEntry> list = kitTable.getList();
-		if (list.isEmpty())
-		{
-			return null;
-		}
-		boolean first = true;
-		for (TableEntry rl : list)
-		{
-			if (!first)
-			{
-				sb.append(Constants.PIPE);
-			}
-			Collection<String> unparse = context.unparse(rl.gear);
-			if (unparse.size() == 1)
-			{
-				sb.append(unparse.iterator().next());
-			}
-			else
-			{
-				for (String s : unparse)
-				{
-					sb.append('[');
-					sb.append(s);
-					sb.append(']');
-				}
-			}
-			sb.append(Constants.PIPE);
-			sb.append(rl.lowRange.toString());
-			if (!rl.lowRange.equals(rl.highRange))
-			{
-				sb.append(',');
-				sb.append(rl.highRange.toString());
-			}
-			first = false;
-		}
-		return new String[]{sb.toString()};
-	}
+    @Override
+    public String[] unparse(LoadContext context, KitTable kitTable) {
+        StringBuilder sb = new StringBuilder();
+        List<TableEntry> list = kitTable.getList();
+        if (list.isEmpty()) {
+            return null;
+        }
+        boolean first = true;
+        for (TableEntry rl : list) {
+            if (!first) {
+                sb.append(Constants.PIPE);
+            }
+            Collection<String> unparse = context.unparse(rl.gear);
+            if (unparse.size() == 1) {
+                sb.append(unparse.iterator().next());
+            } else {
+                for (String s : unparse) {
+                    sb.append('[');
+                    sb.append(s);
+                    sb.append(']');
+                }
+            }
+            sb.append(Constants.PIPE);
+            sb.append(rl.lowRange.toString());
+            if (!rl.lowRange.equals(rl.highRange)) {
+                sb.append(',');
+                sb.append(rl.highRange.toString());
+            }
+            first = false;
+        }
+        return new String[]{sb.toString()};
+    }
 }
