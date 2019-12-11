0195ff0e20f9f2bff34a80c762072db932c5a593
Mark Green <drmoose94@gmail.com>
2019-12-11 20:43:32 +0000

    Manual: remove booleans "&& false"

--- code/src/java/plugin/exporttokens/SkillSitToken.java
+++ code/src/java/plugin/exporttokens/SkillSitToken.java
@@ -216,13 +216,7 @@ public class SkillSitToken extends Token
 	{
 		StringBuilder retValue = new StringBuilder();
 
-		if (((property == SkillToken.SKILL_ABMOD) || (property == SkillToken.SKILL_MISC)) && false)
-			//&& aSkill.get(ObjectKey.KEY_STAT) == null)
-		{
-			retValue.append("n/a");
-		}
-		else
-		{
+
 			Skill skill;
 			boolean isSituation;
 			String situation;
@@ -438,7 +432,7 @@ public class SkillSitToken extends Token
 
 					break;
 			}
-		}
+
 		return retValue.toString();
 	}
 }
