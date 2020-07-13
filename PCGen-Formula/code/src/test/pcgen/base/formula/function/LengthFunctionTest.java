32010b8c947c198a2fbc527adcfea13af3e624cd
Tom <Tom@DESKTOP-NUKKUHT.attlocal.net>
2020-07-13 17:28:54 -0500

    Tests

---
+++ PCGen-Formula/code/src/test/pcgen/base/formula/function/LengthFunctionTest.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2014 (C) Tom Parker <thpr@users.sourceforge.net>
+ * 
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+package pcgen.base.formula.function;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.Test;
+
+import pcgen.base.format.ArrayFormatManager;
+import pcgen.base.format.NumberManager;
+import pcgen.base.formatmanager.FormatUtilities;
+import pcgen.base.formula.base.VariableID;
+import pcgen.base.formula.base.VariableLibrary;
+import pcgen.base.formula.parse.SimpleNode;
+import pcgen.base.testsupport.AbstractFormulaTestCase;
+import pcgen.base.testsupport.TestUtilities;
+
+public class LengthFunctionTest extends AbstractFormulaTestCase
+{
+	private ArrayFormatManager<Number> manager = new ArrayFormatManager<>(
+			new NumberManager(), '\n', ',');
+
+	@Test
+	public void testInvalidTooManyArg()
+	{
+		String formula = "length(2, 3)";
+		SimpleNode node = TestUtilities.doParse(formula);
+		isNotValid(formula, node, manager, Optional.empty());
+	}
+
+	@Test
+	public void testNotValidString()
+	{
+		String formula = "length(\"ab\")";
+		SimpleNode node = TestUtilities.doParse(formula);
+		isNotValid(formula, node, manager, Optional.empty());
+	}
+
+	@Test
+	public void testNotValidNoVar()
+	{
+		String formula = "length(ab)";
+		SimpleNode node = TestUtilities.doParse(formula);
+		isNotValid(formula, node, manager, Optional.empty());
+	}
+
+	@Test
+	public void testVariable()
+	{
+		VariableLibrary variableLibrary = getVariableLibrary();
+		variableLibrary.assertLegalVariableID("a",
+			getInstanceFactory().getScope("Global"), manager);
+		VariableID<Number[]> variable = (VariableID<Number[]>) variableLibrary
+			.getVariableID(getGlobalScopeInst(), "a");
+		getVariableStore().put(variable, new Number[]{5});
+		String formula = "length(a)";
+		SimpleNode node = TestUtilities.doParse(formula);
+		isValid(formula, node, manager, Optional.empty());
+		isStatic(formula, node, false);
+		List<VariableID<?>> vars = getVariables(node);
+		assertEquals(1, vars.size());
+		VariableID<?> var = vars.get(0);
+		assertEquals("a", var.getName());
+		evaluatesTo(FormatUtilities.NUMBER_MANAGER, formula, node, Double.valueOf(1));
+		getVariableStore().put(variable, new Number[]{5, 6, 7, 7, 8});
+		evaluatesTo(FormatUtilities.NUMBER_MANAGER, formula, node, Double.valueOf(5));
+	}
+}
