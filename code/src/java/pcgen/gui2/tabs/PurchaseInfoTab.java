13cece8ac59a965f587d18a2d50e7231147a2679
Martijn Verburg <martijnverburg@gmail.com>
2021-09-06 12:34:18 +0100

    Fix Checkstyle issues

--- code/src/java/pcgen/gui2/tabs/PurchaseInfoTab.java
+++ code/src/java/pcgen/gui2/tabs/PurchaseInfoTab.java
@@ -38,7 +38,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
