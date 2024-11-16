d30055bf90218b978884d2e95086275ce1f8fac2
Vest <Vest@users.noreply.github.com>
2024-11-16 01:54:01 +0100

    Java update from 21.0.4 to 21.0.5 including different depenndencies (#7221)
    * Upgraded Java from 21.0.4 to 21.0.4
    Upgraded CheckStyle from 10.18.1 to 10.20.1
    Upgraded pmd from 7.6.0 to 7.7.0
    Corrected pmd error in ClassLevelTableModel.java
    
    * Changed few Java files to meet pmd recommendations.
    Updated ruleset.xml, renamed deprecated rules.
    
    * Correct the targetPlatform in build.gradle from windows-x64 to win-x64.
    
    Signed-off-by: Vest <Vest@users.noreply.github.com>
    
    * Corrected the "targetPlaform" naming back from "win" to "windows".
    Removed two semicolons.
    
    Signed-off-by: Vest <Vest@users.noreply.github.com>
    
    * Removed redundant spaces
    
    Signed-off-by: Vest <Vest@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Vest <Vest@users.noreply.github.com>

--- code/src/java/pcgen/base/test/InequalityTester.java
+++ code/src/java/pcgen/base/test/InequalityTester.java
@@ -4,12 +4,12 @@
  * the terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 2.1 of the License, or (at your option)
  * any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this library; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
@@ -18,7 +18,7 @@ package pcgen.base.test;
 
 /**
  * An InequalityTester is a method of checking two objects for equality.
- * 
+ * <p>
  * This is done with a system other than .equals() since there may be exceptions
  * to true equality that we need to handle
  */
@@ -27,15 +27,15 @@ public interface InequalityTester
 {
 	/**
 	 * Tests the equality between two objects using this InequalityTester
-	 * 
+	 *
 	 * @param o1
 	 *            The first object to be tested for equality
 	 * @param o2
 	 *            The second object to be tested for equality
 	 * @param location
-	 *            The location of the objects, to assist with debugging
+	 *            The location of the objects to assist with debugging
 	 * @return A String indicating how the items are not equal; null if equal
 	 */
-	@SuppressWarnings("PMD.JUnit4TestShouldUseTestAnnotation")
-	public String testEquality(Object o1, Object o2, String location);
+	@SuppressWarnings("PMD.UnitTestShouldUseTestAnnotation")
+	String testEquality(Object o1, Object o2, String location);
 }
