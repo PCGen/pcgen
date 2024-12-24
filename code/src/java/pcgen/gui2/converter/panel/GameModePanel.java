69262963e9a6b055bdac3c7e5e1ba7accbad015c
Vest <Vest@users.noreply.github.com>
2024-12-24 23:55:08 +0100

    Refactored several classes. The main goal was adding the Optional to methods that return null. In some cases, the null reference/result was not checked, but the code never failed, because it was not executed. There were no tests, that is why it was difficult to check, whether the refactoring was right :(

--- code/src/java/pcgen/gui2/converter/panel/GameModePanel.java
+++ code/src/java/pcgen/gui2/converter/panel/GameModePanel.java
@@ -1,16 +1,16 @@
 /*
  * Copyright (c) 2009 Tom Parker <thpr@users.sourceforge.net>
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 2.1 of the License, or (at your option)
  * any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this library; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
@@ -88,7 +88,8 @@ public class GameModePanel extends ConvertSubPanel
 			SettingsHandler.setGame(gameMode.getName());
 		}
 		//		Globals.emptyLists();
-		Globals.sortPObjectListByName(Globals.getCampaignList());
+		// TODO the campaign list is an unmodifiable list, so it is not possible to sort it.
+		// Globals.sortPObjectListByName(Globals.getCampaignList());
 
 		return saveGameMode(pc);
 	}
