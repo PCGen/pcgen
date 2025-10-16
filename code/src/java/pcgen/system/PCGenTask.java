2c84e6634f13be9d9c369bcd9e8b591a9a9601a2
Vest <Vest@users.noreply.github.com>
2025-10-16 23:18:56 +0200

    Changed PluginBuildTest, because the new plugins are built from corresponding .java files. The slow test has been reverted, because it was broken in old commits (DATA-4372)
    Signed-off-by: Vest <Vest@users.noreply.github.com>

--- code/src/java/pcgen/system/PCGenTask.java
+++ code/src/java/pcgen/system/PCGenTask.java
@@ -1,20 +1,20 @@
 /*
  * Copyright 2010 Connor Petty <cpmeister@users.sourceforge.net>
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- * 
+ *
  */
 package pcgen.system;
 
@@ -27,7 +27,6 @@ import pcgen.util.Logging;
 
 public abstract class PCGenTask implements Runnable, ProgressContainer
 {
-
 	private final EventListenerList listenerList = new EventListenerList();
 	private int progress = 0;
 	private int maximum = 0;
@@ -159,5 +158,4 @@ public abstract class PCGenTask implements Runnable, ProgressContainer
 			}
 		}
 	}
-
 }
