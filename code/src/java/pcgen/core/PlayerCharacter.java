e7f171eab63a197af19f802774744fc03cb723ce
Vest <Vest@users.noreply.github.com>
2025-11-15 15:59:23 +0100

    Reverted a part of #7462, that broke DescriptionTest.
    Signed-off-by: Vest <Vest@users.noreply.github.com>

--- code/src/java/pcgen/core/PlayerCharacter.java
+++ code/src/java/pcgen/core/PlayerCharacter.java
@@ -8015,7 +8015,7 @@ public class PlayerCharacter implements Cloneable, VariableContainer
 		return getDescription(Collections.singletonList(pobj));
 	}
 
-	public String getDescription(List<? extends Object> objList)
+	public String getDescription(List<?> objList)
 	{
 		if (objList.isEmpty())
 		{
