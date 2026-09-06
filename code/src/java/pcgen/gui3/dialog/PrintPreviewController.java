17999fa25689f211ea4d2c5f0acc8ec761960f3f
Vest <Vest@users.noreply.github.com>
2026-09-06 23:23:52 +0200

    CODE-2537: render preview pages to JavaFX images; keep AWT print + zoom

--- code/src/java/pcgen/gui3/dialog/PrintPreviewController.java
+++ code/src/java/pcgen/gui3/dialog/PrintPreviewController.java
@@ -17,10 +17,16 @@
  */
 package pcgen.gui3.dialog;
 
+import java.awt.print.PrinterException;
+import java.awt.print.PrinterJob;
+import java.io.File;
 import java.io.IOException;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
 import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.IntStream;
@@ -31,18 +37,27 @@ import pcgen.core.Globals;
 import pcgen.core.PaperInfo;
 import pcgen.facade.core.CharacterFacade;
 import pcgen.gui2.PCGenFrame;
+import pcgen.system.BatchExporter;
 import pcgen.system.ConfigurationSettings;
 import pcgen.system.PCGenSettings;
 import pcgen.util.Logging;
+import pcgen.util.fop.FopTask;
 
 import javafx.collections.FXCollections;
+import javafx.concurrent.Task;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+
+import org.apache.fop.apps.FOUserAgent;
+import org.apache.fop.render.awt.AWTRenderer;
 
 /**
  * JavaFX print-preview dialog (CODE-2537): renders each character-sheet page to
@@ -74,6 +89,9 @@ public class PrintPreviewController
 	private ProgressBar progress;
 
 	private CharacterFacade character;
+	private AWTRenderer renderer;
+	private double zoom = 0.75;
+	private static final double ZOOM_MULTIPLIER = Math.pow(2, 0.125);
 
 	@FXML
 	void initialize()
@@ -83,6 +101,21 @@ public class PrintPreviewController
 
 		populatePaperBox();
 		populateSheetBox();
+		zoomBox.setItems(FXCollections.observableArrayList(0.25, 0.50, 0.75, 1.00));
+		zoomBox.getSelectionModel().select(Double.valueOf(0.75));
+		zoomBox.getSelectionModel().selectedItemProperty().addListener((obs, old, now) -> {
+			if (now != null)
+			{
+				zoom = now;
+				applyZoom();
+			}
+		});
+		pageBox.getSelectionModel().selectedIndexProperty().addListener((obs, old, now) -> {
+			if (now.intValue() >= 0)
+			{
+				showPage(now.intValue());
+			}
+		});
 		setEditGroupEnabled(false);
 	}
 
@@ -143,28 +176,136 @@ public class PrintPreviewController
 		printButton.setDisable(!enable);
 	}
 
-	// Rendering, zoom and print are added in Task 4.
 	private void loadPreview(URI template)
 	{
-		// Implemented in Task 4.
+		progress.setVisible(true);
+		sheetBox.setDisable(true);
+		setEditGroupEnabled(false);
+
+		Task<AWTRenderer> task = new Task<>()
+		{
+			@Override
+			protected AWTRenderer call() throws Exception
+			{
+				URI osPath = new File(ConfigurationSettings.getOutputSheetsDir()).toURI();
+				File xsltFile = new File(osPath.resolve(template));
+				FOUserAgent userAgent = FopTask.getFactory().newFOUserAgent();
+				AWTRenderer awtRenderer = new AWTRenderer(userAgent, null, false, false);
+				try (PipedOutputStream out = new PipedOutputStream())
+				{
+					FopTask fopTask = FopTask.newFopTask(new PipedInputStream(out), xsltFile, awtRenderer);
+					Thread thread = new Thread(fopTask, "fop-preview");
+					thread.setDaemon(true);
+					thread.start();
+					BatchExporter.exportCharacter(character, out);
+					try
+					{
+						thread.join();
+					}
+					catch (final InterruptedException ex)
+					{
+						thread.interrupt();
+					}
+				}
+				return awtRenderer;
+			}
+		};
+		task.setOnSucceeded(evt -> {
+			renderer = task.getValue();
+			progress.setVisible(false);
+			sheetBox.setDisable(false);
+			setEditGroupEnabled(true);
+			int pages = renderer.getNumberOfPages();
+			String[] labels = new String[pages];
+			Arrays.setAll(labels, i -> (i + 1) + " of " + pages);
+			pageBox.setItems(FXCollections.observableArrayList(labels));
+			if (pages > 0)
+			{
+				pageBox.getSelectionModel().select(0);
+			}
+		});
+		task.setOnFailed(evt -> {
+			progress.setVisible(false);
+			sheetBox.setDisable(false);
+			Logging.errorPrint("Could not load sheet", task.getException());
+		});
+		Thread worker = new Thread(task, "fop-preview-task");
+		worker.setDaemon(true);
+		worker.start();
+	}
+
+	private void showPage(int pageIndex)
+	{
+		if (renderer == null)
+		{
+			return;
+		}
+		try
+		{
+			java.awt.image.BufferedImage bufferedImage = renderer.getPageImage(pageIndex);
+			Image fxImage = SwingFXUtils.toFXImage(bufferedImage, null);
+			previewImage.setImage(fxImage);
+			applyZoom();
+		}
+		catch (final org.apache.fop.apps.FOPException ex)
+		{
+			Logging.errorPrint("Could not render preview page " + pageIndex, ex);
+		}
+	}
+
+	private void applyZoom()
+	{
+		Image image = previewImage.getImage();
+		if (image != null)
+		{
+			previewImage.setFitWidth(image.getWidth() * zoom);
+		}
 	}
 
 	@FXML
 	private void onZoomIn(final ActionEvent actionEvent)
 	{
-		// Implemented in Task 4.
+		zoom *= ZOOM_MULTIPLIER;
+		zoomBox.getSelectionModel().clearSelection();
+		zoomBox.setValue(zoom);
+		applyZoom();
 	}
 
 	@FXML
 	private void onZoomOut(final ActionEvent actionEvent)
 	{
-		// Implemented in Task 4.
+		zoom /= ZOOM_MULTIPLIER;
+		zoomBox.getSelectionModel().clearSelection();
+		zoomBox.setValue(zoom);
+		applyZoom();
 	}
 
 	@FXML
 	private void onPrint(final ActionEvent actionEvent)
 	{
-		// Implemented in Task 4.
+		if (renderer == null)
+		{
+			return;
+		}
+		PrinterJob printerJob = PrinterJob.getPrinterJob();
+		printerJob.setPageable(renderer);
+		if (printerJob.printDialog())
+		{
+			try
+			{
+				printerJob.print();
+				cancelButton.getScene().getWindow().hide();
+			}
+			catch (final PrinterException ex)
+			{
+				String message = "Could not print " + character.getNameRef().get();
+				Logging.errorPrint(message, ex);
+				Alert alert = new Alert(Alert.AlertType.ERROR);
+				alert.setTitle(Constants.APPLICATION_NAME);
+				alert.setContentText(message);
+				alert.show();
+			}
+		}
 	}
 
 	@FXML
